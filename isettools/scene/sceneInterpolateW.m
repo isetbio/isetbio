30fbdf98240a65847b0ebcffa7105a1c6c920a75
Haomiao Jiang <hjiang36@gmail.com>
2015-04-07 21:01:17 -0700

    Small update to sceneInterpolateW

--- isettools/scene/sceneInterpolateW.m
+++ isettools/scene/sceneInterpolateW.m
@@ -10,7 +10,7 @@ function scene = sceneInterpolateW(scene,newWave,pLum)
 % newWave: Wavelengths of the output scene (interpolated)
 % pLum:    Preserve the luminance (1, default) 
 %
-% We do not think you should be extrapolating.  We alert you if the newWave
+% We do not think you should be extrapolating. We alert you if the newWave
 % is outside of the range of the current scene wavelengths.
 %
 % Examples:
@@ -31,38 +31,39 @@ function scene = sceneInterpolateW(scene,newWave,pLum)
 if notDefined('pLum'),  pLum = 1; end
 if notDefined('scene'), scene = vcGetSelectedObject('scene');
 elseif ~strcmp(sceneGet(scene,'type'),'scene')
-    error('sceneInterpolationW structure not a scene!');
+    error('sceneInterpolationW: input structure not a scene');
 end
 
 % Check whether or not to show wait bar
 showBar = ieSessionGet('wait bar');
 
 % Note the current scene properties
-row   = sceneGet(scene,'row');
-col   = sceneGet(scene,'col');
-curWave = sceneGet(scene,'wave');
+curWave = sceneGet(scene, 'wave');
 
 % If the user didn't send in new wavelengths, we ask.
 if notDefined('newWave')
     handles = ieSessionGet('scene image handle');
-    prompt={'Start (nm)','Stop (nm)','Spacing (nm)'};
-    def={num2str(curWave(1)),num2str(curWave(end)),num2str(sceneGet(scene,'binwidth'))};
+    prompt= {'Start (nm)','Stop (nm)','Spacing (nm)'};
+    def   = {num2str(curWave(1)), num2str(curWave(end)), ...
+             num2str(sceneGet(scene,'binwidth'))};
     dlgTitle='Wavelength resampling';
     lineNo=1;
-    val =inputdlg(prompt,dlgTitle,lineNo,def);
+    val = inputdlg(prompt,dlgTitle,lineNo,def);
     if isempty(val), return; end
     
-    low = str2double(val{1}); high = str2double(val{2}); skip = str2double(val{3});
-    if high > low,       waveSpectrum.wave = low:skip:high;
-    elseif high == low,  waveSpectrum.wave = low; % User made monochrome
+    low = str2double(val{1});
+    high = str2double(val{2});
+    skip = str2double(val{3});
+    
+    if high >= low 
+        newWave = low:skip:high;
     else
-        ieInWindowMessage('Bad wavelength ordering:  high < low. Data unchanged.',handles,5);
+        ieInWindowMessage('Stop should be no larger than Start',handles,5);
         return;
     end
-    newWave = waveSpectrum.wave;
-else
-    waveSpectrum.wave = newWave;
 end
+
+waveSpectrum.wave = newWave;
 
 if min(newWave) < min(curWave) ||  max(newWave) > max(curWave)
     error('Wavelength extrapolation not allowed.  Only interpolation');
@@ -71,16 +72,16 @@ end
 %% Get current data and parameters
 photons = sceneGet(scene,'photons');
 if ~isempty(photons) && pLum
-    meanL   = sceneGet(scene,'mean luminance'); 
+    meanL   = sceneGet(scene, 'mean luminance'); 
 end
 
 il = sceneGet(scene,'illuminant');
 if ~isempty(il)
-    illuminantPhotons = sceneGet(scene,'illuminant photons');
+    illuminantPhotons = sceneGet(scene, 'illuminant photons');
 end
 
 % Clear the current data before replacing.  This saves memory.
-scene = sceneSet(scene,'spectrum',waveSpectrum);
+scene = sceneSet(scene, 'spectrum', waveSpectrum);
 scene = sceneClearData(scene);
 
 %% Interpolate the photons to the new wavelength sampling
@@ -95,11 +96,8 @@ if ~isempty(photons)
     % Not sure how big we should allow this to be
     if r*c*w < (640*640*31)
         % This is wavelength x space, rather than XW as usual
-        photons    = RGB2XWFormat(photons)';     
-        newPhotons = interp1(curWave,photons,waveSpectrum.wave, 'linear')';
-        % Replaced 2013.09.29 for speed
-        % newPhotons = interp1(curWave,photons,...
-        %      waveSpectrum.wave, 'linear',min(photons(:))*1e-3)';
+        [photons, row, col] = RGB2XWFormat(photons);     
+        newPhotons = interp1(curWave, photons', newWave, 'linear')';
         newPhotons = XW2RGBFormat(newPhotons,row,col);
     else
         % Big data set condition, so we loop down the rows
@@ -126,7 +124,7 @@ if ~isempty(photons)
 end
 if showBar, close(h); end
 
-%% Now create the new illuminant.  This over-writes the earlier one.
+%% Now create the new illuminant. This over-writes the earlier one.
 if ~isempty(il)
     
     % Interpolate the illuminant data
