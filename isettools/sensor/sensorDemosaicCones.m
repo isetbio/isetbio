1d8ce4377b5cff8b6f6b2a0ae63b15f967f56605
David Brainard <brainard@psych.upenn.edu>
2015-04-14 12:51:13 -0400

    Updates/fixels/comments
    The demosaic'd LMS array is now returned as well as the sRGB image.  Sometimes we want that.
    
    I think there was a row/col bug in the setup and call to griddata, and also a scaling bug on the LMS data before the conversion to sRGB.  I am not 100% sure, so these are commented and I will post an issue in a second
    
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- isettools/sensor/sensorDemosaicCones.m
+++ isettools/sensor/sensorDemosaicCones.m
@@ -1,16 +1,25 @@
-function srgb = sensorDemosaicCones(sensor, method, nFrames)
+function [srgb,LMSphotons] = sensorDemosaicCones(sensor, method, nFrames)
 %% Demosaic and render human cone photon absorptions
-%    srgb = sensorDemosaicCones(sensor, [method], [nFrames])
+%    [srgb,LMSphotons] = sensorDemosaicCones(sensor, [method], [nFrames])
 %
 %  Inputs:
 %    sensor  - sensor structure with photon absorption rates computed
-%    method  - algorithm to be used for interpolation. Now support
-%              'nearest', 'linear' (default), 'natural', 'cubic', 'v4'
+%    method  - algorithm to be used for interpolation. The following
+%              methods are supported via griddata, applied to each
+%              submosaic separately:
+%                'nearest'
+%                'linear' (default)
+%                'natural'
+%                'cubic'
+%                'v4'
 %    nFrames - number of frames to be rendered, should be no larger than
 %              number of frames in sensorGet(sensor, 'photons'). Default 1
 %
 %  Output:
 %    srgb    - rendered srgb image, 4D matrix as row x col x 3 x nFrames
+%    LMSphotons - The interpolated isomerizations in same format.  These
+%                 are not normalized, nor promoted for dichromats. If the
+%                 image is monochrome, this is returned empty.
 %
 %  Notes:
 %    1) this function can only be used for demosaicing human cone mosaic.
@@ -68,30 +77,66 @@ if length(cbType) == 2 % monochrome case
     srgb = p / max(p(:));
     srgb = reshape(srgb, [sz 1 nFrames]);
     srgb = repmat(srgb, [1 1 3 1]);
+    LMSPhotons = [];
 else % trichromatic or dichromatic case
     coneType = sensorGet(sensor, 'cone type');
     
-    % allocate spaces
+    % allocate space
     srgb = zeros([sz 3 nFrames]);
-    LMS  = zeros([sz 3]);
+    LMSphotons  = zeros([sz 3 nFrames]);
     
-    % define const parameters
-    [yq, xq] = meshgrid(1:sz(2), 1:sz(1));
-    scale = max(sensorGet(sensor, 'spectral qe'));
+    % Find scaling so that conversion to sRGB is correct.
+    % That conversion assumes that LMS spectral sensitivities are scaled to peak
+    % of 1 in energy units.  Here we have real quantal efficiences, so that our
+    % LMS values are scaled differently relative to one another.  This will
+    % produce problems if we just use the standard transformation matrix.
+    %
+    % Getting this totally right is a bit involved, particularly if we want to get
+    % the XYZ values in standard units of Y being in cd/m2 or some such.
+    % Here we'll only fuss a little, and get the relative scaling right.
+    %
+    % Note that we need to convert to energy units to make the relative
+    % scaling right.
+    sensorQE = sensorGet(sensor, 'spectral qe');
+    wave = sensorGet(sensor,'wave');
+    sensorEnergyUnits = EnergyToQuanta(wave(:),sensorQE);
+    scale = max(sensorEnergyUnits); 
+    scale = scale/max(scale);
     scale = reshape(scale(2:4), [1 1 3]);
     
-    for curFrame = 1 : nFrames % loop over frames
+    % Set up interpolation mesh
+    %
+    % Haomiao had the line that is now commented out.  My (DHB's)
+    % read of the documentation is that meshgrid works in x,y, so
+    % that the return arguments should be [xq, yq] and not the other way
+    % around.  This change interacts with the ind2sub change in the loop below.
+    % Note that these do changes do not exactly cancel.  You get a similar
+    % but different interpolation if you go back to the way Haomiao had it.
+    %[yq, xq] = meshgrid(1:sz(2), 1:sz(1));
+    [xq, yq] = meshgrid(1:sz(2), 1:sz(1));
+
+    % loop over frames
+    for curFrame = 1 : nFrames 
         curP = p(:,:,curFrame);
-        for ii = 2 : 4 % interpolate for L, M and S
+        % interpolate for L, M and S, which are indexed as 2:4 in isetbio.
+        for ii = 2 : 4 
             indx = find(coneType == ii); 
-            if isempty(indx), LMS(:,:,ii-1) = 0; continue; end
-            [x, y] = ind2sub(sz, indx);
-            LMS(:,:,ii-1) = griddata(x,y,curP(indx), xq, yq, method);
+            if isempty(indx), LMSphotons(:,:,ii-1) = 0; continue; end
+            
+            % Here is my second change.  My read of the documentation is
+            % that ind2sub works in i, j so that the return arguments
+            % should be interpreted as [y, x] and not the other way around.
+            %[x, y] = ind2sub(sz, indx);
+            [y, x] = ind2sub(sz, indx);
+            
+            % Let griddata do the work.
+            LMSphotons(:,:,ii-1,curFrame) = griddata(x,y,curP(indx), xq, yq, method);
         end
+        
         % scale LMS to match stockman normalized spectral qe
         % we need to do this because all xyz2lms and colorblind transform
         % routine are based on stockman normalized spectral qe
-        LMS = bsxfun(@rdivide, LMS, scale);
+        LMS = bsxfun(@rdivide, LMSphotons(:,:,:,curFrame), scale);
         
         % colorblind transformation
         LMS = lms2lmsDichromat(LMS, cbType, 'linear');
