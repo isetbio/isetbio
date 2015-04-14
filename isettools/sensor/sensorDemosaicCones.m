f0bc8be106f62f84d372daecdc545e201a6c603f
Haomiao Jiang <hjiang36@gmail.com>
2015-04-14 11:08:52 -0700

    Accept changes by David, update comments and fix typo

--- isettools/sensor/sensorDemosaicCones.m
+++ isettools/sensor/sensorDemosaicCones.m
@@ -77,7 +77,7 @@ if length(cbType) == 2 % monochrome case
     srgb = p / max(p(:));
     srgb = reshape(srgb, [sz 1 nFrames]);
     srgb = repmat(srgb, [1 1 3 1]);
-    LMSPhotons = [];
+    LMSphotons = [];
 else % trichromatic or dichromatic case
     coneType = sensorGet(sensor, 'cone type');
     
@@ -85,15 +85,16 @@ else % trichromatic or dichromatic case
     srgb = zeros([sz 3 nFrames]);
     LMSphotons  = zeros([sz 3 nFrames]);
     
-    % Find scaling so that conversion to sRGB is correct.
-    % That conversion assumes that LMS spectral sensitivities are scaled to peak
-    % of 1 in energy units.  Here we have real quantal efficiences, so that our
-    % LMS values are scaled differently relative to one another.  This will
+    % Find scaling so that conversion to sRGB is correct. That conversion
+    % assumes that LMS spectral sensitivities are scaled to peak of 1 in
+    % energy units. Here we have real quantal efficiences, so that our LMS
+    % values are scaled differently relative to one another. This will
     % produce problems if we just use the standard transformation matrix.
     %
-    % Getting this totally right is a bit involved, particularly if we want to get
-    % the XYZ values in standard units of Y being in cd/m2 or some such.
-    % Here we'll only fuss a little, and get the relative scaling right.
+    % Getting this totally right is a bit involved, particularly if we want
+    % to get the XYZ values in standard units of Y being in cd/m2 or some
+    % such. Here we'll only fuss a little, and get the relative scaling
+    % right.
     %
     % Note that we need to convert to energy units to make the relative
     % scaling right.
@@ -105,14 +106,6 @@ else % trichromatic or dichromatic case
     scale = reshape(scale(2:4), [1 1 3]);
     
     % Set up interpolation mesh
-    %
-    % Haomiao had the line that is now commented out.  My (DHB's)
-    % read of the documentation is that meshgrid works in x,y, so
-    % that the return arguments should be [xq, yq] and not the other way
-    % around.  This change interacts with the ind2sub change in the loop below.
-    % Note that these do changes do not exactly cancel.  You get a similar
-    % but different interpolation if you go back to the way Haomiao had it.
-    %[yq, xq] = meshgrid(1:sz(2), 1:sz(1));
     [xq, yq] = meshgrid(1:sz(2), 1:sz(1));
 
     % loop over frames
@@ -123,10 +116,8 @@ else % trichromatic or dichromatic case
             indx = find(coneType == ii); 
             if isempty(indx), LMSphotons(:,:,ii-1) = 0; continue; end
             
-            % Here is my second change.  My read of the documentation is
-            % that ind2sub works in i, j so that the return arguments
-            % should be interpreted as [y, x] and not the other way around.
-            %[x, y] = ind2sub(sz, indx);
+            % ind2sub works in i, j so that the return arguments should be
+            % interpreted as [y, x] and not the other way around
             [y, x] = ind2sub(sz, indx);
             
             % Let griddata do the work.
