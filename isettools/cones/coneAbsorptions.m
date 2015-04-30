f1caa1eb7b94dd1e21f2784f7cc3bbfa06435836
Brian Wandell <wandell@stanford.edu>
2015-04-30 12:33:27 -0700

    I don't understand why the validation data is changing. THe other changes were comments in sensorCompute, comments in riekeAddNoise, and a substantial change to eliminate sensorComputeNoiseFree from coneAbsorptions.  Instead, we set the sensor noise flag appropriately.

--- isettools/cones/coneAbsorptions.m
+++ isettools/cones/coneAbsorptions.m
@@ -45,109 +45,69 @@ if notDefined('oi'),      error('Need optical image'); end
 xpos = sensorGet(sensor,'positions x');
 ypos = sensorGet(sensor,'positions y');
 
-% We could sort the positions to make the computation more efficient.  The
-% movie, however, won't match the real eye movements.  Below is the code
-% that HJ used to sort.  It isn't quite right in here, but ihas the idea.
-
-% pos = sensorGet(sensor,'movement positions');
-% [pos,~,ic]  = unique(pos,'rows');
-% %
-% % % Compute frame per position
-% framesPerPosition    = hist(ic,unique(ic));      % frames per position
-% framesPerPosition(1) = framesPerPosition(1) + size(pos,1) - sum(framesPerPosition);
-% x = pos(:,1); y = pos(:,2);
-
 %% Calculate volts.
-% convert (x,y) from degrees to (columns, rows). we multiply the number of
-% rows and columns by 2 because we will add rows or colums to only one side
-% of the sensor (e.g, top or right), which will shift the center of the
-% sensor by 1/2 the number of rows or columns added.
-% fov = sensorGet(sensor, 'fov', [], oi);
-% sz  = sensorGet(sensor, 'size');
 
-% xpos = round(2* x * sz(2) / fov);
-% ypos = round(2* y * sz(1) / fov);
-
-% With the noise flag set to 0, we compute only the mean.  No photon noise
-% or sensor noise.
-% sensor = sensorSet(sensor,'noise flag',0);
 
 % loop across positions, suppress waitbar in sensorCompute
 % wbarState = ieSessionGet('wait bar'); ieSessionSet('wait bar','off');
 nPos = length(xpos);
 
-% OLD CODE STARTS HERE
-% volts = [];
-% txt = sprintf('Looping over %i eye positions',nPositions);
-%
-%     if showBar, wBar = waitbar(0,txt); end
-%     for p = 1:nPositions
-%         if showBar, waitbar(p/nPositions,wBar); end
-%
-%         % if xpos (ypos) is positive, we add columns (rows) to the left
-%         % (bottom), shifting the image  rightward (upward). verify this. it
-%         % could easily be wrong.
-%         if ypos(p) > 0,  rows = [0 ypos(p)]; else rows = [-ypos(p) 0]; end
-%         if xpos(p) > 0,  cols = [xpos(p) 0]; else cols = [0 -xpos(p)]; end
-%
-%         % create a new, resized sensor. Should we worry about exposure time
-%         % each time we move (resize) the sensor?
-%         sensor2 = sensorHumanResize(sensor,rows, cols);
-%
-%         % compute the noiseless mean
-%         sensor2 = sensorCompute(sensor2,oi);
-%
-%         % compute the noisy frame-by-frame samples
-%         noiseType = 1;  % Only photon noise
-%         tmp = sensorComputeSamples(sensor2,framesPerPosition(p),noiseType);
-%
-%         % remove the added rows/columns
-%         tmp = tmp(1+rows(1):end-rows(2), 1+cols(1):end-cols(2), :);
-%
-%         % concatenate the voltage image
-%         volts = cat(3, volts, single(tmp));
-%
-%     end
-%     if showBar == 1, close(wBar); end
-%     ieSessionSet('wait bar',wbarState);
-%
-%     % Return the sensor with the voltage data from all the eye positions as
-%     % an Row x Col x Position matrix.
-%     sensor = sensorSet(sensor, 'volts', volts);
-
 % Pad to the sensor to max size
 rows = [-min([ypos(:); 0]) max([ypos(:); 0])];
 cols = [max([xpos(:); 0]) -min([xpos(:); 0])];
 coneType = sensorGet(sensor, 'cone type');
-sensor2 = sensorHumanResize(sensor, rows, cols);
+sensor2  = sensorHumanResize(sensor, rows, cols);
 
-% Compute all-L/M/S absorptions
+% With the noise flag set to 0, we compute only the mean.  No photon noise
+% or sensor noise.
+
+% Compute a full mosaic of L, M, and S absorptions
 sz = sensorGet(sensor2, 'size');
 LMS = zeros([sz 3]);
 msk = zeros([size(coneType) 3]);
 for ii = 2 : 4 % L, M, S
     pattern = ii * ones(sz);
-    sensor2 = sensorSet(sensor2, 'pattern', pattern);
-    sensor2 = sensorSet(sensor2, 'cone type', pattern);
     
-    sensor2 = sensorComputeNoiseFree(sensor2, oi);
+    % There are two uses of pattern.  They could be in conflict.  We should
+    % eliminate the need for two. Probably we need to preserve 'pattern'
+    % because it is long-standing, and we need to deal with cone type in
+    % some other way.
+    sensor2 = sensorSet(sensor2,'pattern', pattern);
+    sensor2 = sensorSet(sensor2,'cone type', pattern);
+    sensor2 = sensorSet(sensor2,'noise flag',0);
+
+    sensor2 = sensorCompute(sensor2, oi);
     LMS(:,:,ii-1) = sensorGet(sensor2, 'volts');
+    
+    % Store the positions of the cones of this type.  We use this later
+    % when we fill in the voltages in the original sensor.
     msk(:,:,ii-1) = double(coneType == ii);
 end
 
+% The LMS has a large, full-stack representation of the cones.  We know at
+% each moment in time where the eye is.  This next piece of code takes the
+% LMS stick accounts for eye position, and places the relevant LMS data
+% into the volts for the original size sensor.
 sz = sensorGet(sensor, 'size');
-volts = zeros([sz nPos]);
-
+volts = zeros([sz nPos]);   % 3D - Spatial size by number of eye positions
 for p = 1:nPos
+    
     % cropping
     tmp = LMS(1+rows(1)+ypos(p):end-rows(2)+ypos(p), ...
         1+cols(1)-xpos(p):end-cols(2)-xpos(p),:);
-    % select cone type
+    
+    % select cone type to match the mosaic
     volts(:,:,p) = sum(tmp .* msk, 3);
 end
 
-% Add photon noise
+% Add photon noise - though perhaps we should be adding general noise using
+% sensorAddNoise.
+%
 sensor = sensorSet(sensor, 'volts', volts);
 sensor = sensorSet(sensor, 'volts', noiseShot(sensor));
+
+% Someday, consider running Rieke adapt to get the current and then add
+% noise based on Rieke's data (riekeAddNoise))
+
 
 end
