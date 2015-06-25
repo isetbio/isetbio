d533a27e2f88ad9a8e93b28f54fac39583575aeb
james golden <jamesgolden1@gmail.com>
2015-06-25 19:22:17 -0400

    added outersegment class and tutorial

---
+++ isettools/outersegment/@outersegment/outersegmentGet.m
@@ -0,0 +1,27 @@
+function val = outersegmentGet(obj, param, varargin)
+%Get isetbio outersegment object parameters
+% 
+% 
+% 
+% 
+% 6/22/15 James Golden
+
+if ~exist('param','var') || isempty(param)
+    error('Parameter field required.');
+end
+
+val = []; % initialize output variable
+
+param = ieParamFormat(param);  % Lower case and remove spaces
+switch lower(param)
+
+    case {'noiseflag'}
+        
+        val = obj.noiseflag;
+        
+    case{'type'}
+        
+        val = obj.type;
+        
+end
+
---
+++ isettools/outersegment/@outersegment/outersegmentSet.m
@@ -0,0 +1,31 @@
+function obj = outersegmentSet(obj, param, val, varargin)
+%Set isetbio outersegment object parameters
+% 
+% 
+% 
+% 
+% 6/22/15 James Golden
+
+if ~exist('param','var') || isempty(param)
+    error('Parameter field required.');
+end
+if ~exist('val','var'),   error('Value field required.'); end;
+
+
+param = ieParamFormat(param);  % Lower case and remove spaces
+switch lower(param)
+
+    case {'noiseflag'}
+        if ~((val == 0) || (val == 1))
+            error('noiseflag parameter must be 0 or 1.');
+        end
+        obj.noiseflag = val;
+        
+    case{'type'}
+        if ~((val == 0) || (val == 1))
+            error('type parameter must be 0 or 1: 0 = linear temporal filter, 1 = nonlinear adaptation.');
+        end
+        obj.type = val;
+        
+end
+
