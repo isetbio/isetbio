3fd2b88c2a7fe86b57b364283129dc2583aaf877
David Brainard <brainard@psych.upenn.edu>
2015-07-16 10:19:41 -0400

    Minor.
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- validation/scripts/data/v_rdata.m
+++ validation/scripts/data/v_rdata.m
@@ -35,11 +35,32 @@ rdata('cd',remote);
 rdata('ls')  % Returns all the .mat files in the web page listing
 
 %% Loading a variable from inside a matlab file
-
+%
 % This is an ISET scene with HDR data and a depth map
 remote.directory = fullfile('LIGHTFIELD','scene');
+tic;
 scene = rdata('load data',remote,'benchHDR.mat','scene');
+elapsedTime = toc;
+fprintf('Remote read via rdata URL took %g seconds\n',elapsedTime);
 UnitTest.validationData('scene', scene);
+
+%% Load it via a FUSE/SSHFS mounted disk on OS/X and locally
+%
+% This is not run generally because the paths are specific to DHB's machine.
+CHECK_DHBTIMING = false;
+if (CHECK_DHBTIMING)
+    % Load a copy on the mounted crimson disk
+    tic;
+    scene2 = load('/Users/Shared/Volumes/CrimsonSCIEN/LIGHTFIELD/scene/benchHDR.mat','scene');
+    elapsedTime = toc;
+    fprintf('Remote read via FUSE/SSHFS took %g seconds\n',elapsedTime);
+    
+    % Load a copy off of David's desktop
+    tic;
+    scene3 = load('/Users/dhb/Desktop/benchHDR.mat','scene');
+    elapsedTime = toc;
+    fprintf('Local read took %g seconds\n',elapsedTime);
+end
 
 %% Show the scene
 if (runTimeParams.generatePlots)
