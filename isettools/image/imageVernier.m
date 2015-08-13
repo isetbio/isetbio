acb7b8a524bd657b6a1b1c36c65ecad7c1271893
Brian Wandell <wandell@stanford.edu>
2015-08-12 17:16:18 -0700

    Parameters changed

--- isettools/image/imageVernier.m
+++ isettools/image/imageVernier.m
@@ -1,7 +1,7 @@
-function Img = imageVernier(params)
+function [Img,params] = imageVernier(params)
 % Create an RGB image of a vernier line-pair
 %
-%   Img = imageVernier(params)
+%   [Img, params] = imageVernier(params)
 %
 % The vernier image params are
 %   p.sceneSz   = 64
@@ -10,6 +10,7 @@ function Img = imageVernier(params)
 %   p.lineSpace = inf
 %   p.barColor  = 0.99
 %   p.bgColor   = 0;
+%   p.pattern   = ones(...) % For the future
 %   
 % Examples:
 %   img = imageVernier(p); imshow(img)
@@ -26,12 +27,16 @@ function Img = imageVernier(params)
 % HJ/BW ISETBIO Team Copyright 2015
 % init parameters from params
 
+
+% TODO
+%  We would like to put in a 1D function that could be the pattern.  RIght
+%  now we only have a line.  But if we put in a little Gaussian 1D or
+%  anything we could shift that instead of the line
+
 %% Initialize parameters
 if isfield(params, 'sceneSz'),   sz = params.sceneSz; else sz = 64; end
 if isfield(params, 'barWidth'),  width = params.barWidth; else width = 1; end
 if isfield(params, 'offset'),    offset = params.offset; else offset = 1; end
-if isfield(params, 'lineSpace'), lineSpace = params.lineSpace;
-else lineSpace = inf; end
 
 if isfield(params, 'barColor')
     barColor = params.barColor;
@@ -49,7 +54,11 @@ if isscalar(bgColor), bgColor = repmat(bgColor, [1 3]); end
 %% Create image to be shown on display
 if isscalar(sz), sz = [sz sz]; end
 Img = repmat(reshape(bgColor,[1 1 3]),[sz 1]);
-cc = [round(sz(2)/2):-lineSpace:1 round(sz(2)/2):lineSpace:sz];
+
+% Center column, accounting for the line spacing
+cc = round(sz(2)/2);
+
+% We change the width
 width = width - 1;
 for jj = 1 : length(cc)
     barCols = max(round(cc(jj)-width/2),1) : ...
@@ -60,8 +69,18 @@ for jj = 1 : length(cc)
 end
 
 % Shift for offset
-Img(1:round(end/2),:,:) = circshift(Img(1:round(end/2),:,:), ...
-    [0 offset 0]);
+Img(1:round(end/2),:,:) = ...
+    circshift(Img(1:round(end/2),:,:), [0 offset 0]);
+
+if nargout == 2
+    params.sceneSz   = sz;
+    params.barWidth  = width + 1;
+    params.offset    = offset;
+    params.barColor  = barColor;
+    params.bgColor   = bgColor;
+    params.pattern   = [];   % For the future 1D pattern
+end
+
 
 end
 
