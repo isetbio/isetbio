39480ad8e15e05ee8232fbb79bea6aa19a107485
Fred Rieke <fred@Freds-MacBook-Pro.local>
2015-08-14 21:07:47 -0700

    fixed bug in riekeAddNoise

--- isettools/cones/riekeAddNoise.m
+++ isettools/cones/riekeAddNoise.m
@@ -47,25 +47,26 @@ if isvector(curNF), curNF = reshape(curNF, [1 1 length(curNF)]); end
 k = ceil((size(curNF, 3)-1)/2);
 params.freq = (0:k)/ sampTime / size(curNF, 3);
 
-LorentzCoeffs = [0.16*3  55  4;
-                 0.045*3 190 2.5];
+LorentzCoeffs = [0.16  55  4;
+                 0.045 190 2.5];
 noiseSPD = lorentzSum(LorentzCoeffs, params.freq);
 
 % make-up the negative frequency part
 noiseSPD = [noiseSPD noiseSPD(end:-1:1)];
 noiseSPD = noiseSPD(1:size(curNF, 3));
 noiseSPD = reshape(noiseSPD, [1 1 length(noiseSPD)]);
-figure(2);loglog(squeeze(sqrt(noiseSPD)));
+tempNoise = squeeze(noiseSPD);
+%figure(2);loglog(params.freq,tempNoise(1:length(params.freq)));
 
 % generate white gaussian noise
 noise = randn(size(curNF));
-noiseFFT = fft(noise, [], 3) / sqrt(size(noise, 3));
+noiseFFT = fft(noise, [], 3); % / sqrt(size(noise, 3));
 
 % adjust the spectral power distribution of the noise
 noiseFFT = bsxfun(@times, noiseFFT, sqrt(noiseSPD));
 
 % convert back to time domain to recover noise
-noise = real(ifft(noiseFFT, [], 3)) / sampTime; % take real part
+noise = real(ifft(noiseFFT, [], 3)) / sqrt(2*sampTime); % take real part
 %figure(2); plot(squeeze(noise));
 
 % add to noise-free signal
