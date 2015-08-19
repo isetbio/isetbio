093e24c95c495ad7e8a3ee86318ff39b32f7f2ec
Brian Wandell <wandell@stanford.edu>
2015-08-19 00:08:00 -0700

    Added movie functionality

--- isettools/cones/coneImageActivity.m
+++ isettools/cones/coneImageActivity.m
@@ -1,5 +1,5 @@
-function tmp = coneImageActivity(cones,data,step,showFlag)
-% Make a move of activity on the color cone mosaic
+function tmp = coneImageActivity(cones,data,step,dFlag)
+% Make a movie of absorptions on a color version of the cone mosaic
 %
 %   tmp = coneImageActivity(data,cones,step,showFlag)
 %
@@ -11,7 +11,7 @@ function tmp = coneImageActivity(cones,data,step,showFlag)
 
 if ieNotDefined('data'), data = sensorGet(cones,'photons'); end
 if ieNotDefined('step'), step = max(1,size(data,3)/100); end
-if ieNotDefined('showFlag'), showFlag = false; end
+if ieNotDefined('dFlag'), dFlag = false; end
 
 conesToPlot = max(sensorGet(cones,'size'));
 coneSize = 6;
@@ -29,7 +29,7 @@ sz = [size(coneMosaicImage),length(theseFrames)];
 tmp = zeros(sz);
 
 % Convert the image to the size of the mosaic and blur the result
-wbar = waitbar(0,'cone movie');
+wbar = waitbar(0,'creating cone movie');
 g = fspecial('gaussian',6,2);      % Blurring parameters
 cnt = 1;
 for ii=theseFrames
@@ -44,9 +44,38 @@ for ii=theseFrames
     for jj=1:3
         tmp(:,:,jj,cnt) = convolvecirc(coneMosaicImage(:,:,jj) .* fgrid,g);
     end
-    if showFlag, imshow(tmp(:,:,:,cnt).^0.3); drawnow; end
     cnt = cnt+1;
 end
 close(wbar);
 
+% Scale and gamma correct tmp
+tmp = tmp/max(tmp(:));
+tmp = tmp .^ 0.3;
+% vcNewGraphWin; imagescRGB(tmp);
+
+% Show the movie and return it (dFlag == true) Or save it as a movie file
+% in based on the parameters in the dFlag struct
+if isstruct(dFlag)
+    % Normalize tmp for visualization
+    vcNewGraphWin;
+    nframes = size(tmp,4);
+    % Record a movie named by the string in showFlag
+    vObj = VideoWriter(dFlag.vname);
+    vObj.FrameRate = dFlag.FrameRate;
+    open(vObj);
+    for ii = 1:step:nframes
+        image(tmp(:,:,:,ii))
+        drawnow;
+        F = getframe;
+        writeVideo(vObj,F);
+    end
+    close(vObj);
+elseif dFlag
+    % Just show the movie on the screen
+    for ii=1:step:nframes
+        imshow(tmp(:,:,:,ii)); 
+        drawnow;
+    end
+end
+    
 end
