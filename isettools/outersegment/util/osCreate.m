f7ad43ad135bf5901a32296b3de8dfebf40d68be
james golden <jamesgolden1@gmail.com>
2016-01-05 15:44:49 -0800

    slight change to osCreat

--- isettools/outersegment/util/osCreate.m
+++ isettools/outersegment/util/osCreate.m
@@ -1,8 +1,15 @@
 function obj = osCreate(varargin)
-% Generate an @osLinear, @osBioPhys or @osIdentity object
+% Create an outer segment object
 %
 %   os = osCreate([osType = 'linear'],[sensor = null]);
 %
+% There are three osTypes (sublcasses of outer segment objects).  These are
+%    @osLinear   -  uses linear impulse response to calculate cone current.
+%    @osBioPhys  - uses biophysical model by FMR to calculate cone current.
+%    @osIdentity - stores raw RGB data, used for stimulus-referred RGC
+%    models.
+% See sublcass definitions, e.g. @osLinear/osLinear.m, for more details.
+% 
 % Inputs:  Subclass type ('linear','biophys','identity')
 %
 % obj:    the outersegment object
@@ -12,13 +19,6 @@ function obj = osCreate(varargin)
 % See also: @osLinear, @osBioPhys and @osIdentity subclasses for more
 %           details of the specific implementations.
 %
-% There are three osTypes (sublcasses of outer segment objects).  These are
-%    @osLinear   -  uses linear impulse response to calculate cone current.
-%    @osBioPhys  - uses biophysical model by FMR to calculate cone current.
-%    @osIdentity - stores raw RGB data, used for stimulus-referred RGC
-%    models.
-% See sublcass definitions, e.g. @osLinear/osLinear.m, for more details.
-% 
 % Examples:
 %   os1 = osCreate();
 %   os1 = osCreate('biophys');
@@ -29,33 +29,19 @@ function obj = osCreate(varargin)
 %% Set up the defaults
 
 % We want to use the parser structure if possible (BW)
-osType = 'linear';   % Default values
-if ~isempty(varargin),   osType = ieParamFormat(varargin{1}); end %#ok<*NASGU>
+osType = 'linear';    % Default values
+if ~isempty(varargin),   osType = ieParamFormat(varargin{1}); end
 
-if nargin == 0
-    obj = osLinear();
-elseif nargin == 1
-    if strcmpi(varargin{1},'linear');
+%% Create the proper object
+switch osType
+    case {'linear','oslinear'}
         obj = osLinear();
-    elseif (strcmpi(varargin{1},'biophys') || strcmpi(varargin{1},'rieke'));
+    case {'biophys','osbiophys','rieke','osrieke'}
         obj = osBioPhys();
-    elseif strcmpi(varargin{1},'identity');
+    case {'identity','osidentity'}
         obj = osIdentity();
-    else
+    otherwise
         obj = osLinear();
-    end
-elseif nargin == 3
-    if strcmpi(varargin{1},'linear');
-        obj = osLinear(varargin{2},varargin{3});
-    elseif (strcmpi(varargin{1},'biophys') || strcmpi(varargin{1},'rieke'));
-        obj = osBioPhys(varargin{2},varargin{3});
-    elseif strcmpi(varargin{1},'identity');
-        obj = osIdentity(varargin{2},varargin{3});
-    else
-        obj = osLinear(varargin{2},varargin{3});
-    end
-else
-    obj = osLinear();
 end
 
 end
