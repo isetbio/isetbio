fe9c2c8b09c9115b3916ded1d0dfd0f9e43b8a10
David Brainard <brainard@psych.upenn.edu>
2016-01-08 10:43:07 -0500

    Move standalone functions related to cone calculations into isettools/utitlity/functions.
    And did a bit of remaming, reworking, and cleaning of those functions.  This will break the validation stuff, but that is soon to be fixed up.
    
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

---
+++ isettools/utility/functions/sumofdblexponentials.m
@@ -0,0 +1,17 @@
+function y = sumoftwodblexponential(coef, x)
+% y = dblexponentialnomean(coef, x)
+%
+% Compute sum of two double exponential functions, with no mean offset.
+% 
+%  y = A1*exp(-C1*abs(x) + A2*exp(-C2*abs(x);
+%
+% A1: coef(1)
+% C1: coef(2)
+% A2: coef(3)
+% C2: coef(4)
+%
+% 1/8/16  dhb  Added commments
+%         dhb  Changed both name and what is computed to be more standard
+
+% Do it
+y = coef(1) .* exp(-coef(2)*abs(x)) + coef(3) .* exp(-coef(4)*abs(x));
