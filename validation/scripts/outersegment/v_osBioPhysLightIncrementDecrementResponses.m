a10e2f25a13265faf3bd2b34a7c71ff57e0585ec
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-01-14 10:02:18 -0500

    fixed plotting buglet.
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- validation/scripts/outersegment/v_osBioPhysLightIncrementDecrementResponses.m
+++ validation/scripts/outersegment/v_osBioPhysLightIncrementDecrementResponses.m
@@ -11,6 +11,7 @@ function varargout = v_osBioPhysLightIncrementDecrementResponses(varargin)
     varargout = UnitTest.runValidationRun(@ValidationFunction, nargout, varargin);
 end
 
+
 %% Function implementing the isetbio validation code
 function ValidationFunction(runTimeParams)
 
@@ -25,14 +26,13 @@ function ValidationFunction(runTimeParams)
     pulseOnset  = 4000;
     pulseOffset = 7500;
     stimPeriod = [pulseOnset pulseOffset];
-    nSamples   = pulseOffset+2000;
+    nSamples   = pulseOffset+4000;
     simulationTime = (1:nSamples)*simulationTimeIntervalInSeconds;
     
     stimulusPhotonRateAmplitudes = 500 * 2.^(1:7); % photons/sec
     contrastsExamined = [-1 1];
     
     for stepIndex = 1:numel(stimulusPhotonRateAmplitudes)
-        
         % create stimulus temporal profile
         stimulusPhotonRate = zeros(nSamples, 1);
         stimulusPhotonRate(100:nSamples-100,1) = stimulusPhotonRateAmplitudes(stepIndex);
@@ -76,20 +76,22 @@ function ValidationFunction(runTimeParams)
             osBiophysOuterSegmentCurrent(stepIndex, contrastIndex,:) = current(1,1,:);
             
         end % contrastIndex
-        
-        if (runTimeParams.generatePlots)
-            
-            if ((stepIndex == 1))
+
+    
+    
+        if (runTimeParams.generatePlots)  
+            if (stepIndex == 1)
                 h = figure(1); clf;
                 set(h, 'Position', [10 10 900 1200]);
             end
             
+            % plot every 5-th time sample
+        
             % plot stimulus on the left
-            indices = 1:2:numel(simulationTime);
-            subplot(numel(stimulusPhotonRateAmplitudes),2,(stepIndex-1)*2+1); hold on;
-            plot([simulationTime(1) simulationTime(end)], stimulusPhotonRate*[1 1], 'k-');
-            stairs(simulationTime(indices), squeeze(stimulusPhotonRateStep(1,indices)), 'm-', 'LineWidth', 2.0);
-            stairs(simulationTime(indices), squeeze(stimulusPhotonRateStep(2,indices)), 'b-', 'LineWidth', 1.0);
+            subplot(numel(stimulusPhotonRateAmplitudes),2,(stepIndex-1)*2+1); 
+            plot([simulationTime(1) simulationTime(end)], stimulusPhotonRateAmplitudes(stepIndex)*[1 1], 'k-'); hold on;
+            plot(simulationTime, stimulusPhotonRateStep(1,:), 'm-', 'LineWidth', 2.0);
+            plot(simulationTime, stimulusPhotonRateStep(2,:), 'b-', 'LineWidth', 1.0);
             set(gca, 'XLim', [simulationTime(1) simulationTime(end)], 'YLim', [0 15e4]);
             if (stepIndex == numel(stimulusPhotonRateAmplitudes))
                 xlabel('time (sec)','FontSize',12);
@@ -97,12 +99,13 @@ function ValidationFunction(runTimeParams)
                 set(gca, 'XTickLabel', {});
             end
             ylabel('stimulus (photons/sec)','FontSize',12);
-            text(0.1, 10e4, sprintf('pedestal: %d photons/sec',stimulusPhotonRateAmplitudes(stepIndex)), 'FontSize',12);
-
+            text(0.1, stimulusPhotonRateAmplitudes(stepIndex)+10000, sprintf('step: %d ph/sec',stimulusPhotonRateAmplitudes(stepIndex)), 'FontSize',12);
+            drawnow;
+            
             % plot responses on the right
-            subplot(numel(stimulusPhotonRateAmplitudes),2,(stepIndex-1)*2+2); hold on;
-            plot(simulationTime(indices), squeeze(osBiophysOuterSegmentCurrent(stepIndex, 1, indices)), 'm-', 'LineWidth', 2.0);
-            plot(simulationTime(indices), squeeze(osBiophysOuterSegmentCurrent(stepIndex, 2, indices)), 'b-', 'LineWidth', 1.0);
+            subplot(numel(stimulusPhotonRateAmplitudes),2,(stepIndex-1)*2+2); 
+            plot(simulationTime, squeeze(osBiophysOuterSegmentCurrent(stepIndex, 1, :)), 'm-', 'LineWidth', 2.0); hold on;
+            plot(simulationTime, squeeze(osBiophysOuterSegmentCurrent(stepIndex, 2, :)), 'b-', 'LineWidth', 1.0);
             set(gca, 'XLim', [simulationTime(1) simulationTime(end)], 'YLim', [-100 0]);
             if (stepIndex == numel(stimulusPhotonRateAmplitudes))
                 xlabel('time (sec)','FontSize',12);
@@ -110,13 +113,10 @@ function ValidationFunction(runTimeParams)
                 set(gca, 'XTickLabel', {});
             end
             ylabel('current (uAmps)','FontSize',12);
+            
+            drawnow
         end % if (runTimeParams.generatePlots)
     end % stepIndex
-    
-    fprintf('Generating figure. This will take a while ...\n');
-    tic
-    drawnow;
-    fprintf('Figure generation took %2.2f sec\n', toc);
     
     % Save validation data
     UnitTest.validationData('osBiophysCurrent', osBiophysOuterSegmentCurrent);
