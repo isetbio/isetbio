55c0e668b60683ec03818628d8cb9657bb4d2031
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-03-09 12:57:32 -0500

    Added neural validation for incr/decr steps
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- validation/scripts/outersegment/v_osBioPhysLightIncrementDecrementResponses.m
+++ validation/scripts/outersegment/v_osBioPhysLightIncrementDecrementResponses.m
@@ -20,6 +20,9 @@ function ValidationFunction(runTimeParams)
 
     %% Init
     ieInit;
+
+    %% Load measured outer segment data
+    [stimulusPhotonRateAmplitudesNeuralDataPoints, decToIncRatioNeuralDataPoints] = loadMeasuredDecIncRatios();
     
     % Set the simulation time interval. In general, the stimulation time interval should 
     % be set to a small enough value so as to avoid overflow errors.
@@ -33,6 +36,7 @@ function ValidationFunction(runTimeParams)
     simulationTime = (1:nSamples)*simulationTimeIntervalInSeconds;
     
     stimulusPhotonRateAmplitudes = 500 * 2.^(1:7); % photons/sec
+    
     contrastsExamined = [-1 1];
     
     % create human sensor with 1 cone
@@ -40,7 +44,9 @@ function ValidationFunction(runTimeParams)
     sensor = sensorSet(sensor, 'size', [1 1]); % only 1 cone
     sensor = sensorSet(sensor, 'time interval', simulationTimeIntervalInSeconds);
 
-            
+    decrementResponseAmplitude = zeros(1, numel(stimulusPhotonRateAmplitudes));  
+    incrementResponseAmplitude = zeros(1, numel(stimulusPhotonRateAmplitudes)); 
+    
     for stepIndex = 1:numel(stimulusPhotonRateAmplitudes)
         % create stimulus temporal profile
         stimulusPhotonRate = zeros(nSamples, 1);
@@ -79,6 +85,15 @@ function ValidationFunction(runTimeParams)
         end % contrastIndex
 
 
+        % Gauge response amplitude at 3 seconds
+        [~, tBin3seconds] = min(abs(simulationTime-3.0));   % time bin to estimate response to inc/dec pulse
+        [~, tBin5seconds] = min(abs(simulationTime-5.0));   % time bin to estimate response to pedestal
+        adaptedDecrResponse = osBiophysOuterSegmentCurrent(stepIndex, 1, tBin5seconds);
+        adaptedIncrResponse = osBiophysOuterSegmentCurrent(stepIndex, 2, tBin5seconds);
+        decrementResponseAmplitude(stepIndex) = abs(osBiophysOuterSegmentCurrent(stepIndex, 1, tBin3seconds) - adaptedDecrResponse);
+        incrementResponseAmplitude(stepIndex) = abs(osBiophysOuterSegmentCurrent(stepIndex, 2, tBin3seconds) - adaptedIncrResponse);
+        fprintf('StepIndex %d: Decrement response amplitude: %2.2f, Increment response amplitude: %2.1f\n', stepIndex, decrementResponseAmplitude(stepIndex), incrementResponseAmplitude(stepIndex) );
+        
         if (runTimeParams.generatePlots)  
             if (stepIndex == 1)
                 h = figure(1); clf;
@@ -103,6 +118,7 @@ function ValidationFunction(runTimeParams)
             subplot(numel(stimulusPhotonRateAmplitudes),2,(stepIndex-1)*2+2); 
             plot(simulationTime, squeeze(osBiophysOuterSegmentCurrent(stepIndex, 1, :)), 'm-', 'LineWidth', 2.0); hold on;
             plot(simulationTime, squeeze(osBiophysOuterSegmentCurrent(stepIndex, 2, :)), 'b-', 'LineWidth', 1.0);
+            
             set(gca, 'XLim', [simulationTime(1) simulationTime(end)], 'YLim', [-100 0]);
             if (stepIndex == numel(stimulusPhotonRateAmplitudes))
                 xlabel('time (sec)','FontSize',12);
@@ -115,6 +131,32 @@ function ValidationFunction(runTimeParams)
         end % if (runTimeParams.generatePlots)
     end % stepIndex
     
+    showFit = false;
+    decToIncRatioNeural = zeros(1, numel(stimulusPhotonRateAmplitudes));
+    decToIncRatioModel = zeros(1, numel(stimulusPhotonRateAmplitudes));
+    
+    for stepIndex = 1:numel(stimulusPhotonRateAmplitudes)
+        [decToIncRatioNeural(stepIndex), stimulusPhotonRateAxisFit, decToIncRatioNeuralFit] = generateDecIncRatioEstimate(stimulusPhotonRateAmplitudes(stepIndex), showFit, stimulusPhotonRateAmplitudesNeuralDataPoints, decToIncRatioNeuralDataPoints);
+        decToIncRatioModel(stepIndex) = decrementResponseAmplitude(stepIndex) / incrementResponseAmplitude(stepIndex);
+    end
+    
+    h = figure(2); clf;
+    set(h, 'Position', [10 10 1200 700]);
+    hold on;
+    plot(stimulusPhotonRateAmplitudesNeuralDataPoints, decToIncRatioNeuralDataPoints, 'mo', 'MarkerFaceColor', [1 0.7 0.7], 'MarkerSize', 8);
+    %plot(stimulusPhotonRateAxisFit, decToIncRatioNeuralFit, 'r-', 'LineWidth', 2.0);
+    plot(stimulusPhotonRateAmplitudes, decToIncRatioModel, 'bs', 'MarkerSize', 12, 'MarkerFaceColor', [0.7 0.7 1.0]);
+    set(gca, 'FontSize', 12);
+    xlabel('log10 background intensity (R*/cone/sec)', 'FontSize', 14);
+    ylabel('decrement/increment response ratio', 'FontSize', 14);
+    %hLegend = legend('neural data', 'fit to neural data (logistic)', '@osBiophys model data');
+    hLegend = legend('neural data',  '@osBiophys model data');
+    set(hLegend, 'Location', 'SouthEast', 'FontSize', 12);
+    box on;
+    grid on;
+    hold off;
+    drawnow;
+    
     % Save validation data
     UnitTest.validationData('osBiophysCurrent', osBiophysOuterSegmentCurrent);
     UnitTest.validationData('simulationTime', simulationTime);
@@ -122,3 +164,65 @@ function ValidationFunction(runTimeParams)
     UnitTest.validationData('stimulusPhotonRateAmplitudes',stimulusPhotonRateAmplitudes);
 end
 
+function [intensities, decIncRatios] = loadMeasuredDecIncRatios()
+    dataSource = {'resources/data/cones', 'decIncRatios.mat'};
+    fprintf('Fetching remote data: dir=''%s''  file=''%s''. Please wait ...\n', dataSource{1}, dataSource{2});
+    % Download neural data from isetbio's repository
+    client = RdtClient('isetbio');
+    client.crp(dataSource{1});
+    [data, decIncRatiosArtifact] = client.readArtifact(dataSource{2}, 'type', 'mat');
+    fprintf('Done fetching data.\n');
+    intensities = data.intensities;
+    decIncRatios = data.decIncRatios;
+end
+
+    
+    
+function [predictedDecToIncRatio, stimulusPhotonRateAxis, predictedDecToIncFunction] = generateDecIncRatioEstimate(backgroundIntensity, showFit, Intensities, DecIncRatio)
+   
+    logIntensDataPts = log10(Intensities);
+    decIncRatioDataPts = DecIncRatio;
+    if (showFit)
+        figure(100); clf;
+        plot(10.^logIntensDataPts, decIncRatioDataPts, 'ks');
+    end
+    
+    % Initial params for logistic function
+    gain = 4;
+    minDecIncRatio = 1.7;
+    kappa = 4;                % steepness
+    logIntensity50 = 4.5;     % midpoint
+    initialParams = [minDecIncRatio; gain; kappa; logIntensity50];
+    
+    % Fit logistic function to recorded data
+    fittedParams = nlinfit(logIntensDataPts, decIncRatioDataPts, @logisticFunction, initialParams);
+    
+    if (showFit)
+        logIntensityAxis = linspace(min(logIntensDataPts), max(logIntensDataPts), 100);
+        decIncRatioFunction = logisticFunction(fittedParams, logIntensityAxis);
+        hold on;
+        plot(10.^logIntensityAxis, decIncRatioFunction, 'r-', 'LineWidth', 2.0);
+        hold off;
+        legend('data', 'fitted function');
+        drawnow;
+    end
+    
+    if ( (log10(backgroundIntensity) >= min(logIntensDataPts)) && ...
+         (log10(backgroundIntensity) <= max(logIntensDataPts)) )
+        predictedDecToIncRatio = logisticFunction(fittedParams, log10(backgroundIntensity));
+    else
+        predictedDecToIncRatio = nan;
+    end
+    
+    stimulusPhotonRateAxis = 10.^(linspace(min(logIntensDataPts), max(logIntensDataPts), 100));
+    predictedDecToIncFunction = logisticFunction(fittedParams, log10(stimulusPhotonRateAxis));
+end
+
+
+function y = logisticFunction(params, x)
+    yo = params(1);
+    gain = params(2); 
+    kappa = params(3);
+    x50 = params(4);
+    y = yo + gain * (1 ./ (1 + exp(-kappa*(x-x50)))); 
+end
