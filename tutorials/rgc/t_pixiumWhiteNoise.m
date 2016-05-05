d012a90ba329c08d24f66212fe50cc02da24fa00
james golden <jamesgolden1@gmail.com>
2016-05-04 20:22:07 -0700

    small changes to pixium white noise

--- tutorials/rgc/t_pixiumWhiteNoise.m
+++ tutorials/rgc/t_pixiumWhiteNoise.m
@@ -102,7 +102,7 @@ innerRetina = irCompute(innerRetina,os);
 irPlot(innerRetina, 'linear');
 % irPlot(innerRetina, 'psth');
 
-%%
+%% Look at covariance matrix
 
 psth1 = mosaicGet(innerRetina.mosaic{1},'responsePsth');
 
@@ -124,105 +124,17 @@ figure; imagesc(psthCov)
 xlabel('Cell number'); ylabel('Cell number');
 title('Covariance matrix');
 
+% PSTHs are here:
+psthM;
 
-rf = zeros(96,96);
-for fr = 1:1197
-    rf = rf+(sum(psth1{8,4}((fr-1)*100+1:fr*100)))*(iStim.sceneRGB(:,:,fr,1));
-%     rf = rf+(1*(iStim.sceneRGB(:,:,fr,1)));
-end
-figure; imagesc(rf)
+% Stimulus here:
+whiteNoise.sceneRGB;
 
-% %% Compute RGC spiking
-% numberTrials = 1;
-% for tr = 1:numberTrials
-%     innerRetina = irComputeSpikes(innerRetina);
+
+% 
+% rf = zeros(96,96);
+% for fr = 1:1197
+%     rf = rf+(sum(psth1{8,4}((fr-1)*100+1:fr*100)))*(iStim.sceneRGB(:,:,fr,1));
+% %     rf = rf+(1*(iStim.sceneRGB(:,:,fr,1)));
 % end
-% %% Invert representation to form image/movie
-% clear stimulusReconstruction
-% [stimulusReconstruction, paramsRec] = irReconstruct(innerRetina);
-% 
-% 
-% %% Build RGC array for healthy retina
-% 
-% clear paramsIR innerRetinaHealthy
-% paramsIR.name    = 'Macaque inner retina 1'; % This instance
-% paramsIR.eyeSide   = 'left';   % Which eye
-% paramsIR.eyeRadius = 6;        % Radius in mm
-% paramsIR.eyeAngle  = 90;       % Polar angle in degrees
-% 
-% model   = 'LNP';    % Computational model
-% innerRetinaHealthy = irCreate(os,paramsIR);
-% innerRetinaHealthy = rgcMosaicCreate(innerRetinaHealthy,'type','onMidget','model',model);
-% innerRetinaHealthy = rgcMosaicCreate(innerRetinaHealthy,'type','offMidget','model',model);
-% innerRetinaHealthy = rgcMosaicCreate(innerRetinaHealthy,'type','onParasol','model',model);
-% innerRetinaHealthy = rgcMosaicCreate(innerRetinaHealthy,'type','offParasol','model',model);
-% 
-% %%
-% innerRetinaHealthy = irComputeContinuous(innerRetinaHealthy,os);
-% numberTrials = 1;
-% for tr = 1:numberTrials
-%     innerRetinaHealthy = irComputeSpikes(innerRetinaHealthy);
-% end
-% 
-% irPlot(innerRetinaHealthy, 'linear');
-% % irPlot(innerRetinaHealthy, 'mosaic');
-% %% Invert representation to form image/movie
-% clear stimulusReconstructionHealthy
-% [stimulusReconstructionHealthy, paramsRecHealthy] = irReconstruct(innerRetinaHealthy);
-% 
-% %%
-% name_str = 'gratingH_20Hz_Width12_ONM_ONP_OFFM_OFFP_NoSub.mp4';
-% path_str = '/Users/james/Documents/MATLAB/isetbio misc/pixium_videos/meeting_april29/';
-% vObj = VideoWriter([path_str name_str],'MPEG-4');
-% vObj.FrameRate = 10;
-% vObj.Quality = 100;
-% open(vObj);
-% 
-% sizeScene = size(whiteNoise.sceneRGB(:,:,frame1,:));
-% 
-% % Play the movie with the stimulus
-% for loopv = 1%:10
-% h1=figure; set(gcf,'position',[160 60 1070 740]);
-% hold on;
-% for frame1 = 1:params.nSteps%size(whiteNoise.sceneRGB,3)
-%     subplot(221);
-%     imagesc(squeeze(whiteNoise.sceneRGB(:,:,frame1,:)));
-%     colormap gray; 
-%     
-%     subplot(222);
-%     for xPos = 1:numberElectrodesX
-%         for yPos = 1:numberElectrodesY
-%             hold on;
-%             fill(xh+electrodeArray.center(xPos,numberElectrodesY+1-yPos,1),yh+electrodeArray.center(xPos,numberElectrodesY+1-yPos,2),electrodeArray.activation(xPos,yPos,frame1))
-%         end
-%     end
-%     caxis([0 1]);
-%     
-%     
-%     subplot(223);    
-% %     imagesc((stimulusReconstructionHealthy(1:paramsRecHealthy.maxx,1:paramsRecHealthy.maxy,frame1)));
-%     imagesc((stimulusReconstructionHealthy(1:sizeScene(1),1:sizeScene(2),frame1)));
-%      colormap gray
-% %      caxis([1*paramsRecHealthy.minR 1*paramsRecHealthy.maxR]);
-%     caxis([.5*paramsRecHealthy.minR .5*paramsRecHealthy.maxR]);
-% %     caxis([0 .5*paramsRecHealthy.maxR]);
-%     title('Healthy');
-%     
-%     subplot(224);    
-% %     imagesc((stimulusReconstruction(1:paramsRec.maxx,1:paramsRec.maxy,frame1)));
-%     imagesc((stimulusReconstruction(1:sizeScene(1),1:sizeScene(2),frame1)));
-% 
-%      colormap gray
-% % %     caxis([.5*paramsRec.minR .5*paramsRec.maxR]);
-%     caxis([0 1*paramsRec.maxR]);
-%     title('Prosthetic');
-% %     pause(0.1);
-% drawnow
-% 
-%     F = getframe(h1);
-%     writeVideo(vObj,F);
-% end
-% end
-% 
-% 
-% close(vObj)
+% figure; imagesc(rf)
