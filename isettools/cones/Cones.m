c15bd42c9bb4113e09585653ea1ff7a885acf30b
Haomiao Jiang <Killua@HJ-Air.local>
2016-06-24 22:30:17 -0700

    Convert cones, lens and macular struct to class object

---
+++ isettools/cones/Cones.m
@@ -0,0 +1,96 @@
+classdef Cones < handle
+    % Class for single cone
+    %
+    %   cone = Cones();
+    %
+    % This class contains properties for single cone cell. For the full
+    % cone mosaic, see coneMosaic class
+    %
+    % HJ, ISETBIO Team, 2016
+    
+    properties  % public properties
+        wave;            % wavelength samples
+        opticalDensity;  % photopigment optical densities for L,M,S
+        peakEfficiency;  % peak absorptance efficiency
+        spatialDensity;  % spatial density (ratio) of the K-LMS cones
+        width;           % cone width in meters
+        height;          % cone height in meters
+    end
+    
+    properties(Dependent)
+        absorbance;         % spectral absorbance of the cones
+        absorptance;        % cone absorptance without ocular media
+        quantaFundamentals; % normalized cone absorptance
+        energyFundamentals; % normalized cone absorption in energy units
+        area;               % width * height
+    end
+    
+    properties(Access = private)  % private properties
+        wave_;        % internal wavelength samples
+        absorbance_;  % absorbance data sampled at wave_
+    end
+    
+    methods  % public methods
+        % constructor
+        function obj = Cones(varargin)
+            % parse input
+            p = inputParser;
+            p.addParameter('wave', 400:10:700, @isnumeric);
+            p.addParameter('opticalDensity', [0.5 0.5 0.4], @isnumeric);
+            p.addParameter('absorbance', [], @isnumeric);
+            p.addParameter('peakEfficiency', [2 2 2]/3, @isnumeric);
+            p.addParameter('spatialDensity', [0 0.6 0.3 0.1], @isnumeric);
+            p.addParameter('width', 2e-6, @isnumeric);
+            p.addParameter('height', 2e-6, @isnumeric);
+            
+            p.parse(varargin{:});
+            
+            % set object properties
+            obj.wave = p.Results.wave(:);
+            obj.wave_ = (400:10:700)';
+            obj.opticalDensity = p.Results.opticalDensity(:);
+            obj.peakEfficiency = p.Results.peakEfficiency(:);
+            obj.spatialDensity = p.Results.spatialDensity(:);
+            obj.width = p.Results.width;
+            obj.height = p.Results.height;
+            
+            if isempty(p.Results.absorbance)
+                obj.absorbance_ = 10 .^ ...
+                    ieReadSpectra('coneAbsorbance', obj.wave_);
+            else
+                obj.absorbance = p.Results.absorbance;
+            end
+        end
+        
+        % get method for dependent variable
+        function val = get.absorbance(obj) % inerpolate for absorbance
+            val = interp1(obj.wave_,obj.absorbance_,obj.wave,'linear',0);
+        end
+        
+        function val = get.absorptance(obj) % compute absorptance
+            val = 1 - 10.^(-obj.absorbance*diag(obj.opticalDensity));
+        end
+        
+        function val = get.quantaFundamentals(obj)
+            % compute quanta fundamentals
+            val = bsxfun(@rdivide, obj.absorptance, max(obj.absorptance));
+        end
+        
+        function val = get.energyFundamentals(obj)
+            h = vcConstants('planck');
+            c = vcConstants('speed of light');
+            val = 1e-9*bsxfun(@times,obj.quantaFundamentals/h/c,obj.wave);
+        end
+        
+        function val = get.area(obj)
+            val = obj.width * obj.height;
+        end
+        
+        % set method for dependent variable
+        function set.absorbance(obj, val)
+            obj.absorbance_ = interp1(obj.wave,val,obj.wave_,'linear',0);
+        end
+    end
+    
+    
+end
---
+++ isettools/cones/Macular.m
@@ -0,0 +1,108 @@
+classdef Macular < handle
+    % Class for macular pigment properties
+    %
+    %     macular = Macular('wave', wave)
+    %
+    % The human retina contains a pigment that covers the central (macular)
+    % region. This macular pigment passes certain wavelengths of light more
+    % than others. The pigment varies in density from central vision, where
+    % it is highest, to increasingly peripheral vision.
+    %
+    % This function returns several measures of the macular pigment
+    % wavelength properties as a function of macular pigment density (high
+    % in the fovea, lower in the near fovea).
+    %
+    % The returned class object includes a variety of derived terms.
+    % This should help to keep the relationship between entities straight.
+    %
+    % Density is the estimated (average) peak density of the pigment across
+    % a variety of observers.  They estimate the average (across observers)
+    % peak density to be 0.28, with a range of 0.17 to 0.48.
+    %
+    % Useful formulae
+    %
+    %   Absorbance spectra are normalized to a peak value of 1.
+    %   Absorptance spectra are the proportion of quanta actually absorbed.
+    %   Equation: absorptanceSpectra = 1 - 10.^(-OD * absorbanceSpectra)
+    %
+    % The original macular densities values were taken from the Stockman
+    % site. Go to http://cvision.ucsd.edu, then click on Prereceptoral
+    % filters.  At this point in time, I think the Psychtoolbox and the new
+    % Stockman site are authoritative.
+    %
+    %
+    % Examples:
+    %   macular = Macular();
+    %
+    % HJ, ISETBIO TEAM, 2016
+    
+    properties       % public properties
+        wave;        % wavelength samples in nm
+        density;     % macular pigment density
+    end
+    
+    properties (Access=private)
+        wave_;         % internal wavelength samples
+        unitDensity_;  % unit density absorbance sampled with wave_
+    end
+    
+    properties (Dependent)
+        unitDensity;     % spectral absorbance with unit pigment density
+        spectralDensity; % unitDensity scaled by obj.density
+        transmittance;   % proportion of quanta transmitted
+        absorptance;     % proportion of quanta absorbed
+    end
+    
+    methods  % public methods
+        % constructor
+        function obj = Macular(varargin)
+            % parse input
+            p = inputParser;
+            p.addParameter('wave', 400:10:700, @isnumeric);
+            p.addParameter('density', 0.35, @isscalar);
+            p.addParameter('unitDensity', [], @isnumeric);
+            
+            p.parse(varargin{:});
+            
+            % set properties
+            obj.wave = p.Results.wave(:);
+            obj.wave_ = (400:10:700)';
+            obj.density = p.Results.density;
+            
+            if isempty(p.Results.unitDensity)
+                obj.unitDensity_ = ieReadSpectra('macularPigment.mat', ...
+                    obj.wave_)/0.3521;
+            else
+                obj.unitDensity = p.Results.unitDensity;
+            end
+        end
+        
+        % get methods for dependent variables
+        function val = get.unitDensity(obj)
+            % interpolate for wavelength samples
+            val = interp1(obj.wave_,obj.unitDensity_,obj.wave,'linear',0);
+        end
+        
+        function val = get.spectralDensity(obj)
+            % compute scaled absorbance
+            val = obj.unitDensity * obj.density;
+        end
+        
+        function val = get.transmittance(obj)
+            % compute proportion of quanta transmitted
+            val = 10.^(-obj.spectralDensity);
+        end
+        
+        function val = get.absorptance(obj)
+            % comptue proportion of quanta absorbed
+            val = 1 - obj.transmittance;
+        end
+        
+        
+        % set methods for dependent variables
+        function set.unitDensity(obj, val)
+            % interpolate for wavelength samples
+            obj.unitDensity_ = interp1(obj.wave,val,obj.wave_,'linear',0);
+        end
+    end
+end
