dd3498baf547b8eca3ca9bf7df943fdee35d3e23
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-07-11 09:27:36 -0400

    Added NicePlot class for, well, generating nice plots.
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

---
+++ external/brainardlabtoolbox/Plotting/@NicePlot/NicePlot.m
@@ -0,0 +1,44 @@
+% Static class with methods for re-nicing plots
+% 
+% Example usage:
+%
+% (1) Set font sizes for axes, labels, legends, and titles to a base size of 12
+% NicePlot.setFontSizes(figureHandle, 'FontSize', 12);
+%
+% (2) Get position vectors for subplots with desired margins
+% subplotPosVectors = NicePlot.getSubPlotPosVectors(...
+%        'rowsNum', 2, ...
+%        'colsNum', 3, ...
+%        'heighthMargin',  0.06, ...
+%        'widthMargin',    0.05, ...
+%        'leftMargin',     0.07, ...
+%        'rightMargin',    0.03, ...
+%        'bottomMargin',   0.15, ...
+%        'topMargin',      0.1);
+%    
+% 11/14/2014  npc Wrote it.
+%
+
+classdef NicePlot
+    
+    methods (Static = true)
+        % Method to return position vectors for all subplots
+        posVectors = getSubPlotPosVectors(varargin);
+        
+        % Method to set the fonts for the axes, labels
+        setFontSizes(figHandle, varargin);
+        
+        % Method to export fig in figHandle to a PDF doc that looks just like the screen figure
+        exportFigToPDF(pdfFileName,figHandle,dpi, varargin);
+
+        % Method to export fig in figHandle to a PNG doc that looks just like the screen figure
+        exportFigToPNG(pngFileName,figHandle,dpi, varargin);
+        
+        % Method to export fig in figHandle to a separate page in a PDF doc 
+        appendFigAsSeparatePageInPDFdoc(pdfFileName,figHandle,dpi);
+        
+        % Method to combine multiple PDFfiles into a single PDF;
+        % sourcePDFFileNames should be a cell array of PDF filenames
+        combinePDFfilesInSinglePDF(sourcePDFFileNames, pdfFileName);
+    end
+end
---
+++ external/brainardlabtoolbox/Plotting/@NicePlot/appendFigAsSeparatePageInPDFdoc.m
@@ -0,0 +1,64 @@
+function appendFigAsSeparatePageInPDFdoc(pdfFileName,figHandle,dpi)
+
+        % If no handle is provided, use the current figure as default
+    if nargin<1
+        [fileName,pathName] = uiputfile('*.pdf','Save to PDF file:');
+        if fileName == 0; return; end
+        pdfFileName = [pathName,fileName];
+    end
+    if nargin<2
+        handle = gcf;
+    end
+    if nargin<3
+        dpi = 150;
+    end
+        
+    % Backup previous settings
+    prePaperType = get(figHandle,'PaperType');
+    prePaperUnits = get(figHandle,'PaperUnits');
+    preUnits = get(figHandle,'Units');
+    prePaperPosition = get(figHandle,'PaperPosition');
+    prePaperSize = get(figHandle,'PaperSize');
+
+    % Make changing paper type possible
+    set(figHandle,'PaperType','<custom>');
+
+    % Set units to all be the same
+    set(figHandle,'PaperUnits','inches');
+    set(figHandle,'Units','inches');
+
+    % Set the page size and position to match the figure's dimensions
+    paperPosition = get(figHandle,'PaperPosition');
+    position = get(figHandle,'Position');
+    set(figHandle,'PaperPosition',[0,0,position(3:4)]);
+    set(figHandle,'PaperSize',position(3:4));
+
+    set(figHandle,'InvertHardCopy','off');
+    
+    if (exist(pdfFileName, 'file'))
+        tmpFileName = sprintf('tmpPDF.pdf');
+        mergedFileName = sprintf('mergedPDF.pdf');
+        print(figHandle,'-dpdf', tmpFileName,sprintf('-r%d',dpi));
+        status = system(sprintf('/usr/local/bin/pdfunite %s %s %s', pdfFileName, tmpFileName, mergedFileName));
+        if (status ~= 0)
+            fprintf('Error during pdfunite. Have you installed poppler (''brew install poppler'')?');
+            system(sprintf('rm %s', tmpFileName));
+        else
+            system(sprintf('rm %s', tmpFileName));
+            system(sprintf('mv %s %s', mergedFileName, pdfFileName));
+            fprintf('\nNicePlot: PDF appended as the last page of %s.\n', pdfFileName);
+        end
+    else
+        fprintf('\nNicePlot: PDF saved as the first page of %s.\n', pdfFileName);
+        print(figHandle,'-dpdf', pdfFileName,sprintf('-r%d',dpi));
+    end
+
+    % Restore the previous settings
+    set(figHandle,'PaperType',prePaperType);
+    set(figHandle,'PaperUnits',prePaperUnits);
+    set(figHandle,'Units',preUnits);
+    set(figHandle,'PaperPosition',prePaperPosition);
+    set(figHandle,'PaperSize',prePaperSize);
+    
+end
+
