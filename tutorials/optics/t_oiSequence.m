669c19ba0c571a765df3da24e183508f0e3535e4
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-10-25 22:29:46 -0400

    Stremlined t_oiSequence
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- tutorials/optics/t_oiSequence.m
+++ tutorials/optics/t_oiSequence.m
@@ -53,103 +53,42 @@ modulationRegion.radiusInMicrons = 250;
 % oiSequence object for computing a sequence of ois where the oiModulated
 % (uniform field) is ADDED to the oiBackground over an 250 micron radius
 % region using a monophasic modulation function
-theOIsequence = oiSequence(oiBackground, oiModulated, modulationFunction1, 'composition', 'add', 'modulationRegion', modulationRegion);
+theOIsequence(1) = oiSequence(oiBackground, oiModulated, modulationFunction1, 'composition', 'add', 'modulationRegion', modulationRegion);
 
 % oiSequence object for computing a sequence of ois where the oiModulated
 % (a grating) is BLENDED with the oiBackground using a biphasic modulation function
-theOIsequence2 = oiSequence(oiBackground, oiModulated, modulationFunction2, 'composition', 'add', 'modulationRegion', modulationRegion);
+theOIsequence(2) = oiSequence(oiBackground, oiModulated, modulationFunction2, 'composition', 'add', 'modulationRegion', modulationRegion);
 
 % oiSequence object for computing a sequence of ois where the oiModulated
 % (a grating) is ADDED with the oiBackground using a biphasic modulation function
-theOIsequence3 = oiSequence(oiBackground, oiModulatedGabor, modulationFunction1,  'composition', 'add');
+theOIsequence(3) = oiSequence(oiBackground, oiModulatedGabor, modulationFunction1,  'composition', 'add');
 
 % oiSequence object for computing a sequence of ois where the oiModulated
 % (a grating) is BLENDED with the oiBackground using a biphasic modulation function
-theOIsequence4 = oiSequence(oiBackground, oiModulatedGabor, modulationFunction3, 'composition', 'blend');
+theOIsequence(4) = oiSequence(oiBackground, oiModulatedGabor, modulationFunction3, 'composition', 'blend');
 
 % Plot the oisequences
 hFig = figure(1); clf;
 set(hFig, 'Color', [1 1 1], 'Position', [10 10 1500 950]);
-for oiIndex = 1:theOIsequence.length
+for oiIndex = 1:theOIsequence(1).length
+    for k = 1:4
+        % Plot the modulation function
+        subplot(8,round(theOIsequence(k).length/2)+1, 1 + (k-1)*2*(1+round(theOIsequence2.length/2)));
+        plot(1:theOIsequence(k).length, theOIsequence(k).modulationFunction, 'rs-', 'LineWidth', 1.5);
+        set(gca, 'XLim', [1 theOIsequence(k).length]);
+        title(sprintf('mode: %s', theOIsequence(k).composition));
+        xlabel('frame index');
+        ylabel('modulation');
     
-    % Plot the modulation function
-    subplot(8,round(theOIsequence.length/2)+1, 1);
-    plot(1:theOIsequence.length, theOIsequence.modulationFunction, 'ks-', 'LineWidth', 1.5);
-    set(gca, 'XLim', [1 theOIsequence.length]);
-    title(sprintf('mode: %s', theOIsequence.composition));
-    xlabel('frame index');
-    ylabel('modulation');
+        % Ask theOIsequence to return the oiIndex-th frame
+        currentOI = theOIsequence(k).frameAtIndex(oiIndex);
     
-    % Ask theOIsequence to return the oiIndex-th frame
-    currentOI = theOIsequence.frameAtIndex(oiIndex);
-    
-    % plot it
-    subplot(8,round(theOIsequence.length/2)+1, 1+oiIndex);
-    rgbImage = lrgb2srgb(xyz2rgb(oiGet(currentOI, 'xyz')));
-    imagesc(rgbImage, [0 1]);
-    title(sprintf('frame %d', oiIndex));
-    axis 'image'
-    set(gca, 'XTick', [], 'YTick', []);
-    
-    
-    % Plot the modulation function
-    subplot(8,round(theOIsequence2.length/2)+1, 1 + 2*(1+round(theOIsequence2.length/2)));
-    plot(1:theOIsequence2.length, theOIsequence2.modulationFunction, 'ks-', 'LineWidth', 1.5);
-    set(gca, 'XLim', [1 theOIsequence.length]);
-    title(sprintf('mode: %s', theOIsequence2.composition));
-    xlabel('frame index');
-    ylabel('modulation');
-    
-    % Ask theOIsequence2 to return the oiIndex-th frame
-    currentOI = theOIsequence2.frameAtIndex(oiIndex);
-    
-    % plot it
-    subplot(8,round(theOIsequence.length/2)+1, 1+oiIndex+2*(1+round(theOIsequence.length/2)));
-    rgbImage = lrgb2srgb(xyz2rgb(oiGet(currentOI, 'xyz')));
-    imagesc(rgbImage, [0 1]);
-    title(sprintf('frame %d', oiIndex));
-    axis 'image'
-    set(gca, 'XTick', [], 'YTick', []);
-    
-    
-    % Plot the modulation function
-    subplot(8,round(theOIsequence3.length/2)+1, 1 + 4*(1+round(theOIsequence3.length/2)));
-    plot(1:theOIsequence3.length, theOIsequence3.modulationFunction, 'ks-', 'LineWidth', 1.5);
-    set(gca, 'XLim', [1 theOIsequence3.length]);
-    title(sprintf('mode: %s', theOIsequence3.composition));
-    xlabel('frame index');
-    ylabel('modulation');
-    
-    % Ask theOIsequence3 to return the oiIndex-th frame
-    currentOI = theOIsequence3.frameAtIndex(oiIndex);
-    
-    % plot it
-    subplot(8,round(theOIsequence3.length/2)+1, 1+oiIndex+4*(1+round(theOIsequence3.length/2)));
-    rgbImage = lrgb2srgb(xyz2rgb(oiGet(currentOI, 'xyz')));
-    imagesc(rgbImage, [0 1]);
-    title(sprintf('frame %d', oiIndex));
-    axis 'image'
-    set(gca, 'XTick', [], 'YTick', []);
-    
-    
-    % Plot the modulation function
-    subplot(8,round(theOIsequence4.length/2)+1, 1 + 6*(1+round(theOIsequence4.length/2)));
-    plot(1:theOIsequence.length, theOIsequence4.modulationFunction, 'ks-', 'LineWidth', 1.5);
-    set(gca, 'XLim', [1 theOIsequence4.length]);
-    title(sprintf('mode: %s', theOIsequence4.composition));
-    xlabel('frame index');
-    ylabel('modulation');
-    
-    % Ask theOIsequence4 to return the oiIndex-th frame
-    currentOI = theOIsequence4.frameAtIndex(oiIndex);
-    
-    % plot it
-    subplot(8,round(theOIsequence4.length/2)+1, 1+oiIndex+6*(1+round(theOIsequence4.length/2)));
-    rgbImage = lrgb2srgb(xyz2rgb(oiGet(currentOI, 'xyz')));
-    imagesc(rgbImage, [0 1]);
-    title(sprintf('frame %d', oiIndex));
-    axis 'image'
-    set(gca, 'XTick', [], 'YTick', []);
-    
-    
+        % plot it
+        subplot(8,round(theOIsequence(k).length/2)+1, 1+oiIndex+(k-1)*2*(1+round(theOIsequence2.length/2)));
+        rgbImage = lrgb2srgb(xyz2rgb(oiGet(currentOI, 'xyz')));
+        imagesc(rgbImage, [0 1]);
+        title(sprintf('frame %d', oiIndex));
+        axis 'image'
+        set(gca, 'XTick', [], 'YTick', []);
+    end
 end
