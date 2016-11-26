13511ab9e349f7a5fba8a5f88ab800b3e435026f
Brian Wandell <wandell@stanford.edu>
2016-11-25 18:00:49 -0800

    Made this into a real validation function (sort of).  NC will fix or make better, I think.

--- validation/scripts/cones/v_cmCurrentImpulse.m
+++ validation/scripts/cones/v_cmCurrentImpulse.m
@@ -1,3 +1,17 @@
+function varargout = v_cmCurrentImpulse(varargin)
+%
+% Cone mosaic calculation.  We will systematically change parameters and
+% see that the results are stable.
+%
+% BW, ISETBIO Team Copyright 2016
+
+varargout = UnitTest.runValidationRun(@ValidationFunction, nargout, varargin);
+
+end
+
+%% Function implementing the isetbio validation code
+function ValidationFunction(runTimeParams)
+
 %% Show the impulse response
 %
 % The impulse is a flash on a steady background.  You can set the steady
@@ -8,6 +22,7 @@
 %%
 ieInit;
 clear variables
+rng(1);  % Set noise
 
 %% Impulse on a 1 ms time axis.
 integrationTime = 5e-3;
@@ -39,7 +54,18 @@ cMosaic.emGenSequence(tSamples);
 %%  Compute the absorptions
 
 cMosaic.compute(oiImpulse);
-cMosaic.window;
+sumA = sum(cMosaic.absorptions(:));
+
+% How I calculated
+% A = 0;
+% for ii=1:10; cMosaic.compute(oi); A = A + sum(cMosaic.absorptions(:)); end
+% v = A/10;
+
+tolerance = 1e-4;
+
+v = 19000241;
+test = (sumA - v)/v;
+UnitTest.assertIsZero(test,'Total absorptions test',tolerance);
 
 %% Compute the current and get the interpolated filters
 
@@ -48,13 +74,21 @@ cMosaic.window;
 % reliability.
 cMosaic.os.noiseFlag = false;
 interpFilters = cMosaic.computeCurrent;
-cMosaic.window;
+sumF = sum(interpFilters(:));
+UnitTest.assertIsZero((sumF-1.4292)/sumF,'Linear filters test',tolerance);
+
+%
+sumC = sum(cMosaic.current(:));
+UnitTest.assertIsZero((sumC--3.8807e+07)/sumC,'Photocurrent test',tolerance);
+
+% cMosaic.window;
 
 %% If you want to see the interpolated and complete impulse response ..
 
+% Should wrap in the runtime plot condition.
 cMosaic.plot('os impulse response');
 hold on;
 plot(cMosaic.timeAxis, interpFilters,'o');
 grid on; xlabel('Time (sec)');
 
-%%
+end
