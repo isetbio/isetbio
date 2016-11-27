1e3bef5ac619b55dedd4de7cde3ecaccccb11550
Brian Wandell <wandell@stanford.edu>
2016-11-26 17:24:48 -0800

    Updated comments as per NC

--- scripts/s_matlabConv2.m
+++ scripts/s_matlabConv2.m
@@ -8,14 +8,12 @@
 % columns of A are the absorptions, and h is the convolution kernel for a
 % particular type of cone (L,M or S).
 %
-% The lesson illustrated here is that the impulse response function, h, in
-% the conv2(h,A) should be extrapolated to times that match the full set of
-% times of the abosrptions in A.
+% The impulse response function, h, in the conv2(h,A) is always returned to
+% be at least 400 ms, and it is interpolated to match the times of the
+% abosrptions in A.
 %
-% Also, we trunctate the response at the end of the time period in which
-% photon absorptions are specified.
-%
-% The reason for both of these choices are explained below.
+% Also - we trunctate responses after the stimulus (photon absorptions) are
+% specified.
 %
 % BW, ISETBIO Team, 2016
 
@@ -51,27 +49,19 @@ vcNewGraphWin; plot(r(:,1))
 
 %% A value in the 3rd and 30th slot produces two of them
 
-A = zeros(100,3);
+A = zeros(200,3);
 A(3,:) = 1;
 A(30,:) = 1;
 r = conv2(h,A);
 vcNewGraphWin; plot(r(:,1))
 
-%%  Suppose we shorten the duration of the impulse response 
+%% A value in the 3rd and 30th slot produces two of them
 
-% If the IR is shorter than the duration of A ....
-tSamples = 1:20;
-h = exp(-1*tSamples/10);
-h = h(:);
-
-A = zeros(100,3);
+A = zeros(60,3);
 A(3,:) = 1;
 A(30,:) = 1;
 r = conv2(h,A);
-
-% The output is truncated.
 vcNewGraphWin; plot(r(:,1))
-
 
 %% But if the impulse response is longer than the stimulus
 
@@ -88,15 +78,16 @@ r = conv2(h,A);
 % The output is truncated.
 vcNewGraphWin; plot(r(:,1))
 
-%% We decided to truncate the response to the length of the absorptions
+%% We truncate the response to the length of the absorptions
 
+%
 % conv2 produces currents that carry on past the end of the stimulus time
 % (absorptions).  
 %
-% BUT when a user creates a stimulus we think the user should bes
-% specifying the entire time of interest, say by appending a uniform field.
-% The values calculated by conv2() beyond the end of the stimulus assume
-% that there are 0 photon absorptions in the stimulus. We don't think this
+% BUT when a user creates a stimulus we think the user should sspecify the
+% entire time of interest, say by appending a uniform field. The values
+% calculated by conv2() beyond the end of the stimulus assume that there
+% are 0 photon absorptions in the stimulus. We don't think this
 % zero-padding is expected by most (naive) users.
 
 % We seem to be OK
@@ -109,7 +100,9 @@ A(3,:) = 1;
 A(30,:) = 1;
 r = conv2(h,A);
 
-r = r(1:100,:);
+r = r(1:30,:);
 
 % The output is truncated.
 vcNewGraphWin; plot(r(:,1))
+
+%%
