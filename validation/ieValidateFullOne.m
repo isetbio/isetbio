0680522dd7998f1e1f6f62c3909e2e6686dd3367
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-11-28 12:30:58 -0500

    Added FULLONLY validation mode.
    This is now the default mode when running ieValidateFullOne and ieValidateFull with no input args.
    To enable the old mode (which did FULL and FAST), use
    - ieValidateFullOne('doFullAndFastValidation’, true)
    - ieValidateFullAll(‘doFullAndFastValidation’, true)
    
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- validation/ieValidateFullOne.m
+++ validation/ieValidateFullOne.m
@@ -1,4 +1,21 @@
 function ieValidateFullOne(varargin)
+% Full data check (no figures, no publish) of pne validation functions
+%
+%    validateFullOne(param,val, ...)
+%
+% Possible parameters are:
+%    'verbosity' -    high, med, low ...
+%    'numeric tolerance' - val
+%    'graph mismatched data' - true/false
+%    'generate plots' - true, false
+%    'doFullAndFastValidation'  - true, false
+%
+% Examples:
+%   validateFullOne('verbosity','high');
+%   validateFullOne('Numeric Tolerance',1000*eps);
+%   validateFullOne('generate plots',true);
+%
+% NC, ISETBIO Team, Copyright 2015
 
 %% Close all figures so that we start with a clean slate
 close all;
@@ -27,10 +44,44 @@ end
 %% Whether to plot data that do not agree with the ground truth
 UnitTest.setPref('graphMismatchedData', true);
 
+fullValidationMode = 'FULLONLY';
+%% Adjust parameters based on input arguments
+if ~isempty(varargin)
+    if ~isodd(length(varargin))
+        for ii=1:2:length(varargin)
+            param = ieParamFormat(varargin{ii});
+            val   = varargin{ii+1};
+            switch(param)
+                case 'verbosity'
+                    UnitTest.setPref('verbosity',val);
+                case 'numerictolerance'
+                    UnitTest.setPref('numericTolerance', val);
+                case 'graphMismatchedData'
+                    UnitTest.setPref('graphMismatchedData', val);
+                case 'generatePlots'
+                    UnitTest.setPref('generatePlots',  val);
+                case 'dofullandfastvalidation'
+                    if val
+                        fullValidationMode = 'FULL';
+                    end
+                otherwise
+                    error('Unknown validation string %s\n',varargin{ii+1});
+            end
+        end
+     else
+        error('Odd number of arguments, must be param/val pairs');
+     end
+end
+
+
+%% Print current values of isetbioValidation prefs
+UnitTest.listPrefs();
+
 %% Print all existing validation scripts and ask the user to select one for validation
 singleScriptToValidate = UnitTest.selectScriptFromExistingOnes();
 
+fullValidationMode
 %% Validate
-UnitTest.runValidationSession({{singleScriptToValidate, []}}, 'FULL');
+UnitTest.runValidationSession({{singleScriptToValidate, []}}, fullValidationMode);
 
 end
