7a77dd69198b76708b78de61a76b60c5c9ec3d64
Brian Wandell <wandell@stanford.edu>
2016-12-03 00:28:10 -0800

    Testing set up to examine the bipolar temporal impulse response when the cone mosaic integration time changes.  Right now, it is pretty broken.

---
+++ tutorials/bipolar/t_bpTemporal.m
@@ -0,0 +1,20 @@
+%% t_bpTemporal
+%
+%
+
+rows = 5; cols = 5; tSamples = 100;
+cMosaic = coneMosaic('size',[rows,cols]);
+
+cMosaic.absorptions = single(randi(10,rows,cols,tSamples));
+cMosaic.integrationTime = 0.002;
+cMosaic.emGenSequence(tSamples);
+cMosaic.window;
+
+cMosaic.os.linearFilters(cMosaic);
+bp = bipolarCreate(cMosaic);
+bpFilter = bipolarFilter(bp, cMosaic,'graph',true);
+
+If you used a cMosaic with a real time varying signal, then you can do
+this.  See t_bpTemporal
+
+vcNewGraphWin; plot(cMosaic.timeAxis,bpFilter,'o-');
