10e95a730f1ca8c753d0ed9d27e04a3319dfc9bf
David Brainard <brainard@psych.upenn.edu>
2016-12-10 15:20:12 -0500

    Got hte last of the broken validations to run on this branch.
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- validation/ieDeleteValidationFile.m
+++ validation/ieDeleteValidationFile.m
@@ -1,44 +1,48 @@
-% Utility to remove one validation ground truth data set (both fast and full)
+function ieDeleteValidationFile
+% ieDeleteValidationFile
 %
-% Usage: ieDeleteValidationFile()
+%% Utility to remove one validation ground truth data set (both fast and full)
 
-function ieDeleteValidationFile()
+% Which project
+thisProject = 'isetbio';
+validationFileToBeDeleted =  selectValidationFile(thisProject);
 
-    validationFileToBeDeleted =  selectValidationFile();
+list = rdtListLocalArtifacts(...
+    getpref(thisProject, 'remoteDataToolboxConfig'), ...
+    'validation', 'artifactId', validationFileToBeDeleted);
+
+if isempty(list)
+    fprintf('Did not find any artifacts matching ''%s''.\n', validationFileToBeDeleted)
+else
+    fprintf('Found the following local/remote artifacts: \n');
+    for k = 1:numel(list)
+        fprintf('[%02d].  ''%s''  remotePath:%s   localPath: %s  \n', k, list(k).artifactId, list(k).remotePath, list(k).localPath);
+    end
+    fprintf('Hit enter to delete these %d artifacts. ', numel(list))
+    pause;
+    client = RdtClient(getpref(thisProject,'remoteDataToolboxConfig'));
     
-    list = rdtListLocalArtifacts(...
-        getpref('isetbio', 'remoteDataToolboxConfig'), ...
-        'validation', 'artifactId', validationFileToBeDeleted);
-
-    if isempty(list)
-        fprintf('Did not find any artifacts matching ''%s''.', validationFileToBeDeleted)
-    else
-        fprintf('Found the following local/remote artifacts: \n');
-        for k = 1:numel(list)
-            fprintf('[%02d].  ''%s''  remotePath:%s   localPath: %s  \n', k, list(k).artifactId, list(k).remotePath, list(k).localPath);
-        end
-        fprintf('Hit enter to delete these %d artifacts.', numel(list))
-        pause;
-        client = RdtClient('isetbio');
-        % Log into archiva
+    % Log into archiva
+    if isempty(client.configuration.password) ...
+            && ~isempty(client.configuration.username)
         client.credentialsDialog();
-        for k = 1:numel(list)
-            fprintf('Deleting local %s/%s\n', list(k).remotePath, list(k).artifactId);
-            deleted = rdtDeleteLocalArtifacts(client.configuration, list(k));
-            deleted = rdtDeleteArtifacts(client.configuration, list(k));
-        end
+    end
+    
+    for k = 1:numel(list)
+        fprintf('Deleting %s/%s\n', list(k).remotePath, list(k).artifactId);
+        deleted = rdtDeleteLocalArtifacts(client.configuration, list(k));
+        deleted = rdtDeleteArtifacts(client.configuration, list(k));
     end
 end
+end
 
-function validationFileToBeDeleted = selectValidationFile()
+function validationFileToBeDeleted = selectValidationFile(thisProject)
 
-    % We will use preferences for the 'isetbioValidation' project
-    thisProject = 'isetbio';
-    UnitTest.usePreferencesForProject(thisProject, 'reset');
-    
-    validationFileToBeDeleted = UnitTest.selectScriptFromExistingOnes();
-    idx = strfind(validationFileToBeDeleted,'/');
-    validationFileToBeDeleted = validationFileToBeDeleted(idx(end)+1:end);
-    validationFileToBeDeleted = strrep(validationFileToBeDeleted, 'v_', '');
-    validationFileToBeDeleted = strrep(validationFileToBeDeleted, '.m', '');
+UnitTest.usePreferencesForProject(thisProject);
+
+validationFileToBeDeleted = UnitTest.selectScriptFromExistingOnes('prompt','Enter script no. to delete data for that script');
+idx = strfind(validationFileToBeDeleted,'/');
+validationFileToBeDeleted = validationFileToBeDeleted(idx(end)+1:end);
+validationFileToBeDeleted = strrep(validationFileToBeDeleted, 'v_', '');
+validationFileToBeDeleted = strrep(validationFileToBeDeleted, '.m', '');
 end
