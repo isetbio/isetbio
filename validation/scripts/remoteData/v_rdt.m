8cfbcde750c28028571a85b3ba110bccf021050c
Brian Wandell <wandell@stanford.edu>
2016-12-09 16:44:58 -0800

    Formatted properly for validation

--- validation/scripts/remoteData/v_rdt.m
+++ validation/scripts/remoteData/v_rdt.m
@@ -1,10 +1,22 @@
+function varargout = v_rdt(varargin)
+%
+%  Validate ISETBIO-based colorimetric computations by comparing to PTB-based colorimetric computations.
+%
+    varargout = UnitTest.runValidationRun(@ValidationFunction, nargout, varargin);
+end
+
+function ValidationFunction(runTimeParams)
+
 %% Test remote data toolbox access
 
-rd = RdtClient('isetbio');
-rd.crp('/validation/full/color');
-a = rd.listArtifacts('print',true);
-assert(length(a) == 2)
+rd = RdtClient('isetbio');          % Open up the channel
+rd.crp('/validation/full/color');   % Change to the relevant directory
 
-%%
+a = rd.listArtifacts('print',true); % Maybe not needed.
+
+assert(~isempty(a));                % There are a couple of things in there
+
+end
+
     
 
