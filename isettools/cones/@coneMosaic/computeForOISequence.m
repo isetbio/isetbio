25662a06d58e14c39c7e3dd5ad161e75bd73df66
Nicolas Cottaris <cottaris@sas.upenn.edu>
2017-01-24 15:57:59 -0500

    temporary debugging
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- isettools/cones/@coneMosaic/computeForOISequence.m
+++ isettools/cones/@coneMosaic/computeForOISequence.m
@@ -1,4 +1,4 @@
-function [absorptions, photocurrents, LMSfilters, meanCur] = computeForOISequence(obj, oiSequence, varargin)
+function [absorptions, photocurrents, LMSfilters, meanCur, integrationTimeSequences] = computeForOISequence(obj, oiSequence, varargin)
 %COMPUTEFOROISEQUENCE  Compute cone absorptions and optionally photocurrents for a @oiSequence
 %
 %   [absorptions, photocurrents, LMSfilters] = COMPUTEFOROISEQUENCE(obj, oiSequence, varargin)
@@ -170,6 +170,8 @@ else
     blockedTrialIndices{1} = 1:nTrials;
 end
 
+integrationTimeSequences = [];
+
 if (rounded.oiRefreshInterval >= rounded.defaultIntegrationTime)
     % There are two main time sampling scenarios.  This one is when the oi
     % update rate is SLOWER than the cone integration time which is also
@@ -195,6 +197,7 @@ if (rounded.oiRefreshInterval >= rounded.defaultIntegrationTime)
         subplot('Position', [0.01 0.01 0.98 0.98]);
         hold on
     end
+    
     
     % Loop over the optical images
     for oiIndex = 1:oiSequence.length
@@ -248,7 +251,7 @@ if (rounded.oiRefreshInterval >= rounded.defaultIntegrationTime)
             elseif (integrationTimeForSecondPartialAbsorption < eps(rounded.eyeMovementTimeAxis(idx)+rounded.defaultIntegrationTime))
                 integrationTimeForSecondPartialAbsorption = 0;
             end
-
+            
             % Partial absorptions (p1 in graph above) with previous oi
             % (across all instances)
             if (oiIndex > 1) && (integrationTimeForFirstPartialAbsorption > 0)
@@ -330,7 +333,13 @@ if (rounded.oiRefreshInterval >= rounded.defaultIntegrationTime)
                         drawnow;
                     end
                 end
+                integrationTimeSequences(size(integrationTimeSequences,1)+1,:) = ...
+                    [integrationTimeForFirstPartialAbsorption integrationTimeForSecondPartialAbsorption rounded.defaultIntegrationTime];
+            else
+                integrationTimeSequences(size(integrationTimeSequences,1)+1,:) = ...
+                    [integrationTimeForFirstPartialAbsorption integrationTimeForSecondPartialAbsorption 0];
             end
+            integrationTimeSequences
         end % iTrialBlock
         
         % If we are in the last OIframe, and there are still some EmInsertionIndices that are
