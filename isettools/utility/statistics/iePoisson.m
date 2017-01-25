5d188cd7a81992c13c9ae7d1fab7a6ec801320a0
Nicolas Cottaris <cottaris@sas.upenn.edu>
2017-01-25 10:28:12 -0500

    iePoisson: now only using poissrnd from the Statistics Toolbox
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- isettools/utility/statistics/iePoisson.m
+++ isettools/utility/statistics/iePoisson.m
@@ -45,6 +45,8 @@ function [val, seed] = iePoisson(lambda, varargin)
 % 1/24/17 npc Now checking first for Statistics Toolbox, and using 
 %             poissrnd if it exists. If not it uses the local equivalent,
 %             iePoissrnd
+% 1/25/17 npc Now only using poissrnd from the Statistics Toolbox.
+
 
 %% Parse parameters
 p = inputParser;
@@ -57,82 +59,23 @@ p.addParameter('nSamp',1,@isnumeric);
 vFunc = @(x)(ismember(x,{'random','frozen'}));
 p.addParameter('noiseFlag','random',vFunc);
 p.addParameter('seed',1,@isnumeric);
-
 p.parse(lambda,varargin{:});
 
 nSamp     = p.Results.nSamp;
 noiseFlag = p.Results.noiseFlag;
 seed      = p.Results.seed;
 
-if checkToolbox('Statistics and Machine Learning Toolbox')
-    switch noiseFlag
-        case 'frozen'
-            rng(seed);
-        case 'random'
-            seed = rng;
-    end
-
-    % Matlab toolbox version is present. Use it.
-    if isscalar(lambda)
-        val = poissrnd(lambda, nSamp);
-    else
-        val = poissrnd(lambda);
-    end
-       
-    return;
-end
-
-%% Check if we have MEX function
-if (exist('iePoissrnd','file')==3)
-    switch noiseFlag
-        case 'frozen'
-            val = iePoissrnd(lambda, nSamp, seed);
-        case 'random'
-            seed = rand * 12345701;
-            val = iePoissrnd(lambda, nSamp, rand * 12345701);
-    end
-    return;
-end
-
-
-
-%% No toolbox, no mex file, making do with home grown Knuth code
 switch noiseFlag
     case 'frozen'
         rng(seed);
     case 'random'
         seed = rng;
 end
-    
-% Use the local method. This is slow for large lambda, but a desperate
-% move.
-warning('Using slow Poission random variable generation');
-if isscalar(lambda)
-    % Scalar version of the routine
-    % Probably we want multiple samples for a single lambda
-    val = zeros(1, nSamp);
-    for nn = 1 : nSamp
-        kk = 0;
-        p = 0;
-        while p < lambda
-            kk = kk + 1;
-            p = p - log(rand);
-        end
-        val(nn) = kk - 1;
-    end
-    % figure(1); hist(val,50)
-else
-    % A matrix or vector of lambdas and we return samples for each
-    val = zeros(size(lambda));
-    for ii = 1 : numel(lambda)
-        kk = 0;
-        p = 0;
-        while p < lambda(ii)
-            kk = kk + 1;
-            p = p - log(rand);
-        end
-        val(ii) = kk - 1;
-    end
-end
 
+if isscalar(lambda)
+    val = poissrnd(lambda, nSamp);
+else
+    val = poissrnd(lambda);
+end
+       
 end
