2f24868a2e928cdabdba4b6154a44a08c986c42c
David Brainard <brainard@psych.upenn.edu>
2017-02-05 15:51:44 -0500

    This is almost working.
    I think the only problem is that the spacing is not created properly in opticsGet, case ‘psfsupport’.  There is a comment by that code that it should be fixed, and indeed the code in oiPlot looks like what I think it should be.  I will deal with this soon.
    
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- tutorials/optics/t_opticsGetAndSetPsf.m
+++ tutorials/optics/t_opticsGetAndSetPsf.m
@@ -1,73 +1,132 @@
 % t_opticsGetAndSetPsf
 %
-% Show how to get and set psf's in units of minutes of arc.
+% Show how to get and set psf's in units of minutes of arc, or from otfs
+% specified over frequencies in cycles/deg. This ability is useful when we
+% want to build isetbio optics using various estimates of optical quality
+% in the literature.
 
 % 2/5/17  dhb  Wrote starting with an extant tutorial.
 
 %% Initialize
-ieInit;
+clear; ieInit;
 
 %% Create the oi structure and pull out the optics
-oi = oiCreate;
-optics = oiGet(oi,'optics');
-
-%% Select spatial samples and wavelength for plotting
-nSamp = 25; thisWave = 500; units = 'um';
-
-%% Get the spatial frequency support of the otf
-clear fSupport
-val = opticsGet(optics,'dlFSupport',thisWave,units,nSamp);
-[sfSupportCyclesMicron(:,:,1),sfSupportCyclesMicron(:,:,2)] = meshgrid(val{1},val{2});
-
-% Over sample to make a smooth image. This move increases the spatial
-% frequency resolution (highest spatial frequency) by a factor of 4.
-sfSupportCyclesMicron = sfSupportCyclesMicron*4;  
-
-% Frequency units are cycles/micron. The spatial frequency support runs
-% from -Nyquist:Nyquist. With this support, the Nyquist frequency is
-% actually the highest (peak) frequency value. There are two samples per
-% Nyquist, so the sample spacing is 1/(2*peakF)
-peakF = max(sfSupportCyclesMicron(:));
-deltaSpace = 1/(2*peakF);
-
-%% Calculate the OTF using diffraction limited MTF (dlMTF)
-otf = dlMTF(oi,sfSupportCyclesMicron,thisWave,units);
-
-% Derive the psf from the OTF
-psf = fftshift(ifft2(otf));
-
-% Make the spatial support for the PSF
-clear sSupport
-samp = (-nSamp:(nSamp-1));
-[X,Y] = meshgrid(samp,samp);
-sSupport(:,:,1) = X*deltaSpace;
-sSupport(:,:,2) = Y*deltaSpace;
-
-% Calculate the Airy disk
-fNumber = opticsGet(optics,'fNumber');
-
-% This is the Airy disk radius, by formula
-radius = (2.44*fNumber*thisWave*10^-9)/2 * ieUnitScaleFactor(units);
-
-% Draw a circle at the first zero crossing (Airy disk)
-nCircleSamples = 200;
-[~, ptsXY]=ieShape('circle','nSamp',nCircleSamples,'radius',radius);
-adX = ptsXY(:,1); adY = ptsXY(:,2); adZ = zeros(size(ptsXY(:,1)));
-%% Plot the diffraction limited PSF.  
-x = sSupport(:,:,1); y = sSupport(:,:,2);
-mesh(x,y,psf);
-colormap(jet)
-
-% Label the graph and draw the Airy disk
-hold on; plot3(adX,adY,adZ,'k.'); hold off;
-xlabel('Position (um)'); ylabel('Position (um)');
-zlabel('Irradiance (relative)');
-title(sprintf('Point spread (%.0f nm)',thisWave));
-
-% Return the plotted values to the user.  They can retrieve these using
 %
-% uData = get(gcf,'userData');   %  Note that case sensitivity is ignored
-udata.x = x; udata.y = y; udata.psf = psf;
-set(gcf,'userdata',udata);
+% Snag the wavelengths while we're at it.
+oi = oiCreate('wvf human');
+optics = oiGet(oi,'optics');
+wls = opticsGet(optics,'wave');
 
-%% End
+%% Make the support an even number of samples, if it is odd
+%
+% The PTB code for optics requires even number of samples, because at one
+% point we were more confident of how space and frequency got packed for
+% even samples than for odd.  That could be relaxed in the PTB code, but
+% the approach we take here is to work in isetbio with support that has an
+% even number of samples.
+%
+% Check is only on x spatial frequencies.  This is OK as long as support is square, 
+% which is almost surely true.  We do check that, though.
+sfValuesCyclesMm = opticsGet(optics,'otf support','mm');
+if (length(sfValuesCyclesMm{1}) ~= length(sfValuesCyclesMm{2}))
+    error('Our code assumes that sf support for otf is square, but it isn''t here.')
+end
+if (rem(length(sfValuesCyclesMm{1}),2) ~= 0)
+    % Lop off highest postive frequency in both x and y and put back.
+    sfValuesCyclesMm{1} = sfValuesCyclesMm{1}(1:end-1);
+    optics = opticsSet(optics,'otffx',sfValuesCyclesMm{1});
+    sfValuesCyclesMm{2} = sfValuesCyclesMm{2}(1:end-1);
+    optics = opticsSet(optics,'otffy',sfValuesCyclesMm{2});
+    
+    % Lop off highest postive frequency in the otf. Isetbio stores the otf
+    % in Matlab's first entry is zero freuqency format, but the lopping is
+    % easier to think about in the zero frequency at the center format.
+    % Use fftshift and ifftshift to go back and forth, and do the lopping
+    % in between.  We need to do all the wavelengths, and put the whole
+    % resized cube back in, in one fell swoop.
+    otfOddSupport = opticsGet(optics,'otf data');
+    for ii = 1:length(wls)
+        otfCentered = fftshift(otfOddSupport(:,:,ii));
+        otfCentered = otfCentered(1:end-1,1:end-1);
+        otfEvenSupport(:,:,ii) = ifftshift(otfCentered);
+    end
+    optics = opticsSet(optics,'otf data',otfEvenSupport);
+    clear otfOddSupport otfEvenSupport otfCentered
+end
+
+%% Get the gridded spatial frequency support of the otf in cycles/deg.
+%
+% We'll also keep it around in cycles/mm.
+%
+% And convert to support in cycles per degree using 300 um per degree,
+% which is the number that appears to be baked into the optics object.
+uMPerMm = 1000;
+uMPerDegree = 300;
+[xSfGridCyclesMm,ySfGridCyclesMm] = meshgrid(sfValuesCyclesMm{1},sfValuesCyclesMm{2});
+xSfGridCyclesDegree = uMPerDegree*xSfGridCyclesMm/uMPerMm;
+ySfGridCyclesDegree = uMPerDegree*ySfGridCyclesMm/uMPerMm;
+
+%% Get isetbio format OTF at a specified wavelength
+theWl = 550;
+otf = opticsGet(optics,'otf data',theWl);
+
+%% Derive the psf from the otf
+%
+% We have to convert to the zero sf at center representation to use
+% OtfToPsf, using ifftshift.
+[xGridMinutes,yGridMinutes,psf] = OtfToPsf(xSfGridCyclesDegree,ySfGridCyclesDegree,ifftshift(otf));
+centerPosition = length(sfValuesCyclesMm{1})/2+1;
+position1DMinutes = xGridMinutes(centerPosition,:);
+wvfHuman1DPsf = psf(centerPosition,:);
+
+%% Show otf and psf in 2D
+figure; clf;
+subplot(1,2,1); hold on;
+mesh(xSfGridCyclesDegree,ySfGridCyclesDegree,fftshift(abs(otf)));
+xlim([-100 100]); ylim([-100 100]);
+axis('square');
+xlabel('X SF (cycles/deg)');
+ylabel('Y SF (cycles/deg)');
+title('OTF');
+subplot(1,2,2); hold on
+mesh(xGridMinutes,yGridMinutes,psf);
+xlim([-10 10]); ylim([-10 10]);
+axis('square');
+xlabel('x (minutes)');
+ylabel('y (minutes)');
+title('PSF');
+
+%% Get Davila-Geisler PSF and plot against what isetbio wvf looks like
+DavilaGeislerLsf = DavilaGeislerLSFMinutes(position1DMinutes);
+DavilaGeislerPsf = LsfToPsf(DavilaGeislerLsf);
+psfFig = figure; clf; hold on
+plot(position1DMinutes,wvfHuman1DPsf/max(wvfHuman1DPsf),'r','LineWidth',3);
+plot(position1DMinutes,DavilaGeislerPsf(centerPosition,:)/max(DavilaGeislerPsf(centerPosition,:)),'g-','LineWidth',2);
+xlim([-4 4]);
+xlabel('Position (minutes');
+ylabel('Normalized PSF Slice');
+title('PSF')
+legend({sprintf('Wvf Human @%d nm',theWl),'Davila-Geisler'});
+
+%% Stick DavilaGeisler into the optics structure
+%
+% The ifftshift puts things into the isetbio format.
+[~,~,DavilaGeislerOtfCentered] = PsfToOtf(xGridMinutes,yGridMinutes,DavilaGeislerPsf);
+DavilaGeislerOtfIsetbio = ifftshift(DavilaGeislerOtfCentered);
+insertOtf = zeros(size(opticsGet(optics,'otf data')));
+for ii = 1:length(wls)
+    insertOtf(:,:,ii) = DavilaGeislerOtfIsetbio;
+end
+optics = opticsSet(optics,'otf data',insertOtf);
+
+%% Make sure everything is hunky-dory by making the plot using isetbio's fcn\
+oi = oiSet(oi,'optics',optics);
+udata = oiPlot(oi,'psf',[],theWl);
+
+figure(psfFig);
+plot(60*udata.x(centerPosition,:)/300,udata.psf(centerPosition,:)/max(udata.psf(centerPosition,:)),'k:','LineWidth',2);
+
+
+
+
+
