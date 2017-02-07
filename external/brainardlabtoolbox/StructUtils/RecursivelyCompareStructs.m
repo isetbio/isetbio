e5ea6e290c1f3a176d701d29218804cd785b7016
Nicolas Cottaris <cottaris@sas.upenn.edu>
2017-02-07 14:57:14 -0500

    More comments, added ability to plot differentials
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- external/brainardlabtoolbox/StructUtils/RecursivelyCompareStructs.m
+++ external/brainardlabtoolbox/StructUtils/RecursivelyCompareStructs.m
@@ -1,15 +1,18 @@
 function result = RecursivelyCompareStructs(struct1Name, struct1, struct2Name, struct2, varargin)
 % result = RecursivelyCompareStructsTests(struct1Name, struct1, struct2Name, struct2, defaultTolerance, customTolerances)
 %
-% Method to compare nested structs with arbitrary internal organization
-% using a defaultTolerance as well as an optional customTolerance for any desired field.
+% Method to compare nested structs with arbitrary internal organization.
+% Key/value pairs
+% 'defaultTolerance', a numeric value applied to all differentials
+% 'customTolerance', a containers.Map() with tolerance values for specific fields (see RecursivelyCompareStructsTests).
+% 'graphMismatchedData', boolean, specifying wether to plot any mismatched (numerical only) arrays.
 %
 % Simple usage:
 % s1 = struct('a', 1);
 % s2 = struct('a', 2);
 % result = RecursivelyCompareStructs('s1', s1, 's2', s2)
-
-% For more extreme usage examples see RecursivelyCompareStructsTests.
+%
+% For more elabored usage examples see RecursivelyCompareStructsTests.
 %
 % 2/7/17  NPC  Wrote it.
 
@@ -17,15 +20,17 @@ function result = RecursivelyCompareStructs(struct1Name, struct1, struct2Name, s
 p = inputParser;
 p.addParameter('defaultTolerance', 0, @isnumeric);
 p.addParameter('customTolerances', containers.Map());
+p.addParameter('graphMismatchedData', true);
 p.parse(varargin{:});
 defaultTolerance = p.Results.defaultTolerance;
 customTolerances = p.Results.customTolerances;
-
+graphMismatchedData = p.Results.graphMismatchedData;
+ 
 % Go
 [result, resultOperations, ~, ~, ~] = doRecursion(...
     struct1Name, struct1, struct2Name, struct2, ...
     defaultTolerance, customTolerances, {}, {}, {}, ...
-    [], [], struct2Name);
+    [], [], struct2Name, graphMismatchedData);
 
 % Print the results
 if (isempty(result))
@@ -58,7 +63,7 @@ end
 function [result, resultOperations, toleranceFieldPaths, flatStruct1FieldNames, flatStruct2FieldNames] = doRecursion(...
     struct1Name, struct1, struct2Name, struct2, ...
     tolerance, customTolerances, toleranceFieldPaths, flatStruct1FieldNames, flatStruct2FieldNames, ...
-     oldResult, oldResultOperations, topLevelStruct2Name)
+    oldResult, oldResultOperations, topLevelStruct2Name, graphMismatchedData)
 
     result = oldResult;
     resultOperations = oldResultOperations;
@@ -123,7 +128,7 @@ function [result, resultOperations, toleranceFieldPaths, flatStruct1FieldNames, 
                 [result, resultOperations, toleranceFieldPaths, flatStruct1FieldNames, flatStruct2FieldNames] = doRecursion(...
                     fullFieldName1, field1, fullFieldName2, field2, ...
                     tolerance, customTolerances, toleranceFieldPaths, flatStruct1FieldNames, flatStruct2FieldNames, ...
-                    result, oldResultOperations, topLevelStruct2Name);
+                    result, oldResultOperations, topLevelStruct2Name, graphMismatchedData);
 
             elseif ((isstruct(field1)) && (~isstruct(field2))) || ((isstruct(field2)) && (~isstruct(field1)))
                 error('One is struct the other one is not.');
@@ -146,7 +151,7 @@ function [result, resultOperations, toleranceFieldPaths, flatStruct1FieldNames, 
                         [result, resultOperations, toleranceFieldPaths, flatStruct1FieldNames, flatStruct2FieldNames] = doRecursion(...
                             sprintf('%s{%d}',fullFieldName1, cellIndex), cell1, sprintf('%s{%d}',fullFieldName2, cellIndex), cell2, ...
                             tolerance, customTolerances, toleranceFieldPaths, flatStruct1FieldNames, flatStruct2FieldNames, ...
-                            result, oldResultOperations, topLevelStruct2Name);
+                            result, oldResultOperations, topLevelStruct2Name, graphMismatchedData);
 
                     elseif (isnumeric(cell1)) && (isnumeric(cell2))
                         try
@@ -164,6 +169,9 @@ function [result, resultOperations, toleranceFieldPaths, flatStruct1FieldNames, 
                         maxDiff = max(abs(cell1(:)-cell2(:)));
                         if (maxDiff  > toleranceEmployed)
                             result{numel(result)+1} = sprintf('Max difference between ''%s'' and ''%s'' <strong>(%g)</strong> is greater than the set tolerance <strong>(%g)</strong>.', flatStruct1FieldNames{numel(flatStruct1FieldNames)}, flatStruct2FieldNames{numel(flatStruct2FieldNames)}, maxDiff, toleranceEmployed);
+                            if (graphMismatchedData)
+                                figureName = plotDataAndTheirDifference(cell1, cell2, flatStruct1FieldNames{numel(flatStruct1FieldNames)}, flatStruct2FieldNames{numel(flatStruct2FieldNames)});
+                            end
                         end
 
                     elseif (islogical(cell1)) && (islogical(cell2))
@@ -206,6 +214,9 @@ function [result, resultOperations, toleranceFieldPaths, flatStruct1FieldNames, 
                 maxDiff = max(abs(field1(:)-field2(:)));
                 if (maxDiff  > toleranceEmployed)
                     result{numel(result)+1} = sprintf('Max difference between ''%s'' and ''%s'' <strong>(%g)</strong> is greater than the set tolerance <strong>(%g)</strong>.', flatStruct1FieldNames{numel(flatStruct1FieldNames)}, flatStruct2FieldNames{numel(flatStruct2FieldNames)}, maxDiff, toleranceEmployed);
+                    if (graphMismatchedData)
+                       figureName = plotDataAndTheirDifference(field1, field2, flatStruct1FieldNames{numel(flatStruct1FieldNames)}, flatStruct2FieldNames{numel(flatStruct2FieldNames)});
+                    end
                 end
 
             % Fields are logical arrays
@@ -276,3 +287,263 @@ function assertStructsHaveMatchingLength(struct1Name, struct1, struct2Name, stru
     end
 end
 
+% Plotting subroutines
+
+% Method to plot mistmatched data
+function figureName = plotDataAndTheirDifference(field1, field2, field1Name, field2Name)
+  
+    h = figure();
+    figureName = sprintf('''%s'' vs. ''%s''', field1Name, field2Name);
+    set(h, 'Name', figureName);
+    clf;
+    
+    if (numel(find(size(field1)>1)) == 1)
+        % essentially a 1D vector
+        field1 = field1(:);
+        field2 = field2(:);
+        
+        diff = field1 - field2;
+        minAll = min([min(field1(:)) min(field2(:))]);
+        maxAll = max([max(field1(:)) max(field2(:))]);
+        maxDiff = max(abs(diff(:)));
+        
+        set(h, 'Position', [100 100 1400 380]);
+        subplot(1,3,1);
+        plot(1:numel(field1), field1, 'r-');
+        set(gca, 'YLim', [minAll maxAll]);
+        title(sprintf('''%s''', field1Name));
+
+        subplot(1,3,2);
+        plot(1:numel(field2), field2, 'b-');
+        set(gca, 'YLim', [minAll maxAll]);
+        title(sprintf('''%s''', field2Name));
+
+        subplot(1,3,3);
+        plot(1:numel(field1), field1-field2, 'k-');
+        set(gca, 'YLim', maxDiff * [-1 1]);
+        title(sprintf('''%s'' - \n''%s''', field1Name, field2Name));
+        
+    elseif (ndims(field1) == 2) && (all(size(field1) > 1))
+        diff = field1 - field2;
+        minAll = min([min(field1(:)) min(field2(:))]);
+        maxAll = max([max(field1(:)) max(field2(:))]);
+        maxDiff = max(abs(diff(:)));
+        
+        set(h, 'Position', [100 100 1400 380]);
+        subplot(1,3,1);
+        imagesc(field1);
+        colorbar
+        set(gca, 'CLim', [minAll maxAll]);
+        title(sprintf('''%s''', field1Name));
+
+        subplot(1,3,2);
+        imagesc(field2);
+        set(gca, 'CLim', [minAll maxAll]);
+        colorbar
+        title(sprintf('''%s''', field2Name));
+
+        subplot(1,3,3);
+        imagesc(diff);
+        set(gca, 'CLim', [-maxDiff maxDiff]);
+        colorbar
+        title(sprintf('''%s'' - \n''%s''', field1Name, field2Name));
+        colormap(gray(256));
+        
+    elseif (ndims(field1) == 1) || ((ndims(field1)==2) && (any(size(field1)==1)))  
+        diff = field1 - field2;
+        minAll = min([min(field1(:)) min(field2(:))]);
+        maxAll = max([max(field1(:)) max(field2(:))]);
+        maxDiff = max(abs(diff(:)));
+        delta = (maxAll-minAll)/10;
+        
+        set(h, 'Position', [100 100 1400 380]);
+        subplot(1,3,1);
+        plot(field1, 'bs-', 'MarkerFaceColor', [0.8 0.8 1]);
+        set(gca, 'YLim', [minAll-delta maxAll+delta]);
+        title(sprintf('''%s''', field1Name));
+        
+        subplot(1,3,2);
+        plot(field2, 'bs-',  'MarkerFaceColor', [0.8 0.8 1]);
+        set(gca, 'YLim', [minAll-delta maxAll+delta]);
+        title(sprintf('''%s''', field2Name));
+        
+        subplot(1,3,3);
+        plot(field1 - field2, 'bs-',  'MarkerFaceColor', [0.8 0.8 1]);
+        set(gca, 'YLim', double(maxDiff)*[-1.1 1.1]);
+        title(sprintf('''%s'' - \n''%s''', field1Name, field2Name));
+
+    elseif (ndims(field1) == 3)
+        diff = field1 - field2;
+        minAll = min([min(field1(:)) min(field2(:))]);
+        maxAll = max([max(field1(:)) max(field2(:))]);
+        maxDiff = max(abs(diff(:)));
+        
+        [d1, d2, d3] = size(field1);
+        if (d3 > 50)
+            figSize = [2350 810];
+            showTicks = false;
+            wMargin = 0.005;
+        else
+            figSize = [1800 800];
+            showTicks = true;
+            wMargin = 0.01;
+        end
+        set(h, 'Position', [100 100  figSize(1)  figSize(2)]);
+        
+        halfD = round(d3/2);
+        posVectors = getSubPlotPosVectors(...
+            'rowsNum', 6, 'colsNum', halfD, ...
+            'widthMargin', wMargin, 'heightMargin', 0.01, ...
+            'leftMargin', 0.02', ...
+            'bottomMargin', 0.01, 'topMargin', 0.01);
+
+        for k = 1:halfD
+           row = 1;
+           col = k;
+           subplot('Position', posVectors(row,col).v);
+           
+            if (~isreal(field1))
+               warndlg('Displaying the ABS(field2)', 'Complex Variable !!');
+               field2 = abs(field1);
+           end
+           imagesc(squeeze(field1(:,:,k)));
+           if (k == 1)
+               ylabel(field1Name, 'Color', [1 0 0]);
+           end
+           axis 'image'
+           if (~showTicks)
+              set(gca, 'XTick', [], 'YTick', []);
+           end
+           set(gca, 'CLim', [minAll maxAll]);
+           title(sprintf('i=%d', k), 'Color', [1 0 0]);
+           %colorbar('horiz');
+           
+           row = 2;
+           col = k;
+           subplot('Position', posVectors(row,col).v);
+           
+           if (~isreal(field2))
+               warndlg('Displaying the ABS(field2)', 'Complex Variable !!');
+               field2 = abs(field2);
+           end
+           imagesc(squeeze(field2(:,:,k)));
+           if (k == 1)
+               ylabel(field2Name, 'Color', [0 0 1]);
+           end
+           axis 'image'
+           if (~showTicks)
+              set(gca, 'XTick', [], 'YTick', []);
+           end
+           set(gca, 'CLim', [minAll maxAll]);
+           title(sprintf('i=%d', k), 'Color', [0 0 1]);
+           %colorbar('horiz');
+           
+           row = 3;
+           col = k;
+           subplot('Position', posVectors(row,col).v);
+           if (~isreal(diff))
+               warndlg('Displaying the ABS(diff)', 'Complex Variable !!');
+               diff = abs(diff);
+           end
+           imagesc(squeeze(diff(:,:,k)));
+           if (k == 1)
+               ylabel(sprintf('groundTruthData\n - validationData'));
+           end
+           axis 'image'
+           if (~showTicks)
+              set(gca, 'XTick', [], 'YTick', []);
+           end
+
+           set(gca, 'CLim', [-maxDiff maxDiff]);
+           title(sprintf('i=%d', k));
+           %colorbar('horiz');
+        end
+        
+        for k = halfD+1:d3
+           row = 4;
+           col = k-halfD;
+           subplot('Position', posVectors(row,col).v);
+           imagesc(squeeze(field1(:,:,k)));
+           if (k == halfD+1)
+               ylabel(field1Name, 'Color', [1 0 0]);
+           end
+           axis 'image'
+           if (~showTicks)
+              set(gca, 'XTick', [], 'YTick', []);
+           end
+           set(gca, 'CLim', [minAll maxAll]);
+           title(sprintf('i=%d', k), 'Color', [1 0 0]);
+           %colorbar('horiz');
+           
+           row = 5;
+           col = k-halfD;
+           subplot('Position', posVectors(row,col).v);
+           imagesc(squeeze(field2(:,:,k)));
+           if (k == halfD+1)
+               ylabel(field2Name, 'Color', [0 0 1]);
+           end
+           axis 'image'
+           if (~showTicks)
+              set(gca, 'XTick', [], 'YTick', []);
+           end
+           set(gca, 'CLim', [minAll maxAll]);
+           title(sprintf('i=%d', k), 'Color', [0 0 1]);
+           %colorbar('horiz');
+           
+           row = 6;
+           col = k-halfD;
+           subplot('Position', posVectors(row,col).v);
+           imagesc(squeeze(diff(:,:,k)));
+           if (k == halfD+1)
+               ylabel(sprintf('groundTruthData\n - validationData'));
+           end
+           axis 'image'
+           if (~showTicks)
+              set(gca, 'XTick', [], 'YTick', []);
+           end
+           set(gca, 'CLim', [-maxDiff maxDiff]);
+           title(sprintf('i=%d', k));
+        end
+        colormap(gray(512));
+    end
+end
+
+function posVectors = getSubPlotPosVectors(varargin)
+
+    self.rowsNum        = 2;
+    self.colsNum        = 2;
+    self.widthMargin    = 0.01;
+    self.heightMargin   = 0.01;
+    self.leftMargin     = 0.06;
+    self.bottomMargin   = 0.08;
+    self.topMargin      = 0.08;
+    
+    % parse inputs
+    parser = inputParser;
+    parser.addParamValue('rowsNum',         self.rowsNum);
+    parser.addParamValue('colsNum',         self.colsNum);
+    parser.addParamValue('widthMargin',     self.widthMargin);
+    parser.addParamValue('heightMargin',    self.heightMargin);
+    parser.addParamValue('leftMargin',      self.leftMargin);
+    parser.addParamValue('bottomMargin',    self.bottomMargin); 
+    parser.addParamValue('topMargin',       self.topMargin);
+
+    
+    % Execute the parser to make sure input is good
+    parser.parse(varargin{:});
+    pNames = fieldnames(parser.Results);
+    for k = 1:length(pNames)
+       self.(pNames{k}) = parser.Results.(pNames{k}); 
+    end
+    
+    plotWidth  = ((1.0-self.leftMargin) - self.widthMargin*(self.colsNum-1) - 0.01)/self.colsNum;
+    plotHeight = ((1.0-self.bottomMargin-self.topMargin) - self.heightMargin*(self.rowsNum-1) - 0.01)/self.rowsNum;
+    
+    for row = 1:self.rowsNum
+        yo = 0.99 - self.topMargin - (row)*(plotHeight+self.heightMargin) + self.heightMargin;
+        for col = 1:self.colsNum
+            xo = self.leftMargin + (col-1)*(plotWidth+self.widthMargin);
+            posVectors(row,col).v = [xo yo plotWidth plotHeight];
+        end
+    end
+end
