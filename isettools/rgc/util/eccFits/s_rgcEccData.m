27d04598d02fa7f36cc4ab0ead0b9c9abd09bfbc
Brian Wandell <wandell@stanford.edu>
2017-06-07 18:32:17 -0700

    Small edits.

--- isettools/rgc/util/eccFits/s_rgcEccData.m
+++ isettools/rgc/util/eccFits/s_rgcEccData.m
@@ -1,27 +1,27 @@
 % t_rgcEccData
-% 
-% Plot and fit data captured from plots in Coner & Kaplan (1995) and Dacey
+%
+% Plot and fit data captured from plots in Croner & Kaplan (1995) and Dacey
 % (2004) on RGC RF size as a function of eccentricity.
-% 
+%
 % This data is used to scale ISETBIO RGC RF sizes as a function of
 % eccentricity.
-% 
+%
 % Data was digitized using 'digitize2' and ginput:
 % https://www.mathworks.com/matlabcentral/fileexchange/928-digitize2-m
-% 
+%
 % Croner & Kaplan, 1995, Figure 4:
 % https://pdfs.semanticscholar.org/be5f/d167a456b4bce52e7edb7bd187616e79adc6.pdf
-% 
+%
 % Dacey, 2004, Figure 2A:
 % https://pdfs.semanticscholar.org/5f15/a3de07ccdbf2ef3763da262ede1c876f6b6a.pdf
 
-%%
+%% For plotting ...??
 movingAverageFlag = true;
 
 %% Dacey Parasol Data
 
 load([isetbioRootPath '/isettools/data/rgc/parasolData.mat'])
-vcNewGraphWin([],'wide'); 
+vcNewGraphWin([],'wide');
 subplot(1,3,1);
 scatter(parasolData(:,1),parasolData(:,2))
 
@@ -32,16 +32,16 @@ hold on; plot(.1:.1:15,(.1:.1:15).*parasolFit(2)+parasolFit(1));
 
 % Moving average bin
 if movingAverageFlag
-xRange = 1;
-for xRangeInd = 1:floor(max(parasolData(:,1))/xRange)
-    xRangeVal(xRangeInd) = xRangeInd*xRange;
-    xRangePts = find(abs(parasolData(:,1)-xRangeVal(xRangeInd))<xRange);
-    if ~isempty(xRangePts)
-    sizeAvg(xRangeInd) = mean(parasolData(xRangePts,2));
+    xRange = 1;
+    for xRangeInd = 1:floor(max(parasolData(:,1))/xRange)
+        xRangeVal(xRangeInd) = xRangeInd*xRange;
+        xRangePts = find(abs(parasolData(:,1)-xRangeVal(xRangeInd))<xRange);
+        if ~isempty(xRangePts)
+            sizeAvg(xRangeInd) = mean(parasolData(xRangePts,2));
+        end
+        clear xRangePts
     end
-    clear xRangePts
-end
-hold on;plot(xRangeVal,sizeAvg,'b')
+    hold on;plot(xRangeVal,sizeAvg,'b')
 end
 
 title('Parasol DF Size over Eccentricity, Dacey 2004');
@@ -60,21 +60,24 @@ axis([0 18 0 450]);
 % measurements
 
 % figure; scatter([5 15 25]*.3,[.1 .18 .23]*.3)
-% 
+%
 % midgetFit2 = ([ones(size([5 15 25]')) .3*[5 15 25]'])\(.3*[.1 .18 .23]')
 % x = .1:.1:25; y = midgetFit2(1) + midgetFit2(2)*x;
 % hold on; scatter(x,y)
 
-% clear 
-load([isetbioRootPath '/isettools/data/rgc/croner_kaplan_parasol_rgc.mat'])
+% clear
+ckData = load(fullfile(isetbioRootPath,'isettools','data','rgc','croner_kaplan_parasol_rgc.mat'));
+d1 = ckData.d1;
+d2 = ckData.d2;
 
-% Microns to degrees
-d1 = .2253*d1; 
+% Microns to degrees for monkey.  Should be functionalized - or maybe it
+% is.
+d1 = .2253*d1;
 
 % Radius (mm) to diameter (um)
 d1(:,2) = 2*d1(:,2)*1000;
 
-% figure; 
+% figure;
 hold on;
 scatter(d1(:,1),d1(:,2),'r')
 
@@ -117,7 +120,7 @@ legend('Data','Fit','Binned Average');
 %% Dacey Midget data
 
 load([isetbioRootPath '/isettools/data/rgc/midgetData.mat'])
-% figure; 
+% figure;
 subplot(1,3,2);
 scatter(midgetData(:,1),midgetData(:,2))
 
@@ -128,17 +131,17 @@ hold on; plot(.1:.1:15,(.1:.1:15).*midgetFit(2)+midgetFit(1));
 
 % Moving average bin
 if movingAverageFlag
-clear xRange xRangeVal sizeAvg
-xRange = 1;
-for xRangeInd = 1:floor(max(midgetData(:,1))/xRange)
-    xRangeVal(xRangeInd) = xRangeInd*xRange;
-    xRangePts = find(abs(midgetData(:,1)-xRangeVal(xRangeInd))<xRange);
-    if ~isempty(xRangePts)
-    sizeAvg(xRangeInd) = mean(midgetData(xRangePts,2));
+    clear xRange xRangeVal sizeAvg
+    xRange = 1;
+    for xRangeInd = 1:floor(max(midgetData(:,1))/xRange)
+        xRangeVal(xRangeInd) = xRangeInd*xRange;
+        xRangePts = find(abs(midgetData(:,1)-xRangeVal(xRangeInd))<xRange);
+        if ~isempty(xRangePts)
+            sizeAvg(xRangeInd) = mean(midgetData(xRangePts,2));
+        end
+        clear xRangePts
     end
-    clear xRangePts
-end
-hold on;plot(xRangeVal,sizeAvg,'b')
+    hold on;plot(xRangeVal,sizeAvg,'b')
 end
 
 % Format plot
@@ -154,7 +157,7 @@ axis([0 18 0 450]);
 %% Dacey SBC data
 
 load([isetbioRootPath '/isettools/data/rgc/sbcData.mat'])
-% figure; 
+% figure;
 subplot(1,3,3);
 scatter(sbcData(:,1),-sbcData(:,2))
 
@@ -166,17 +169,17 @@ sbcx = sbcData(:,1); sbcy = -sbcData(:,2);
 
 % Moving average bin
 if movingAverageFlag
-clear xRange xRangeVal sizeAvg
-xRange = 1;
-for xRangeInd = 1:floor(max(sbcData(:,1))/xRange)
-    xRangeVal(xRangeInd) = xRangeInd*xRange;
-    xRangePts = find(abs(sbcData(:,1)-xRangeVal(xRangeInd))<xRange);
-    if ~isempty(xRangePts)
-    sizeAvg(xRangeInd) = mean(-sbcData(xRangePts,2));
+    clear xRange xRangeVal sizeAvg
+    xRange = 1;
+    for xRangeInd = 1:floor(max(sbcData(:,1))/xRange)
+        xRangeVal(xRangeInd) = xRangeInd*xRange;
+        xRangePts = find(abs(sbcData(:,1)-xRangeVal(xRangeInd))<xRange);
+        if ~isempty(xRangePts)
+            sizeAvg(xRangeInd) = mean(-sbcData(xRangePts,2));
+        end
+        clear xRangePts
     end
-    clear xRangePts
-end
-hold on;plot(xRangeVal,sizeAvg,'b')
+    hold on;plot(xRangeVal,sizeAvg,'b')
 end
 
 % Format plot
@@ -194,23 +197,23 @@ plotFlag = false;
 % measurements
 
 if plotFlag
-deg2mm = 0.3;
-figure; scatter([2.5 7.5 15 25 35]*deg2mm,[.03 .05 .07 .09 .15]*deg2mm)
-
-% midgetFit2 = ([ones(size([2.5 7.5 15 25 35]')) [.03 .05 .07 .09 .15]'])\[2.5 7.5 15 25 35]';
-
-% Linear regression
-midgetFit2 = ([ones(size([2.5 7.5 15 25 35]')) deg2mm*[2.5 7.5 15 25 35]'])\(deg2mm*[.03 .05 .07 .09 .15]')
-% Plot regression
-x = .1:.1:35*.3; y = midgetFit2(1) + midgetFit2(2)*x;
-hold on; scatter(x,y);
+    deg2mm = 0.3;
+    figure; scatter([2.5 7.5 15 25 35]*deg2mm,[.03 .05 .07 .09 .15]*deg2mm)
+    
+    % midgetFit2 = ([ones(size([2.5 7.5 15 25 35]')) [.03 .05 .07 .09 .15]'])\[2.5 7.5 15 25 35]';
+    
+    % Linear regression
+    midgetFit2 = ([ones(size([2.5 7.5 15 25 35]')) deg2mm*[2.5 7.5 15 25 35]'])\(deg2mm*[.03 .05 .07 .09 .15]')
+    % Plot regression
+    x = .1:.1:35*.3; y = midgetFit2(1) + midgetFit2(2)*x;
+    hold on; scatter(x,y);
 end
 
 
 %% Watson RGC Formula - 2014
 
 
-%% Get lookup table for how large RF is 
+%% Get lookup table for how large RF is
 % There is an asymmetry in the size of RGC RFs over the retina
 szCols = 128; fovRows = 90; fovCols = 90; scaleFactor = 1;
 cellType = 'Midget';
@@ -221,16 +224,16 @@ cellType = 'Midget';
 % rgcDiameterLUT = scaleFactor*sqrt(2)*watsonRGCSpacing(szCols,szCols,fovRows)';
 [rgcDiameterLUT, radDeg, rgc1d] = watsonRGCSpacing(szCols,szCols,fovRows);
 
-figure; plot(radDeg,rgc1d); 
- xlabel('Eccentricity (degrees)'); ylabel('RF Size (degrees)'); grid on;
+figure; plot(radDeg,rgc1d);
+xlabel('Eccentricity (degrees)'); ylabel('RF Size (degrees)'); grid on;
 legend('Nasal','Superior','Temporal','Inferior');
- 
+
 title(sprintf('Human %s RGC RF Size (degrees)',cellType));
 % figure; degStart = -fovCols/2; degEnd = fovCols/2;
 % degarr = [degStart: (degEnd-degStart)/szCols : degEnd];
 % contourf(degarr,degarr,rgcDiameterLUT,[0:max(rgcDiameterLUT(:))/20:max(rgcDiameterLUT(:))] ); axis square
 % % surfc(degarr,degarr,rgcDiameterLUT); shading flat; %[0:max(rgcDiameterLUT(:))/20:max(rgcDiameterLUT(:))] ); % axis square
-% title(sprintf('Human %s RGC RF Size (degrees)',cellType)); colorbar; 
+% title(sprintf('Human %s RGC RF Size (degrees)',cellType)); colorbar;
 
 %%
 % figure; hold on;
@@ -240,7 +243,7 @@ title(sprintf('Human %s RGC RF Size (degrees)',cellType));
 % plot(rgcDiameterLUT(65,65:end));
 % grid on;
 
-%% Bipolar 
+%% Bipolar
 
 % Parasol and SBC
 
@@ -253,8 +256,8 @@ bipolarsPerRGC = (2 + (3/10)*(radDeg));
 plot(radDeg,rgc1d(1,:).*bipolarsPerRGC/sqrt(2)); grid on;
 axis([0 100 0 16]);
 title('Diffuse Bipolar: Parasol and SBC');
- xlabel('Eccentricity (degrees)'); ylabel('RF Size (degrees)'); grid on;
- 
+xlabel('Eccentricity (degrees)'); ylabel('RF Size (degrees)'); grid on;
+
 % Midget
 subplot(1,2,2);
 
@@ -262,5 +265,5 @@ bipolarsPerRGC = (1 + (2/10)*(radDeg));
 plot(radDeg,rgc1d(1,:).*bipolarsPerRGC/sqrt(2)); grid on;
 axis([0 100 0 16]);
 title('Midget Bipolar');
- xlabel('Eccentricity (degrees)'); ylabel('RF Size (degrees)'); grid on;
+xlabel('Eccentricity (degrees)'); ylabel('RF Size (degrees)'); grid on;
 % scatter(midgetData(:,1),midgetData(:,2))
