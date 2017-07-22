116b38987f1ad4b5d008251baeeac5c060c52590
Brian Wandell <wandell@stanford.edu>
2017-07-21 17:33:52 -0700

    Grid test for RGC.

--- isettools/rgc/tests/s_rgcGrid.m
+++ isettools/rgc/tests/s_rgcGrid.m
@@ -1,51 +1,51 @@
-%% s_bpGrid
+%% RGC grid test
 %
-% Create a scene of grid lines and check that they render on the bipolar
-% cell mosaic as a grid.
+% Check that the grid appears as a grid on the RGC response.
 %
 % BW, ISETBIO Team, 2017
 
 %%
 ieInit
 
-%% Scene and oi grid
+%% Scene, oi, grid, cone mosaic
 
-imSize = 128;
-lineSpacing = 48;
-fov = 1; % deg
+imSize = 128; lineSpacing = 48; fov = 2; % deg
 scene = sceneCreate('grid lines',imSize,lineSpacing);
 scene = sceneSet(scene,'fov',fov);
-
 oi = oiCreate;    % Standard human optics
 oi = oiCompute(oi,scene);
-
-%%  Make the mosaic
 
 nMovements = 50;
 cMosaic = coneMosaic;
 cMosaic.setSizeToFOV(fov);
 cMosaic.emGenSequence(nMovements);
-
 cMosaic.compute(oi);
 cMosaic.computeCurrent;
-
-% cMosaic.window;
 
 %% Make the biplar layer with just one mosaic 
 
 clear bpL bpMosaicParams
-
 bpL = bipolarLayer(cMosaic);
-
-% Spread and stride are not working
 
 ii = 1;
 bpMosaicParams.spread  = 2;  % RF diameter w.r.t. input samples
 bpMosaicParams.stride  = 2;  % RF diameter w.r.t. input samples
 bpL.mosaic{ii} = bipolarMosaic(cMosaic,'on midget',bpMosaicParams);
-
-% Maybe we should have a bpL.compute
 bpL.mosaic{ii}.compute;
-bpL.window;
+
+% bpL.window;
+%% Make the RGC layer and show it
+
+clear rgcL rgcParams
+rgcL = rgcLayer(bpL);
+
+% Spread and stride are not working
+rgcParams.rfDiameter = 2;
+
+% rgcL.mosaic{ii} = rgcGLM(rgcL, bpL.mosaic{1},'on midget');
+rgcL.mosaic{ii} = rgcGLM(rgcL, bpL.mosaic{1},'on midget',rgcParams);
+rgcL.compute;
+
+rgcL.window;
 
 %%
