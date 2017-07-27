b70eacba266ff064bbd4de3572ad558d82955908
David Brainard <brainard@psych.upenn.edu>
2017-07-27 08:15:35 -0400

    Little update
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- Contents.m
+++ Contents.m
@@ -7,7 +7,7 @@
 %
 % configuration                 - Configure isetbio preferences. Mostly for developers.
 % extenral                      - Code from other projects (e.g. Psychtoolbox). Inluded here for convenience.
-% isettools                     - Main code tree for isetbio.
-% scripts                       - These will eventually become tutorials, validations or moved to their own project.
+% isettools                     - Main code tree for isetbio.  Also contains the isetbio data tree, for now.
+% scripts                       - For developers. These will eventually become tutorials, validations or moved to their own project.
 % tutorials                     - Tutorial scripts that illustate how to do things in isetbio.
 % validation                    - Unit and regression tests. May also be useful for illustration.
