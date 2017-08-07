086f55af7fa4e785bf20ad735e2c3ce6a401e3d0
David Brainard <brainard@psych.upenn.edu>
2017-08-07 11:40:00 -0400

    Tutorial of the day.
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- isettools/cones/outersegment/utilities/osAddNoise.m
+++ isettools/cones/outersegment/utilities/osAddNoise.m
@@ -1,52 +1,59 @@
-function [adaptedCur, freq] = osAddNoise(curNF, varargin)
-% Add noise to noise-free membrane current (curNF)
+function [noisyCurrent, freq] = osAddNoise(noiseFreeCurrent, varargin)
+%osAddNoise  Add noise to noise-free membrane current
 %
-%   [adaptedCur, freq] = osAddNoise(curNF, varargun)
+% Syntax:
+%    [noisyCurrent, freq] = osAddNoise(noiseFreeCurrent)
 %
-% Cone noise is additive and independent of cone signal. The noise is
-% Gaussian and the spectral power distribution can be characterized by the
-% sum of two Lorentz functions.  (Discuss this.  The SPD of a Gaussian is a
-% Gaussian, BW thought).
+% Description:
+%    Cone photocurrent noise is additive and independent of the current. The noise is
+%    Gaussian and the noise spectral power distribution is characterized by the
+%    sum of two Lorentz functions.
+%
+%    This function is called by the os object.
 % 
-% See Angueyra and Rieke (2013, Nature Neuroscience) for details.
+%    See Angueyra and Rieke (2013, Nature Neuroscience) for details.
 %
-% Inputs:
-%    curNF  - noise free cone adapted membrane photo current.  The units
-%             are pA (picoAmps) (CHECK!).
-%   sampTime - sample time interval (secs)
-%   **NYI**  seed     - noise seed for reproducibility
+%    [DHB NOTE, BW NOTE]: The SPD of a Gaussian is a Gaussian, so something about the
+%    comment above is incorrect. Maybe the noise is additive but not Gaussian?  Or
+%    maybe we don't understand something fundamental.
 %
-%  Outputs:
-%    adaptedCur - membrane current with noise added
-%    freq       - temporal frequency for checking the noise
+% Input:
+%    noiseFreeCurrent       Noise free cone adapted membrane photo current.  The units
+%                           are pA (picoAmps). [DHB NOTE: There was a comment here
+%                           that said to double check that these are in fact the units.]
+%
+%  Outputs
+%    noisyCurrent           Membrane current with noise added
+%
+%    freq                   Temporal frequency domain on which noise is generated.
+%                           Could be useful for checking the noise
+%
+% Optional key/value pairs:
+%    'sampTime'              Number.  Sample time interval in seconds (default 0.001).
+%
+%    'seed'                  Set random number generator seed to this value (default empty, don't set).
 %
 %  Example:
 %    nSamp = 10000; deltaT = 1/5000;
 %    [noise, freq] = osAddNoise(zeros(10,10,nSamp), 'sampTime',deltaT);
 %    fprintf('Should be close to zero:  Mean noise %f (pA)\n',mean(noise(:)));
-%
-%  This formula for the noise frequency response amplitude (from Fred R)
-%  The 2 is there for some negative frequency, and the correction for the
-%  number of time samples is the usual FFT correction.
-%    noiseF = squeeze(mean(mean(abs(fft(noise, [], 3)*(2/nSamp)).^2)));
-%    vcNewGraphWin; loglog(freq, noiseF(1:length(freq)));
-%    line('Xdata',[0.1 freq(end)],'Ydata',[.205 .205]);
-%    xlabel('Frequency(Hz)'); ylabel('Power Spectrum (pA^2/Hz)'); 
 %    
-%
 %  See also:
-%    coneAdapt, osAdaptSteadyState, osAdaptTemporal
-%
+%    osAdaptSteadyState, t_osNoise
+
 %  (HJ) ISETBIO, 2014
+%
+% 08/07/17  dhb  Make header comments a closer match to reality.
 
 %% Init
 p = inputParser;
-p.addRequired('curNF',@isnumeric);
+p.addRequired('noiseFreeCurrent',@isnumeric);
 p.addParameter('sampTime',0.001,@isscalar);
 p.addParameter('seed', [] ,@isscalar);
-p.parse(curNF,varargin{:});
-
+p.parse(noiseFreeCurrent,varargin{:});
 sampTime = p.Results.sampTime;
+
+%% Freeze rng seed if desired.
 seed = p.Results.seed;
 if (~isempty(seed))
     rng(seed);
@@ -54,46 +61,39 @@ end
 
 %% Build model and generate noise
 
-%  Make sure curNF is [row, col, time] 3D matrix
-if isvector(curNF), curNF = reshape(curNF, [1 1 length(curNF)]); end
+%  Make sure noiseFreeCurrent is [row, col, time] 3D matrix
+if isvector(noiseFreeCurrent), noiseFreeCurrent = reshape(noiseFreeCurrent, [1 1 length(noiseFreeCurrent)]); end
 
-if (ndims(curNF) == 3),     temporalDimIndex = 3;
-elseif (ismatrix(curNF)),   temporalDimIndex = 2;
-else error('unexpected curNF format\n');
+if (ndims(noiseFreeCurrent) == 3),     temporalDimIndex = 3;
+elseif (ismatrix(noiseFreeCurrent)),   temporalDimIndex = 2;
+else error('Unexpected noiseFreeCurrent format\n');
 end
 
 % Generate the noise according to the noise spectral distribution
-k = ceil((size(curNF, temporalDimIndex)-1)/2);
-freq = (0:k)/ sampTime / size(curNF, temporalDimIndex);
-
+k = ceil((size(noiseFreeCurrent, temporalDimIndex)-1)/2);
+freq = (0:k)/ sampTime / size(noiseFreeCurrent, temporalDimIndex);
 LorentzCoeffs = [0.16  55  4;
                  0.045 190 2.5];
 noiseSPD = lorentzSum(LorentzCoeffs, freq);
 
 % Make-up the negative frequency part
 noiseSPD = [noiseSPD noiseSPD(end:-1:1)];
-
-noiseSPD = noiseSPD(1:size(curNF, temporalDimIndex));
+noiseSPD = noiseSPD(1:size(noiseFreeCurrent, temporalDimIndex));
 if (temporalDimIndex == 3)
     noiseSPD = reshape(noiseSPD, [1 1 length(noiseSPD)]);
 elseif (temporalDimIndex == 2)
     noiseSPD = reshape(noiseSPD, [1 length(noiseSPD)]);
 end
-% Have a look at the noise in the frequency domain
-% vcNewGraphWin;loglog(squeeze(sqrt(noiseSPD)));
 
 % Generate white Gaussian noise
-noise = randn(size(curNF));
+noise = randn(size(noiseFreeCurrent));
 noiseFFT = fft(noise, [], temporalDimIndex); % / sqrt(size(noise, 3));
 
-% adjust the spectral power distribution of the noise
+% Adjust the spectral power distribution of the noise
 noiseFFT = bsxfun(@times, noiseFFT, sqrt(noiseSPD));
-
 noise = real(ifft(noiseFFT, [], temporalDimIndex)) / sqrt(2*sampTime); % take real part
 
-% vcNewGraphWin; plot(squeeze(noise));
-
-% add to noise-free signal
-adaptedCur = curNF + noise;
+% Add to noise-free signal
+noisyCurrent = noiseFreeCurrent + noise;
 
 end
--- tutorials/t_outersegment/t_osNoise.m
+++ tutorials/t_outersegment/t_osNoise.m
@@ -1,33 +1,39 @@
-%% t_osNoise
+%%t_osNoise  Illustrate the noise spectrum from the outer segment biophysical model
 %
-% Illustrate the noise spectrum from the outer segment biophysical model we
-% use for converting photon absorptions to photo current.
+% Description:
+%    
+%    Illustrate the noise spectrum from the outer segment biophysical model we
+%    use for converting photon absorptions to photo current.
 %
+%    Notes from FR on obtaining the noise frequency response amplitude.
+%    The formula used is for the noise frequency response amplitude.
+%    The 2 is there for some negative frequency, and the correction for the
+%    number of time samples is the usual FFT correction.
+%    [DHB NOTE: I don't understand this, really.  If possible, understand and
+%    expand the comment.]
+%
+% See also: osAddNoise.
+
 % BW ISETBIO Team, 2016
+%
+% 08/07/17  dhb  Commenting pass.
 
 %%  Show that the mean is near zero
-
-nSamp = 10000; deltaT = 1/5000;
+%
 % We are putting in absorption rate of zero for a 10 x 10 array of cones,
 % over the nSamp sample times. 
 % The timing is 0.2 ms (1/5000)
+nSamp = 10000; deltaT = 1/5000;
 [noise, freq] = osAddNoise(zeros(10,10,nSamp), 'sampTime',deltaT);
-
 fprintf('Mean noise should be close to zero: %f (pA)\n',mean(noise(:)));
 
 %% Calculate the spectrum
-
-% Notes from FR.
-%
-% This formula for the noise frequency response amplitude (from Fred R)
-% The 2 is there for some negative frequency, and the correction for the
-% number of time samples is the usual FFT correction.
-
 noiseF = squeeze(mean(mean(abs(fft(noise, [], 3)*(2/nSamp)).^2)));
 
+% Plot
 vcNewGraphWin; loglog(freq, noiseF(1:length(freq)));
 line('Xdata',[0.1 freq(end)],'Ydata',[.205 .205]);
 xlabel('Frequency(Hz)'); ylabel('Power Spectrum (pA^2/Hz)');
 grid on
 
-%%
+
