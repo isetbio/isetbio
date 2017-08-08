e104046f81360f381f8ad28dfabf42097b5c8f60
David Brainard <brainard@psych.upenn.edu>
2017-08-08 08:32:02 -0400

    Added tutorial to here, and Contents.m
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

---
+++ tutorials/t_cones/advancedTutorials_cones/Contents.m
@@ -0,0 +1,11 @@
+% isetbio/tutorials/t_cones/advancedTutorials_cones
+%
+% Illustrates more advanced usage of coneMosaic object.
+%
+% t_conesMosaicHex1             - 
+% t_conesMosaicHex2             - 
+% t_conesMosaicHex3             - 
+% t_conesMosaicHex4             - 
+% t_conesMosaicHex5             - 
+% t_conesMosaicHex6             - 
+% t_conesMosaicLowPassResponses - Demonstrates how to spatially low-pass a mosaic's responses.
