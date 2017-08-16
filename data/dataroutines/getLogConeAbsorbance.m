41e0bff816823abc6834a660865d15470b3e49bc
David Brainard <brainard@psych.upenn.edu>
2017-08-16 11:56:53 -0400

    Put back correct version of getLogConeAbsorbance

--- data/dataroutines/getLogConeAbsorbance.m
+++ data/dataroutines/getLogConeAbsorbance.m
@@ -20,19 +20,28 @@ function [log10absorbance,wave,params,comment] = getLogConeAbsorbance(varargin)
 %
 % Output:
 %     log10absorbance            Log10 of the cone absorbance, in the columns of a matrix.
+%
 %     wave                       Column vector of sample wavelengths, in nm.
+%
 %     params                     Structure of key/value pairs used to generate data.
+%
 %     comment                    A short comment describing the data, returned as a string.
 %
 % Optional key/value pairs:
 %     'species'                  String specifying species
 %                                  'human' (default) - Human L, M and S cones.
 %                                  'monkey' - Monkey L, M and S cones.  Currently the same as human.
+%
 %     'coneAbsorbanceSource'     Source of the data
 %                                  'StockmanSharpe' (default).
 %                                   Values are StockmanSharpe estimates.  Valid when 'species' is 'human' or
 %                                   'monkey'.  Data taken from Psychtoolbox mat file T_log10coneabsorbance_ss.
 %                                   These in turn came from CVRL (http://cvrl.org).
+%
+%                                  The value for 'coneAbsorbanceSource' may be passed as a function handle, in
+%                                  which case the passed function is called direclty with the key/value pairs passed to this
+%                                  routine. 
+%
 %     'wave'                     Column vector of evenly spaced sample wavelengths in nm (default, (390:830)').
 %
 % See also: getRawData
@@ -47,6 +56,15 @@ p.addParameter('coneAbsorbanceSource','StockmanSharpe', @ischar);
 p.addParameter('wave',(390:830)', @isnumeric);
 p.parse(varargin{:});
 params = p.Results;
+
+%% Take care of case where a function handle is specified as source
+%
+% This allows for custom data to be defined by a user, via a function that
+% could live outside of ISETBio.
+if (isa(params.coneAbsorbanceSource,'function_handle'))
+    [log10absorbance,wave,params,comment] = params.coneAbsorbanceSource(varargin{:});
+    return;
+end
 
 %% Handle choices
 switch (params.species)
