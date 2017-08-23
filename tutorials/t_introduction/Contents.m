a447cad90f0a1cb0b54db5c7e5781bc5312d2270
Nicolas Cottaris <cottaris@sas.upenn.edu>
2017-08-23 16:50:28 -0400

    Typo

--- tutorials/t_introduction/Contents.m
+++ tutorials/t_introduction/Contents.m
@@ -3,5 +3,5 @@
 % Introduces various isetbio building blocks and how these blocks work
 % together to perform some computation
 %
-% t_t_dynamicStimulusToPhotocurrent - Illustrates how  to compute photocurrent responses to a dynamic stimulus
+% t_dynamicStimulusToPhotocurrent - Illustrates how  to compute photocurrent responses to a dynamic stimulus
 %
