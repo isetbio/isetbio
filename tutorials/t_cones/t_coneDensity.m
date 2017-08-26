25ae30e6997de25a8b2934af9320d823f55469c4
Brian Wandell <wandell@stanford.edu>
2017-08-26 11:22:32 +0800

    Illustrates using the coneDensity function and shows the density across the field.

---
+++ tutorials/t_cones/t_coneDensity.m
@@ -0,0 +1,41 @@
+%% t_coneDensity
+%
+% Plot the log10 cone density across the retinal surface.
+%
+% The plot shows different angles and up to an eccentricity of 10 mm, which
+% is about +/-30 deg.
+%
+% BW, ISETBIO Team, 2017
+
+%%
+pos = 0:(1e-4):10e-3;       % Eccentricity in meters
+ang = (0:.02:1.02)*2*pi;    % Angle around all 360 deg (plus a little)
+
+coneDensity = zeros(length(pos),length(ang));
+for jj=1:length(ang)
+    for ii = 1:length(pos)
+        coneDensity(ii,jj) = coneDensityReadData('eccentricity',pos(ii),...
+            'angle',ang(jj),...
+            'whichEye','left');
+    end
+end
+
+% You could plot the individual lines.
+% vcNewGraphWin;
+% semilogx(pos*1e3*(1/3),coneDensity)
+% xlabel('Position (deg)')
+% ylabel('Density');
+% grid on
+
+%%  But I prefer this mesh plot.
+[A,T] = meshgrid(ang,pos);
+[X,Y] = pol2cart(A,T);
+
+surf(X*1e3,Y*1e3,log10(coneDensity))
+colormap(hsv)
+
+xlabel('Position (mm)')
+ylabel('Position (mm)')
+zlabel('log_{10} Cones / mm^2 ')
+
+%%
