c814ce01dac2d20922ba3951ccb7d05629e37762
Nicolas Cottaris <cottaris@sas.upenn.edu>
2017-09-25 21:31:14 -0400

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- tutorials/t_cones/t_conesMosaicHex.m
+++ tutorials/t_cones/t_conesMosaicHex.m
@@ -20,13 +20,44 @@ ieInit; clear; close all;
 %% Set mosaic parameters
 mosaicParams = struct(...
     'name', 'the hex mosaic', ...
-    'resamplingFactor', 9, ...                      % Sets underlying pixel spacing; controls the accuracy of the hex mosaic grid
+    'resamplingFactor', 9, ...                      % Sets underlying pixel spacing; controls the rectangular sampling of the hex mosaic grid
     'fovDegs', 0.3, ...                             % FOV in degrees
     'eccBasedConeDensity', true, ...                % Whether to have an eccentricity based, spatially - varying density
     'sConeMinDistanceFactor', 3.0, ...              % Min distance between neighboring S-cones = f * local cone separation - used to make the S-cone lattice semi-regular
     'sConeFreeRadiusMicrons', 0.15*300, ...         % Radius of S-cone free retina, in microns (300 microns/deg).
     'spatialDensity', [0 6/10 3/10 1/10]...         % With a LMS density of of 6:3:1
     );
+
+% 360 seconds
+lowQuality.tolerance1 = 0.01*10;
+lowQuality.tolerance2 = 0.001*10;
+lowQuality.marginF = 1.3;
+lowQuality.mosaicFileName = 'lowQMosaic.mat';
+lowQuality.saveMosaic = true;
+
+% 1040 seconds
+medQuality.tolerance1 = 0.01*3;
+medQuality.tolerance2 = 0.001*3;
+medQuality.marginF = 1.5;
+medQuality.mosaicFileName = 'medQMosaic.mat';
+medQuality.saveMosaic = true;
+
+highQuality.tolerance1 = 0.01;
+highQuality.tolerance2 = 0.001;
+highQuality.marginF = 1.5;
+highQuality.mosaicFileName = 'highQMosaic.mat';
+highQuality.saveMosaic = true;
+
+highQuality2.tolerance1 = 0.01;
+highQuality2.tolerance2 = 0.001;
+highQuality2.marginF = 2.0;
+highQuality2.mosaicFileName = 'highQ2Mosaic.mat';
+highQuality2.saveMosaic = true;
+
+
+% Choose between low and high quality params
+qParams = lowQuality;
+qParams = highQuality2;
 
 tic
 %% Generate the mosaic.  This takes a little while.
@@ -37,10 +68,16 @@ theHexMosaic = coneMosaicHex(mosaicParams.resamplingFactor, ...
     'sConeMinDistanceFactor', mosaicParams.sConeMinDistanceFactor, ... 
     'sConeFreeRadiusMicrons', mosaicParams.sConeFreeRadiusMicrons, ...                   
     'spatialDensity', mosaicParams.spatialDensity, ...
-    'latticeAdjustmentPositionalToleranceF', 0.01/5, ...  % This value is too high and is chosen to reduce the compute time. For production work, this should either not get passed or get set to equal or lower than 0.01      
-    'latticeAdjustmentDelaunayToleranceF', 0.001/5 ...    % This value is too high and is chosen to reduce the compute time. For production work, this should either not get passed or get set to equal or lower than 0.001
+    'latticeAdjustmentPositionalToleranceF', qParams.tolerance1, ...   % This value is too high and is chosen to reduce the compute time. For production work, either do not set, or set to equal or lower than 0.01      
+    'latticeAdjustmentDelaunayToleranceF', qParams.tolerance2, ...     % This value is too high and is chosen to reduce the compute time. For production work, either do not set, or set to equal or lower than 0.001
+    'marginF', qParams.marginF ...                                     % How much larger lattice to generate so as to minimize artifacts in cone spacing near the edges. For production work, do not set this option.
 );
 toc
+
+% Save the mosaic for later analysis
+if (qParams.saveMosaic)
+    save(qParams.mosaicFileName, 'theHexMosaic', '-v7.3');
+end
 
 %% Print some grid info
 theHexMosaic.displayInfo();
