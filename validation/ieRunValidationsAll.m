bf3e3408367ffd46c441dace74b268993908740f
David Brainard <brainard@psych.upenn.edu>
2017-10-20 09:09:56 -0400

    Wrote this.  It runs all the validations, but doesn’t check against the validation server.
    Useful for (e.g.) Windows users where the RDTB doesn’t work.  It allows
    at least a check of whether they run without crashing.

--- validation/ieRunValidationsAll.m
+++ validation/ieRunValidationsAll.m
@@ -1,23 +1,26 @@
 function ieRunTutorialsAll
 %ieRunTutorialsAll
 %
+% Description:
 %   Run all of the isetbio validations that we think should work, and print out a report at the end
 %   as to whether they threw errors, or not.  This does not check the validation data, it just
 %   runs through the scripts.
 
-% 07/26/17  dhb  Wrote this, because we care.
+% 10/19/17  dhb  Wrote this from tutorials script
 
 % User/project specific preferences
 p = struct(...
     'rootDirectory',            fileparts(which(mfilename())), ...
-    'tutorialsSourceDir',       fullfile(isetbioRootPath, 'tutorials') ...                % local directory where tutorial scripts are located
+    'tutorialsSourceDir',       fullfile(isetbioRootPath, 'validation', 'scripts') ...                % local directory where tutorial scripts are located
     );
 
 %% List of scripts to be skipped from automatic publishing.
 %
 % Anything with this in its path name is skipped.
 scriptsToSkip = {...
-    'underDevelopment' ...
+    'codedevscripts' ...
+    'xNeedChecking' ...
+    'rgc' ...
     };
 
 %% Use UnitTestToolbox method to do this.
