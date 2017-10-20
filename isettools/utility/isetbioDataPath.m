20d17191ec2d49ee458efa64f86af630b17bebc9
David Brainard <brainard@psych.upenn.edu>
2017-10-20 08:18:55 -0400

    Update isetbioRootPath so it works even if the isetbio dir isn’t called isetbio.
    Also improved comments in this and other path fetching routines.

--- isettools/utility/isetbioDataPath.m
+++ isettools/utility/isetbioDataPath.m
@@ -1,8 +1,18 @@
 function dataPath=isetbioDataPath()
-%ISETBIODATAPATH  Return the path to the directory containing data bundled with isetbio.
-%   dataPath=isetbioDataPath
+%%isetbioDataPath  Return the path to the oldsytle directory containing data bundled with isetbio.
 %
-%   See also: isetbioRootPath.
+% Syntax:
+%    dataPath=isetbioDataPath;
+%
+% Description:
+%    Return path to the isetbio data directory that is inside of isettools.
+%
+% Notes:
+%    We are trying to make this directory go away, in favor for the data directory
+%    and associated functions at the top level of isetbio.  But, this is a slow
+%    process and we still need this for some older functions to work.
+%
+% See also: isetbioRootPath.
 
 dataPath=fullfile(isetbioRootPath,'isettools','data',[]);
 
