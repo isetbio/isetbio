1ab3777aef1e1abe3748017f4456ddb0c28f6d68
Jennifer Maxwell <32528018+jenmaxwell@users.noreply.github.com>
2017-10-25 17:44:45 -0700

    BipolarLayer Updates #2
    2nd Pass for David & Brian's evaluation

--- isettools/bipolar/@bipolarLayer/plot.m
+++ isettools/bipolar/@bipolarLayer/plot.m
@@ -1,4 +1,6 @@
 function hdl = plot(obj, pType, varargin)
+% Plot the values from one of the mosaics of a bipolarLayer object
+%
 % Syntax:
 %   hdl = bipolarLayer.plot(pType, ...)
 % 
@@ -7,21 +9,25 @@ function hdl = plot(obj, pType, varargin)
 %
 %    Plot the values from one of the mosaics of a bipolarLayer object
 %
-% Required Input:
+% Inputs:
 %    pType     - The plot type. Use @bipolarLayer.plot('help','nMosaic',1)
 %                to see implemented plot types.
 %  
 % Optional Key/Value Pairs:
 %    nMosaic   - Mosaic number to plot
-%    gamma     - ???
-%    pos       - ???
-%    newWindow - ???
+%    gamma     - controls image display
+%    pos       - positions to plot for time series
+%    newWindow - whether or not to open in a new window
+%
+% Outputs:
+%    hdl       - bipolar mosaic plot according to the provided parameters
 %
 % Notes:
 % * For most plot types this routine calls rgcMosaic.plot. The selected
 %   mosaic from the layer is based on the integer nMosaic.
 % * We plan to implement cases for the layer that compare across several
 %   mosaics.
+% * 'help' described in Inputs is broken?
 %
 
 % History:
@@ -42,17 +48,16 @@ p.FunctionName  = mfilename;
 p.KeepUnmatched = true;
 
 p.addRequired('pType', @ischar);
-
 p.addParameter('gamma', 1, @isscalar);
-%%%
+
 % If parameter pos not passed, then assume 1, 1
 p.addParameter('pos', [1, 1], @isvector);
 p.addParameter('newWindow', false, @islogical);
-%%%
+
 % Will this be one of the mosaics, or use multiple mosaics (0, or maybe a
 % vector?)
 p.addParameter('nMosaic', 0, @(x)(isscalar(x) && (x >= 0)));
-%%%
+
 % Parse pType
 p.parse(pType, varargin{:}); 
 
@@ -66,13 +71,11 @@ end
 if p.Results.newWindow; hdl = vcNewGraphWin; end
 
 if nMosaic == 0
-    %%%
     % A plot that uses more than one mosaic. What the layer is for.
     % We need to make some stuff up for here.
     disp('No layer plots implemented yet.')
     return;
 else
-    %%%
     % A plot based on one mosaic. Call the bipolarMosaic.plot funciton.
     hdl = obj.mosaic{nMosaic}.plot(pType, p.Results);
 end
