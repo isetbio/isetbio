4cf95a2f35ab4f954532b02f000b847acc582401
David Brainard <brainard@psych.upenn.edu>
2017-10-26 10:12:20 -0400

    Put into standard isetbio form.

--- validation/scripts/rgc/v_rgcPillowModelGainCheck.m
+++ validation/scripts/rgc/v_rgcPillowModelGainCheck.m
@@ -1,8 +1,24 @@
-% v_rgcNumericalCheck
-% 
-% A validation for the precomputed retinal ganglion cell response models.
-% 
+function varargout = v_rgcPillowModelGainCheck(varargin)
+%v_rgcPillowModelGainCheck  Validate features of the Pillow RGC model.
+%
+% Description:
+%   Validate features of the Pillow RGC model. Currently checks gain.
+%   TRG asserts that this should always be one, independent of sampling
+%   rate.  Currently, this is not the case.
+%
+% Notes:
+%   [Note: DHB]  Once this is working, we can tuck away actual validation
+%   data and add this to the scripts that get run.
+
 % TRG 10/2017, (c) isetbio team
+%
+% 10/26/17  dhb  Put into standard isetbio validation function form.
+
+varargout = UnitTest.runValidationRun(@ValidationFunction, nargout, varargin);
+end
+
+%% Function implementing the isetbio validation code
+function ValidationFunction(runTimeParams)
 
 %% Initialize
 clear; close all; ieInit;
@@ -20,11 +36,13 @@ for i=1:length(cellTypes)
         params.cellType = cellTypes{i};
         [rgcFilter,rgcTime ] = rgcImpulseResponsePillow(params);
         rgcGain = sum(rgcFilter(:));
-
+        
         if abs(rgcGain - 1.0) < 1e-6
             fprintf('Pillow %s filter with sample rate %0.4f has correct unit gain.\n', cellTypes{i}, sampleRates(j));
         else
             fprintf('Pillow %s filter with sample rate %0.4f has gain of %0.4f which should be 1.0\n', cellTypes{i}, sampleRates(j), rgcGain);
         end
     end
+end
+
 end
