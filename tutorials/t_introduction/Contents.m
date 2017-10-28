3fdda897270832c58252ff12efa45e1aa8ecc12b
David Brainard <brainard@psych.upenn.edu>
2017-10-28 10:48:15 -0400

    Moved t_introduction to t_recipes
    This seems like a more accurate description of the one tutorial in there.
    
    We probably want to write some very simple introductory tutorials at some point, and
    recreate a t_introduction to put them in.

--- tutorials/t_introduction/Contents.m
+++
@@ -1,6 +0,0 @@
-% isetbio/tutorials/t_introduction
-%
-% Introduces various isetbio building blocks and how these blocks work
-% together to perform some computation
-%
-% t_dynamicStimulusToPhotocurrent - Illustrates how to compute photocurrent responses to a dynamic stimulus.
