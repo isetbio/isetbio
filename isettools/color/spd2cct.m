d6f5a967bd1fd264c743dc49cc1a2e65b2cbfaae
David Brainard <brainard@psych.upenn.edu>
2017-10-31 11:15:14 -0400

    Docmentation review
    * Syntax variable naming did not match function declaration, and
       there was a stray line under description.  Cleaned these up.
    * Dealt with the note and removed the unused units argument.
    * Little code cleaning.
    * Added some see alsos.

--- isettools/color/spd2cct.m
+++ isettools/color/spd2cct.m
@@ -1,32 +1,32 @@
-function [temp, uv] = spd2cct(wave, spds, units)
+function [CCT, uv] = spd2cct(wave, spds)
 % Convert a spectral power distribution to a correlated color temperature 
 %
 % Syntax:
-%   [CCT, uv] = spd2cct(wave, spd, units)
+%   [CCT, uv] = spd2cct(wave, spd)
 %
 % Description:
 %    Calculates the correlated color temperature of a light from its
-%    spectral power distribution in energy
+%    spectral power distribution in energy units, result is in degrees
+%    Kelvin.
 %    CCT: Correlated color temperature.
 %
 % Inputs:
 %	 wave  - Wavelengths of SPD.
 %    spds  - Spectral power disbution of the lights. Can be in the columns
 %            of a matrix.
-%    units - SPD's units, to be converted to degrees Kelvin [Deprecated?]
 %
 % Outputs:
-%    temp  - Color Temperature, in degrees Kelvin
+%    CCT   - Correlated color temperature, in degrees Kelvin
 %    uv    - Chromacity coordinates
 %
-% Notes:
-%    * Are units still necessary? They aren't used anywhere in the function
-%      or returned. 
-%
+% See also:
+%    cct, xyz2uv
 
 % History:
 %    xx/xx/03       Copyright ImagEval Consultants, LLC.
 %    10/27/17  jnm  Comments & formatting
+%    10/31/17  dhb  Remove unused units arg, as per note query.  Deleted
+%                   the note.
 
 % Examples:
 %{
@@ -39,12 +39,18 @@ function [temp, uv] = spd2cct(wave, spds, units)
    d = blackbody(400:10:700, 8500);
    spd2cct(400:10:700,d)
 %}
+
+% Convert to XYZ
 XYZ = ieXYZFromEnergy(spds',wave);
 
 % ISET returns uprime and vprime, which were defined in the 1960s. The flag
 % makes sure we get 'uv' instead.
 [u,v] =  xyz2uv(XYZ, 'uv');
-uv = [u,v]';   % Format Jeff wrote for cct. u in first row, v in second
-temp = cct( uv );
+
+% Format for cct routine. u in first row, v in second
+uv = [u,v]';  
+
+% Get temp
+CCT = cct( uv );
 
 end
