98b3a83ffe064fa177010bd61f24b3448cf36e23
David Brainard <brainard@psych.upenn.edu>
2017-11-26 14:47:01 -0500

    Added some notes and cleaned up code format a little
    Notes are posted in an issue.  Cleaning was mostly deleting commented out sections as well as stray vertical space.

--- isettools/wavefront/iset/wvf2oi.m
+++ isettools/wavefront/iset/wvf2oi.m
@@ -10,11 +10,32 @@ function oi = wvf2oi(wvf)
 %
 %    Before calling this function, compute the PSF of the wvf structure.
 %
+%    Non-optics aspects of the oi structure take on default values.
+%
 % Inputs:
 %    wvf - A wavefront parameters structure (with a computed PSF)
 %
 % Outputs:
 %    oi  - ISETBIO optical image
+%
+% Notes:
+%  * [NOTE: DHB - There is an interpolation in the loop that computes the
+%     otf wavelength by wavelength.  This appears to be there to handle the
+%     possibility that the frequency support in the wvf structure could be
+%     different for different wavelengths. Does that ever happen?  If we
+%     check and it doesn't, I think we could save a little time by getting
+%     rid of the interpolation.]
+%  * [NOTE: DHB - NCP and I spent a lot of time last summer suffering
+%     through the psf <-> otf calculations as part of the IBIOColorDetect
+%     project, and the fftshift conventions.]
+%  * [NOTE: DHB - There is a note that PSF might start real but that the
+%     PSF implied by the OTF computed here might not be.  We should check
+%     into that.  We don't want imaginary PSFs showing up in calculations.
+%     Perhaps this is handled by the oi methods, in that perhaps they
+%     enforce that the psf obtained from the otf is in fact real.]
+%  * [NOTE: DHB - It might be worth checking that it is OK just to set the
+%     wavelength on the OTF, even if the oi itself has different wavelength
+%     sampling.]
 %
 % See Also:
 %    oiCreate, oiPlot
@@ -23,7 +44,6 @@ function oi = wvf2oi(wvf)
 % History:
 %	 xx/xx/12       Copyright Wavefront Toolbox Team 2012
 %    11/13/17  jnm  Comments & formatting
-%
 
 % Examples
 %{
@@ -42,10 +62,9 @@ function oi = wvf2oi(wvf)
 
 %% Set up parameters
 if notDefined('wvf'), error('Wavefront structure required.'); end
-
 wave = wvfGet(wvf, 'calc wave');
 
-% First we figure out the frequency support.
+%% First we figure out the frequency support.
 fMax = 0;
 for ww=1:length(wave)
     f = wvfGet(wvf, 'otf support', 'mm', wave(ww));
@@ -64,12 +83,14 @@ c0 = find(X(1, :) == 0);
 r0 = find(Y(:, 1) == 0);
 
 %% Set up the OTF variable for use in the ISETBIO representation
-
 nWave = length(wave);
 nSamps = length(fx);
 otf = zeros(nSamps, nSamps, nWave);
 
-% Interpolate the WVF OTF data into the ISET OTF data for each wavelength.
+%% Interpolate the WVF OTF data into the ISET OTF data for each wavelength.
+%
+% The interpolation seems to be here in case there is different frequency
+% support in the wvf structure at different wavelengths.
 for ww=1:length(wave)
     f = wvfGet(wvf, 'otf support', 'mm', wave(ww));
     thisOTF = wvfGet(wvf, 'otf', wave(ww));
@@ -87,33 +108,25 @@ for ww=1:length(wave)
     
     % We identified the (r, c) that represent frequencies of 0 (i.e., DC).
     % We circularly shift so that that (r, c) is at the (1, 1) position.
-    otf(:, :, ww) = circshift(est, -1 * [r0 - 1, c0 - 1]);
-    
+    otf(:, :, ww) = circshift(est, -1 * [r0 - 1, c0 - 1]);  
 end
 
-% % I sure wish this was real all the time. Sometimes (often?) it is. 
+%% Is PSF real?
+%
+% I sure wish this was real all the time. Sometimes (often?) it is. 
 % psf = otf2psf(otf(:, :, ww));
 % if ~isreal(psf), disp('psf not real'); end
 % vcNewGraphWin; mesh(psf)
 
 %% Place the frequency support and OTF data into an ISET structure.
-
-% Build template
+%
+% Build template with standard defaults
 oi = oiCreate;
 
-% Same wavelengths
-% oi = oiSet(oi, 'wave', wave);
-
 % Copy the OTF parameters.
-% Note the awful repetition of setting the wave. Sigh. This should get
-% better. 
 oi = oiSet(oi, 'optics OTF fx', fx);
 oi = oiSet(oi, 'optics OTF fy', fy);
 oi = oiSet(oi, 'optics otfdata', otf);
-% oi = oiSet(oi, 'optics wave', wave);
 oi = oiSet(oi, 'optics OTF wave', wave);
-
-% Z = wvfGet(wvf, 'zcoeffs');
-% oi = oiSet(oi, 'zernike', Z);
 
 end
