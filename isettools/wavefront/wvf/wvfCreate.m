59a866f33f02149e1b320a5e1e623df65a760733
David Brainard <brainard@psych.upenn.edu>
2017-12-06 08:48:29 -0500

    Take key value/pairs
    Previously, this took no inputs and simply set things up with default values.  The documentation indicated, however, that it took key value pairs.  I rewrote so that it does take key/value pairs.
    
    As part of this, I implemented into ieParamFormat the ability to take a cell array, in which case all the odd entries (the keys) are converted to ieParam format.
    
    Not yet fully tested.
    
    Need to go back and deal with all the examples, which previously ignored any pass key/value pairs.

--- isettools/wavefront/wvf/wvfCreate.m
+++ isettools/wavefront/wvf/wvfCreate.m
@@ -40,40 +40,72 @@ function wvf = wvfCreate(varargin)
 %    wvfSet, wvfGet, sceCreate, sceGet
 %
 
-% History
+% History:
 %    xx/xx/11       (c) Wavefront Toolbox Team 2011, 2012
 %    07/20/12  dhb  Get rid of weighting spectrum, replace with cone psf
 %                   info structure
 
 % Examples:
 %{
-	wvf = wvfCreate('wave', [400:10:700]);
+	wvf = wvfCreate('calc wavelengths', [400:10:700]);
 %}
 
-%% Book-keeping
+%% Input parse
+%
+% Run ieParamFormat over varargin before passing to the parser,
+% so that keys are put into standard format
+p = inputParser;
+p.addParameter('name', 'default', @ischar);
+p.addParameter('type', 'wvf', @ischar);
+
+% Zernike coefficients and related
+p.addParameter('zcoeffs', 0, @isscalar);
+p.addParameter('measuredpupil', 8, @isscalar);
+p.addParameter('measuredwl', 550, @isscalar);
+p.addParameter('measuredopticalaxis', 0, @isscalar);
+p.addParameter('measuredobserveraccommodation', 0, @isscalar);
+p.addParameter('measuredobserverfocuscorrection', 0, @isscalar);
+
+% Spatial sampling parameters
+p.addParameter('sampleintervaldomain', 'psf', @ischar);
+p.addParameter('spatialsamples', 201, @isscalar);
+p.addParameter('refpupilplanesize', 16.212, @isscalar);
+
+% Calculation parameters
+p.addParameter('calcpupilsize', 3, @isscalar);
+p.addParameter('calcwavelengths', 550, @isnumeric);
+p.addParameter('calcopticalaxis', 0, @isscalar);
+p.addParameter('calcobserveraccommodation', 0), @isscalar;
+p.addParameter('calcobserverfocuscorrection', 0, @isscalar);
+ieVarargin = ieParamFormat(varargin);
+p.parse(ieVarargin{:});
+
+%% Now set all of the properties that are specified by the parse above.
+%
+% This is done via wvfSet.  
 wvf = [];
-wvf = wvfSet(wvf, 'name', 'default');
-wvf = wvfSet(wvf, 'type', 'wvf');
+wvf = wvfSet(wvf, 'name', p.Results.name);
+wvf = wvfSet(wvf, 'type', p.Results.type);
 
-%% Zernike coefficients and related
-wvf = wvfSet(wvf, 'zcoeffs', 0);
-wvf = wvfSet(wvf, 'measured pupil', 8);
-wvf = wvfSet(wvf, 'measured wl', 550);
-wvf = wvfSet(wvf, 'measured optical axis', 0);
-wvf = wvfSet(wvf, 'measured observer accommodation', 0);
-wvf = wvfSet(wvf, 'measured observer focus correction', 0);
+% Zernike coefficients and related
+wvf = wvfSet(wvf, 'zcoeffs', p.Results.zcoeffs);
+wvf = wvfSet(wvf, 'measured pupil', p.Results.measuredpupil);
+wvf = wvfSet(wvf, 'measured wl', p.Results.measuredwl);
+wvf = wvfSet(wvf, 'measured optical axis', p.Results.measuredopticalaxis);
+wvf = wvfSet(wvf, 'measured observer accommodation', p.Results.measuredobserveraccommodation);
+wvf = wvfSet(wvf, 'measured observer focus correction', p.Results.measuredobserverfocuscorrection);
 
-%% Spatial sampling parameters
-wvf = wvfSet(wvf, 'sample interval domain', 'psf');
-wvf = wvfSet(wvf, 'spatial samples', 201);
-wvf = wvfSet(wvf, 'ref pupil plane size', 16.212);
+% Spatial sampling parameters
+wvf = wvfSet(wvf, 'sample interval domain', p.Results.sampleintervaldomain);
+wvf = wvfSet(wvf, 'spatial samples', p.Results.spatialsamples);
+wvf = wvfSet(wvf, 'ref pupil plane size', p.Results.refpupilplanesize);
 
-%% Calculation parameters
-wvf = wvfSet(wvf, 'calc pupil size', 3);
-wvf = wvfSet(wvf, 'calc wavelengths', 550);
-wvf = wvfSet(wvf, 'calc optical axis', 0);
-wvf = wvfSet(wvf, 'calc observer accommodation', 0);
-wvf = wvfSet(wvf, 'calc observer focus correction', 0);
+% Calculation parameters
+wvf = wvfSet(wvf, 'calc pupil size', p.Results.calcpupilsize);
+wvf = wvfSet(wvf, 'calc wavelengths', p.Results.calcwavelengths);
+wvf = wvfSet(wvf, 'calc optical axis', p.Results.calcopticalaxis);
+wvf = wvfSet(wvf, 'calc observer accommodation', p.Results.calcobserveraccommodation);
+wvf = wvfSet(wvf, 'calc observer focus correction', p.Results.calcobserverfocuscorrection);
 
 % Cone sensitivities and weighting spectrum for combining the PSFs across
 % wavelengths. We keep these as a structure at something resembling a
