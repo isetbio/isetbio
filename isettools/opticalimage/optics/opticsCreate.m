e705fda3b21e0ba83eef96db51cacfe2a25b2304
David Brainard <brainard@psych.upenn.edu>
2017-12-13 17:23:53 -0500

    Trying to make hard coded constants consitent
    And improve comments.  More work/thinking needed
    
    % 12/13/17  dhb    For wvfhuman, make sure oi focal length and f number are set to match specified pupil diameter and umPerDegree.

--- isettools/opticalimage/optics/opticsCreate.m
+++ isettools/opticalimage/optics/opticsCreate.m
@@ -18,12 +18,14 @@ function optics = opticsCreate(opticsType,varargin)
 % Specifying human optics  creates a shift-invariant optics structure with
 % human OTF data.
 %
-%      {'human'}     - Uses Marimont and Wandell (Hopkins) method (DEFAULT)
+%      {'human'}     - Uses Marimont and Wandell (Hopkins) method (default)
+%                      opticsCreate('human',[pupilRadiusMeters=0.00015])
 %      {'wvf human'} - Uses Wavefront toolbox and Thibos data
-%         opticsCreate('wvf human',[wave=400:10:700],
-%                                  [pupilMM=3],
-%                                  [zCoefs=wvfLoadThibosVirtualEyes]);
-%      {'diffraction'} - Typically f/4 optics
+%                      opticsCreate('wvf human',[wave=400:10:700],
+%                                   [pupilMM=3],
+%                                   [zCoefs=wvfLoadThibosVirtualEyes]
+%                                   [umPerDegree=300]);
+%      {'diffraction'} - 46 deg field of view, f number 4 optics.
 %
 % Human and general shift-invariant models can also be created by
 % specifying wavefront aberrations using Zernike polynomials.  There is a
@@ -37,7 +39,12 @@ function optics = opticsCreate(opticsType,varargin)
 %
 % See also: oiCreate, opticsSet, opticsGet
 %
-% Copyright ImagEval Consultants, LLC, 2003.
+
+% History
+%                  Copyright ImagEval Consultants, LLC, 2003.
+% 12/11/17  dhb    Allow umPerDegree to be passed for wvfhuman.
+% 12/13/17  dhb    For wvfhuman, make sure oi focal length and f number are
+%                  set to match specified pupil diameter and umPerDegree.
 
 if notDefined('opticsType'), opticsType = 'default'; end
 
@@ -51,60 +58,71 @@ switch lower(opticsType)
         % For diffraction case, set lens transmittance to 1
         % Perhaps we should allow the transmittance to be set freely as in
         % ISET?  Or ...
-        optics.lens = Lens;  % Human lens object
+        optics.lens = Lens;        % Human lens object
         optics.lens.density = 0;   % Pigment density set to 0 
         
     case {'default','human','humanmw'}
         % Optics for the Marimont and Wandell human eye
-        % Pupil radius in meters.  Default is 3 mm
-        pupilRadius = 0.0015; % 3mm diameter default
-        if ~isempty(varargin), pupilRadius = varargin{1}; end
         
-        % This creates a shift-invariant optics. The other standard forms
-        % are diffraction limited.
+        % Pupil radius in meters.  Default is 3 mm
+        pupilRadius = 0.0015;
+        if (~isempty(varargin) & ~isempty(varargin{1})), pupilRadius = varargin{1}; end
+        
+        % This creates shift-invariant optics.
         optics = opticsHuman(pupilRadius);
         optics = opticsSet(optics, 'model', 'shift invariant');
         optics = opticsSet(optics, 'name', 'human-MW');
-        optics.lens = Lens;  % Default human lens object
+        
+        % Add default huamn Lens by default
+        optics.lens = Lens;
 
     case {'wvfhuman'}
         % Optics based on Zernike polynomial wavefront model estimated by
-        % Thibos.
-        % 
-        % opticsCreate('wvf human',[pupilMM],[zCoefs],[wave],[umPerDegree]);      
+        % Thibos.    
         
         % Defaults
-        pupilMM = 3;
-        zCoefs = wvfLoadThibosVirtualEyes(pupilMM);
+        pupilDiameterMM = 3;
+        zCoefs = wvfLoadThibosVirtualEyes(pupilDiameterMM);
         wave = 400:10:700; wave = wave(:);
         umPerDegree = 300;
 
-        if (~isempty(varargin) & ~isempty(varargin{1})), pupilMM = varargin{1}; end
+        if (~isempty(varargin) & ~isempty(varargin{1})), pupilDiameterMM = varargin{1}; end
         if (length(varargin)>1 & ~isempty(varargin{2})), zCoefs = varargin{2};  end
         if (length(varargin)>2 & ~isempty(varargin{3})), wave = varargin{3}; wave = wave(:); end 
         if (length(varargin)>3 & ~isempty(varargin{4})), umPerDegree = varargin{4}; end
         
         % Create wavefront parameters
-        wvfP = wvfCreate('calc wavelengths',wave,'zcoeffs',zCoefs,'name',sprintf('human-%d',pupilMM),'umPerDegree',umPerDegree);
-        wvfP = wvfSet(wvfP,'calc pupil size',pupilMM);
+        wvfP = wvfCreate('calc wavelengths',wave,'zcoeffs',zCoefs,'name',sprintf('human-%d',pupilDiameterMM),'umPerDegree',umPerDegree);
+        wvfP = wvfSet(wvfP,'calc pupil size',pupilDiameterMM);
         wvfP = wvfComputePSF(wvfP);
-        % [u,p,f] = wvfPlot(wvfP,'2d psf space','um',550);
-        % set(gca,'xlim',[-20 20],'ylim',[-20 20]);
         
         optics = oiGet(wvf2oi(wvfP),'optics');        
-        optics = opticsSet(optics, 'model', 'shiftInvariant');
-        optics = opticsSet(optics, 'name', 'human-wvf');        
+        optics = opticsSet(optics, 'model', 'shift invariant');
+        optics = opticsSet(optics, 'name', 'human-wvf');  
         
-        % Add Lens by default, now.
+        % Convert from pupil size and focal length to f number and focal length,
+        % because that is what we can set.  This implies a number of mm per degree,
+        % and we back it out the other way here so that it is all
+        % consistent.
+        focalLengthMM = (umPerDegree*1e-3)/(2*tand(0.5));
+        fLength = focalLengthMM*1e-3;
+        pupilRadius = (pupilDiameterMM/2)*1e-3;
+        optics = opticsSet(optics, 'fnumber', fLength/(2*pupilRadius));  
+        optics = opticsSet(optics, 'focalLength', fLength); 
+        
+        % Add default Lens by default
         optics.lens = Lens;
 
     case 'mouse'
+        % Some day might add in a default mouse optics.  Here are some
+        % guesses about the right parameters:
+        %
         % Pupil radius in meters.  
-        % Dilated pupil : 1.009mm = 0.001009m
-        % Contracted pupil : 0.178 mm
-        % (Source : From candelas to photoisomerizations in the mouse eye by 
-        % rhodopsin bleaching in situ and the light-rearing dependence of 
-        % the major components of the mouse ERG, Pugh, 2004)
+        %   Dilated pupil : 1.009mm = 0.001009m
+        %   Contracted pupil : 0.178 mm
+        %   (Source : From candelas to photoisomerizations in the mouse eye by 
+        %   rhodopsin bleaching in situ and the light-rearing dependence of 
+        %   the major components of the mouse ERG, Pugh, 2004)
         % We use a default value, in between : 0.59 mm.
         %         if ~isempty(varargin)
         %             pupilRadius = varargin{1};
@@ -127,13 +145,16 @@ switch lower(opticsType)
         %         optics = opticsTwoThirdInch;
         %     case {'standard(1-inch)','oneinch'}
         %         optics = opticsOneInch;
+        
     otherwise
         error('Unknown optics type.');
 end
 
 % Default computational settings for the optical image
 optics = opticsSet(optics,'offAxisMethod','cos4th');
-optics.vignetting =    0;   % Pixel vignetting is off
+
+% Pixel vignetting is off
+optics.vignetting =  0;   
 
 end
 
@@ -144,7 +165,7 @@ function optics = opticsDiffraction
 
 optics.type = 'optics';
 optics = opticsSet(optics,'name','ideal (small)');
-optics = opticsSet(optics,'model','diffractionLimited');
+optics = opticsSet(optics,'model','diffraction limited');
 
 % Standard 1/4-inch sensor parameters
 sensorDiagonal = 0.004;
@@ -163,32 +184,35 @@ function optics = opticsHuman(pupilRadius)
 % human OTF, using humanOTF from Marimont and Wandell, in the OTF fields.
 % 
 % The frequency support is calculated in cyc/deg but stored in units of
-% cyc/mm. (Sorry for that).  Elsewhere we use 300 microns/deg as the
-% conversion factor. This value corresponds to a distance of 17mm (human
-% focal length).  (Where? BW).
-
-% We place fnumber and focal length values that are approximate for
-% diffraction-limited in those fields, too.  But they are not a good
-% description, just the diffraction limit bounds for this type of a system.
+% cyc/mm.
 %
-% The pupilRadius should is specified in meters.
+% In the wvf code, use 300 microns/deg as the conversion factor. This value
+% corresponds to a distance of 17.188mm (human focal length).
 
+% The pupil radius is specified in meters.
 if notDefined('pupilRadius'), pupilRadius = 0.0015; end
-fLength = 0.017;  %Human focal length is 17 mm
+
+% Human focal length is ~17 mm.  The number below corresponds more
+% exactly to 300 um per degree. This number is pretty baked into the
+% Marimont and Wandell optics, so we bake it in here.
+umPerDegree = 300;
+focalLengthMM = (umPerDegree*1e-3)/(2*tand(0.5));
+fLength = focalLengthMM*1e-3;  
 
 optics.type = 'optics';
 optics.name = 'human';
 optics      = opticsSet(optics, 'model', 'shiftInvariant');
 
-% Ratio of focal length to diameter.  
+% Convert from pupil size and focal length to f number and focal length,
+% because that is what we can set.  This implies a number of mm per degree.
 optics = opticsSet(optics, 'fnumber', fLength/(2*pupilRadius));  
 optics = opticsSet(optics, 'focalLength', fLength);  
 
+% Specify method for optics
 optics = opticsSet(optics, 'otfMethod', 'humanOTF');
 
 % Compute the OTF and store it.  We use a default pupil radius, dioptric
 % power, and so forth.
-
 dioptricPower = 1/fLength;      % About 60 diopters
 
 % We used to assign the same wave as in the current scene to optics, if the
@@ -203,13 +227,12 @@ optics = opticsSet(optics, 'otfData', OTF2D);
 % microns, so there are about 3 cyc/mm.  To convert from cyc/deg to cyc/mm
 % we divide by 0.3. That is:
 %  (cyc/deg * (1/mm/deg)) cyc/mm.  1/mm/deg = 1/.3
-fSupport = fSupport * (1/0.3);  % Convert to cyc/mm
+fSupport = fSupport * (1/(umPerDegree*1e-3));  
 
 fx     = fSupport(1, :, 1);
 fy     = fSupport(:, 1, 2);
 optics = opticsSet(optics, 'otffx', fx(:)');
 optics = opticsSet(optics, 'otffy', fy(:)');
-
 optics = opticsSet(optics, 'otfWave', wave);
 
 end
