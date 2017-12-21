cf49788cd72800df26f3f8e777d6c07db375bfde
David Brainard <brainard@psych.upenn.edu>
2017-12-21 16:51:43 -0500

    Reduce verbosity so running with failures doesn’t lock up my machine.

--- validation/ieValidateFullAll.m
+++ validation/ieValidateFullAll.m
@@ -37,7 +37,7 @@ UnitTest.setPref('closeFigsOnInit', true);
 
 %% Verbosity Level
 % valid options are: 'none', min', 'low', 'med', 'high', 'max'
-UnitTest.setPref('verbosity', 'high');
+UnitTest.setPref('verbosity', 'low');
 
 %% Numeric tolerance for comparison to ground truth data
 if (~ispref(thisProject, 'numericTolerance'))
