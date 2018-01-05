05efbe581e5d4ed0d92c5290174aa80e9c4ecc32
David Brainard <brainard@psych.upenn.edu>
2018-01-05 07:42:23 -0500

    Fix call to visualize for new required plot type param.

--- tutorials/t_recipes/t_dynamicStimulusToPhotocurrent.m
+++ tutorials/t_recipes/t_dynamicStimulusToPhotocurrent.m
@@ -125,7 +125,7 @@ oiModulated = oiBackground;
 % the background, which will be illustrated below.
 fprintf('Computing sequence of retinal images for background.\n');
 zeroContrastOIsequence = oiSequence(oiBackground, oiModulated, temporalParams.sampleTimes, temporalParams.TemporalWindow, 'composition', 'blend');
-zeroContrastOIsequence.visualize('format', 'montage', 'showIlluminanceMap', true);
+zeroContrastOIsequence.visualize('montage', 'showIlluminanceMap', true);
 
 %% Generate the eye movement path for the adapting stimulus sequence
 fprintf('Creating eye movement path\n');
