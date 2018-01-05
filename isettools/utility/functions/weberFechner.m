d164743dc2e365edd908c05b2eb02ea1a21edb61
David Brainard <brainard@psych.upenn.edu>
2018-01-05 14:31:12 -0500

    Handling issue 283.

--- isettools/utility/functions/weberFechner.m
+++ isettools/utility/functions/weberFechner.m
@@ -5,20 +5,42 @@ function y = weberFechner(coef, x)
 %   y = weberFechner(coef, x)
 %
 % Description:
-%    Compute the Weber-Fechner Threshold-versus-Intensity Step function
+%    Compute the Weber-Fechner sensitivity function used by Angueyra and
+%    Rieke, 2013. to describe how rod flash response gain changes as a
+%    function of background. This is their equation 1, with a log10
+%    applied.
 %       y = log10(1 ./ (1 + x / coef(1)));
 %
+%    They refer to this equation as a Weber-Fechner function, hence the
+%    name of this function.
+%    
 % Inputs
-%    coef - Coefficient
-%    x    - Step intensities (scalar or vector)
+%    coef - Coefficient of the equation, their I0.  This is the background
+%           intensity that reduces the gain by a factor of 2 from its level
+%           in the dark.
+%    x    - Background intensities
 %
 %  Outputs:
-%    y    - Calculated luminance
+%    y    - Calculated log10 of gain ratio gammaB/gammaD as a function of
+%           background, where gammaB is the gain on a given background to
+%           gain in darkness.  Gain is expressed as pA/R* where pA is the
+%           photocurrent and R* is the number of isomerizations in the
+%           flash.
+% 
+% Examples are included within the code.
 %
-% Notes:
-%    * [Note: BW - I do not understand this function.  I put in an
-%    example and made it run.  What is this output?] 
+% References:
+%    Angueyra, Juan M., and Fred Rieke. "Origin and effect of
+%    phototransduction noise in primate cone photoreceptors." Nature
+%    neuroscience 16.11 (2013): 1692-1700.
 %
+% See also: v_osStepFlash
+
+% History:
+%    01/08/16  dhb  Added comment.
+%    12/05/17  jnm  Formatting
+%    01/06/18  dhb  Added comments that express our current lack of
+%                   understanding.
 
 % Example
 %{
@@ -26,9 +48,6 @@ function y = weberFechner(coef, x)
    y = weberFechner(1,x);
    vcNewGraphWin; loglog(x,y);
 %}
-% History:
-%    01/08/16  dhb  Added comment.
-%    12/05/17  jnm  Formatting
 
 % Do it.
 y = log10(1 ./ (1 + x ./ coef(1)));
