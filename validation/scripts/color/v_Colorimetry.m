e5ae130e46bb44284069835ccf236b01c5791685
David Brainard <brainard@psych.upenn.edu>
2018-01-14 10:09:42 -0500

    Restoring function so it works again.

--- validation/scripts/color/v_Colorimetry.m
+++ validation/scripts/color/v_Colorimetry.m
@@ -1,4 +1,4 @@
-arrgh function varargout = v_Colorimetry(varargin)
+function varargout = v_Colorimetry(varargin)
 %
 %  Validate ISETBIO-based colorimetric computations by comparing to PTB-based colorimetric computations.
 %
