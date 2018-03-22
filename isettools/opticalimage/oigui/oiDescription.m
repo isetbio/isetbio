c4082a35a5064d1869920cb9b6f74054a5f30767
jenmaxwell <jen.maxwell@oculus.com>
2018-03-22 11:25:16 -0700

    Optics Pass I Part II
    Wanted to get my current progress checked in before leaving for Dallas. I will update issues list by Monday.
    Optics Pass I Part II
    This set covers the following (sub)directories:
        opticalImage/oiSequence
        opticalImage/oigui
    
    Cosmetic/formatting adjustments only:
        oigui/oiClose
        oigui/oiDescription
    
    Notes/Other:
        oiSequence/oisCreate - Assign someone to complete TODO (hunt down what's causing function failures as displayed with examples)
        oiSequence/s_oisHarmonic
            are script files meant to be in the same folder structure as the regular functions, or are they meant to be separated out like the tutorials are?
            We should determine what kind of formatting we want for scripts. I didn't make too many modifications to this script, but I would like to see a bit more of an extended description of what the script accomplishes at least.
        oigui/oiKeyPress
           TODO: Assign someone to evaluate the duplicate case 8's? (L41 & L48) -- and determine what, if anything, needs to be fixed.
        oigui/oiSetEditsAndButtons - TODO: Remove raytrace case (even though empty)
        oigui/oiWindow
            TODO: Work on standardization of function naming for classes. There seems to be an existing pattern, but it isn't followed religiously.
            Assign someone to fill out key/values section
            TODO: replace isstr with ischar?
            Do all of the online help sites intentionally point to the base of isetbio?
            TODO: in btnSimulate_Callback -- check TODO/Note
            not sure about my short summary or characterizations for a few of the methods... Like menuAnLineIllumVertFFT_Callback, menuAnalyzeLinePlots_Callback,
            Completely unsure (blank) of the following:
                menuP1CIE_Callback (Needs summary & description) -- Is this an incorrectly-named duplicate of menuPlotCIE_Callback?
            The following do not match same naming convention:
                plotOTF_Callback
                menuPlotPS550_Callback
                plotIrradiance_Callback
                PlotMenu_Callback
                Diffraction_Callback
                oiNew
                editGamma
            There are others outside of convention, but I want to have a discussion about solidifying that convention before revisiting.
    
    Raytrace references:
        oiSetEditsAndButtons
        oiDescription
        oiWindow

--- isettools/opticalimage/oigui/oiDescription.m
+++ isettools/opticalimage/oigui/oiDescription.m
@@ -1,80 +1,113 @@
 function txt = oiDescription(oi)
-%oiDescription:  Generate optical image text description for window
+% Generate optical image text description for window
 %
-%   txt = oiDescription(oi,[handles])
+% Syntax:
+%   txt = oiDescription(oi)
 %
-% Writes the OI description for the box on the upper right of the window.
-% Manages cases of diffraction-limited, shift-invariant, and ray trace
-% separately.
+% Description:
+%    Writes the OI description for the box on the upper right of the
+%    window. Manages cases of diffraction-limited, shift-invariant, and ray
+%    trace separately.
 %
-% Copyright ImagEval Consultants, LLC, 2003.
+% Inputs:
+%    oi  - Struct. An optical image structure.
+%
+% Outputs:
+%    txt - String. The generated optical image's description.
+%
+% Optional key/value pairs:
+%    None.
+%
+
+% History:
+%    xx/xx/03       Copyright ImagEval Consultants, LLC, 2003.
+%    03/19/18  jnm  Formatting
 
 txt = sprintf('\nOptical image\n');
 
-if isempty(oi), txt = 'No image'; return;
-else sz = oiGet(oi,'size');
-    if isempty(oiGet(oi,'photons')), txt = addText(txt,sprintf('  No image\n'));
+if isempty(oi)
+    txt = 'No image'; return;
+else
+    sz = oiGet(oi, 'size');
+    if isempty(oiGet(oi, 'photons'))
+        txt = addText(txt, sprintf('  No image\n'));
     else
-        str = sprintf('  Size:       [%.0f, %.0f] samples\n',sz(1),sz(2));
-        txt = addText(txt,str);
+        str = sprintf('  Size:       [%.0f, %.0f] samples\n', ...
+            sz(1), sz(2));
+        txt = addText(txt, str);
 
-        u = round(log10(oiGet(oi,'height','m')));
-        if (u >= 0 ),     str = sprintf('  Hgt,wdth: [%.2f, %.2f] m\n',oiGet(oi,'height','m'),oiGet(oi,'width','m'));
-        elseif (u >= -3), str = sprintf('  Hgt,wdth: [%.2f, %.2f] mm\n',oiGet(oi,'height','mm'),oiGet(oi,'width','mm'));
-        else             str = sprintf('  Hgt,wdth: [%.2f, %.2f] um\n',oiGet(oi,'height','um'),oiGet(oi,'width','um'));
+        u = round(log10(oiGet(oi, 'height', 'm')));
+        if (u >= 0 )
+            str = sprintf('  Hgt, wdth: [%.2f, %.2f] m\n', ...
+                oiGet(oi, 'height', 'm'), oiGet(oi, 'width', 'm'));
+        elseif (u >= -3)
+            str = sprintf('  Hgt, wdth: [%.2f, %.2f] mm\n', ...
+                oiGet(oi, 'height', 'mm'), oiGet(oi, 'width', 'mm'));
+        else
+            str = sprintf('  Hgt, wdth: [%.2f, %.2f] um\n', ...
+                oiGet(oi, 'height', 'um'), oiGet(oi, 'width', 'um'));
         end
-        txt = addText(txt,str);
+        txt = addText(txt, str);
 
-        u = round(log10(oiGet(oi,'sampleSize')));
-        if (u >= 0 ),     str = sprintf('  Sample:  %.2f  m\n',oiGet(oi,'sampleSize','m'));
-        elseif (u >= -3), str = sprintf('  Sample:  %.2f mm\n',oiGet(oi,'sampleSize','mm'));
-        else              str = sprintf('  Sample:  %.2f um\n',oiGet(oi,'sampleSize','um'));
+        u = round(log10(oiGet(oi, 'sampleSize')));
+        if (u >= 0 )
+            str = sprintf('  Sample:  %.2f  m\n', ...
+                oiGet(oi, 'sampleSize', 'm'));
+        elseif (u >= -3)
+            str = sprintf('  Sample:  %.2f mm\n', ...
+                oiGet(oi, 'sampleSize', 'mm'));
+        else
+            str = sprintf('  Sample:  %.2f um\n', ...
+                oiGet(oi, 'sampleSize', 'um'));
         end
-        txt = addText(txt,str);
+        txt = addText(txt, str);
 
-        wave = oiGet(oi,'wave');
-        spacing = oiGet(oi,'binwidth');
-        str = sprintf('  Wave:     %.0d:%.0d:%.0d nm\n',min(wave(:)),spacing,max(wave(:)));
-        txt = addText(txt,str);
+        wave = oiGet(oi, 'wave');
+        spacing = oiGet(oi, 'binwidth');
+        str = sprintf('  Wave:     %.0d:%.0d:%.0d nm\n', ...
+            min(wave(:)), spacing, max(wave(:)));
+        txt = addText(txt, str);
 
-        meanIll = oiGet(oi,'meanilluminance');
+        meanIll = oiGet(oi, 'meanilluminance');
         if ~isempty(meanIll)
-            txt = addText(txt, sprintf('  Illum:       %.1f lux\n',meanIll));
+            txt = addText(txt, sprintf('  Illum:       %.1f lux\n', ...
+                meanIll));
         end
-        
-        fov = oiGet(oi,'hfov');
-        txt = addText(txt, sprintf('  FOV (wide):    %.1f deg\n',fov));
-        
+
+        fov = oiGet(oi, 'hfov');
+        txt = addText(txt, sprintf('  FOV (wide):    %.1f deg\n', fov));
     end
 end
 
 % Write out the optics parameters, either DL, SI or RT
-optics = oiGet(oi,'optics');
-opticsModel = opticsGet(optics,'model');
+optics = oiGet(oi, 'optics');
+opticsModel = opticsGet(optics, 'model');
 
 switch lower(opticsModel)
-    case {'diffractionlimited','dlmtf'}
+    case {'diffractionlimited', 'dlmtf'}
         txt = [txt, sprintf('Optics (DL)\n')];
-        txt = [txt, sprintf('  Mag:  %.2e\n',opticsGet(optics,'magnification'))];
-        diameter = opticsGet(optics,'aperturediameter','mm');
-        txt = [txt, sprintf('  Diameter:  %.2f mm\n',diameter)];
-       
+        txt = [txt, sprintf('  Mag:  %.2e\n', ...
+            opticsGet(optics, 'magnification'))];
+        diameter = opticsGet(optics, 'aperturediameter', 'mm');
+        txt = [txt, sprintf('  Diameter:  %.2f mm\n', diameter)];
+
     case 'shiftinvariant'
         txt = [txt, sprintf('Optics (SI)\n')];
         % See above
-        % txt = [txt, sprintf('  Mag:  %.2e\n',opticsGet(optics,'magnification'))];
-        diameter = opticsGet(optics,'aperture diameter','mm');
-        txt = [txt, sprintf('  Diameter:  %.2f mm\n',diameter)];
-        if checkfields(oi,'optics','lens')
-            d = oiGet(oi,'lens density');
-            txt = [txt, sprintf('  Lens density:  %.2f \n',d)];
+        % txt = [txt, sprintf('  Mag:  %.2e\n', ...
+        %     opticsGet(optics, 'magnification'))];
+        diameter = opticsGet(optics, 'aperture diameter', 'mm');
+        txt = [txt, sprintf('  Diameter:  %.2f mm\n', diameter)];
+        if checkfields(oi, 'optics', 'lens')
+            d = oiGet(oi, 'lens density');
+            txt = [txt, sprintf('  Lens density:  %.2f \n', d)];
         end
     case 'raytrace'
         txt = [txt, sprintf('Optics (RT)\n')];
-        diameter = opticsGet(optics,'aperture diameter','mm');
-        txt = [txt, sprintf('  Diameter:  %.2f mm\n',diameter)];
+        diameter = opticsGet(optics, 'aperture diameter', 'mm');
+        txt = [txt, sprintf('  Diameter:  %.2f mm\n', diameter)];
     otherwise
-        error('Unknown optics model %s. ',opticsModel);
+        error('Unknown optics model %s. ', opticsModel);
 end
 
 return;
--- isettools/opticalimage/oigui/oiSetEditsAndButtons.m
+++ isettools/opticalimage/oigui/oiSetEditsAndButtons.m
@@ -1,123 +1,139 @@
 function oiSetEditsAndButtons(handles)
 % Refresh the buttons and edit fields in the optical image window
 %
+% Syntax:
 %   oiSetEditsAndButtons(handles)
 %
-% Refresh the current optical image window. If there is no optical image,
-% then a default oi is created.
+% Description:
+%    Refresh the current optical image window. If there is no optical image, 
+%    then a default oi is created.
 %
-% Perhaps we should have a flag to leave the inline message in place?
+%    Perhaps we should have a flag to leave the inline message in place?
 %
-% Copyright ImagEval Consultants, LLC, 2003.
+% Inputs:
+%    handles - Handle to the optical image's window.
+%
+% Outputs:
+%    None.
+%
+% Optional key/value pairs:
+%    None.
+%
 
-[val,oi] = vcGetSelectedObject('OPTICALIMAGE');
+% History:
+%    xx/xx/03       Copyright ImagEval Consultants, LLC, 2003.
+%    03/19/18  jnm  Formatting
+
+[val, oi] = vcGetSelectedObject('OPTICALIMAGE');
 if isempty(oi)
     oi = oiCreate;
     val = 1;
 end
 
 % Clear the inline message
-optics = oiGet(oi,'optics');
-ieInWindowMessage('',handles,[]);
+optics = oiGet(oi, 'optics');
+ieInWindowMessage('', handles, []);
 
 % Which optics model
-opticsModel = opticsGet(optics,'model');
+opticsModel = opticsGet(optics, 'model');
 
 % Append the oi names to 'New'
 names = {'New'};
 oiNames = vcGetObjectNames('OPTICALIMAGE');
-for ii=1:length(oiNames),names{ii+1} = char(oiNames(ii)); end
+for ii = 1:length(oiNames), names{ii + 1} = char(oiNames(ii)); end
 
 % Select the appropriate OI entry to display.
-if length(names) > 1, val = val+1; end
-set(handles.SelectOptImg,'String',names,'Value',val);
+if length(names) > 1, val = val + 1; end
+set(handles.SelectOptImg, 'String', names, 'Value', val);
 % We use a slightly different logic in sceneSetEditsAndButtons.
 
 % Set up the buttons for different optics models.
 switch lower(opticsModel)
-    
-    case {'diffractionlimited','dlmtf'}
-        set(handles.popOpticsModel,'Value',1);
-        switchControlVisibility(handles,'on');
-        
+    case {'diffractionlimited', 'dlmtf'}
+        set(handles.popOpticsModel, 'Value', 1);
+        switchControlVisibility(handles, 'on');
+
         % Set the diffraction limited optics parameters
-        optics = oiGet(oi,'optics');
-        str = sprintf('%2.2f',opticsGet(optics,'focal Length','mm'));
-        set(handles.editFocalLength,'String',str);
-        str = sprintf('%1.2f',opticsGet(optics,'fnumber'));
-        set(handles.editFnumber,'String',str);
-        
-        val = opticsGet(optics,'off axis method');
-        if strcmpi(val,'skip'), set(handles.btnOffAxis, 'Value',0);
-        else set(handles.btnOffAxis, 'Value',1);
+        optics = oiGet(oi, 'optics');
+        str = sprintf('%2.2f', opticsGet(optics, 'focal Length', 'mm'));
+        set(handles.editFocalLength, 'String', str);
+        str = sprintf('%1.2f', opticsGet(optics, 'fnumber'));
+        set(handles.editFnumber, 'String', str);
+
+        val = opticsGet(optics, 'off axis method');
+        if strcmpi(val, 'skip'), set(handles.btnOffAxis, 'Value', 0);
+        else, set(handles.btnOffAxis, 'Value', 1);
         end
-        
+
     case 'shiftinvariant'
-        set(handles.popOpticsModel,'Value',2);
-        switchControlVisibility(handles,'off');
+        set(handles.popOpticsModel, 'Value', 2);
+        switchControlVisibility(handles, 'off');
+
     case 'raytrace'
         % TL: What to do here?
+
     otherwise
         error('Unknown optics model')
 end
 
 % Adjust the anti-alias filter
-dMethod = oiGet(oi,'diffuserMethod');
+dMethod = oiGet(oi, 'diffuserMethod');
 
 switch lower(dMethod)
     case 'skip'
-        set(handles.popDiffuser,'val',1)
-        set(handles.editDiffuserBlur,'visible','off');
-        set(handles.txtBlurSD,'visible','off');
-        set(handles.popDiffuser,'Position',[0.756 0.109 0.11 0.043])
-        set(handles.txtDiffuser,'Position',[0.756 .165 0.082 0.029])
+        set(handles.popDiffuser, 'val', 1)
+        set(handles.editDiffuserBlur, 'visible', 'off');
+        set(handles.txtBlurSD, 'visible', 'off');
+        set(handles.popDiffuser, 'Position', [0.756 0.109 0.11 0.043])
+        set(handles.txtDiffuser, 'Position', [0.756 .165 0.082 0.029])
         
     case 'blur'
-        set(handles.popDiffuser,'val',2)
-        set(handles.editDiffuserBlur,'visible','on');
-        set(handles.txtBlurSD,'string','FWHM (um)');
-        set(handles.txtBlurSD,'visible','on');
-        set(handles.txtBlurSD,'TooltipString','Full width half maximum of Gaussian spread');
+        set(handles.popDiffuser, 'val', 2)
+        set(handles.editDiffuserBlur, 'visible', 'on');
+        set(handles.txtBlurSD, 'string', 'FWHM (um)');
+        set(handles.txtBlurSD, 'visible', 'on');
+        set(handles.txtBlurSD, 'TooltipString', 'Full width half maximum of Gaussian spread');
         
-        set(handles.txtBlurSD,'Position',[0.8693 .117 0.089 0.029])
-        set(handles.editDiffuserBlur,'Position',[0.82 0.115 0.038 0.033])
-        set(handles.popDiffuser,'Position',[0.693 0.109 0.11 0.043])
-        set(handles.txtDiffuser,'Position',[0.693 .165 0.082 0.029])
+        set(handles.txtBlurSD, 'Position', [0.8693 .117 0.089 0.029])
+        set(handles.editDiffuserBlur, 'Position', [0.82 0.115 0.038 0.033])
+        set(handles.popDiffuser, 'Position', [0.693 0.109 0.11 0.043])
+        set(handles.txtDiffuser, 'Position', [0.693 .165 0.082 0.029])
         
-        val = oiGet(oi,'diffuserBlur','um');
+        val = oiGet(oi, 'diffuserBlur', 'um');
         if isempty(val)
-            val = ieReadNumber('Enter blur sd (FWHM, um)',2,' %.2f');
-            oi = oiSet(oi,'diffuserBlur',val*10^-6);
+            val = ieReadNumber('Enter blur sd (FWHM, um)', 2, ' %.2f');
+            oi = oiSet(oi, 'diffuserBlur', val*10^-6);
             vcReplaceObject(oi);
         end
-        set(handles.editDiffuserBlur,'String',num2str(val));
+        set(handles.editDiffuserBlur, 'String', num2str(val));
+
     case 'birefringent'
-        set(handles.popDiffuser,'val',3)
-        set(handles.popDiffuser,'Position',[0.756 0.109 0.11 0.043])
-        set(handles.txtDiffuser,'Position',[0.756 .165 0.082 0.029])
-        
+        set(handles.popDiffuser, 'val', 3)
+        set(handles.popDiffuser, 'Position', [0.756 0.109 0.11 0.043])
+        set(handles.txtDiffuser, 'Position', [0.756 .165 0.082 0.029])
+
         % For now off, but if we decide to allow the value to change, then
         % we can use the buttons like this.
-        set(handles.editDiffuserBlur,'visible','off');
-        set(handles.txtBlurSD,'visible','off');
-        set(handles.txtBlurSD,'string','Disp (um)');
-        set(handles.txtBlurSD,'TooltipString','Birefringent displacement in microns');
+        set(handles.editDiffuserBlur, 'visible', 'off');
+        set(handles.txtBlurSD, 'visible', 'off');
+        set(handles.txtBlurSD, 'string', 'Disp (um)');
+        set(handles.txtBlurSD, 'TooltipString', 'Birefringent displacement in microns');
     otherwise
-        error('Unknown diffuser method %s\n',dMethod);
+        error('Unknown diffuser method %s\n', dMethod);
 end
 
 % If the incoming call set consistency true, then we eliminate the red
 % square on the window.  Otherwise, consistency is false.  We always set it
 % to false on the way out.
-if oiGet(oi,'consistency')
-    set(handles.txtConsistency,'BackgroundColor',[0.831373 0.815686 0.784314]);
-    oi = oiSet(oi,'consistency',0);
+if oiGet(oi, 'consistency')
+    set(handles.txtConsistency, 'BackgroundColor', [0.831373 0.815686 0.784314]);
+    oi = oiSet(oi, 'consistency', 0);
     vcReplaceObject(oi);
 else
-    set(handles.txtConsistency,'BackgroundColor',[1,0,0]);
+    set(handles.txtConsistency, 'BackgroundColor', [1, 0, 0]);
 end
 
-gam = str2double(get(handles.editGamma,'String'));
+gam = str2double(get(handles.editGamma, 'String'));
 
 % Select the figure
 % figure(ieSessionGet('oi figure'));
@@ -127,14 +143,14 @@ figure(figNum);
 % For NIR, SWIR and so forth we might use a different displayFlag value.
 % See oiShowImage.  In the future, we will read the displayFlag from
 % either a global or a setting in the oi GUI.
-displayFlag = get(handles.popupDisplay,'Value');
-oiShowImage(oi,displayFlag,gam);
-set(handles.txtOpticalImage,'String',oiDescription(oi));
+displayFlag = get(handles.popupDisplay, 'Value');
+oiShowImage(oi, displayFlag, gam);
+set(handles.txtOpticalImage, 'String', oiDescription(oi));
 
 end
 
 %------------------------------------------------------
-function switchControlVisibility(handles,state)
+function switchControlVisibility(handles, state)
 %Turn on/off the diffraction limited buttons and edit fields
 %On turns on the diffraction.
 %Off turns off the diffraction and puts up the custom popup menu
@@ -142,26 +158,26 @@ function switchControlVisibility(handles,state)
 
 switch state
     case 'on'
-        set(handles.txtFocalLength,'visible','on')
-        set(handles.txtFnumber,'visible','on')
-        set(handles.editFocalLength,'visible','on')
-        set(handles.editFnumber,'visible','on')
-        set(handles.txtM,'visible','on')
-        % set(handles.btnOffAxis,'visible','on');
-        % set(handles.txtDiffractionLimitedOptics,'visible','on');
-        set(handles.editDiffuserBlur,'visible','on');
-        set(handles.txtBlurSD,'visible','on');
+        set(handles.txtFocalLength, 'visible', 'on')
+        set(handles.txtFnumber, 'visible', 'on')
+        set(handles.editFocalLength, 'visible', 'on')
+        set(handles.editFnumber, 'visible', 'on')
+        set(handles.txtM, 'visible', 'on')
+        % set(handles.btnOffAxis, 'visible', 'on');
+        % set(handles.txtDiffractionLimitedOptics, 'visible', 'on');
+        set(handles.editDiffuserBlur, 'visible', 'on');
+        set(handles.txtBlurSD, 'visible', 'on');
         
     case 'off'
-        set(handles.txtFocalLength,'visible','off')
-        set(handles.txtFnumber,'visible','off')
-        set(handles.editFocalLength,'visible','off')
-        set(handles.editFnumber,'visible','off')
-        set(handles.txtM,'visible','off')
-        % set(handles.btnOffAxis,'visible','off');
-        % set(handles.txtDiffractionLimitedOptics,'visible','off');
-        set(handles.editDiffuserBlur,'visible','off');
-        set(handles.txtBlurSD,'visible','off');
+        set(handles.txtFocalLength, 'visible', 'off')
+        set(handles.txtFnumber, 'visible', 'off')
+        set(handles.editFocalLength, 'visible', 'off')
+        set(handles.editFnumber, 'visible', 'off')
+        set(handles.txtM, 'visible', 'off')
+        % set(handles.btnOffAxis, 'visible', 'off');
+        % set(handles.txtDiffractionLimitedOptics, 'visible', 'off');
+        set(handles.editDiffuserBlur, 'visible', 'off');
+        set(handles.txtBlurSD, 'visible', 'off');
         
     otherwise
         error('Unknown state.');
