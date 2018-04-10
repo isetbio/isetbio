8d1c356ce37c9d7ae2f27bf532dcaafaac1729b5
elinekupers <elinekupers@gmail.com>
2018-04-10 19:43:54 -0400

    implement higher order aberrations of Jaeken and Artal 2012 paper

--- data/datafiles/human/Contents.m
+++ data/datafiles/human/Contents.m
@@ -3,3 +3,4 @@
 % Files containing data pertaining to humans
 %
 % cones                         - Data relevant to cones and the cone mosaic
+% wvf                           - Data relevant to wavefront and optical quality
---
+++ data/datafiles/human/wvf/Contents.m
@@ -0,0 +1,8 @@
+% isetbio/data/datafiles/human/wvf
+%
+% File containing data pertaining to human optical quality (zernike
+% coefficients (Z1-Z14), for 130 patients, L/R eye, between -40:40 deg
+% eccentricity)
+%
+% zCoeffsJaekenArtal2012        - Zernike Coefficients. Digitized from Figure 1 of Jaeken & Artal (2012).  See wvfLoadJaekenArtal2012Data.
+
