aafbb2cb73da23e5aa201fc34b4cdfc02be3efe3
Nicolas Cottaris <cottaris@sas.upenn.edu>
2018-06-20 12:14:29 -0400

    Finalized tutorial and added mosaic.mat
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- tutorials/t_cones/t_conesMosaicAbsorptionsEccBased.m
+++ tutorials/t_cones/t_conesMosaicAbsorptionsEccBased.m
@@ -1,22 +1,21 @@
-%%t_coneMosaicAbsorptionsEccBased  Ecc-based cone efficiency correction
+%%t_conesMosaicAbsorptionsEccBased  Ecc-based cone efficiency correction
 %
 % Description:
 %    Demonstrates the effect of applying eccentricity-based cone efficiency
-%    correction to a static stimulus. This demo includes eye movements.
+%    correction to a static (e.g., not modulated over time) stimulus. 
+%    This demo includes eye movements.
 %
 % 
 % NPC, ISETBIO Team, 2018
 %
 % 06/16/18  npc  Wrote it.
 
+function t_conesMosaicAbsorptionsEccBased
 %% Initialize
-ieInit; clear all;
+ieInit;
 
-%% Build a simple scene and oi (retinal image) for computing
-%
-% Make a gabor scene
+%% Build a Gabor scene
 stimFOV = 0.4;
-mosaicFOV = 0.6;
 parms.freq = 5;
 parms.contrast = 1.0;
 parms.ph = 0;
@@ -27,19 +26,16 @@ parms.col = 128;
 [scene, parms] = sceneCreate('harmonic', parms);
 scene = sceneSet(scene, 'fov', stimFOV);
 
-% Then the oi
-oi = oiCreate;
-oi = oiCompute(oi,scene);
-
-%% Build a default cone mosaic and compute isomerizatoins
+%% Build a cone mosaic or load existing one
 loadPreviouslySavedMosaic = true;
+
 mosaicFileName = sprintf('mosaic.mat');
 if (~loadPreviouslySavedMosaic)
+    mosaicFOV = 0.6;
     cMosaic  = coneMosaicHex(9, ...
             'name', 'test', ...
             'fovDegs', mosaicFOV, ...
             'eccBasedConeDensity', true, ...
-            'eccBasedConeQuantalEfficiency', ~true, ...
             'sConeMinDistanceFactor', 2.0, ... 
             'sConeFreeRadiusMicrons', 0.15*300, ...                   
             'spatialDensity', [0 6/10 3/10 1/10], ...
@@ -51,182 +47,109 @@ if (~loadPreviouslySavedMosaic)
     save(mosaicFileName, 'cMosaic', '-v7.3');
 else
     load(mosaicFileName, 'cMosaic');
+    mosaicFOV = max(cMosaic.fov);
 end
 
-% Add photon noise
-cMosaic.noiseFlag = 'none'; % 'random';
+%% No photon noise because we want to see the mean isomerizations
+cMosaic.noiseFlag = 'none';
 
-% Set the desired integration time, here 5 milliseconds
-cMosaic.integrationTime = 5/1000;
+%% 1 msec integration time
+cMosaic.integrationTime = 1/1000;
 
-% No correction for ecc-dependent change in cone efficiency
-cMosaic.eccBasedConeQuantalEfficiency = ~true;
+%% No correction for ecc-dependent change in cone efficiency
+cMosaic.eccBasedConeQuantalEfficiency = false;
 
-% Make another mosaic, identical to the first one, but with ecc-based
-% cone efficiency corrections
+%% Make an identical copy of first mosaic
 cMosaic2 = cMosaic.copy();
+%% but with ecc-based cone efficiency corrections
 cMosaic2.eccBasedConeQuantalEfficiency = true;
 
-%% Generate two em sequences each with  40 eye posistions.
-eyeMovementsNum = 40;
-cMosaic.emGenSequence(eyeMovementsNum, 'nTrials', 2);
+%% Generate two trials of eye movements, each with 100 eye posistions, 
+%% simulating a 100 msec trial
+eyeMovementsNum = 100; nTrials = 2;
+cMosaic.emGenSequence(eyeMovementsNum, 'nTrials', nTrials);
+
+%% Save the eye movement paths so we can apply it to second mosaic
 theEmPositions = cMosaic.emPositions;
 
-subplotPosVectors = NicePlot.getSubPlotPosVectors(...
-       'rowsNum', 2, ...
-       'colsNum', 3, ...
-       'heightMargin',  0.03, ...
-       'widthMargin',    0.03, ...
-       'leftMargin',     0.02, ...
-       'rightMargin',    0.01, ...
-       'bottomMargin',   0.03, ...
-       'topMargin',      0.02);
+%% Compute the optical image for our static scene 
+oi = oiCreate;
+oi = oiCompute(oi,scene);
 
-% Generate figure
-hFig = figure(1); clf;
-set(hFig, 'Position', [10 10 1295 792], 'Color', [1 1 1]); 
-
-generateVideo = true;
-if (generateVideo)
-   writerObj = VideoWriter('video.m4v', 'MPEG-4'); % H264 format
-   writerObj.FrameRate = 30; 
-   writerObj.Quality = 100;
-   % Open video stream
-   open(writerObj);
-end
-
-% Plot the scene
-subplot('Position', subplotPosVectors(2,3).v);
-xspace = (1:sceneGet(scene, 'cols'));
-xspace = xspace - mean(xspace);
-xspace = xspace/max(xspace) * stimFOV/2;
-yspace = (1:sceneGet(scene, 'rows'));
-yspace = yspace - mean(yspace);
-yspace = yspace/max(yspace) * stimFOV/2;
-luminanceMap = sceneGet(scene, 'luminance');
-
-meanLum = sceneGet(scene, 'mean luminance');
-imagesc(xspace, yspace, 0.5 + 0.5*(luminanceMap - meanLum)/meanLum);
-set(gca, 'CLim', [0 1]);
-axis('image');
-hold on;
-plot([xspace(1) xspace(end)], [0 0], 'g-', 'LineWidth', 1.5);
-plot([0 0],[yspace(1) yspace(end)], 'g-', 'LineWidth', 1.5);
-hold off;        
-
-xlabel('space (degs)');
-lut = gray(1024);
-colormap(gca,lut);
-set(gca, 'XTick', stimFOV/2*[-1 0 1], 'YTick', stimFOV/2*[-1 0 1],  ...
-    'XLim', stimFOV/2*[-1 1],  'YLim', stimFOV/2*[-1 1], 'YTickLabels', {}, ...
-    'FontSize', 12, 'Color', squeeze(lut(512,:)));
-hC = colorbar();
-hC.TickLabels = {};
-title('scene luminance');
-    
-% Plot the two mosaics
-ax = subplot('Position', subplotPosVectors(1,1).v);
-cMosaic.visualizeGrid(...
-    'axesHandle', ax, ...
-    'visualizedConeAperture', 'lightCollectingArea', ...
-    'apertureShape', 'disks', ...
-    'overlayHexMesh', true);
-set(ax, 'XLim', stimFOV/2*[-1 1]*cMosaic2.micronsPerDegree*1e-6, ...
-     'YLim', stimFOV/2*[-1 1]*cMosaic2.micronsPerDegree*1e-6, ...
-     'XTick', [], 'YTick', [], 'FontSize', 12);
-title(ax,'mosaic with constant cone efficiency');
-colormap(ax, gray(1024));
-hC = colorbar(ax);
-hC.Ticks = [];
-
-ax2 = subplot('Position', subplotPosVectors(2,1).v);
-cMosaic2.visualizeGrid(...
-    'axesHandle', ax2, ...
-    'visualizedConeAperture', 'lightCollectingArea', ...
-    'apertureShape', 'disks', ...
-    'overlayHexMesh', true);
-set(ax2, 'XLim', stimFOV/2*[-1 1]*cMosaic2.micronsPerDegree*1e-6, ...
-     'YLim', stimFOV/2*[-1 1]*cMosaic2.micronsPerDegree*1e-6, ...
-     'XTick', [], 'YTick', [], 'FontSize', 12);
-colormap(ax2, gray(1024));
-hC = colorbar(ax2);
-hC.Ticks = [];
-title(ax2,'mosaic with ecc-based cone efficiency');
-    
+%% Apply the emPositions to both mosaics
 cMosaic.emPositions = theEmPositions;
 cMosaic2.emPositions = theEmPositions;
-    
-%% Compute isomerizations
+
+%% Compute isomerizations in both mosaics
 absorptions1 = cMosaic.compute(oi);
 absorptions2 = cMosaic2.compute(oi);
-    
-[~,idx] = max(absorptions1(:));
-[~,maxConeRow,maxConeCol,timeBinOfMaxActivation] = ind2sub(size(absorptions2), idx);
-    
-signalRange = [min(absorptions2(:)) max(absorptions2(:))];
 
-% Plot the time course of the responses
-for visualizedTrialIndex = 1:2
+%% Extract response time axis
+timeAxisMsec = 1000*cMosaic.timeAxis;
 
-    % Plot mean activation during the course of the emPath
-    for k = 1:eyeMovementsNum
+%% Extract absorptions range across both mosaics
+signalRange = [...
+    min([min(absorptions1(:)) min(absorptions2(:))])...
+    max([max(absorptions1(:)) max(absorptions2(:))])...
+];
+
+%% Init figure and video
+generateVideo = true;
+[hFig, subplotPosVectors, writerObj] = initFigure(generateVideo);
+
+%% Plot the scene/optical image
+visualizedFOV = mosaicFOV;
+%% Plot the scene
+%plotScene(scene, subplotPosVectors, [2 3], visualizedFOV);
+
+%% or plot the optical image
+plotOpticalImage(oi, subplotPosVectors, [2 3], visualizedFOV);
+
+%% Plot the two mosaics
+plotMosaic(cMosaic,  subplotPosVectors, [1 1], visualizedFOV, 'mosaic with constant cone efficiency');
+plotMosaic(cMosaic2, subplotPosVectors, [2 1], visualizedFOV, 'mosaic with ecc-based cone efficiency');
+  
+%% Generate LUTs to represent mosaic activation
+activationLUT = brewermap(1024, '*greys');
+diffActivationLUT = brewermap(1024, '*RdBu');
+
+%% Plot each time bin of the two mosaic activations (and their difference)
+for visualizedTrialIndex = 1:size(theEmPositions,1)
+    for timeBin = 1:size(theEmPositions,2)
+        %% Extract the activation patterns for current trial and time bin
+        visualizedActivationPattern1 = squeeze(absorptions1(visualizedTrialIndex,:,:, timeBin));
+        visualizedActivationPattern2 = squeeze(absorptions2(visualizedTrialIndex,:,:, timeBin));
+        differentialActivationPattern = visualizedActivationPattern2-visualizedActivationPattern1;
         
-        % Extract the activation patterns for current trial and time bin
-        visualizedActivationPattern1 = squeeze(absorptions1(visualizedTrialIndex,:,:, k));
-        visualizedActivationPattern2 = squeeze(absorptions2(visualizedTrialIndex,:,:, k));
-        residual = visualizedActivationPattern2-visualizedActivationPattern1;
+        %% Plot activation of first mosaic
+        plotMosaicActivation(cMosaic, visualizedActivationPattern1, ...
+            signalRange, theEmPositions, visualizedTrialIndex, timeBin, ...
+            activationLUT, [0 0 0], visualizedFOV, subplotPosVectors, [1 2], ...
+            'absorptions in constant cone efficiency mosaic', ...
+            'displaylabelX', false, 'displaylabelY', false, ...
+            'displayTicksX', false, 'displayTicksY', false);
+
+        %% Plot activation of second mosaic
+        plotMosaicActivation(cMosaic2, visualizedActivationPattern2, ...
+            signalRange, theEmPositions, visualizedTrialIndex, timeBin, ...
+            activationLUT, [0 0 0], visualizedFOV, subplotPosVectors, [2 2], ...
+            'absorptions in ecc-based cone efficiency mosaic', ...
+            'displaylabelY', false, 'displayTicksY', false);
+
+        %% Plot the difference in absorptions b/n the 2 mosaics
+        plotMosaicActivation(cMosaic2, differentialActivationPattern, ...
+            round(max(signalRange)/1.8)*[-1 1], theEmPositions, ...
+            visualizedTrialIndex, timeBin, diffActivationLUT, [1 1 1], ...
+            visualizedFOV, subplotPosVectors, [1 3], ...
+            sprintf('absorptions difference (trial:%d, t:%2.1f ms)', ...
+            visualizedTrialIndex, timeAxisMsec(timeBin)), ...
+            'displaylabelX', false, 'displayTicksX', false, ...
+            'displaylabelY', false, 'displayTicksY', false);
         
-        % Plot activation of first mosaic
-        subplot('Position', subplotPosVectors(1,2).v);
-        cMosaic.renderActivationMap(gca, visualizedActivationPattern1, ...
-             'signalRange', signalRange, ...
-             'visualizedConeAperture', 'geometricArea', ...
-             'mapType', 'modulated disks', ...
-             'showColorBar', true, ...
-             'labelColorBarTicks', true, ...
-             'titleForColorBar', 'R*/cone/5msec', ...
-             'colorMap', brewermap(1024, '*greys'), ...
-             'crossHairPosition', squeeze(theEmPositions(visualizedTrialIndex,k,:))*cMosaic2.patternSampleSize(1), ...
-             'visualizedFOV', mosaicFOV, ...
-             'backgroundColor', [0 0 0]);
-        xlabel(''); ylabel('');  set(gca, 'XTickLabels', {}, 'YTickLabels', {});
-        title('absorptions in mosaic with constant cone efficiency');
-
-        % Plot activation of second mosaic
-        subplot('Position', subplotPosVectors(2,2).v);
-        cMosaic2.renderActivationMap(gca, visualizedActivationPattern2, ...
-             'signalRange', signalRange, ...
-             'visualizedConeAperture', 'geometricArea', ...
-             'mapType', 'modulated disks', ...
-             'showColorBar', true, ...
-             'labelColorBarTicks', true, ...
-             'titleForColorBar', 'R*/cone/5msec', ...
-             'colorMap', brewermap(1024, '*greys'), ...
-             'crossHairPosition', squeeze(theEmPositions(visualizedTrialIndex,k,:))*cMosaic2.patternSampleSize(1), ...
-             'visualizedFOV', mosaicFOV, ...
-             'backgroundColor', [0 0 0]);
-        ylabel(''); set(gca, 'YTickLabels', {});
-        title('absorptions in mosaic with ecc-based cone efficiency');
-
-        % Plot the difference in absorptions b/n the 2 mosaics
-        subplot('Position', subplotPosVectors(1,3).v);
-
-        cMosaic2.renderActivationMap(gca, residual, ...
-             'signalRange', 50*[-1 1], ...
-             'visualizedConeAperture', 'geometricArea', ...
-             'mapType', 'modulated disks', ...
-             'showColorBar', true, ...
-             'labelColorBarTicks', true, ...
-             'titleForColorBar', 'R*/cone/5msec', ...
-             'crossHairPosition', squeeze(theEmPositions(visualizedTrialIndex,k,:))*cMosaic2.patternSampleSize(1), ...
-             'visualizedFOV', mosaicFOV, ...
-             'colorMap', brewermap(1024, '*RdBu'), ...
-             'backgroundColor', [1 1 1]);
-        ylabel(''); xlabel('');  set(gca, 'YTickLabels', {});
-        title('absorptions difference');
-    
+        %% Render frame
         drawnow;
         if (generateVideo)
+            %% Add frame to video stream
             frame = getframe(gcf);
             writeVideo(writerObj, frame);
         end
@@ -234,6 +157,176 @@ for visualizedTrialIndex = 1:2
 end % visualizedTrialIndex
 
 if (generateVideo)
+    %% Close video stream
     close(writerObj);
 end
+end
+
+% ---------------- SUPPORT FUNCTIONS --------------------
+function [hFig, subplotPosVectors, writerObj] = initFigure(generateVideo)
+    subplotPosVectors = NicePlot.getSubPlotPosVectors(...
+       'rowsNum', 2, ...
+       'colsNum', 3, ...
+       'heightMargin',  0.03, ...
+       'widthMargin',    0.03, ...
+       'leftMargin',     0.01, ...
+       'rightMargin',    0.03, ...
+       'bottomMargin',   0.04, ...
+       'topMargin',      0.01);
+
+    % Generate figure
+    hFig = figure(1); clf;
+    set(hFig, 'Position', [10 10 1295 792], 'Color', [1 1 1]); 
+
+
+    if (generateVideo)
+       writerObj = VideoWriter('video.m4v', 'MPEG-4'); % H264 format
+       writerObj.FrameRate = 30; 
+       writerObj.Quality = 100;
+       % Open video stream
+       open(writerObj);
+    else
+        writerObj = [];
+    end
+end
+
+function plotScene(scene, subplotPosVectors, subplotPos, visualizedFOV)
+    stimFOV =  sceneGet(scene, 'fov');     
+    % Plot the scene
+    ax = subplot('Position', subplotPosVectors(subplotPos(1),subplotPos(2)).v);
+    xspace = (1:sceneGet(scene, 'cols'));
+    xspace = xspace - mean(xspace);
+    xspace = xspace/max(xspace) * stimFOV/2;
+    yspace = (1:sceneGet(scene, 'rows'));
+    yspace = yspace - mean(yspace);
+    yspace = yspace/max(yspace) * stimFOV/2;
+    luminanceMap = sceneGet(scene, 'luminance');
     
+    maxLuminance = ceil(max(luminanceMap(:)));
+    minLuminance = 0;
+    
+    imagesc(ax, xspace, yspace, (luminanceMap - minLuminance)/(maxLuminance-minLuminance));
+    set(gca, 'CLim', [0 1]);
+    axis('image');
+    hold on;
+    plot([xspace(1) xspace(end)], [0 0], 'g-', 'LineWidth', 1.5);
+    plot([0 0],[yspace(1) yspace(end)], 'g-', 'LineWidth', 1.5);
+    hold off;        
+
+    xlabel(ax,'space (degs)');
+    lut = gray(1024);
+    colormap(ax,lut);
+    set(ax, 'XTick', visualizedFOV/2*[-1 0 1], 'YTick', visualizedFOV/2*[-1 0 1],  ...
+        'XLim', visualizedFOV/2*[-1 1],  'YLim', visualizedFOV/2*[-1 1], 'YTickLabels', {}, ...
+        'FontSize', 14, 'Color', [0 0 0]);
+    xtickformat(ax, '%0.1f');
+    
+    lumTicks = 0:0.1:1.0;
+    hC = colorbar();
+    hC.Ticks = lumTicks; 
+    hC.TickLabels = sprintf('%2.1f\n',(lumTicks * (maxLuminance - minLuminance) + minLuminance));
+    hC.Label.String = 'luminance (cd/m2)';
+    title(ax,'scene luminance');
+end
+
+function plotOpticalImage(oi, subplotPosVectors, subplotPos, visualizedFOV)
+    stimFOV =  oiGet(oi, 'fov');     
+    % Plot the scene
+    ax = subplot('Position', subplotPosVectors(subplotPos(1),subplotPos(2)).v);
+    xspace = (1:oiGet(oi, 'cols'));
+    xspace = xspace - mean(xspace);
+    xspace = xspace/max(xspace) * stimFOV/2;
+    yspace = (1:oiGet(oi, 'rows'));
+    yspace = yspace - mean(yspace);
+    yspace = yspace/max(yspace) * stimFOV/2;
+    
+    % Get the retinal photons map
+    retinalPhotonsMap = oiGet(oi, 'photons');
+   
+    % Get the retinal photons map at 550 nm
+    wave = oiGet(oi, 'wave');
+    targetWave = 550;
+    [~,idx] = min(abs(wave-targetWave));
+    targetWave = wave(idx);
+    retinalPhotonsMap550nm = retinalPhotonsMap(:,:,idx);
+
+    maxPhotons = ceil(max(retinalPhotonsMap550nm(:)));
+    minPhotons = 0;
+    
+    imagesc(ax, xspace, yspace, (retinalPhotonsMap550nm - minPhotons)/(maxPhotons-minPhotons));
+    set(ax, 'CLim', [0 1]);
+    axis('image');
+    hold on;
+    plot([xspace(1) xspace(end)], [0 0], 'g-', 'LineWidth', 1.5);
+    plot([0 0],[yspace(1) yspace(end)], 'g-', 'LineWidth', 1.5);
+    hold off;        
+
+    xlabel(ax, 'space (degs)');
+    
+    lut = gray(1024);
+    colormap(gca,lut);
+    set(ax, 'XTick', visualizedFOV/2*[-1 0 1], 'YTick', visualizedFOV/2*[-1 0 1],  ...
+        'XLim', visualizedFOV/2*[-1 1],  'YLim', visualizedFOV/2*[-1 1], 'YTickLabels', {}, ...
+        'FontSize', 14, 'Color', [0 0 0]);
+    xtickformat(ax,'%0.1f');
+    
+    photonsTicks = 0:0.1:1.0;
+    scaleFactor = 1e9;
+    scaleFactorSting = '1E9';
+    hC = colorbar();
+    hC.Ticks = photonsTicks; 
+    hC.TickLabels = sprintf('%2.1f\n',(photonsTicks/1e3 * (maxPhotons - minPhotons) + minPhotons)/scaleFactor);
+    hC.Label.String = sprintf('photon rate (photons / msec) x %s', scaleFactorSting);
+    
+    title(ax, sprintf('retinal photon rate @%2.0f nm', targetWave));
+end
+
+
+function plotMosaic(cMosaic, subplotPosVectors, subplotPos, visualizedFOV, titleText)
+    % Plot the mosaic
+    ax = subplot('Position', subplotPosVectors(subplotPos(1),subplotPos(2)).v);
+    cMosaic.visualizeGrid(...
+        'axesHandle', ax, ...
+        'visualizedConeAperture', 'lightCollectingArea', ...
+        'apertureShape', 'disks', ...
+        'overlayHexMesh', true);
+    set(ax, 'XLim', visualizedFOV/2*[-1 1]*cMosaic.micronsPerDegree*1e-6, ...
+         'YLim', visualizedFOV/2*[-1 1]*cMosaic.micronsPerDegree*1e-6, ...
+         'XTick', [], 'YTick', [], 'FontSize', 14);
+    title(ax,titleText);
+    colormap(ax, gray(1024));
+    hC = colorbar(ax);
+    hC.Ticks = [];
+end
+
+function plotMosaicActivation(cMosaic, visualizedActivationPattern, ...
+            signalRange, theEmPositions, visualizedTrialIndex, timeBin, ...
+            activationLUT, backgroundColor, visualizedFOV, subplotPosVectors, subplotPos, ...
+            titleText, varargin)
+
+    p = inputParser;
+    p.addParameter('displaylabelX',true, @islogical);   
+    p.addParameter('displaylabelY',true, @islogical); 
+    p.addParameter('displayTicksX',true, @islogical); 
+    p.addParameter('displayTicksY',true, @islogical); 
+    p.parse(varargin{:});
+
+    ax = subplot('Position', subplotPosVectors(subplotPos(1),subplotPos(2)).v);
+    cMosaic.renderActivationMap(ax, visualizedActivationPattern, ...
+             'signalRange', signalRange, ...
+             'visualizedConeAperture', 'geometricArea', ...
+             'mapType', 'modulated disks', ...
+             'showColorBar', true, ...
+             'labelColorBarTicks', true, ...
+             'titleForColorBar', sprintf('R* / cone /%2.0fmsec', 1000*cMosaic.integrationTime), ...
+             'colorMap', activationLUT, ...
+             'crossHairPosition', squeeze(theEmPositions(visualizedTrialIndex,timeBin,:))*cMosaic.patternSampleSize(1), ...
+             'visualizedFOV', visualizedFOV, ...
+             'backgroundColor', backgroundColor);
+    set(ax, 'FontSize', 14);     
+    if (~p.Results.displaylabelX); xlabel(ax, ''); end
+    if (~p.Results.displaylabelY); ylabel(ax, ''); end
+    if (~p.Results.displayTicksX); set(ax, 'XTickLabels', {}); end
+    if (~p.Results.displayTicksY); set(ax, 'YTickLabels', {}); end
+    title(ax, titleText);
+end
