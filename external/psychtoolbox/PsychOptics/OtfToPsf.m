5cdcc32c80b54800698ef96c817fdc84d6f26d30
David Brainard <brainard@psych.upenn.edu>
2018-10-17 16:55:17 -0400

    Updating PTB and BLTB routines

--- external/psychtoolbox/PsychOptics/OtfToPsf.m
+++ external/psychtoolbox/PsychOptics/OtfToPsf.m
@@ -70,12 +70,11 @@ function [xGridMinutes,yGridMinutes,psf] = OtfToPsf(xSfGridCyclesDeg,ySfGridCycl
 %   03/31/18  dhb   Document key/value pair added by someone else.
 %             dhb   Add key/value pair for negative value tolerance.
 %                   This is now 1e-3 rather than 1e-10
-%   10/08/18  dhb   Increase fractional tolerance for neg values to 1% of max.
 
 %% Parse input
 p = inputParser;
 p.addParameter('warningInsteadOfErrorForNegativeValuedPSF', 0, @isnumeric);
-p.addParameter('negativeFractionalTolerance', 1e-2, @isnumeric);
+p.addParameter('negativeFractionalTolerance', 1e-3, @isnumeric);
 p.parse(varargin{:});
 
 %% Handle sf args and converstion
