7ce865aa6b41a3e9177d099c4acf500529f45d4d
Nicolas Cottaris <nicolas@manta.PSYCH.UPENN.EDU>
2018-10-18 16:36:59 -0400

    testing Jenkins

--- isettools/opticalimage/optics/opticsCreate.m
+++ isettools/opticalimage/optics/opticsCreate.m
@@ -317,13 +317,13 @@ wave = opticsGet(optics, 'wave');
 
 % Decide whether to use the legacy frequency support (Nyquist:  60 c/deg)
 % or 120 c/deg, which results in a more focused PSF
-legacyFrequencySupport = true;
+legacyFrequencySupport = ~true;
 if (legacyFrequencySupport)
     % Fsupport used to be [], which defaults to 60 c/deg
     fSupport = [];
 else
     % Up to 120 c/deg, with 240 samples to get better PSF
-    maxF =120;
+    maxF = 120;
     N = 240;
     fList = unitFrequencyList(N);
     fList = fList * maxF;
@@ -334,22 +334,6 @@ end
    
 % The human optics are an SI case, and we store the OTF at this point. 
 [OTF2D, fSupport] = humanOTF(pupilRadius, dioptricPower, fSupport, wave);
-% X = squeeze(fSupport(:, :, 1));
-% Y = squeeze(fSupport(:,:,2));
-% idx = find((X(:) == 0) & (Y(:) == 0))
-% [row,col] = ind2sub(size(X), idx)
-% for k = 1:31
-%     OTFatBand = fftshift(squeeze(OTF2D(:,:,k)));
-%     figure(223); clf;
-%     imagesc(X(row,:),Y(:,col),OTFatBand)
-%     axis 'image'
-%     OTFAtZero = [squeeze(OTFatBand(row,col)) OTF2D(1,1,k)]
-%     pause
-% end
-% 
-% size(fSupport)
-% size(OTF2D)
-% pause
 
 optics = opticsSet(optics, 'otfData', OTF2D);
 umPerDegreeForSupport = umPerDegree;
