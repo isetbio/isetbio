b791896e2b643942d36ba93b04ec48959381ac6c
jenmaxwell <jen.maxwell@oculus.com>
2018-10-19 10:26:36 -0700

    Tutorials Pass I Part VII: OI Sequences
    Tutorials Pass I Part VII
    
    This check-in covers oisequences, and the creation of their Contents.m file.
    
    This set covers the following sub-directories of tutorials:
        tutorials\t_oisequences
    
    Cosmetic/Formatting adjustments only:
        t_oisHarmonic
    
    Notes/Other:
        General
            Added missing oisequences Contents file!
        t_oisCreate
            Primarily formatting adjustments
            Assign someone to complete TODO associated with BW's note
            Review JNM note questioning figure impermanence.
        t_oisequence
            Review JNM note questioning figure impermanence.
            Determine if(when) we want to add a TODO for the additional cases mentioned in the first note.

---
+++ tutorials/t_oisequences/Contents.m
@@ -0,0 +1,9 @@
+% isetbio/tutorials/t_oisequences
+%
+% Tutorials detailing use of oiSequence functions.
+%
+% Files
+%   t_oisCreate   - Script demonstrating how to use the oiSequences function.
+%   t_oiSequence  - Demonstrate the oiSequence class.
+%   t_oisHarmonic - Generate a sinusoid oiSequence with contrast ramping by Gaussian tmf.
+%
