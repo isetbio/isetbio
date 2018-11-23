285ba6cd8d717e6208595e4e6b590ae68201797a
Jennifer Maxwell <32528018+jenmaxwell@users.noreply.github.com>
2018-11-23 11:06:43 -0800

    Tutorials Pass I Part VIII
    Tutorials Pass I Part VIII
    
    This check-in covers
    
    This set covers the following sub-directories of tutorials:
    tutorials\t_code
    tutorials\t_data
    
    Syntax & Formatting only:
    	t_code\t_codeWindowAppearance
    	t_code\t_codeSESSION
    	t_code\t_codeRendering
    	t_code\t_codeImageMultiview
    	t_code\t_codeFFTinMatlab
    	t_data\t_rgcWatson
    	t_data\t_dataDaylights
    
    Structural/Major:
    	Created contents files
    		t_code\Contents
    		t_data\Contents
    
    Notes/Other:
    	t_code\t_codeISETPref
    		Note from file: [Note: JNM - This script was listed as broken due to a missing script, however, it appears to execute?]

--- tutorials/t_data/t_rgcWatson.m
+++ tutorials/t_data/t_rgcWatson.m
@@ -1,44 +1,66 @@
-%% Watson RGC Formula - 2014
+% Watson RGC Formula - 2014
 %
-% Retinal ganglion cell density estimates from 
+% Description:
+%    The retinal ganglion cell density estimates from 
+%       Watson 2014: A formula for human retinal ganglion cell receptive
+%       field density as a function of visual field location  
+%       http://jov.arvojournals.org/article.aspx?articleid=2279458
 %
-%  Watson 2014
-%  A formula for human retinal ganglion cell receptive field density as a
-%  function of visual field location  
-%  http://jov.arvojournals.org/article.aspx?articleid=2279458
+% See Also:
+%    t_rgcEccData
 %
-% See also:  t_rgcEccData
-%
-% JRG/BW ISETBIO Team, 2017
+
+% History:
+%    XX/XX/17  JRG/BW  ISETBIO Team, 2017
+%    11/22/18  JNM     Formatting
 
 %% Get lookup table for how large RF is 
 % There is an asymmetry in the size of RGC RFs over the retina
-szCols = 128; fovRows = 90; fovCols = 90; scaleFactor = 1;
+szCols = 128;
+fovRows = 90;
+fovCols = 90;
+scaleFactor = 1;
 cellType = 'Midget';
 
 % Top page 7, right column: "Typically we are concerned with the spacing
 % within one class, in which case density is halved and the spacings should
 % be multiplied by sqrt(2)."
-% rgcDiameterLUT = scaleFactor*sqrt(2)*watsonRGCSpacing(szCols,szCols,fovRows)';
-% [rgcDiameterLUT, radDeg, rgc1d] = watsonRGCSpacing(szCols,szCols,fovRows);
+% rgcDiameterLUT = scaleFactor * sqrt(2) ...
+%    * watsonRGCSpacing(szCols, szCols, fovRows)';
+% [rgcDiameterLUT, radDeg, rgc1d] = ...
+%    watsonRGCSpacing(szCols, szCols, fovRows);
 [rgcDiameterLUT, radDeg, rgc1d] = watsonRGCSpacing(fovRows);
 
 %% Plots
-arcMinPerDegree = 60; convertDensityFactor = sqrt(2);
-vcNewGraphWin; 
-cind = 'rbgk'; hold on;
+arcMinPerDegree = 60;
+convertDensityFactor = sqrt(2);
+vcNewGraphWin;
+cind = 'rbgk';
+hold on;
 for k = 1:4
-    plot(radDeg,convertDensityFactor*arcMinPerDegree*rgc1d(k,:),cind(k),'linewidth',2);
+    plot(radDeg, convertDensityFactor * arcMinPerDegree * rgc1d(k, :), ...
+        cind(k), 'linewidth', 2);
 end
-xlabel('Eccentricity (degrees)'); ylabel('RF Size (degrees)'); grid on;
-legend('Temporal','Superior','Nasal','Inferior','location','nw')
-title(sprintf('Human %s RGC RF Size (degrees)',cellType));
-axis([0 10 0 6]); set(gca,'fontsize',14);
+xlabel('Eccentricity (degrees)');
+ylabel('RF Size (degrees)');
+grid on;
+legend('Temporal', 'Superior', 'Nasal', 'Inferior', 'location', 'nw')
+title(sprintf('Human %s RGC RF Size (degrees)', cellType));
+axis([0 10 0 6]);
+set(gca, 'fontsize', 14);
 
-% figure; degStart = -fovCols/2; degEnd = fovCols/2;
-% degarr = [degStart: (degEnd-degStart)/szCols : degEnd];
-% contourf(degarr,degarr,convertDensityFactor*rgcDiameterLUT,[0:max(rgcDiameterLUT(:))/20:max(rgcDiameterLUT(:))] ); axis square
-% % surfc(degarr,degarr,rgcDiameterLUT); shading flat; %[0:max(rgcDiameterLUT(:))/20:max(rgcDiameterLUT(:))] ); % axis square
-% title(sprintf('Human %s RGC RF Size (degrees)',cellType)); colorbar; 
+% figure;
+% degStart = -fovCols / 2;
+% degEnd = fovCols / 2;
+% degarr = [degStart: (degEnd - degStart) / szCols : degEnd];
+% contourf(degarr, degarr, convertDensityFactor * rgcDiameterLUT, ...
+%    [0:max(rgcDiameterLUT(:)) / 20:max(rgcDiameterLUT(:))] );
+% axis square
+% % surfc(degarr, degarr, rgcDiameterLUT);
+% shading flat;
+% %[0:max(rgcDiameterLUT(:)) / 20:max(rgcDiameterLUT(:))] );
+% % axis square
+% title(sprintf('Human %s RGC RF Size (degrees)', cellType));
+% colorbar;
 
 %%
