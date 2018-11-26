bfc3729f1a9b71fd21ebcdcb26356990c513fc62
jenmaxwell <jen.maxwell@oculus.com>
2018-11-26 12:06:17 -0800

    Tutorials Pass I Part IX: OuterSegment
    Tutorials Pass I Part IX
    
    This set covers the following sub-directories of tutorials:
        tutorials\t_oisequences
    
    Cosmetic/Formatting adjustments only:
        t_oisHarmonic
        t_osNoise
        t_osFoveaPeriphery
    
    Structural/Major Changes:
        Created advancedTutorials_os\Contents.m
    
    Notes/Other:
        General
            Added missing oisequences Contents file!
        t_oisCreate
            Primarily formatting adjustments
            Assign someone to complete TODO associated with BW's note
            Review JNM note questioning figure impermanence.
        t_oisequence
            Review JNM note questioning figure impermanence.
            Determine if(when) we want to add a TODO for the additional cases mentioned in the first note.
        t_osLinearize
            Fovea and Peripherary are the SAME??? This is definitely not what the comments imply.
        t_linearFilters
            Hasn't the first note from the Notes section been completed (switch to function) -> that redone to be executable from the command window?
        advancedTutorials_os\t_osTimeStep
            Can I ask why the helper functions (runSimulation, plotEverything) are pulled out? They are called once each, and appear straightforward and it seems to add unnecessary complication to the function? Especially considering the number of input and output variables.
            inside makeConditionSet, case 3 - is this incomplete, since eyeMovementsPerStimulusRefresh is created, and then never used?
            The code analyzer is upset with the use of oiIndex in the for loop inside plotEverything due to the index's value being accessible from within the loop. (Multiple places)
            Note: "if (oiIndex == 1)" can also just be "if oiIndex"
            Why is the function plotStimulusTimes embedded in the function plotEverything?

---
+++ tutorials/t_outersegment/advancedTutorials_os/Contents.m
@@ -0,0 +1,7 @@
+% ADVANCEDTUTORIALS_OS
+%
+% Advanced tutorials covering outersegment functions and usage.
+%
+% Files
+%   t_osTimeStep - Show how to change timebase of os calculation.
+%
--- tutorials/t_outersegment/t_linearFilters.m
+++ tutorials/t_outersegment/t_linearFilters.m
@@ -1,37 +1,52 @@
 function t_linearFilters
 % Compute the photocurrent at different mean field levels
 %
+% Syntax:
+%   t_linearFilters
+%
 % Description:
-%   Computes L-, M- and S-cone outer segment photocurrent responses to
-%   luminance step stimuli of fixed height presented on different
-%   backgrounds. Visualizes isomerization responses, outer segement impulse
-%   responses and outersegment photocurrent responses.
+%    Computes L-, M- and S-cone outer segment photocurrent responses to
+%    luminance step stimuli of fixed height presented on different
+%    backgrounds. Visualizes isomerization responses, outer segement
+%    impulse responses and outersegment photocurrent responses.
 %
-%   This tutorial mainly shows how the background luminance (adapting
-%   stimulus) affects the cone outer-segment linear impulse response
-%   (luminance adaptation) and, thus, the ensuing cone photocurrent
-%   responses, and how this adaptation depends on cone type.
+%    This tutorial mainly shows how the background luminance (adapting
+%    stimulus) affects the cone outer-segment linear impulse response
+%    (luminance adaptation) and, thus, the ensuing cone photocurrent
+%    responses, and how this adaptation depends on cone type.
 %
-% NOTES
-%   * Copying and pasting the cells doesn't work because it has internal
-%   functions (e.g., oiGenerate).  We should re-write so the tutorial can
-%   be run from the editor.
+% Inputs:
+%    None.
 %
-%   * Should we separate the plots by cone type? Should we use
-%   vcNewGraphWin? 
+% Outputs:
+%   None.
 %
-% NPC, ISETBIO TEAM, 2016
+% Optional key/value pairs:
+%    None.
 %
-% See also: t_osTimeStep
+% Notes:
+%    * Copying and pasting the cells doesn't work because it has internal
+%      functions (e.g., oiGenerate). We should re-write so the tutorial can
+%      be run from the editor.
+%
+%    * Questions:
+%      - Should we separate the plots by cone type?
+%      - Should we use vcNewGraphWin?
+%
+% See Also:
+%   t_osTimeStep
 
-% 01/07/18  npc  Cleaned up, comments
+% History:
+%    XX/XX/16  NPC  ISETBIO TEAM, 2016
+%    01/07/18  npc  Cleaned up, comments
+%    11/07/18  JNM  Formatting
 
 % Define the time axis for the simulation
-stimulusSamplingInterval = 10/1000;
-oiTimeAxis = -0.5:stimulusSamplingInterval:1;  
+stimulusSamplingInterval = 10 / 1000;
+oiTimeAxis = -0.5:stimulusSamplingInterval:1;
 
-% Define the bsckground luminace levels examined and the pedestal step (cd/m2)
-backgroundLuminances = [25 50 100 200 400]; 
+% Define background luminace levels examined & the pedestal step (cd/m2)
+backgroundLuminances = [25 50 100 200 400];
 lumStep = 20;
 
 % Generate optics
@@ -39,104 +54,153 @@ noOptics = false;
 theOI = oiGenerate(noOptics);
 
 % Generate a cone mosaic with 1 L-, 1 M-, and 1 S-cone only
-mosaicSize =  nan;                    
-integrationTime = 1/1000;            
-photonNoise = 'none';                                        
-osNoise = 'none'; 
-osTimeStep = 0.2/1000;
-theConeMosaic = coneMosaicGenerate(mosaicSize, photonNoise, osNoise, integrationTime, osTimeStep);
+mosaicSize = nan;
+integrationTime = 1 / 1000;
+photonNoise = 'none';
+osNoise = 'none';
+osTimeStep = 0.2 / 1000;
+theConeMosaic = coneMosaicGenerate(...
+    mosaicSize, photonNoise, osNoise, integrationTime, osTimeStep);
 
-modulationFunction = cell(numel(backgroundLuminances),1);
-photocurrents      = cell(numel(backgroundLuminances),1);
-isomerizations     = cell(numel(backgroundLuminances),1);
-osLinearFilters    = cell(numel(backgroundLuminances),1);
+modulationFunction = cell(numel(backgroundLuminances), 1);
+photocurrents = cell(numel(backgroundLuminances), 1);
+isomerizations = cell(numel(backgroundLuminances), 1);
+osLinearFilters = cell(numel(backgroundLuminances), 1);
 for iLum = 1:numel(backgroundLuminances)
-    fprintf('Computing os linear filters for background luminance %2.1f cd/m2  [%d/%d]\n',  backgroundLuminances(iLum), iLum, numel(backgroundLuminances));
-    
+    fprintf(strcat("Computing os linear filters for background ", ...
+        "luminance %2.1f cd/m2  [%d/%d]\n"), ...
+        backgroundLuminances(iLum), iLum, numel(backgroundLuminances));
+
     % Compute scene
     FOV = 1;
     theScene = uniformFieldSceneCreate(FOV, backgroundLuminances(iLum));
-    
+
     % Compute the optical images
     oiBackground = oiCompute(theOI, theScene);
-    oiModulated  = oiBackground;
+    oiModulated = oiBackground;
 
     % Generate the sequence of optical images
     % Step stimulus modulation function
-    modulationFunction{iLum} = zeros(1,numel(oiTimeAxis));
-    modulationFunction{iLum}(oiTimeAxis>0.2 & oiTimeAxis<0.6) = lumStep/backgroundLuminances(iLum);
-    theOIsequence = oiSequence(oiBackground, oiModulated, oiTimeAxis, modulationFunction{iLum});
+    modulationFunction{iLum} = zeros(1, numel(oiTimeAxis));
+    modulationFunction{iLum}(oiTimeAxis > 0.2 & oiTimeAxis < 0.6) = ...
+        lumStep / backgroundLuminances(iLum);
+    theOIsequence = oiSequence(oiBackground, oiModulated, oiTimeAxis, ...
+        modulationFunction{iLum});
 
     % Generate eye movement path with zero movement
-    eyeMovementsNum = theOIsequence.maxEyeMovementsNumGivenIntegrationTime(theConeMosaic.integrationTime);
+    eyeMovementsNum = ...
+        theOIsequence.maxEyeMovementsNumGivenIntegrationTime(...
+        theConeMosaic.integrationTime);
     instancesNum = 1;
-    emPaths = zeros(instancesNum, eyeMovementsNum,2);
+    emPaths = zeros(instancesNum, eyeMovementsNum, 2);
 
     % Compute responses
-    [isomerizations{iLum}, photocurrents{iLum}, osLinearFilters{iLum}] = ...
-            theConeMosaic.computeForOISequence(theOIsequence, ...
-            'emPaths', emPaths, ...
-            'currentFlag', true);
+    [isomerizations{iLum}, photocurrents{iLum}, ...
+        osLinearFilters{iLum}] = ...
+        theConeMosaic.computeForOISequence(theOIsequence, ...
+        'emPaths', emPaths, 'currentFlag', true);
     timeAxis = theConeMosaic.timeAxis + theOIsequence.timeAxis(1);
 end
 
 visualizedTrange = [0 1];
 % Visualize the luminance time course
-render(backgroundLuminances, modulationFunction, theConeMosaic.integrationTime, 'stimulus luminance (cd/m2)', [0 500], oiTimeAxis, visualizedTrange,  1);
+render(backgroundLuminances, modulationFunction, ...
+    theConeMosaic.integrationTime, 'stimulus luminance (cd/m2)', ...
+    [0 500], oiTimeAxis, visualizedTrange, 1);
 
 % Visualize the isomerizations time courses
-render(backgroundLuminances, isomerizations, theConeMosaic.integrationTime, sprintf('isomerizations/%d ms',theConeMosaic.integrationTime*1000), [0 40], timeAxis, visualizedTrange,  2);
+render(backgroundLuminances, isomerizations, ...
+    theConeMosaic.integrationTime, sprintf('isomerizations/%d ms', ...
+    theConeMosaic.integrationTime * 1000), [0 40], ...
+    timeAxis, visualizedTrange, 2);
 
 % Visualize the outer-segment linear filters
-render(backgroundLuminances, osLinearFilters, theConeMosaic.integrationTime, '', [-0.1 1.0]*0.18, [], [], 3);
+render(backgroundLuminances, osLinearFilters, ...
+    theConeMosaic.integrationTime, '', [-0.1 1.0] * 0.18, [], [], 3);
 
 % Visualize the outer-segement photocurrent responses
-render(backgroundLuminances, photocurrents, theConeMosaic.integrationTime, 'photocurrent (pAmps)', [-90 -40], timeAxis, visualizedTrange, 4);
+render(backgroundLuminances, photocurrents, ...
+    theConeMosaic.integrationTime, 'photocurrent (pAmps)', [-90 -40], ...
+    timeAxis, visualizedTrange, 4);
 end
 
-function render(backgroundLuminances, responses, integrationTime, yLabelTitle, yAxisRange, timeAxis, tRange, figNo)    
+function render(backgroundLuminances, responses, integrationTime, ...
+    yLabelTitle, yAxisRange, timeAxis, tRange, figNo)
+% Embedded function to visualize the requested portion of the outer segment
+%
+% Syntax:
+%   render(backgroundLuminances, responses, integrationTime, ...
+%       yLabelTitle, yAxisRange, timeAxis, tRange, figNo)
+%
+% Description:
+%    An embedded function inside t_linearFilters designed to render the
+%    image(s) of the requested portion of the outer segment.
+%
+% Inputs:
+%    backgroundLuminances - Vector. A numerical vector containing the
+%                           background luminances for the image.
+%    responses            - Array. This is a cell array containing a 4D
+%                           matrix of responses.
+%    integrationTime      - Numeric. The integration time.
+%    yLabelTitle          - String. The title for the Y-axis
+%    yAxisRange           - Vector. A 1x2 vector of the Y axis range.
+%    timeAxis             - Vector. A numeric vector of the time axis.
+%    tRange               - Vector. A 1x2 vector of the time range.
+%    figNo                - Numeric. The figure number.
+%
+% Outputs:
+%    None.
+%
+% Optional key/value pairs:
+%    None.
+%
 subplotPosVectors = NicePlot.getSubPlotPosVectors(...
        'rowsNum', 1, ...
        'colsNum', 3, ...
-       'heightMargin',   0.04, ...
-       'widthMargin',    0.05, ...
-       'leftMargin',     0.05, ...
-       'rightMargin',    0.00, ...
-       'bottomMargin',   0.08, ...
-       'topMargin',      0.04);
+       'heightMargin', 0.04, ...
+       'widthMargin', 0.05, ...
+       'leftMargin', 0.05, ...
+       'rightMargin', 0.00, ...
+       'bottomMargin', 0.08, ...
+       'topMargin', 0.04);
 
 coneNames = {'L', 'M', 'S'};
 colorIR = (jet(numel(backgroundLuminances)));
 
-for iLum = 1:numel(backgroundLuminances)  
+for iLum = 1:numel(backgroundLuminances)
     if (iLum == 1)
         legends = {};
-        hFig = figure(figNo); clf;
-        set(hFig, 'Position', [10 10 1380 650+50*iLum], 'Color', [0.1 0.1 0.1]);
+        hFig = figure(figNo);
+        clf;
+        set(hFig, 'Position', [10, 10, 1380, 650 + 50 * iLum], ...
+            'Color', [0.1 0.1 0.1]);
     end
 
-    legends{numel(legends)+1} = sprintf('lum: %2.1f cd/m2', backgroundLuminances(iLum)); %#ok<AGROW>
-    color = squeeze(colorIR(iLum,:));
+    legends{numel(legends) + 1} = sprintf('lum: %2.1f cd/m2', ...
+        backgroundLuminances(iLum)); %#ok<AGROW>
+    color = squeeze(colorIR(iLum, :));
     if (isempty(timeAxis))
-        theTimeAxis = (1:length(responses{iLum}))*integrationTime;
+        theTimeAxis = (1:length(responses{iLum})) * integrationTime;
     else
         theTimeAxis = timeAxis;
     end
     for coneIndex = 1:3
-        subplot('Position', subplotPosVectors(1,coneIndex).v);
+        subplot('Position', subplotPosVectors(1, coneIndex).v);
 
         if (~strcmp(yLabelTitle, 'stimulus luminance (cd/m2)'))
             coneResponses = squeeze(responses{iLum});
-            if (size(coneResponses,1) == 3)
-                response = coneResponses(coneIndex,:);
+            if (size(coneResponses, 1) == 3)
+                response = coneResponses(coneIndex, :);
             else
-                response = coneResponses(:,coneIndex);
+                response = coneResponses(:, coneIndex);
             end
         else
-           response = backgroundLuminances(iLum) * (1+squeeze(responses{iLum}));
+           response = backgroundLuminances(iLum) ...
+               * (1 + squeeze(responses{iLum}));
         end
 
-        plot(theTimeAxis, response, 'k-', 'Color', color, 'LineWidth', 1.5);
+        plot(theTimeAxis, response, 'k-', 'Color', color, ...
+            'LineWidth', 1.5);
         hold on;
 
         if isempty(tRange)
@@ -145,7 +209,8 @@ for iLum = 1:numel(backgroundLuminances)
             set(gca, 'XLim', tRange)
         end
 
-        set(gca, 'FontSize', 14, 'Color', [0.3 0.3 0.3], 'XColor', [0.9 0.9 0.9], 'YColor', [0.9 0.9 0.9]);
+        set(gca, 'FontSize', 14, 'Color', [0.3 0.3 0.3], ...
+            'XColor', [0.9 0.9 0.9], 'YColor', [0.9 0.9 0.9]);
         if (~isempty(yAxisRange))
             set(gca, 'YLim', yAxisRange);
         end
@@ -155,17 +220,48 @@ for iLum = 1:numel(backgroundLuminances)
         end
         xlabel('time (seconds)', 'FontWeight', 'bold');
 
-        grid on; box on;
+        grid on;
+        box on;
         hL = legend(legends);
         set(hL, 'FontSize', 14, 'Color', [0.6 0.6 0.6]);
-        title(sprintf('%s-cone', coneNames{coneIndex}), 'FontSize', 14, 'FontWeight', 'bold', 'Color', [1 1 1]);
+        title(sprintf('%s-cone', coneNames{coneIndex}), 'FontSize', 14, ...
+            'FontWeight', 'bold', 'Color', [1 1 1]);
     end % coneIndex
     drawnow;
 end %iLum
+
 end
 
 % Internal utility
-function theConeMosaic = coneMosaicGenerate(mosaicSize, photonNoise, osNoise, integrationTime, osTimeStep)
+function theConeMosaic = coneMosaicGenerate(mosaicSize, photonNoise, ...
+    osNoise, integrationTime, osTimeStep)
+% An internal utility to generate a cone mosaic
+%
+% Syntax:
+%   theConeMosaic = coneMosaicGenerate(mosaicSize, photonNoise, ...
+%       osNoise, integrationTime, osTimeStep)
+%
+% Description:
+%    An internal utility to generate a cone mosaic.
+%
+% Inputs:
+%    mosaicSize      - (Optional) Struct. A structure containing the
+%                      relevant size information for a cone mosaic. Can
+%                      submit NaN to use defaults. Default is a human cone
+%                      mosaic with 1 of each of the L, M, and S cones,
+%                      using the 2:4 pattern.
+%    photonNoise     - String. A string describing the photon noise type.
+%    osNoise         - String. A string describing the OS noise type.
+%    integrationTime - Numeric. The numeric value of the integration time.
+%    osTimeStep      - Numeric. The outer segment time step.
+%
+% Outputs:
+%    theConeMosaic   - Object. The created cone mosaic object.
+%
+% Optional key/value pairs:
+%    None.
+%
+
 % Default human mosaic
 theConeMosaic = coneMosaic;
 
@@ -196,27 +292,70 @@ theOuterSegment.timeStep = osTimeStep;
 theConeMosaic.os = theOuterSegment;
 end
 
-
 function theOI = oiGenerate(noOptics)
 % Generate optics
+%
+% Syntax:
+%   theOI = oiGenerate(noOptics)
+%
+% Description:
+%    An embedded function of t_linearFilters designed to take care of
+%    generating the necessary Optical Image optics.
+%
+% Inputs:
+%    noOptics - Boolean. A boolean indicating which type of topics
+%               (diffraction limited, or human) to create. True for DL,
+%               false for human.
+%
+% Outputs:
+%    theOI    - Struct. A created optical image structure.
+%
+% Optional key/value pairs:
+%    None.
+%
 if (noOptics)
     theOI = oiCreate('diffraction limited');
-    optics = oiGet(theOI,'optics');           
-    optics = opticsSet(optics,'fnumber',0);
+    optics = oiGet(theOI, 'optics');
+    optics = opticsSet(optics, 'fnumber', 0);
     optics = opticsSet(optics, 'off axis method', 'skip');
-    theOI = oiSet(theOI,'optics', optics);
+    theOI = oiSet(theOI, 'optics', optics);
 else
     theOI = oiCreate('human');
 end
+
 end
 
 % Internal utility.
 function uniformScene = uniformFieldSceneCreate(FOV, meanLuminance)
+% An internal function to create a uniform field scene
+%
+% Syntax:
+%   uniformScene = uniformFieldSceneCreate(FOV, meanLuminance)
+%
+% Description:
+%    An embedded function inside of t_linearFilters designed to create a
+%    uniform field scene.
+%
+% Inputs:
+%    FOV           - Numeric. The field of view.
+%    meanLuminance - Numeric. The mean luminance.
+%
+% Outputs:
+%    uniformScene  - Struct. A structure containing the created scene.
+%
+% Optional key/value pairs:
+%    None.
+%
+
 uniformScene = sceneCreate('uniform equal photon', 128);
+
 % square scene with desired FOV
 uniformScene = sceneSet(uniformScene, 'wAngular', FOV);
+
 % 1 meter away
 uniformScene = sceneSet(uniformScene, 'distance', 1.0);
+
 % adjust radiance according to desired  mean luminance
 uniformScene = sceneAdjustLuminance(uniformScene, meanLuminance);
+
 end
