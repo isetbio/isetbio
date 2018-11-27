7635b913b04a61aa6943fdfe1ba5e0d93d03553f
jenmaxwell <jen.maxwell@oculus.com>
2018-11-27 09:50:07 -0800

    Tutorials Pass I Part XI
    Tutorials Pass I Part XI
    
    This set covers the following sub-directories of tutorials:
        tutorials\t_human
    
    Cosmetic/Formatting adjustments only:
        t_humanLineSpreadOI
    
    Structural/Major Changes:
        Added Contents.m

--- tutorials/t_human/t_humanLineSpreadOI.m
+++ tutorials/t_human/t_humanLineSpreadOI.m
@@ -1,9 +1,13 @@
-%% t_humanLineSpreadOI
+% Demonstrate retinal irradiance of line stimuli at varying wavelengths
 %
-% Illustrate the retinal irradiance of a line stimuli at different
-% wavelengths. 
+% Description:
+%    This function is intended to illustrate the retinal irradiance of a
+%    line stimuli at different wavelengths. 
 %
-% Copyright ISETBIO Team, 2015
+
+% History:
+%    XX/XX/15       Copyright ISETBIO Team, 2015
+%    11/26/18  JNM  Formatting
 
 % Initialize
 ieInit;
@@ -11,8 +15,8 @@ ieInit;
 %% Create a line scene, human optics, and a human sensor
 % This is a broad band stimulus, with a spectral power distribution of
 % daylight, 6500 K.  We set the field of view to one degree.
-lineS = sceneCreate('line d65',128);
-lineS = sceneSet(lineS,'h fov',1);
+lineS = sceneCreate('line d65', 128);
+lineS = sceneSet(lineS, 'h fov', 1);
 
 % The optics are the estimated human optics, as per Marimont and Wandell in
 % the mid-90s. We could (and should) illustrate this using some of the more
@@ -20,20 +24,19 @@ lineS = sceneSet(lineS,'h fov',1);
 oi = oiCreate('human');
 
 %%  Compute and display the broad band
-oi = oiCompute(lineS,oi);
+oi = oiCompute(lineS, oi);
 
 % Get rid of the surrounding area with the image mean.  This makes the
 % visualization a little nicer, and we are only interested in the center
 % part anyway.
-oi = oiCrop(oi,[20 20 120 120]);
-vcAddObject(oi); oiWindow('scale',true);
+oi = oiCrop(oi, [20 20 120 120]);
+vcAddObject(oi);
+oiWindow('scale', true);
 
 roi = [];
 wList = [450, 550, 650];  % nm
 gSpacing = 40;            % microns
 for ww = 1:length(wList)
     thisWave = wList(ww); 
-    oiPlot(oi,'irradiance image wave grid',roi,thisWave,gSpacing);
+    oiPlot(oi, 'irradiance image wave grid', roi, thisWave, gSpacing);
 end
-
-%%
