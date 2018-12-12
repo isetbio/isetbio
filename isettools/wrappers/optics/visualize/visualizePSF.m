2f473ea7da0ecd33a09f1bba2913c85a4cd97c9c
Nicolas Cottaris <cottaris@sas.upenn.edu>
2018-12-12 16:48:32 -0500

    Ability to visualize the PSF on top of a cone mosaic.
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- isettools/wrappers/optics/visualize/visualizePSF.m
+++ isettools/wrappers/optics/visualize/visualizePSF.m
@@ -1,12 +1,16 @@
 function visualizePSF(theOI, targetWavelength, psfRangeArcMin, varargin)
 p = inputParser;
 p.addParameter('axesHandle', [], @ishandle);
+p.addParameter('withSuperimposedMosaic', [], @(x)(isa(x, 'coneMosaicHex')));
 % Parse input
 p.parse(varargin{:});
 axesHandle = p.Results.axesHandle;
+theMosaic = p.Results.withSuperimposedMosaic;
+
+
 
 psfRangeArcMin = 0.5*psfRangeArcMin;
-psfTicksMin = 2*(-8:2:8);
+psfTicksMin = (-30:5:30);
 if (psfRangeArcMin <= 10)
     psfTicks = psfTicksMin;
 elseif (psfRangeArcMin <= 20)
@@ -15,6 +19,8 @@ elseif (psfRangeArcMin <= 40)
     psfTicks = 4*psfTicksMin;
 elseif (psfRangeArcMin <= 50)
     psfTicks = 5*psfTicksMin; 
+elseif (psfRangeArcMin <= 60)
+    psfTicks = 6*psfTicksMin; 
 elseif (psfRangeArcMin <= 100)
     psfTicks = 10*psfTicksMin; 
 elseif (psfRangeArcMin <= 200)
@@ -22,6 +28,8 @@ elseif (psfRangeArcMin <= 200)
 elseif (psfRangeArcMin <= 400)
     psfTicks = 40*psfTicksMin; 
 end
+psfTickLabels = sprintf('%d\n', psfTicks);
+
 optics = oiGet(theOI, 'optics');
 wavelengthSupport = opticsGet(optics, 'wave');
 [~,idx] = min(abs(wavelengthSupport-targetWavelength));
@@ -58,16 +66,42 @@ else
 end
 axes(axesHandle);
 
-contourLevels = 0:0.05:1.0;
-contourf(xSupportMinutes, ySupportMinutes, wavePSF/max(wavePSF(:)), contourLevels);
-hold on;
-plot(xSupportMinutes, psfRangeArcMin*(2*psfSlice-1), 'c-', 'LineWidth', 4.0);
-plot(xSupportMinutes, psfRangeArcMin*(2*psfSlice-1), 'b-', 'LineWidth', 1.0);
+if (~isempty(theMosaic))
+   theMosaic.visualizeGrid('axesHandle', axesHandle, ...
+       'backgroundColor', [1 1 1], ...
+       'labelConeTypes', false);
+   drawnow;
+   hold on; 
+   % transform minutes to meters
+   xSupportMinutes = xSupportMinutes / 60 * theMosaic.micronsPerDegree * 1e-6;
+   ySupportMinutes = ySupportMinutes / 60 * theMosaic.micronsPerDegree * 1e-6;
+   psfRangeArcMin = psfRangeArcMin / 60 * theMosaic.micronsPerDegree * 1e-6;
+   psfTicks = psfTicks / 60 * theMosaic.micronsPerDegree * 1e-6;
+end
+
+contourLevels = 0:0.1:1.0;
+if (~isempty(theMosaic))
+    contour(xSupportMinutes, ySupportMinutes, wavePSF/max(wavePSF(:)), contourLevels, ...
+        'Color', 'c', 'LineWidth', 4);
+    contour(xSupportMinutes, ySupportMinutes, wavePSF/max(wavePSF(:)), contourLevels, ...
+        'Color', 'b', 'LineWidth', 1);
+    hold on;
+    plot(xSupportMinutes, psfRangeArcMin*(psfSlice-1), 'c-', 'LineWidth', 4.0);
+    plot(xSupportMinutes, psfRangeArcMin*(psfSlice-1), 'b-', 'LineWidth', 1.0);
+else
+    contourf(xSupportMinutes, ySupportMinutes, wavePSF/max(wavePSF(:)), contourLevels, ...
+        'Color', [0 0 0], 'LineWidth', 1.5);
+
+    hold on;
+    plot(xSupportMinutes, psfRangeArcMin*(psfSlice-1), 'c-', 'LineWidth', 4.0);
+    plot(xSupportMinutes, psfRangeArcMin*(psfSlice-1), 'b-', 'LineWidth', 1.0);
+end
+
 axis 'image'; axis 'xy';
-grid on
+grid on; box on
 set(gca, 'XLim', psfRangeArcMin*1.05*[-1 1], 'YLim', psfRangeArcMin*1.05*[-1 1], 'CLim', [0 1], ...
-            'XTick', psfTicks, 'YTick', psfTicks);
-set(gca, 'XColor', [0 0 0], 'YColor', [0 0 0], 'Color', [0 0 0]);
+            'XTick', psfTicks, 'YTick', psfTicks, 'XTickLabel', psfTickLabels, 'YTickLabel', psfTickLabels);
+set(gca, 'XColor', [0 0 0], 'YColor', [0 0 0]);
 xlabel('\it space (arc min)');
 set(gca, 'FontSize', fontSize);
 cmap = brewermap(1024, 'greys');
