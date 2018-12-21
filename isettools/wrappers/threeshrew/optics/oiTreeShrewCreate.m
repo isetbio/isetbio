d3e6352d3d3e99fee62bc2f78f902dd8b4d0a979
Nicolas Cottaris <cottaris@sas.upenn.edu>
2018-12-21 14:04:07 -0500

    Updated treeShrew lens method; added interpolation method argument in demosaicing method.
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- isettools/wrappers/threeshrew/optics/oiTreeShrewCreate.m
+++ isettools/wrappers/threeshrew/optics/oiTreeShrewCreate.m
@@ -35,27 +35,11 @@ function oi = oiTreeShrewCreate(varargin)
     end  
     
     % TreeShrew lens absorption.
-    theTreeShrewLens = treeShrewLens(oiGet(oi, 'optics wave'));
+    theTreeShrewLens = lensTreeShrewCreate('wave', oiGet(oi, 'optics wave'));
     
     % Update the oi.lens
     oi = oiSet(oi, 'lens', theTreeShrewLens);
     
     % Set the same lens in the optics structure too
     oi.optics.lens = theTreeShrewLens;
-end
-
-% Method to generate a Lens object with treeshrew - based optical density
-function theTreeShrewLens = treeShrewLens(targetWavelenth)
-    % TreeShrew lens absorption. Start with the human lens.
-    theTreeShrewLens = Lens();
-    
-    % Load TreeShrew lens unit-density
-    load('treeshrewLensAbsorbance.mat', 'wavelength', 'data');
-    
-    % Interpolate to optics wavelength support
-    unitDensity = interp1(wavelength,data,targetWavelenth, 'pchip');
-    
-    % Update the lens
-    set(theTreeShrewLens,'wave', targetWavelenth);
-    set(theTreeShrewLens,'unitDensity',unitDensity);
 end
