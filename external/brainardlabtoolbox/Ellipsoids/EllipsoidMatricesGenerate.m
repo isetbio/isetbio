e858ee059c7d8307e5dbed2af13edd578fd622d5
David Brainard <brainard@psych.upenn.edu>
2019-02-02 08:35:35 -0500

    Update PTB and BLTB externals
    Main thing about PTB was that a routine (NavarroMTF) was added here but not merged back into PTB.  I did that, and also added to PTB’s Contents.m.
    
    In BLTP, EllipsoidMatricesGenerate now has a transpose where it didn’t before, but I can’t see anywhere we are using that in ISETBio or related projects (IBIOColorDetect).  So I don’t think this will break anything.

--- external/brainardlabtoolbox/Ellipsoids/EllipsoidMatricesGenerate.m
+++ external/brainardlabtoolbox/Ellipsoids/EllipsoidMatricesGenerate.m
@@ -35,12 +35,22 @@ function [A,Ainv,Q] = EllipsoidMatricesGenerate(ellParams,varargin)
 %       characterizations of color thresholds. J. Opt. Soc. Am. A 7: 783-89.
 %     See particularly pp. 784-785.
 %
+%     We had some trouble deciding the right convention for angles.  For
+%     the two dimensional case, this is set up so that the major axis of
+%     the ellipse is rotated counter-clockwise relative to the x-axis
+%     according to the passed angle.  Need to check more carefully, and
+%     particularly for the 3-D case.
+%
 % Optional key/value pairs:
 %    'dimension'    - What dimension is the ellipsoid? Can be 2 or 3.
 %                     Default is 3. Passing 2 means an ellipse.
-%
-% 06/27/16  dhb  Back to the future.  Wrote this.  It feels like 1988.
-% 08/16/18  dhb  Change parameterization to match paper.
+
+% History:
+%   06/27/16  dhb  Back to the future.  Wrote this.  It feels like 1988.
+%   08/16/18  dhb  Change parameterization to match paper.
+%   11/20/18  dhb  Remove transpose from scalar arg to deg2rotm.
+%   11/24/18  dhb  Flip convention of V, V' to match intuition about
+%                  angles.  Checked for two-dimensional case.
 
 % Examples:
 %{
@@ -61,13 +71,13 @@ p.parse(ellParams,varargin{:});
 switch(p.Results.dimension)
     case 2
         S = diag(ellParams(1:2));
-        V = deg2rotm(ellParams(3)');  
+        V = deg2rotm(ellParams(3))';  
         if (length(ellParams) == 5)
             error('Translation not yet implemented');
         end
     case 3
         S = diag(ellParams(1:3));
-        V = eul2rotm(deg2rad(ellParams(4:6)'));  
+        V = eul2rotm(deg2rad(ellParams(4:6)'))';  
         if (length(ellParams) == 9)
             error('Translation not yet implemented')
         end
