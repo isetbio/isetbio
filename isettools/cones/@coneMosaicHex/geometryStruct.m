d26db7946e42cace85f85015d11a32d8824d4324
Nicolas Cottaris <cottaris@sas.upenn.edu>
2019-03-03 17:04:55 -0500

    buglet fix.

--- isettools/cones/@coneMosaicHex/geometryStruct.m
+++ isettools/cones/@coneMosaicHex/geometryStruct.m
@@ -28,5 +28,56 @@ function cmStruct = geometryStruct(obj)
     cmStruct.coneApertures = obj.computeApertureDiameters()/obj.micronsPerDegree;
     
     % Delaunayn triangularization
-    cmStruct.triangles = delaunayn(conePositions);
+    xHex = cmStruct.coneLocs(:,1);
+    yHex = cmStruct.coneLocs(:,2);
+    cmStruct.triangles = delaunayn([xHex(:), yHex(:)]);
+    
+    plotData = false;
+    if (plotData)
+    trianglesNum = size(cmStruct.triangles, 1);
+    
+    figure();
+    axis 'equal'
+    hold on
+    for triangleIndex = 1:trianglesNum 
+        coneIndices = cmStruct.triangles(triangleIndex, :);
+        xCoords(1:3) = xHex(coneIndices); xCoords(4) = xCoords(1);
+        yCoords(1:3) = yHex(coneIndices); yCoords(4) = yCoords(1);
+        plot(xCoords, yCoords,'k-');
+        if (mod(triangleIndex,1000) == 0)
+        drawnow;
+        end
+    end
+    
+    % Compute hex-index  for all triangles
+    hexIndices = computeHexIndex(cmStruct.coneLocs, cmStruct.triangles);
+    hexIndicesBins = 0.0:0.01:1.0;
+    [counts,centers] = hist(hexIndices, hexIndicesBins);
+    
+    
+    figure()
+    bar(centers,counts,1)
+    end
+    
+end
+
+function hexIndices = computeHexIndex(coneLocs, triangles)
+    
+    trianglesNum = size(triangles,1);
+    Xlocs = coneLocs(:,1);
+    Ylocs = coneLocs(:,2);
+    
+    hexIndices = zeros(1,trianglesNum);
+    x = zeros(1,3); y = x;
+    for triangleIndex = 1:trianglesNum
+        for node = 1:3
+            x(node) = Xlocs(triangles(triangleIndex,node));
+            y(node) = Ylocs(triangles(triangleIndex,node));
+        end 
+        aLength = sqrt((x(1)-x(2))^2 + (y(1)-y(2))^2);
+        bLength = sqrt((x(1)-x(3))^2 + (y(1)-y(3))^2);
+        cLength = sqrt((x(2)-x(3))^2 + (y(2)-y(3))^2);
+        hexIndices(triangleIndex) = (bLength+cLength-aLength)*(cLength+aLength-bLength)*(aLength+bLength-cLength)/(aLength*bLength*cLength);
+    end
+
 end
