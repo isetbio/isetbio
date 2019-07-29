32427c27edd9af88d142fe79b6362cb7360abf31
tlian7 <tlian@stanford.edu>
2019-07-29 14:13:06 -0700

    Added in previously missing parameters for new sceneEye scene ("lettersAtDepthPlus")

--- isettools/sceneEye/Utilities/loadPbrtScene.m
+++ isettools/sceneEye/Utilities/loadPbrtScene.m
@@ -84,12 +84,17 @@ p.addParameter('eccentricity',0,@isnumeric);
 p.addParameter('objectDistance',1,@isnumeric); 
 p.addParameter('objectSize',0.3,@isnumeric); 
 
-%lettersAtDepth
+%lettersAtDepth and lettersAtDepthPlus
 p.addParameter('Adist',0.1,@isnumeric); 
 p.addParameter('Bdist',0.2,@isnumeric); 
 p.addParameter('Cdist',0.3,@isnumeric); 
+p.addParameter('Ddist',[],@isnumeric); 
+p.addParameter('Edist',[],@isnumeric); 
 p.addParameter('Adeg',6,@isnumeric); 
+p.addParameter('Bdeg',[],@isnumeric);
 p.addParameter('Cdeg',4,@isnumeric); 
+p.addParameter('Ddeg',[],@isnumeric);
+p.addParameter('Edeg',[],@isnumeric);
 p.addParameter('nchecks',[64 64],@isnumeric); % [wall,ground]
 
 p.parse(pbrtFile, varargin{:});
@@ -226,6 +231,16 @@ if(sceneNameFlag)
             
             pullSceneFromRDT('lettersAtDepth',scenePath);
             
+        case('lettersAtDepthPlus')
+            % A, B, C, D and E at different depths.
+            
+            scenePath = fullfile(piRootPath,'local','scenes',...
+                'lettersAtDepthPlus',...
+                'lettersAtDepthPlus.pbrt');
+            sceneUnits = 'm';
+            
+            pullSceneFromRDT('lettersAtDepthPlus',scenePath);
+            
         otherwise
             error('Did not recognize scene type.');
     end
@@ -267,6 +282,20 @@ if(sceneNameFlag)
                 'Cdist',p.Results.Cdist,...
                 'Adeg',p.Results.Adeg,...
                 'Cdeg',p.Results.Cdeg,...
+                'nchecks',p.Results.nchecks);
+            
+        case('lettersAtDepthPlus')
+            % Move the letters in the scene. To do this, we're actually
+            % going to remake the scene.
+            recipe = piCreateLettersAtDepthPlus('Adist',p.Results.Adist,...
+                'Bdist',p.Results.Bdist,...
+                'Cdist',p.Results.Cdist,...
+                'Ddist',p.Results.Ddist,...
+                'Edist',p.Results.Edist,...
+                'Adeg',p.Results.Adeg,...
+                'Bdeg',p.Results.Bdeg,...
+                'Ddeg',p.Results.Ddeg,...
+                'Edeg',p.Results.Edeg,...
                 'nchecks',p.Results.nchecks);
             
         case('slantedBar')
