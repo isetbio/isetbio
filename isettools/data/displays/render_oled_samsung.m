0b5ab63cd0bcf0662da2b75981f3e4f671b51f93
jenmaxwell <jen.maxwell@oculus.com>
2019-08-07 10:26:00 -0700

    Revert "isettools Pass I Part VII"
    This reverts commit c7cba8d2b9e633245bc92f270d1514a26a08bfc7.

--- isettools/data/displays/render_oled_samsung.m
+++ isettools/data/displays/render_oled_samsung.m
@@ -1,49 +1,40 @@
 function outImg = render_oled_samsung(inImg, d, sz)
 % render image for Samsung S-strip display
 %
-% Syntax:
-%   I = render_oled_samsung(inImg, [d], [sz])
+%     I = render_oled_samsung
 %
-% Description:
-%    This is the render function for OLED-Samsung display
-%    The display can be created with
-%       d = displayCreate('OLED-Samsung');
+% This is the render function for OLED-Samsung display
+% The display can be created with
+%    d = displayCreate('OLED-Samsung');
 %
-%    The sub-pixel design is Samsung S-strip, whose repeating unit contains
-%    2x2 pixels
-%
+% The sub-pixel design is Samsung S-strip, whose repeating unit contains
+% 2x2 pixels
+% 
 % Inputs:
-%    inImg  - Matrix. An input image matrix.
-%    d      - (Optional) Struct. A display structure. Default is created by
-%             calling displayCreate.
-%    sz     - (Optional) Numeric. The pixels per dixel. Default is to not
-%             provide and instead use over sampling.
+%   inImg  - input image, size(inImg, 3) should be same as nprimaries
+%   d      - display structure, can be created by displayCreate
 %
 % Output:
-%   outImg - Matrix. The rendered image.
+%   outImg - rendered image
 %
-% Optional key/value pairs:
-%    None.
+% Example:
+%   d = displayCreate('OLED-Samsung');
+%   I = 0.5*(sin(2*pi*(1:32)/32)+1); I = repmat(I,32,1);
+%   outI = render_oled_samsung(I, d);
 %
-% See Also:
-%   displayCompute, render_lcd_samsung_rgbw
+% See also:
+%   displayCompute
 %
-
-% History:
-%    XX/XX/14  HJ   ISETBIO TEAM, 2014
-%    07/18/19  JNM  Formatting
-
-% Examples:
-%{
-    d = displayCreate('OLED-Samsung');
-    I = 0.5 * (sin(2 * pi * (1:32) / 32) + 1);
-    I = repmat(I, 32, 1);
-    outI = render_oled_samsung(I, d);
-%}
+%  HJ, ISETBIO TEAM, 2014
 
 %% Init
 if notDefined('inImg'), error('input image required'); end
 if notDefined('d'), d = displayCreate('OLED-Samsung'); end
+
+%% Render
+% Get parameters from display structure 
+pixels_per_dixel = displayGet(d, 'pixels per dixel');
+nprimaries = size(inImg, 3);
 
 if notDefined('sz')
     s = displayGet(d, 'over sample');
@@ -53,19 +44,14 @@ else
     controlMap = displayGet(d, 'dixel control map', sz);
 end
 
-%% Render
-% Get parameters from display structure
-pixels_per_dixel = displayGet(d, 'pixels per dixel');
-nprimaries = size(inImg, 3);
-
 % process by block
-outImg = zeros(size(inImg, 1) * s(1), size(inImg, 2) * s(2), nprimaries);
+outImg = zeros(size(inImg, 1)*s(1), size(inImg,2) * s(2), nprimaries);
 for ii = 1 : nprimaries
     % define function handle
-    hf = @(x) x.data(controlMap(:, :, ii));
-
+    hf = @(x) x.data(controlMap(:,:,ii));
+    
     % process
-    outImg(:, :, ii) = blockproc(inImg(:, :, ii), pixels_per_dixel, hf);
+    outImg(:, :, ii) = blockproc(inImg(:,:,ii), pixels_per_dixel, hf);
 end
 
 end
