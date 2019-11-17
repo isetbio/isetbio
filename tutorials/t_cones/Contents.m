b5bad432c30594b49f5f5d8401220bbdce6ab697
Javi Ribera <jprat@purdue.edu>
2019-11-16 22:54:19 -0800

    sort list of cone tutorials by basic to advanced

--- tutorials/t_cones/Contents.m
+++ tutorials/t_cones/Contents.m
@@ -2,11 +2,11 @@
 %
 % Illustrates cone mosaic object and computation of cone isomerizations.
 %
+%   t_conesMosaicBasic                   - Basic introduction to the cone mosaic object.
 %   t_conesBigArray                      - Simply show absorptions of a big cone mosaic
-%   t_conesDensity                       - Show how human cone density varies across the retina
 %   t_conesEyeSensitivity                - Illustrate basic principles of how eye parameters affect isomerizations
 %   t_conesMosaicAbsorptionNoise         - Illustrate noise generation for the coneMosaic object
-%   t_conesMosaicBasic                   - Basic introduction to the cone mosaic object.
+%   t_conesDensity                       - Show how human cone density varies across the retina
 %   t_conesMosaicHex                     - Create & use a hexagonal cone mosaic with eccentricity-based cone spacing
 %   t_conesMosaicHexReg                  - Create/use hex. mosaic w/ custom fixed cone spacing & inner segment diam.
 %   t_conesPhotoPigment                  - Illustrate photoPigment object
