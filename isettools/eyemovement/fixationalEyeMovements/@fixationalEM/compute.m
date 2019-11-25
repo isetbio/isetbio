05bb1ab6cc458fa7959875d3c7e0990318627658
Nicolas Cottaris <cottaris@upenn.edu>
2019-11-25 17:56:47 -0500

    Single quotation marks
    Signed-off-by: Nicolas Cottaris <cottaris@upenn.edu>

--- isettools/eyemovement/fixationalEyeMovements/@fixationalEM/compute.m
+++ isettools/eyemovement/fixationalEyeMovements/@fixationalEM/compute.m
@@ -56,10 +56,10 @@ obj.initOutputs();
 % Set random seed
 if (isempty(obj.randomSeed))
     %rng('shuffle');                         % 2018b and before
-    rng('shuffle', "CombRecursive");         % 2019a and after
+    rng('shuffle', 'CombRecursive');         % 2019a and after
 else
     %rng(obj.randomSeed);                    % 2018b and before
-    rng(obj.randomSeed, "CombRecursive");    % 2019a and after
+    rng(obj.randomSeed, 'CombRecursive');    % 2019a and after
 end
 
 if (obj.beVerbose)
