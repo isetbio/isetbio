a6d71568f5e36f984a69369c94de66bba1ab9d21
Nicolas Cottaris <cottaris@upenn.edu>
2020-04-14 14:50:25 -0400

    Full mosaic analyses

--- calculators/mosaicConnector/MosaicConnectorFull.m
+++ calculators/mosaicConnector/MosaicConnectorFull.m
@@ -2,6 +2,7 @@ function MosaicConnectorFull
 
     recomputePhase1 = ~true;
     tmpDir = '/Volumes/SamsungT3/MATLAB/toolboxes/isetbio/calculators/mosaicConnector';
+    tmpDir = '/Users/nicolas/Documents/MATLAB/toolboxes/isetbio/calculators/mosaicConnector';
     
     if (recomputePhase1)
         % Select mosaics to load
@@ -36,7 +37,7 @@ function MosaicConnectorFull
     thresholdFractionForMosaicIncosistencyCorrection = 0.5;
         
     horizEccDegs = [0 0];
-    fovDegs = [0.5 0.2];
+    fovDegs = [20 0.3];  % up to 10
         
     % Specify center in microns
     roi.center = round(1000*WatsonRGCModel.rhoDegsToMMs(horizEccDegs));
@@ -63,7 +64,7 @@ function MosaicConnectorFull
         'figureWidthInches', figHeightInches*(roi.size(1)-2*roi.margin)/(roi.size(2)-2*roi.margin), ...
         'figureHeightInches', figHeightInches);
 
-    computeStep1 = ~true;
+    computeStep1 = true;
     if (computeStep1)
         load(fullfile(tmpDir,'tmp2.mat'), 'RGCRFPositionsMicrons', 'conePositionsMicrons', 'RGCRFSpacingsMicrons', 'desiredConesToRGCratios');
         % Step 1. Remove inconstencies within and across mosaics
@@ -76,7 +77,7 @@ function MosaicConnectorFull
 
         save(fullfile(tmpDir,'tmp2_step1.mat'), 'RGCRFPositionsMicrons', 'conePositionsMicrons', ...
             'RGCRFSpacingsMicrons', 'coneSpacingsMicrons', 'desiredConesToRGCratios');
-        return;
+        %return;
     else
         load(fullfile(tmpDir,'tmp2_step1.mat'), 'RGCRFPositionsMicrons', 'conePositionsMicrons', ...
             'RGCRFSpacingsMicrons', 'coneSpacingsMicrons', 'desiredConesToRGCratios');
@@ -85,7 +86,7 @@ function MosaicConnectorFull
     
     %visualizeRGCmosaic(90,RGCRFPositionsMicrons, RGCRFSpacingsMicrons, roi, 'original', plotlabOBJ);
                    
-    computeStep2 = ~true;
+    computeStep2 = true;
     if (computeStep2)    
         % Step 2. Assign types (L,M,S) in the cone mosaic
         tritanopicAreaDiameterMicrons = 0.25 * 300;
@@ -94,13 +95,13 @@ function MosaicConnectorFull
         coneTypes = assignConeTypes(conePositionsMicrons, coneSpacingsMicrons, ...
             tritanopicAreaDiameterMicrons, relativeSconeSpacing, LtoMratio, roi, visualizeMosaic, plotlabOBJ);
         save(fullfile(tmpDir,'tmp2_step2.mat'), 'coneTypes');
-        return;
+        %return;
     else
         load(fullfile(tmpDir,'tmp2_step2.mat'), 'coneTypes');
     end
     
     
-    computeStep3 = ~true;
+    computeStep3 = true;
     if (computeStep3)  
     % Step 3. Connect cone to the midget RGC mosaic
         [midgetRGCconnectionMatrix, ...
@@ -112,7 +113,7 @@ function MosaicConnectorFull
                 orphanRGCpolicy, visualizeConnectionProcess);
         save(fullfile(tmpDir,'tmp2_step3.mat'), 'midgetRGCconnectionMatrix', ...
                     'RGCRFPositionsMicrons', 'RGCRFSpacingsMicrons');
-        return;
+        %return;
     else
         load(fullfile(tmpDir,'tmp2_step3.mat'), 'midgetRGCconnectionMatrix', ...
                     'RGCRFPositionsMicrons', 'RGCRFSpacingsMicrons');
@@ -121,7 +122,7 @@ function MosaicConnectorFull
     zLevels = [0.3 1 ];
     whichLevelsToContour = [1];
     
-    depictRGCTesselation = true;
+    depictRGCTesselation = ~true;
     if (depictRGCTesselation)
         displayIDs = ~true;
         fitEllipse = true;
@@ -134,12 +135,23 @@ function MosaicConnectorFull
     % Compute RF sizes via ellipse fitting
     [semiAxes,rfCenters] = computeRFsizes(zLevels, whichLevelsToContour, midgetRGCconnectionMatrix, conePositionsMicrons, RGCRFPositionsMicrons, coneSpacingsMicrons, coneTypes, roi);
 
+    save(fullfile(tmpDir,'tmp2_step4.mat'), 'semiAxes','rfCenters');
+                
+    plotlabOBJ.applyRecipe(...
+        'renderer', 'painters', ... %'opengl', ...
+        'axesBox', 'on', ...
+        'colorOrder', [0 0 0; 1 0 0.5], ...
+        'axesTickLength', [0.015 0.01]/2,...
+        'axesFontSize', 22, ...
+        'figureWidthInches', 9, ...
+        'figureHeightInches', 9);
+    
     hFig = figure(222);
     eccNeuronsMicrons = sqrt(sum(rfCenters.^2,2));
     eccNeuronsDegs = WatsonRGCModel.rhoMMsToDegs(eccNeuronsMicrons/1000);
     rfCenterRadiusDegs = WatsonRGCModel.rhoMMsToDegs(mean(semiAxes,2)/1000);
     plot(eccNeuronsDegs, rfCenterRadiusDegs, '.'); hold on;
-    set(gca, 'XLim', [0 10], 'YLim', [0 0.3]);
+    set(gca, 'XLim', [0 10], 'YLim', [0 0.1]);
     drawnow;
 
     fName = sprintf('SemiAxes');
