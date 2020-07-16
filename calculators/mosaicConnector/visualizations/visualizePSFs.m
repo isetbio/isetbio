c3a73189839ca267435aa0b8e23a2cae7c7a6a88
Nicolas Cottaris <cottaris@upenn.edu>
2020-07-15 20:38:35 -0400

    Better PSF plotting

--- calculators/mosaicConnector/visualizations/visualizePSFs.m
+++ calculators/mosaicConnector/visualizations/visualizePSFs.m
@@ -16,14 +16,45 @@ function visualizePSFs(theOI, eccX, eccY)
     xGridDegs = psfSupportMicrons{1}/micronsPerDegree;
     thePSFsupportDegs = xGridDegs(1,:);
     
-    xRange = 5/60*[-1 1];
-    figure(1); clf;
-    for wIndex = 1:numel(wavelengthSupport)
-        subplot(3,round(numel(wavelengthSupport)/3)+1,wIndex);
-        imagesc(thePSFsupportDegs, thePSFsupportDegs, squeeze(thePSFs(:,:,wIndex)));
+    xRange = 5*[-1 1];
+    
+    hFig = figure(1); clf;
+    set(hFig,  'Color', [1 1 1]);
+    colsNum = 4;
+    rowsNum = 2;
+    subplotPosVectors = NicePlot.getSubPlotPosVectors(...
+               'rowsNum', rowsNum, ...
+               'colsNum', colsNum, ...
+               'heightMargin',  0.03, ...
+               'widthMargin',    0.02, ...
+               'leftMargin',     0.05, ...
+               'rightMargin',    0.00, ...
+               'bottomMargin',   0.04, ...
+               'topMargin',      0.02);
+           
+    maxPSF = max(thePSFs(:));
+    for wIndex = 1:2:numel(wavelengthSupport)
+        row = floor((wIndex-1)/colsNum)+1;
+        col = mod(wIndex-1,colsNum)+1;
+        subplot('Position', subplotPosVectors(row,col).v);
+        thePSF = squeeze(thePSFs(:,:,wIndex));
+        imagesc(thePSFsupportDegs*60, thePSFsupportDegs*60, thePSF);
+        hold on
+        contour(thePSFsupportDegs*60, thePSFsupportDegs*60, squeeze(thePSFs(:,:,wIndex)), [0.1:0.1:0.9]*maxPSF, 'black', 'LineWidth', 1.0);
         axis 'square'
-        set(gca, 'XLim', xRange, 'YLim', xRange);
-        title(sprintf('%2.0f nm (%2.1f, %2.1f) degs', wavelengthSupport(wIndex), eccX, eccY));
+        set(gca, 'XLim', xRange, 'YLim', xRange, 'CLim', [0 maxPSF], 'ZLim', [0 maxPSF]);
+        set(gca, 'XTick', [-6:2:6], 'YTick', [-6:2:6]);
+        if (row ==rowsNum)&&(col == 1)
+           title(sprintf('%2.0f nm (ecc = %2.1f degs)', wavelengthSupport(wIndex), sqrt(eccX^2+eccY^2)));
+           xlabel('retinal space (arc min)');
+        else
+            title(sprintf('%2.0f nm', wavelengthSupport(wIndex)));
+             set(gca, 'XTickLabel', {}, 'YTickLabel', {})
+        end
+        grid 'on'
+        colormap(brewermap(1024, 'greys'));
+        drawnow;
     end
-    colormap(gray);
+    
+    NicePlot.exportFigToPDF('PSFs', hFig, 300);
 end
