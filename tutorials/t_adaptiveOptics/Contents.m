0c72116dbde5789ef1ccb029a415cd3b02faeb0f
David Brainard <brainard@psych.upenn.edu>
2020-12-13 15:44:43 -0500

    Add corneal irradiance tutorial.

--- tutorials/t_adaptiveOptics/Contents.m
+++ tutorials/t_adaptiveOptics/Contents.m
@@ -3,5 +3,5 @@
 % Illustrates colorimetric calculations in isetbio.
 %
 %   t_AODisplay         - Show how to model an AO display
-%   t_NoLcaOptics       - Create an optical image object with no lca modeled.
-
+%   t_noLcaOptics       - Create an optical image object with no lca  modeled.
+%   t_radianceToCornealIrradiance - Convert spectral radiance to corneal irradiance.
