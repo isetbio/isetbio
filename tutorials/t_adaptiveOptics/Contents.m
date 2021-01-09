b4278a5c2bb98572df58fedbafe3393f4ce0561d
David Brainard <brainard@psych.upenn.edu>
2021-01-09 15:07:06 -0500

    Add bleaching tutorial

--- tutorials/t_adaptiveOptics/Contents.m
+++ tutorials/t_adaptiveOptics/Contents.m
@@ -2,6 +2,7 @@
 %
 % Illustrates colorimetric calculations in isetbio.
 %
+%   t_AOBleachingKinetics - Show how to compute fraction pigment bleached over time in response to series of flashes
 %   t_AODisplay         - Show how to model an AO display
 %   t_noLcaOptics       - Create an optical image object with no lca  modeled.
 %   t_radianceToCornealIrradiance - Convert spectral radiance to corneal irradiance.
