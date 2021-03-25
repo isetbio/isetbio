6bebe483f52c8af4b1e25ac0cc7977837a9d7158
Brian Wandell <wandell@stanford.edu>
2021-03-25 16:55:11 -0700

    Update s_excitationsEccentricity.m

--- calculators/preComputeConeMosaics/s_excitationsEccentricity.m
+++ calculators/preComputeConeMosaics/s_excitationsEccentricity.m
@@ -1,41 +1,55 @@
+%% Caclulate sum of excitations per unit area over different eccentricities
+%
+% Cone apertures increase, but the total number of cones decrease.  What is
+% the next tradeoff as we measure at increasing eccentricities?
+%
+% BW/DHB
+%
+% See also
+%  s_preComputeMosaics
+
+
+%% Build a uniform scene
+thisFOV = 9;
+
+scene = sceneCreate('uniform');
+scene = sceneSet(scene,'fov',thisFOV + 0.5);
+oi = oiCreate; oi = oiCompute(oi,scene);
+
 %% Count excitations as different eccentricities
 
-thisFOV = 1;
 fname = sprintf('cm-%ddeg-22-Mar-2021.mat',thisFOV);
 fname = fullfile(isetRootPath,'local',fname);
 foo = load(fname);
 cm = foo.cm;
-cm.visualize;
-
-vParams = cm.visualize('params');
-cm.visualize(vParams);
-cm.visualize;
-
-scene = sceneCreate('uniform');
-scene = sceneSet(scene,'fov',6);
-oi = oiCreate; oi = oiCompute(oi,scene);
 cm.integrationTime = 50e-3;
-noiseFree = cm.compute(oi);
+% cm.visualize;
 
+
+%{
 vParams = cm.visualize('params');
 vParams.activation = noiseFree;
 vParams.activationColorMap = hot(512);
 vParams.verticalActivationColorBar = true;
 vParams.activationRange = [0 max(noiseFree(:))];
 cm.visualize(vParams);
+%}
 
 %%
 
-c = 0:.5:(thisFOV-1);
-centers = [];
+c = 0:.5:(thisFOV-0.1);
+centers = zeros(numel(c),2);
 for ii=1:numel(c)
-    centers(end+1,:) = [c(ii) c(ii)]/2;
+    centers(ii,:) = [c(ii) c(ii)]/2;
 end
+
+noiseFree = cm.compute(oi);
 
 %%
 
 fprintf('\n\n**********\n\n');
-
+eccResponse = zeros(numel(c),1);
+ecc = c(:);
 for ii=1:size(centers,1)
     
     roi = struct('center', centers(ii,:), 'radius', 0.1);
@@ -52,6 +66,16 @@ for ii=1:size(centers,1)
     % Form a response vector in which only cones within the ROI have a non-zero response
     roiResponse = 0*noiseFree;
     roiResponse(indicesOfConesWithinROI) = noiseFree(indicesOfConesWithinROI);
+    eccResponse(ii) = sum(roiResponse(:));
     fprintf('Center %.2f Excitations %.2f\n',centers(ii,1),sum(roiResponse(:)));
+    
 end
 
+%% 
+ieNewGraphWin;
+plot(ecc,eccResponse,'-o','LineWidth',2);
+xlabel('Eccentricity (deg)');
+ylabel('Sum of excitations');
+grid on
+
+%%
