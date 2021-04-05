d5ee053020ebe697596e1edbb4d8afb97364af02
Nicolas Cottaris <nicolas@santorini.attlocal.net>
2021-04-04 22:32:38 -0400

    Improved renderPSF

--- isettools/eccentricityvarying/+PolansOptics/renderPSF.m
+++ isettools/eccentricityvarying/+PolansOptics/renderPSF.m
@@ -2,22 +2,34 @@ function renderPSF(axesHandle, xSupport, ySupport, thePSF, xyRange, zLevels, cma
     % Parse input
     p = inputParser;
     p.addParameter('superimposedConeMosaic', [], @(x)(isempty(x)||isa(x, 'coneMosaicHex')));
+    p.addParameter('withConeData', [],  @(x)(isempty(x)||isstruct(x)));
     p.addParameter('fontSize', 14, @isnumeric);
     p.addParameter('plotTitle', '', @ischar);
     p.addParameter('xyTicks', -8:1:8, @isnumeric);
     
     p.parse(varargin{:});
     theConeMosaic = p.Results.superimposedConeMosaic;
+    theConeData = p.Results.withConeData;
     xyTicks = p.Results.xyTicks;
     fontSize = p.Results.fontSize;
     plotTitle = p.Results.plotTitle;
     
-    if (~isempty(theConeMosaic))
+    if (~isempty(theConeData))
+        conePositionsArcMin = theConeData.conePositionsArcMin;
+        coneAperturesArcMin = theConeData.coneAperturesArcMin;
+        faceColors = [0.7 0.7 0.9];
+        faceAlpha = 0.32;
+        lineWidth = 1.0;
+        edgeColor = [0.3 0.3 0.9];
+        renderPatchArray(axesHandle, coneAperturesArcMin, conePositionsArcMin, ...
+    faceColors, edgeColor, lineWidth, faceAlpha)
+
+    elseif (~isempty(theConeMosaic))
         % Retrieve cone positions (microns), cone spacings, and cone types
         cmStruct = theConeMosaic.geometryStructAlignedWithSerializedConeMosaicResponse();
         conePositionsArcMin = cmStruct.coneLocs * 60;
         coneAperturesArcMin = cmStruct.coneApertures * 60;
-        renderConeApertures(axesHandle, conePositionsArcMin, coneAperturesArcMin, [1 1 1]-contourLineColor);
+        renderConeApertures(axesHandle, conePositionsArcMin, coneAperturesArcMin, [1 0 0]);
     end
     
     
@@ -74,20 +86,66 @@ function renderPSF(axesHandle, xSupport, ySupport, thePSF, xyRange, zLevels, cma
         title(axesHandle,plotTitle);
     end
     
+    hold(axesHandle, 'off');
     drawnow;
 end
 
 function renderConeApertures(axesHandle, conePositionsArcMin, coneAperturesArcMin, color)
-    hold(axesHandle, 'on');
-    
-    xOutline = cosd(0:5:360);
-    yOutline = sind(0:5:360);
+  
+    xOutline = cosd(0:30:360);
+    yOutline = sind(0:30:360);
+    apertureShape.x = cosd(0:30:360)
     for iCone = 1:size(conePositionsArcMin,1)
         r = 0.5*coneAperturesArcMin(iCone);
         plot(axesHandle, xOutline*r +  conePositionsArcMin(iCone,1), ...
                          yOutline*r +  conePositionsArcMin(iCone,2), ...
-                         'k-', 'Color', color, 'LineWidth', 1.5);
+                         'k-', 'Color', color, 'LineWidth', 1.0);
+                     if (iCone > 1)
+                         hold(axesHandle, 'on');
+                     end
     end
                  
 end
 
+
+function renderPatchArray(axesHandle, apertureRadii, rfCoords, ...
+    faceColors, edgeColor, lineWidth, faceAlpha)
+
+    apertureShape.x = cosd(0:30:360);
+    apertureShape.y = sind(0:30:360);
+    
+    conesNum = numel(apertureRadii);
+    if (conesNum == 0)
+        return;
+    end
+    
+    verticesPerCone = numel(apertureShape.x);
+    
+    verticesList = zeros(verticesPerCone * conesNum, 2);
+    facesList = [];
+    
+    if (numel(faceColors) == 3)
+        colors = repmat(faceColors, [verticesPerCone*conesNum 1]);
+    else
+        colors = [];
+    end
+    
+    for coneIndex = 1:conesNum
+        idx = (coneIndex - 1) * verticesPerCone + (1:verticesPerCone);
+        verticesList(idx, 1) = apertureShape.x*0.5*apertureRadii(coneIndex) + rfCoords(coneIndex,1);
+        verticesList(idx, 2) = apertureShape.y*0.5*apertureRadii(coneIndex) + rfCoords(coneIndex,2);
+        if (numel(faceColors) == conesNum)
+            colors = cat(1, colors, repmat(faceColors(coneIndex), [verticesPerCone 1]));
+        end
+        facesList = cat(1, facesList, idx);
+    end
+
+    S.Vertices = verticesList;
+    S.Faces = facesList;
+    S.FaceVertexCData = colors;
+    S.FaceColor = 'flat';
+    S.EdgeColor = edgeColor;
+    S.FaceAlpha = faceAlpha;
+    S.LineWidth = lineWidth;
+    patch(S, 'Parent', axesHandle);
+end
