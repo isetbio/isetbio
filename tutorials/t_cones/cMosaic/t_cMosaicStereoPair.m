73f897dacf988c9d8d548a80c83a51e5f70726d8
David Brainard <brainard@psych.upenn.edu>
2021-04-10 16:26:34 -0400

    Fixed so it runs without the data file

--- tutorials/t_cones/cMosaic/t_cMosaicStereoPair.m
+++ tutorials/t_cones/cMosaic/t_cMosaicStereoPair.m
@@ -17,13 +17,13 @@ ieInit;
 clear;
 close all;
 
-if (1==2)
+if (1==1)
     mosaicFOV = [3 3];
 
     % Generate left mosaic
     cmLeft = cMosaic(...
         'whichEye', 'left eye', ...                     % Generate mosaic for the left eye
-        'sizeDegs', mosaicFOV, ...                      % SIZE: x=1 degs, y=1 degs
+        'sizeDegs', mosaicFOV, ...                      % SIZE: x,y in degs
         'eccentricityDegs', [0 0], ...                  % ECC:  x=0.0 degs, y= 0.0 degs
         'computeMeshFromScratch', true, ...             % generate mesh on-line, will take some time
         'maxMeshIterations', 300, ...                   % stop iterative procedure after this many iterations
@@ -33,7 +33,7 @@ if (1==2)
     % Generate right mosaic
     cmRight= cMosaic(...
         'whichEye', 'right eye', ...                    % Generate mosaic for the right eye
-        'sizeDegs', mosaicFOV, ...                      % SIZE: x=1 degs, y=1 degs
+        'sizeDegs', mosaicFOV, ...                      % SIZE: x,y in degs
         'eccentricityDegs', [0 0], ...                  % ECC:  x=0.0 degs, y= 0.0 degs
         'computeMeshFromScratch', true, ...             % generate mesh on-line, will take some time
         'maxMeshIterations', 300, ...                   % stop iterative procedure after this many iterations
