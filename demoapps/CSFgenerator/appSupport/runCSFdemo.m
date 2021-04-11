41c641a9ec24fef2306c0f604e43504cbb007f26
Nicolas Cottaris <cottaris@upenn.edu>
2021-04-10 23:06:17 -0400

    Work on CSF app
    Signed-off-by: Nicolas Cottaris <cottaris@upenn.edu>

--- demoapps/CSFgenerator/appSupport/runCSFdemo.m
+++ demoapps/CSFgenerator/appSupport/runCSFdemo.m
@@ -9,12 +9,20 @@ function runCSFdemo()
     % Compute the CSF for the test spatial frequencies using the data exported from the app
     csf = CSFgenerator.computeCSF(examinedSpatialFrequencies, exportedData.stimData, exportedData.pipeline);
     
+    % Load another pipeline also exported from the CSF app.
+    % In this pipeline optics are corrected for the central defocus
     load('CSFappExportSubtractCentralRefraction.mat');
     csfRefracted = CSFgenerator.computeCSF(examinedSpatialFrequencies, exportedData.stimData, exportedData.pipeline);
     
     % Plot data
-    figure();
-    plot(examinedSpatialFrequencies, csf, 'ko-'); hold on;
-    plot(examinedSpatialFrequencies, csfRefracted, 'ro-');
+    hFig = figure(1);
+    set(hFig, 'Position', [100 100 500 800]);
+    plot(examinedSpatialFrequencies, csf, 'ko-', 'LineWidth', 1.5, 'MarkerSize', 12, 'MarkerFaceColor', [0.5 0.5 0.5]); hold on;
+    plot(examinedSpatialFrequencies, csfRefracted, 'ro-', 'LineWidth', 1.5, 'MarkerSize', 12, 'MarkerFaceColor', [1 0.5 0.5]);
+    legend({'uncorrected foveal defocus', 'zero foveal defocus'});
     set(gca, 'XScale', 'log', 'YScale', 'log', 'XLim', [1 100], 'YLim', [1 1000]);
+    set(gca, 'FontSize', 14);
+    xlabel('spatial frequency (c/deg)');
+    ylabel('sensitivity');
+    
 end
