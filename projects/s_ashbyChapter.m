98a1c1b1bc6a095163f56b8be816758cdf6305f2
Brian Wandell <wandell@stanford.edu>
2021-07-09 21:58:54 -0700

    Ashby chapter figures

--- projects/s_ashbyChapter.m
+++ projects/s_ashbyChapter.m
@@ -68,6 +68,8 @@ h = histogram(val(:,3),'Normalization','pdf');
 h.BinLimits = [-5 40]; h.BinWidth = 1; % h.FaceColor = [0.2 0.2 0.2];
 grid on
 title('Normal');
+xlabel('Counts');
+ylabel('Probability density')
 
 %% Calculate cone fundamentals
 
@@ -94,18 +96,61 @@ theLens.density = 1; % 1 is default
 %%
 ieNewGraphWin([],'wide');
 subplot(1,2,1)
+theCones.macular.density = theCones.macular.eccDensity(0);
 coneFundamentals = bsxfun(@times, theLens.transmittance', theCones.qe);
-plot(wave,coneFundamentals,'Linewidth',2);
+plot(wave,coneFundamentals,'Linewidth',3);
 xlabel('Wavelength (nm)');
-ylabel('Efficiency')
+ylabel('Absorptance')
 grid on;
+legend('L-cones','M-cones','S-cones');
+title('Central fovea');
 
 subplot(1,2,2)
 theCones.macular.density = theCones.macular.eccDensity(10); % 0.35 is Default
 coneFundamentals = bsxfun(@times, theLens.transmittance', theCones.qe);
-plot(wave,coneFundamentals,'Linewidth',2);
+plot(wave,coneFundamentals,'Linewidth',3);
 xlabel('Wavelength (nm)');
 ylabel('Absorptance')
 grid on;
+title('10 deg eccentricity');
 
-%%
+%% PSFs ...???
+
+% I stopped the ISETBioCSFGenerator inside the cMosaic.oiEnsembleGenerate
+% function and saved the key variables in the 'projects' directory.
+%
+% save('oiData','obj','oiSamplingGridDegs','varargin');
+
+% Apparently, we can load the objects and parameters in.
+chdir(fullfile(isetbioRootPath,'projects'));
+foo = load('oiData');
+
+[X,Y] = foo.obj.oiEnsembleGenerate(foo.oiSamplingGridDegs,foo.varargin{:});
+
+% Then this runs
+load('oiData','obj','oiSamplingGridDegs','varargin');
+[oiEnsemble, psfEnsemble] = obj.oiEnsembleGenerate(oiSamplingGridDegs,varargin{:});
+
+% To see the code and learn more, 
+%    edit cMosaic.oiEnsembleGenerate
+%
+% The wavelengths and images are in the psfEnsemble variable that is
+% returned.
+
+%{
+varargin =
+
+  1×14 cell array
+
+  Columns 1 through 6
+
+    {'zernikeDataBase'}    {'Polans2015'}    {'subjectID'}    {[10]}    {'pupilDiameterMM'}    {[3]}
+
+  Columns 7 through 13
+
+    {'subtractCentral…'}    {[0]}    {'zeroCenterPSF'}    {[1]}    {'flipPSFUpsideDown'}    {[1]}    {'wavefrontSpatia…'}
+
+  Column 14
+
+    {[301]}
+%}
