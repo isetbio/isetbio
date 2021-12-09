43cf61612854e0b783b782da9d577db992eb2121
Nicolas Cottaris <cottaris@upenn.edu>
2021-12-09 14:10:20 -0500

    Working on @mRGCMosaic class

--- isettools/ganglioncells/@mRGCMosaic/mRGCMosaic.m
+++ isettools/ganglioncells/@mRGCMosaic/mRGCMosaic.m
@@ -1,79 +1,87 @@
 classdef mRGCMosaic < handle
-% Create a midget RGC mosaic connected to a @cMosaic
-
+    % Create an midget RGC mosaic object
+    %
+    % Syntax:
+    %   theMRGCmosaic = mRGCMosaic;
+    %
+    % Description
+    %
+    %
+    % Inputs:
+    %    None required.
+    %
+    % Outputs:
+    %    mRGCMosaic           The created mRGCMosaic object.
+    %
+    % Optional key/value pairs:
+    %    'name'                                 - String. Mosaic name. Default: 'mRGCmosaic'.
+    %    'centerPolarity'                       - String. Center polarity.  Valid options are: {'ON', 'OFF'}. Default: 'ON'.
+    %    'whichEye'                             - Which eye. Valid options are: {'left eye', 'right eye'}. Default: 'right eye'.
+    %    'eccentricityDegs'                     - [x,y]. Center of the mosaic, in degrees. Default: [0 0]
+    %    'sizeDegs'                             - [sx, sy]. Width and height of the mosaic, in degrees. Default: [0.4 0.4]
+    %    'computeMeshFromScratch'               - Logical, indicating whether to recompute the mesh from scratch. Default: false
+    %    'visualizeMeshConvergence'             - Logical, indicating whether to visualize the convergence of the mesh. Default: false 
+    %    'exportMeshConvergenceHistoryToFile'   - Logical, indicating whether to save the convergence of the mesh. Default:false
+    %    'maxMeshIterations'                    - Scalar. Max number of iterations for the mesh generation. Default: 100.
+    
     % Public properties
     properties  (GetAccess=public, SetAccess=public)
         % Name of the mosaic
         name;
-        
-        % Poisson noise flag for cone excitations 
-        noiseFlag;
-        
-        % Random seed
-        randomSeed;
     end
-    
+
     % Read-only properties
     properties (GetAccess=public, SetAccess=private)
-        % [n x 2] matrix of RGC positions, in microns
+
+        % [n x 2] matrix of mRGC RF positions, in microns
         rgcRFpositionsMicrons;
         
-        % [n x 2] matrix of RGC positions, in degrees
+        % [n x 2] matrix of mRGC RF positions, in degrees
         rgcRFpositionsDegs;
         
-        % [n x 1] vector of RGC spacings, in degrees
+        % [n x 1] vector of mRGC RF spacings, in degrees
         rgcRFspacingsDegs;
         
-        % [n x 1] vector of rRGC spacings, in microns
+        % [n x 1] vector of mRGC RF spacings, in microns
         rgcRFspacingsMicrons;
-        
-        % Eccentricity [x,y] of the center of the RGC mosaic (degs)
+
+        % Eccentricity [x,y] of the center of the mRGC mosaic (degs)
         eccentricityDegs;
-        
-        % Eccentricity [x,y] of the center of the RGC mosaic (microns)
-        eccentricityMicrons; 
-        
-        % Size of the RGC mosaic [width, height]
+
+        % Size of the mRGC mosaic [width, height]
         sizeDegs;
         
         % Either 'right eye' or 'left eye'
         whichEye;
-        
-        % Generated input cone mosaic
+
+        % The input cone mosaic (@cMosaic)
         inputConeMosaic;
+
+        % Custom degs <-> mm conversions
+        customDegsToMMsConversionFunction;
+        customMMsToDegsConversionFunction;
         
-        % Passed from input cone mosaic
-        micronsPerDegreeApproximation;
     end
-    
+
     % Private properties
     properties (GetAccess=private, SetAccess=private)
         % Size (in degs) of source lattice from which to crop positions for
         % the desired eccentricity
         sourceLatticeSizeDegs = 58;
-        
-        % Whether to use parfor in computations
+
+         % Whether to use parfor in computations
         useParfor = true;
-        
-        % Min and max cone positions
-        minRFpositionMicrons;
-        maxRFpositionMicrons;
-        
-        % Sparse [conesNum x rgcsNum] sparse  connectivity matrix 
-        % of cone -> rgc. 
-        % To find which cones are connected to a target RGC:
-        %  connectivityVector = full(squeeze(obj.coneConnectivityMatrix(:, targetRGC)));
-        %  inputConeIDs = find(connectivityVector > 0.01);
-        coneConnectivityMatrix;
     end
-    
+
     % Public methods
     methods
+        
         % Constructor
         function obj = mRGCMosaic(varargin)
             % Parse input
             p = inputParser;
             p.addParameter('name', 'mRGC mosaic', @ischar);
+            p.addParameter('centerPolarity', 'ON', @(x)(ischar(x) && (ismember(x, {'ON', 'OFF'}))));
             p.addParameter('whichEye', 'left eye', @(x)(ischar(x) && (ismember(x, {'left eye', 'right eye'}))));
             p.addParameter('eccentricityDegs', [0 0], @(x)(isnumeric(x) && (numel(x) == 2)));
             p.addParameter('sizeDegs', [0.4 0.4], @(x)(isnumeric(x) && (numel(x) == 2)));
@@ -82,37 +90,85 @@ classdef mRGCMosaic < handle
             p.addParameter('visualizeMeshConvergence', false, @islogical);
             p.addParameter('exportMeshConvergenceHistoryToFile', false, @islogical);
             p.addParameter('inputConeMosaic', [], @(x)(isempty(x) || isa(x, 'cMosaic')));
-            p.addParameter('noiseFlag', 'random', @(x)(ischar(x) && (ismember(x, {'random', 'frozen', 'none'}))));
-            p.addParameter('randomSeed', [], @isscalar);
+            p.addParameter('useParfor', true, @islogical);
             p.parse(varargin{:});
-            
+
             obj.name = p.Results.name;
             obj.eccentricityDegs = p.Results.eccentricityDegs;
             obj.sizeDegs = p.Results.sizeDegs;
-            obj.whichEye = p.Results.whichEye;
             
-            obj.noiseFlag = p.Results.noiseFlag;
-            obj.randomSeed = p.Results.randomSeed;
+            % Parallel computations
+            obj.useParfor = p.Results.useParfor;
             
             % Set the inputConeMosaic
             if (isempty(p.Results.inputConeMosaic))
-                % Generate input cone mosaic (make a bit larger so we have
-                % enough cones for the surrounds of the most eccentric RGCs)
+                % If an inputConeMosaic is not passed during instantiation,
+                % we generate one here. The size of the generated cone
+                % mosaic is a little bit larger than the size of the mRGC
+                % mosaic so as to allow even the most peripheral mRGCs
+                % to have inputs from cones in their surround regions
                 maxEccentricityDegs = sqrt(sum((obj.eccentricityDegs + 0.5*obj.sizeDegs).^2,2));
-                extraDegsForRGCSurround = 2.0 * RGCmodels.CronerKaplan.constants.surroundCharacteristicRadiusFromFitToPandMcells(maxEccentricityDegs);
+                maxSurroundSigmaDegs = 1/sqrt(2.0) * RGCmodels.CronerKaplan.constants.surroundCharacteristicRadiusFromFitToPandMcells(maxEccentricityDegs);
+                % At 2.145 x sigma, a 2D Gaussian is at 1% of the peak
+                extraDegsForRGCSurround = 2.146 * maxSurroundSigmaDegs;
 
+                % Compute the input cone mosaic
                 obj.inputConeMosaic = cMosaic(...
-                    'whichEye', obj.whichEye, ...
+                    'whichEye', p.Results.whichEye, ...
                     'sizeDegs', obj.sizeDegs + 2 * extraDegsForRGCSurround, ...
                     'eccentricityDegs', obj.eccentricityDegs, ...
                     'opticalImagePositionDegs', 'mosaic-centered');
+
+                  % Watson's non-linear transformations
+                  obj.customDegsToMMsConversionFunction = @(x) RGCmodels.Watson.convert.rhoDegsToMMs(x);
+                  obj.customMMsToDegsConversionFunction = @(x) RGCmodels.Watson.convert.rhoMMsToDegs(x);
+
             else
+                % Set the input cone mosaic to the one passed during
+                % instantiation
                 obj.inputConeMosaic = p.Results.inputConeMosaic;
+                
+                % Match mm <-> degs conversion functions to those of the inputConeMosaic
+                if (~isempty(obj.inputConeMosaic.micronsPerDegreeApproximation))
+                    % Generate conversion functions based on the inputConeMosaic.micronsPerDegreeApproximation
+                    obj.customDegsToMMsConversionFunction = @(x)(x*1e-3 * obj.inputConeMosaic.micronsPerDegreeApproximation);
+                    obj.customMMsToDegsConversionFunction = @(x)(x*1e+3 / obj.inputConeMosaic.micronsPerDegreeApproximation);
+                else
+                    if (~isempty(obj.inputConeMosaic.customDegsToMMsConversionFunction))
+                        % Use the cMosaic conversion functions
+                        obj.customDegsToMMsConversionFunction = obj.inputConeMosaic.customDegsToMMsConversionFunction;
+                        obj.customMMsToDegsConversionFunction = obj.inputConeMosaic.customMMsToDegsConversionFunction;
+                    else
+                        % Watson's non-linear transformations
+                        obj.customDegsToMMsConversionFunction = @(x) RGCmodels.Watson.convert.rhoDegsToMMs(x);
+                        obj.customMMsToDegsConversionFunction = @(x) RGCmodels.Watson.convert.rhoMMsToDegs(x);
+                    end
+                end
+
+                % Since we were passed an input cone mosaic, we must ensure
+                % that the size of the mRGC mosaic is a little bit smaller
+                % than the cone mosaic, so as to allow even the most peripheral mRGCs
+                % to have inputs from cones in their surround regions
+                maxEccentricityDegs = sqrt(sum((obj.eccentricityDegs + 0.5*obj.sizeDegs).^2,2));
+                maxSurroundSigmaDegs = 1/sqrt(2.0) * RGCmodels.CronerKaplan.constants.surroundCharacteristicRadiusFromFitToPandMcells(maxEccentricityDegs);
+                % At 2.145 x sigma, a 2D Gaussian is at 1% of the peak
+                extraDegsForRGCSurround = 2.146 * maxSurroundSigmaDegs;
+                maxSizeDegs = obj.inputConeMosaic.sizeDegs - extraDegsForRGCSurround;
+
+                for k = 1:2
+                    newSizeDegs = min([obj.sizeDegs(k) maxSizeDegs(k)]);
+                    if (newSizeDegs ~= obj.sizeDegs(k))
+                        fprintf(2, 'mRGCmosaic size (dim.%d) decreased from %2.1f to %2.1f to interface with input cone mosaic\n', ...
+                            k, obj.sizeDegs(k), newSizeDegs);
+                        obj.sizeDegs(k) = newSizeDegs;
+                    end
+                end
             end
-            
-            % Set the microns per deg approximation
-            obj.micronsPerDegreeApproximation = obj.inputConeMosaic.micronsPerDegreeApproximation;
-            
+
+            % Set the whichEye from the input cone mosaic
+            obj.whichEye = obj.inputConeMosaic.whichEye;
+
+            % Compute mRGC RF positions
             if (p.Results.computeMeshFromScratch)
                 % Re-generate lattice
                 obj.regenerateRFPositions(...
@@ -123,58 +179,21 @@ classdef mRGCMosaic < handle
                 % Import positions by cropping a large pre-computed patch
                 obj.initializeRFPositions();
             end
-            
+
             % Remove RFs within the optic disk
             obj.removeRFsWithinOpticNerveHead();
-                
-            % Set random seed
-            if (isempty(obj.randomSeed))
-               rng('shuffle');
-            else
-               rng(obj.randomSeed);
-            end
+            
+        end % Constructor
 
-            % Compute min and max cone position
-            obj.minRFpositionMicrons = squeeze(min(obj.rgcRFpositionsMicrons,[],1));
-            obj.maxRFpositionMicrons = squeeze(max(obj.rgcRFpositionsMicrons,[],1));
-            
-            % Compute ecc in microns
-            if (isempty(obj.minRFpositionMicrons))
-                % No rgcs, set value differently
-                if (~isempty(obj.micronsPerDegreeApproximation))
-                    obj.eccentricityMicrons = obj.eccentricityDegs * obj.micronsPerDegreeApproximation;
-                else
-                    obj.eccentricityMicrons = obj.eccentricityDegs *  300;
-                end
-            else
-                obj.eccentricityMicrons = 0.5*(obj.minRFpositionMicrons + obj.maxRFpositionMicrons);
-            end
-            
-            % Wire mRGCs RF centers to cones of the input cone mosaic
-            visualizeAlignment = false; 
-            visualizeWiringStages = false;
-            obj.wireRFcentersToInputCones(visualizeAlignment, visualizeWiringStages);
-        end
-        
-        % Method to return indices of RFs within an ROI
-        rfIndices = indicesOfRFsWithinROI(obj, roi);
-        
-        % Method to return the cone-to-RGC density ratio for all RGCs
-        densityRatios = coneToRGCDensityRatios(obj);
-        
-        % Method to visualize the mRGCmosaic
-        visualize(obj, varargin);
-        
-        % Method to visualize how the cone mosaic is tesselated by the RGC RF centers
-        visualizeConeMosaicTesselation(obj, ...
-            coneRFpositions, coneRFspacings, ...
-            rgcRFpositions, rgcRFspacings, ...
-            domain, varargin);
-        
-        % Method to report the distribution of cones / RF center
-        connectivityStats(obj, figNo);
-    end
-    
+        % Convert a retinal size in visual degrees at given ecc (also in degs) to its
+        % equivalent linear size in retinal microns
+        sizeMicrons = sizeVisualDegsToSizeRetinalMicrons(obj,sizeVisualDegs, eccDegs);
+
+        % Convert a retinal size in microns at given ecc (also in microns) to its
+        % equivalent angular size in visual degrees
+        sizeDegs = sizeRetinalMicronsToSizeVisualDegs(obj, sizeRetinalMicrons, eccMicrons);
+    end % Public methods
+
     methods (Access=private)
         % Initialize RF positions by importing them from a large previously-computed mesh
         initializeRFPositions(obj);
@@ -184,19 +203,6 @@ classdef mRGCMosaic < handle
         
         % Remove RFs located within the optic disk
         removeRFsWithinOpticNerveHead(obj);
-        
-        % Method to wire RF centers to cones of the input cone mosaic
-        wireRFcentersToInputCones(obj, visualizeAlignment, visualizeWiringStages);
-        
-        % Method to align RGC RFs to cones in the central retina.
-        % Called by obj.wireRFcenterToInputCones()
-        alignRGCsWithCones(obj, coneRFpositionsMicrons, coneRFpositionsDegs, visualizeAlignment)
-    
-        % Method to connect cones to RGC RF centers
-        % Called by obj.wireRFcenterToInputCones()
-        connectConesToRGCcenters(obj, idxConesInsideRGCmosaic, visualizeConnection)
-    
-    end
-    
-end
+    end % Private methods
 
+end
--- isettools/ganglioncells/@mRGCMosaic/regenerateRFPositions.m
+++ isettools/ganglioncells/@mRGCMosaic/regenerateRFPositions.m
@@ -4,23 +4,27 @@ function regenerateRFPositions(obj, maxIterations, visualizeConvergence, exportH
     fovDegs = sqrt(sum(obj.eccentricityDegs.^2,2)) + max(obj.sizeDegs)*1.3;
     
     obj.rgcRFpositionsMicrons = retinalattice.generatePatch(fovDegs, ...
-        'midget ganglion cells', obj.whichEye, exportHistoryToFile, visualizeConvergence, ...
-        obj.useParfor, maxIterations);
-    
+        'midget ganglion cells', obj.whichEye, exportHistoryToFile, ...
+        visualizeConvergence, ...
+        obj.useParfor, maxIterations, ...
+        'customDegsToMMsConversionFunction', obj.customDegsToMMsConversionFunction, ...
+        'customRFspacingFunction', [], ... % do not use a custom mRGC spacing function
+        'customMinRFspacing', [] ...       % do not use a custom min mRGC spacing
+        );
+
     % Convert to degs
-    obj.rgcRFpositionsMicrons = RGCmodels.Watson.convert.rhoMMsToDegs(obj.rgcRFpositionsMicrons*1e-3);
-    
+    obj.rgcRFpositionsDegs = obj.customMMsToDegsConversionFunction(obj.rgcRFpositionsMicrons*1e-3);
+
     % Compute spacings
     obj.rgcRFspacingsDegs = RGCmodels.Watson.convert.positionsToSpacings(obj.rgcRFpositionsDegs);
     obj.rgcRFspacingsMicrons = RGCmodels.Watson.convert.positionsToSpacings(obj.rgcRFpositionsMicrons);
     
     % Crop to desired ROI in degs
     diff = abs(bsxfun(@minus, obj.rgcRFpositionsDegs, obj.eccentricityDegs));
-    idx = find((diff(:,1) <= 0.5*obj.sizeDegs(1)) & (diff(:,2) <= 0.5*obj.sizeDegs(2)));
+    idx = find((diff(:,1) <= 0.55*obj.sizeDegs(1)) & (diff(:,2) <= 0.55*obj.sizeDegs(2)));
     obj.rgcRFpositionsDegs = obj.rgcRFpositionsDegs(idx,:);
     obj.rgcRFpositionsMicrons = obj.rgcRFpositionsMicrons(idx,:);
-    obj.rgcRFspacingsDegs = obj.crgcRFspacingsDegs(idx);
+    obj.rgcRFspacingsDegs = obj.rgcRFspacingsDegs(idx);
     obj.rgcRFspacingsMicrons = obj.rgcRFspacingsMicrons(idx);
     
 end
-
