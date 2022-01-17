cde48f5e5de9059163c03bf5b0dc87c1391b90ce
David Brainard <brainard@psych.upenn.edu>
2022-01-17 15:36:26 -0500

    Starting on this.

--- tutorials/t_cones/t_conesPhotoPigment.m
+++ tutorials/t_cones/t_conesPhotoPigment.m
@@ -12,7 +12,23 @@
 %    
 
 %% Initialize
-ieInit;
+ieInit; clear; close all;
+
+ e 
+lensUnitDensity1 = lens0.unitDensity;
+lensPeakDensity1 = lens0.density;
+lens1 = Lens('wave',wls,'unitDensity',lensUnitDensity1,'density',lensPeakDensity1);
+theOI = oiSet(theOI,'lens',lens1);
+
+% Compute the optical image
+oi = oiCompute(scene, oi);
+
+%% Generate the mosaic
+cm = cMosaic(...
+    'sizeDegs', [1.0 1.5], ...     % SIZE: 1.0 degs (x) 0.5 degs (y)
+    'eccentricityDegs', [0 0], ...  % ECC: (0,0)
+    'eccVaryingConeBlur', true ...
+    );
 
 %% Create photoPigment object
 % Plot spacing at 5 nm looks a little nicer than the default spacing.
@@ -23,7 +39,7 @@ pp = photoPigment('wave', 400:5:700);
 % at 1 nm. The user can set the wavelength representation used for
 % interface with other routines. By default this is 400:10:700.
 
-%%  Absorbance
+%% Absorbance
 % The cone absorbance function used by default is obtained via data routine
 % coneAbsorbanceReadData.
 %
@@ -37,11 +53,12 @@ xlabel('Wavelength (nm)');
 ylabel('Relative sensitivity');
 
 %% Geometry
+%
 % See coneDensityReadData for explanation of retinal coordinate system.
 eccentricity = 0.0;
 angle = 0;
 
 % Spacing (um), aperture (um), density (cones/mm2)
-[s, a, d] = coneSizeReadData('eccentricity', eccentricity, 'angle', angle)
+[s, a, d] = coneSizeReadData('eccentricity', eccentricity, 'angle', angle);
 
 %%
