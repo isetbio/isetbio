c9c9da10ff8eed799b4f84af278d76972264864c
Nicolas Cottaris <cottaris@upenn.edu>
2022-10-11 18:56:01 -0400

    Removed old RGCconnector code which is now replaced with @MosaicConnector

--- isettools/ganglioncells/@RGCconnector/shadedAreaPlot.m
+++
@@ -1,9 +0,0 @@
-function shadedAreaPlot(ax,x,y, baseline, faceColor, edgeColor, faceAlpha, lineWidth, lineStyle)
-    x = [x fliplr(x)];
-    y = [y y*0+baseline];
-    px = reshape(x, [1 numel(x)]);
-    py = reshape(y, [1 numel(y)]);
-    pz = -10*eps*ones(size(py)); 
-    patch(ax,px,py,pz,'FaceColor',faceColor,'EdgeColor', edgeColor, ...
-        'FaceAlpha', faceAlpha, 'LineWidth', lineWidth, 'LineStyle', lineStyle);
-end
