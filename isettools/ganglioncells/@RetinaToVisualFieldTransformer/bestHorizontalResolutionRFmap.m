a7d9bdfb1ea9daf7a91e46e2fa51869d16cd0e5f
Nicolas Cottaris <cottaris@upenn.edu>
2022-11-04 21:22:14 -0400

    Estimate RF rotation via the Radon transform

--- isettools/ganglioncells/@RetinaToVisualFieldTransformer/bestHorizontalResolutionRFmap.m
+++ isettools/ganglioncells/@RetinaToVisualFieldTransformer/bestHorizontalResolutionRFmap.m
@@ -1,31 +1,41 @@
 function [theRotatedRF, rotationDegs] = bestHorizontalResolutionRFmap(theRF, rotationDegs)
-    % Determine rotation that maximizes horizontal resolution
-    if (isempty(rotationDegs))
-        binaryImage = theRF;
-        m1 = min(binaryImage(:));
-        m2 = max(binaryImage(:));
-        binaryImage = imbinarize((binaryImage - m1)/(m2-m1));
-        s = regionprops('table', binaryImage,'Orientation');
-        rotationDegs = s.Orientation(1);
-        fprintf(2,'RF center horizontal resolution maximizing rotation: %2.1f degs\n', rotationDegs);
-    end
 
-    % Rotate RF so as to maximize horizontal resolution
-    theRotatedRF = imrotate(theRF, 90-rotationDegs, 'bilinear', 'crop');
+    if (isempty(rotationDegs))  
+        theta = 0:1:179;
+        [R,xOffset] = radon(theRF,theta);
 
-    debugRotation = false;
-    if (debugRotation)
-        figure(999); clf;
-        subplot(1,2,1);
-        imagesc(theRF);
-        axis 'image';
-    
-        subplot(1,2,2);
-        imagesc(theRotatedRF)
-        axis 'image';
-    
-        colormap(gray(1024));
-        pause
-    end
+        rowIndices = find(abs(xOffset) < 0.2*max(abs(xOffset)));
+        xOffset = xOffset(rowIndices);
+        R = R(rowIndices,:);
 
+        [~,idx] = max(R(:));
+        [row, col] = ind2sub(size(R), idx)
+        rotationDegs = -theta(col);
+
+        debugRadon = false;
+        if (debugRadon)
+            theRotatedRF = imrotate(theRF, rotationDegs, 'bilinear', 'crop');
+            theRotatedRF2 = imrotate(theRF, -rotationDegs, 'bilinear', 'crop');
+
+            figure(1999)
+            subplot(2,2,1);
+            imagesc(theRF); axis 'image'
+        
+            subplot(2,2,2)
+            imshow(R,[],'Xdata',theta,'Ydata',xOffset,'InitialMagnification','fit')
+            set(gca, 'XTick', theta)
+            xlabel('\theta (degrees)')
+            ylabel('offset (pixels from center)')
+            colormap(gca,hot), colorbar
+        
+            subplot(2,2,3);
+            imagesc(theRotatedRF); axis 'image'
+        
+            subplot(2,2,4);
+            imagesc(theRotatedRF2); axis 'image'
+            drawnow
+        end
+   end
+
+   theRotatedRF = imrotate(theRF, rotationDegs, 'bilinear', 'crop');
 end
