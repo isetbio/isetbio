3fa6c1d60a1eb7834430ada384fffc5a7d937c79
Brian Wandell <wandell@stanford.edu>
2022-11-26 14:30:50 -0800

    Update t_cMosaicLetter.m
    Neatening to show students.

--- tutorials/cones/t_cMosaicLetter.m
+++ tutorials/cones/t_cMosaicLetter.m
@@ -1,16 +1,17 @@
-%% Show a letter on a display imaged at the level of the cone mosaic
+%% Simulate a letter imaged at the level of the cone mosaic
 %
+% The letter is simulated on a display. It is then rendered through the
+% optics onto the modern cone mosaic (cMosaic) object.
 %
-% TODO:
+% See also
+%   sceneCreate('letter', ...), displayCreate, ....
+%
+
+% Things TODO:
 %   Write a routine that takes the outline of the letter and superimposes
 %   it on the color image of the cm.
 %
 %   Implement this for the hex mosaic.
-%
-% Wandell, 2019
-%
-% See also
-%   sceneCreate('letter', ...), displayCreate, ....
 %
 
 %%
@@ -25,6 +26,8 @@ scene = sceneCreate('letter', font, display);
 scene = sceneSet(scene,'wangular',0.5);
 
 %{
+% To make a second letter and combine, perhaps?
+
 % family, size, dpi
 scene = sceneCrop(scene,[96 1 sz(2)-97 sz(1)-1]);
 sz = sceneGet(scene,'size');
@@ -39,35 +42,42 @@ scene = sceneCombine(scene,scene2,'direction','horizontal');
 
 sceneWindow(scene);
 
-%% Push the scene through human optics
+%% Calculate through human optics
 
 oi = oiCreate;
 oi = oiCompute(oi,scene);
+
 %oiWindow(oi);
 
 %%  Now image it on the cone mosaic with some fixational eye movements
 
+% Here is a stored cMosaic
 cm = mosaicLoad('cmosaic_0.5-0.5_0.0-0.0.mat'); ecc = 0;
 % cm = mosaicLoad('cmosaic_0.5-0.5_10.0-0.0.mat'); ecc = 10;
-
 % cm = mosaicLoad('cmosaic_1.0-1.0_0.0-0.0.mat'); ecc = 0;
 % cm = mosaicLoad('cmosaic_1.0-1.0_10.0-0.0.mat'); ecc = 10;
+
+% Visualize the loaded mosaic
 cm.visualize();
 
-%% Illustrate the cone excitations
+%% Calculate the cone excitations from a brief exposure
 
 cm.integrationTime = 5/1000;  % 5 ms
 [~,excitations] = cm.compute(oi);
 
-params = cm.visualize('params');
+% To control the visualization, we set these parameters
 % cm.visualize('help');
 
+params = cm.visualize('params');
 params.activation = excitations.^0.5;
 params.activationColorMap = hot(1024);
 params.verticalActivationColorBar = true;
+
 cm.visualize(params);
 
-%% Add eye movements
+%% Visualize the impact of eye movements
+
+% These calculations can take a little while (few minutes)
 eyeMovementDurationSeconds = 400/1000;
 cm.emGenSequence(eyeMovementDurationSeconds, ...
         'microsaccadeType', 'none', ...
@@ -75,33 +85,13 @@ cm.emGenSequence(eyeMovementDurationSeconds, ...
         'randomSeed', 10);
     
 %% Compute noisy response instances of cone excitation response to the same eye movement path
+
 instancesNum = 1;
+
 % (Instances, Time Samples, Cone index)
 [~, excitations, ~,~,timeAxis] = cm.compute(oi, ...
     'withFixationalEyeMovements', true, ...
     'nTrials', instancesNum);
-
-%% Visualize time-series response of a single cone
-%{
-% Find the cone with max noise-free response
-[~,idx] = max(excitations(:));
-[~,~,targetConeID] = ind2sub(size(excitations), idx);
-
-ieNewGraphWin;
-
-% Plot the time series response for individual trials
-plot(timeAxis, squeeze(excitations(:,:,targetConeID)), 'b--');
-hold on;
-
-% Plot the time series response for the mean of the individual instances
-plot(timeAxis, squeeze(mean(excitations(:,:,targetConeID),1)), 'k-', 'LineWidth', 2.0);
-hold on;
-
-xlabel('Time (seconds)');
-ylabel('Excitations per integration time');
-set(gca, 'FontSize', 16);
-grid on;
-%}
 
 %% Plot a movie of the excitations
 fname = sprintf('%s-ecc-%d.mp4',fullfile(isetRootPath,'local','excitations'),ecc);
@@ -137,4 +127,26 @@ cm.movie(timeAxis,current,'filename',fname);
 implay(fname);
 
 %% END
+
+%% Visualize time-series response of a single cone
+%{
+% Find the cone with max noise-free response
+[~,idx] = max(excitations(:));
+[~,~,targetConeID] = ind2sub(size(excitations), idx);
+
+ieNewGraphWin;
+
+% Plot the time series response for individual trials
+plot(timeAxis, squeeze(excitations(:,:,targetConeID)), 'b--');
+hold on;
+
+% Plot the time series response for the mean of the individual instances
+plot(timeAxis, squeeze(mean(excitations(:,:,targetConeID),1)), 'k-', 'LineWidth', 2.0);
+hold on;
+
+xlabel('Time (seconds)');
+ylabel('Excitations per integration time');
+set(gca, 'FontSize', 16);
+grid on;
+%}
 
