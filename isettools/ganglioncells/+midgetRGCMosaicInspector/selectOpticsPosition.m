fed9f2904752024982f05acfa3e1b1443b678e61
Nicolas Cottaris <cottaris@upenn.edu>
2023-01-05 20:20:38 -0500

    Display position of RTVF objects

--- isettools/ganglioncells/+midgetRGCMosaicInspector/selectOpticsPosition.m
+++ isettools/ganglioncells/+midgetRGCMosaicInspector/selectOpticsPosition.m
@@ -3,11 +3,16 @@ function  opticsPositionDegs = selectOpticsPosition(theMidgetRGCmosaic)
     opticsPositionDegs = [];
     while (numel(opticsPositionDegs) ~= 2)
         fprintf('\nSelect the desired optics position. For reference, in this midgetRGCMosaic, RTVFmodels were fitted at the following positions: ')
-        for idx = 1:numel(theMidgetRGCmosaic.theConesNumPooledByTheRFcenterGrid)
-            fprintf('\n[%+2.2f %+2.2f] (for %d-cone RF centers)', ...
-                theMidgetRGCmosaic.theSamplingPositionGrid(idx,1), ...
-                theMidgetRGCmosaic.theSamplingPositionGrid(idx,2), ...
-                theMidgetRGCmosaic.theConesNumPooledByTheRFcenterGrid(idx));
+        centerConesNumExamined = sort(unique(theMidgetRGCmosaic.theConesNumPooledByTheRFcenterGrid), 'ascend');
+        for iCone = 1:numel(centerConesNumExamined)
+            iidx = find(theMidgetRGCmosaic.theConesNumPooledByTheRFcenterGrid == centerConesNumExamined(iCone));
+            for idx = 1:numel(iidx)
+                iObj = iidx(idx);
+                fprintf('\n[%+2.2f %+2.2f] (for %d-cone RF centers)', ...
+                    theMidgetRGCmosaic.theSamplingPositionGrid(iObj,1), ...
+                    theMidgetRGCmosaic.theSamplingPositionGrid(iObj,2), ...
+                    theMidgetRGCmosaic.theConesNumPooledByTheRFcenterGrid(iObj));
+            end
         end
 
         opticsPositionDegs = input('\nEnter the optics position to use for computing retinal stimulus images ([x y]): ');
