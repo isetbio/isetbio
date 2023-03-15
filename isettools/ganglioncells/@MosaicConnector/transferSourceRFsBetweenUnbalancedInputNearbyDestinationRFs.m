e6a02f4175a871fc0cb6637fedd19d062ded2558
Nicolas Cottaris <cottaris@upenn.edu>
2023-03-15 19:31:19 -0400

    MosaicConnector: Transfer cones between nearby RGCs now working for up to 10 center cones.

--- isettools/ganglioncells/@MosaicConnector/transferSourceRFsBetweenUnbalancedInputNearbyDestinationRFs.m
+++ isettools/ganglioncells/@MosaicConnector/transferSourceRFsBetweenUnbalancedInputNearbyDestinationRFs.m
@@ -7,11 +7,11 @@ function transferSourceRFsBetweenUnbalancedInputNearbyDestinationRFs(obj, vararg
     generateProgressVideo = p.Results.generateProgressVideo;
 
     % We only transfer inputs from destinationRF-A to a nearby destination RF-B
-    % if the destinationRF-A has between 3 and 6 inputs
+    % if the destinationRF-A has between 3 and 10 inputs
     % If it has less than 3 inputs, i.e. 2, then this will be dealt by the
     % swapSourceRFsBetweenNearbyDestinationRFs() method which is called later on
     minInputsNum = 3;
-    maxInputsNum = 6;
+    maxInputsNum = 10;
 
     ss = squeeze(sum(obj.connectivityMatrix,1));
     targetedDestinationRFindices = find((ss >= minInputsNum) & (ss <= maxInputsNum));
