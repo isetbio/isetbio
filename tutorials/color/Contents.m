4c81ddd6ec4f80e8eb109488e00860e0362d1262
Brian Wandell <wandell@stanford.edu>
2023-05-24 13:20:58 -0700

    removing more duplication in tutorials.  Scripts next.
    I seem to have lost oiSequence, however.

--- tutorials/color/Contents.m
+++
@@ -1,7 +0,0 @@
-% isetbio/tutorials/t_color
-%
-% Illustrates colorimetric calculations in isetbio.
-%
-%   t_colorEnergyQuanta - Explain the relation between units of energy and unit of quanta.
-%   t_colorMatching     - Tutorial on color matching and displays
-%   t_colorSpectrum     - Apply colorimetric methods to render visible spectrum approximation.
