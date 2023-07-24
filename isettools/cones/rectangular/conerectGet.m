2f3d178a5c9a797e2b691df9926f6b47df5cb7d1
Brian Wandell <wandell@stanford.edu>
2023-07-24 16:08:54 -0700

    Starting to add some functionality, but mostly calibration

--- isettools/cones/rectangular/conerectGet.m
+++ isettools/cones/rectangular/conerectGet.m
@@ -1,4 +1,4 @@
-function val = conrectGet(cm,param,varargin)
+function val = conerectGet(cm,param,varargin)
 % Get method for rectangular cone mosaics
 %
 % Synopsis
@@ -10,3 +10,30 @@ function val = conrectGet(cm,param,varargin)
 %
 %   Not sure we should do it this way or using get.variable name.
 %
+
+if notDefined('cm'), error('coneRectMosaic required.'); end
+if notDefined('param'), error('Parameter required.'); end
+val = [];
+
+switch param
+    case 'absorptions'
+        % cm.get('absorptions','conetype')
+        if isempty(varargin)
+            val = cm.absorptions;
+        else
+            switch ieParamFormat(varargin{1})
+                case {'lcones','l'}
+                    val = cm.absorptions(cm.pattern == 2);
+                case {'mcones','m'}
+                    val = cm.absorptions(cm.pattern == 3);
+                case {'scones','s'}
+                    val = cm.absorptions(cm.pattern == 4);
+            end
+        end
+
+    otherwise
+        error('Unknown parameter %s\n',param);
+end
+
+end
+
