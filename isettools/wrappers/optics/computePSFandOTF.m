a7425aa1c24e57aef3d4a80049e707a1e3de5593
David Brainard <brainard@psych.upenn.edu>
2023-12-12 17:58:49 -0500

    Added comments so there is some hope of figuring out what we did here.

--- isettools/wrappers/optics/computePSFandOTF.m
+++ isettools/wrappers/optics/computePSFandOTF.m
@@ -1,23 +1,16 @@
 function [PSFs, OTFs, xSfCyclesDeg, ySfCyclesDeg, xMinutes, yMinutes, theWVF] = ...
         computePSFandOTF(zcoeffsMicrons, wavelengthsListToCompute, wavefrontSpatialSamples, measPupilDiamMM, ...
         targetPupilDiamMM, measWavelength, showTranslation, varargin)
-    
-% BW:
-% Uncommented upon arrival.
-% 
-% This function really shouldn't exist. If we want a wvf to provide a psf
-% and otf, we should use wvfGet.  This function doesn't respect the fact
-% that we have a wvf struct and that we have already built methods to
-% calculate the PSF and OTF from the OTF.
+% This function centers the PSF.  It appears to do so by adjusting the
+% phase of the OTF, so the OTF an PSF remain matched.
 %
-% In the ISETCam branch, I am going to remove it and replace it with the
-% wavefront functions.  This may cause some slight differences in the
-% numerical values.
-% 
-% NC seems to be using it as part of oiForSubjectAtEccentricity and
-% diffractionLimitedOptics.
-%
-%
+% When we changed from ISETBio to ISETCam, the convention of the OTF
+% returned by wvfGet changed from zero sf in the upper left (ISETBio) to
+% zero sf in the middle (ISETCam).  That broke this routine, but we have
+% fixed it as commented below by putting a call to ifftshift() around the 
+% OTFs returned by wvfGet.  We are currently thinking this is all that is
+% needed.  Comments about those places are below.
+
     p = inputParser;
     p.addParameter('doNotZeroCenterPSF', false, @islogical);
     p.addParameter('micronsPerDegree', 300, @isscalar);
@@ -56,6 +49,11 @@ function [PSFs, OTFs, xSfCyclesDeg, ySfCyclesDeg, xMinutes, yMinutes, theWVF] = 
             % Retrieve OTF closest to the measurement wavelength]
             theWavelengthSupport = wvfGet(theWVF, 'wave');
             [~,idx] = min(abs(theWavelengthSupport-measWavelength));
+
+            % In the ISETCam branch, wvfGet returns the OTF with zero sf in the
+            % middle.  But this routine was expecting it to have zero in the
+            % upper left, as accomplished by ifftshift. Thus we apply an
+            % ifftshift() here.
             theCenteringOTF = ifftshift(wvfGet(theWVF, 'otf', theWavelengthSupport(idx)));
             theCenteringPSF = wvfGet(theWVF, 'psf', theWavelengthSupport(idx));
             translationVector = []; showTranslation = false;
@@ -68,13 +66,21 @@ function [PSFs, OTFs, xSfCyclesDeg, ySfCyclesDeg, xMinutes, yMinutes, theWVF] = 
         end
     end
     
-    
     for wIndex = 1:numel(wavelengthsListToCompute)
+        % In the ISETCam branch, wvfGet returns the OTF with zero sf in the
+        % middle.  But this routine was expecting it to have zero in the
+        % upper left, as accomplished by ifftshift. Thus we apply an
+        % ifftshift here.
         theWaveOTF = ifftshift(wvfGet(theWVF, 'otf', wavelengthsListToCompute(wIndex)));
+
+        % The rest of this code then works as it did in the ISETBio branch,
+        % where the OTF had zero sf in the upper left.
         theWavePSF = wvfGet(theWVF, 'psf', wavelengthsListToCompute(wIndex));
         [theWaveOTF, ~, ~, ~,~] = ...
             otfWithZeroCenteredPSF(theWaveOTF, theWavePSF, translationVector,  xSfGridCyclesDegGrid, ySfGridCyclesDegGrid, showTranslation);
         
+        % OtfToPsf expects zero sf in the middle, hence the fftshift() in
+        % this call, as was there in the ISETBio version.
         [xGridMinutes,yGridMinutes, theWavePSF] = ...
             OtfToPsf(xSfGridCyclesDegGrid,ySfGridCyclesDegGrid,fftshift(theWaveOTF));
         if (wIndex == 1)
@@ -82,6 +88,11 @@ function [PSFs, OTFs, xSfCyclesDeg, ySfCyclesDeg, xMinutes, yMinutes, theWVF] = 
             PSFs = zeros(size(theWavePSF,1), size(theWavePSF,2), numel(wavelengthsListToCompute));
         end
         
+        % The fftshift() puts the OTF back with zero sf in the middle.
+        % I'm not sure why this was being done in the ISETBio version, but
+        % it seems right. And, OTF was never picked up on return by any of
+        % the existing calls to this routine, so I think it is OK to have
+        % it now match the format returned by wvfGet().
         OTFs(:,:,wIndex) = fftshift(theWaveOTF);
         PSFs(:,:,wIndex) = theWavePSF;
         
