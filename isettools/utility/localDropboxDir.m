0edd63597007d4189ecb634c05fe359080d48607
Nicolas Cottaris <cottaris@upenn.edu>
2024-01-01 23:14:23 -0500

    Fixed & simplified t_hyperspectralSceneTutorial

---
+++ isettools/utility/localDropboxDir.m
@@ -0,0 +1,9 @@
+function dropboxDirPath = localDropboxDir()
+    dbJsonConfigFile = '~/.dropbox/info.json';
+    fid = fopen(dbJsonConfigFile);
+    raw = fread(fid,inf);
+    str = char(raw');
+    fclose(fid);
+    val = jsondecode(str);
+    dropboxDirPath = val.business.path;
+end
