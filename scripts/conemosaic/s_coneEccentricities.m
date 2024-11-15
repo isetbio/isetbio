46f5351006ea5bc3e124bf7c479f2896e7019a6b
Brian Wandell <1837145+wandell@users.noreply.github.com>
2024-11-15 15:14:53 -0800

    Update s_coneEccentricities.m

--- scripts/conemosaic/s_coneEccentricities.m
+++ scripts/conemosaic/s_coneEccentricities.m
@@ -1,19 +1,22 @@
 % s_coneEccentricities
 % 
-% Obtain the cone eccentricities (signed) from a cmosaic
+% Estimate the cone apertures as a function of eccentricity.
+% Uses the cMosaic
 %
+% See also
+%   cMosaic,
 
 %% Whether to enable rod intrusion or not
-rodIntrusionAdjustedConeAperture = true;
+rodIntrusion = true;
 
 % Generate a 80x1 deg @cMosaic
 c = cMosaic(...
   'sourceLatticeSizeDegs', 64, ...
   'positionDegs', [0 0], ...
   'sizeDegs', [80 1.0], ...
-  'rodIntrusionAdjustedConeAperture', rodIntrusionAdjustedConeAperture);
+  'rodIntrusionAdjustedConeAperture', rodIntrusion);
 
-theConeSignedEccentricityDegs = sqrt(sum(c.coneRFpositionsDegs.^2,2)) .* sign(c.coneRFpositionsDegs(:,1));
+coneEccentricity = sqrt(sum(c.coneRFpositionsDegs.^2,2)) .* sign(c.coneRFpositionsDegs(:,1));
 
 %% Setup figure
 ieNewGraphWin([],'tall');
@@ -21,35 +24,38 @@ tiledlayout(3,1);
 
 % Plot the variation in cone spacing with eccentricity
 nexttile
-plot(theConeSignedEccentricityDegs, c.coneRFspacingsDegs*60, 'r.');
+plot(coneEccentricity, c.coneRFspacingsDegs*60, 'r.');
 set(gca, 'XTick', -40:5:40, 'XLim', 40*[-1 1], 'YLim', [0 4], 'YTick', 0:0.5:10);
 xtickangle(0)
 grid on
-xlabel('(temporal retina) eccentricity (degs) (nasal retina)');
-ylabel('cone spacings (arc min)');
+xlabel('(  temporal  ) eccentricity (degs) (  nasal  )');
+ylabel('cone spacing (arc min)');
 set(gca, 'FontSize', 20);
 
-%% Plot the variation in cone aperture with eccentricity
+%% Plot the variation in cone aperture (microns) with eccentricity
+
 nexttile
-plot(theConeSignedEccentricityDegs, c.coneApertureDiametersDegs*c.micronsPerDegree, 'r.');
+apertureMicrons = c.coneApertureDiametersDegs*c.micronsPerDegree;
+plot(coneEccentricity, apertureMicrons, 'r.');
 set(gca, 'XTick', -40:5:40, 'XLim', 40*[-1 1], 'YLim', [0 10], 'YTick', 0:2:10);
 xtickangle(0)
 grid on
-ylabel('cone apertures (arc min)');
-xlabel('(temporal retina) eccentricity (degs) (nasal retina)');
-set(gca, 'FontSize', 20);
-
-%% Cone aperture area (um^2)
-
-nexttile;
-coneApertureArea = (c.coneApertureDiametersDegs*c.micronsPerDegree).^2;
-plot(theConeSignedEccentricityDegs, coneApertureArea, 'r.');
-set(gca, 'XTick', -40:5:40, 'XLim', 40*[-1 1], 'YLim', [0 50], 'YTick', 0:10:50);
-xtickangle(0)
-grid on 
-ylabel('cone apertures (um^2)');
+ylabel('cone aperture (arc min)');
 xlabel('(  temporal  ) eccentricity (degs) (  nasal  )');
 set(gca, 'FontSize', 20);
+
+%% Cone aperture area (um^2) assuming circular aperture (pi r2)
+
+% nexttile;
+ieNewGraphWin;
+coneApertureArea = pi*(apertureMicrons/2).^2;
+p = plot(coneEccentricity, coneApertureArea, 'r.');
+set(gca, 'XTick', -40:10:40, 'XLim', 40*[-1 1], 'YLim', [0 50], 'YTick', 0:10:50);
+xtickangle(0)
+grid on
+ylabel('cone aperture (um^2)');
+xlabel('(  temporal  ) eccentricity (degs) (  nasal  )');
+set(gca, 'FontSize', 24);
 
 
 %% Export PDFs
