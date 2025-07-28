b56ba204e00251c2f98d63b25dea7075fd3bbd1d
Nicolas Cottaris <cottaris@upenn.edu>
2025-07-28 10:59:35 +0300

    Generate left eye 64 dec cone mosaic
    Signed-off-by: Nicolas Cottaris <cottaris@upenn.edu>

--- isettools/ganglioncells/demos/testLatticeGenerator.m
+++ isettools/ganglioncells/demos/testLatticeGenerator.m
@@ -1,32 +1,47 @@
 % Function to either generate a lattice or save the lattice at some point
 % in its progression.
 function testLatticeGenerator
-    fovDegs = 60;
+    fovDegs = 64;
     exportHistoryToFile = true;
     visualizeConvergence = true; 
     useParfor = true;
     maxIterations = 8000;
-    neuronType = 'midget ganglion cells'; % Select from { 'cones', 'midget ganglion cells'};
+    neuronType = 'cones'; % midget ganglion cells'; % Select from { 'cones', 'midget ganglion cells'};
 
-    generateNewPatch = ~true;
-    
+    generateNewPatch = true;
+    generateLeftEyeMosaic = true;
+    generateRightEyeMosaic = ~true;
+
     if (generateNewPatch)
-        whichEye = 'right eye';
-        retinalattice.generatePatch(fovDegs, neuronType, whichEye, ...
-            exportHistoryToFile, visualizeConvergence, useParfor, maxIterations, ...
-                'eccentricityLookUpTableSamplesNum', 256);
+        if (generateRightEyeMosaic)
+            whichEye = 'right eye';
+            retinalattice.generatePatch(fovDegs, neuronType, whichEye, ...
+                exportHistoryToFile, visualizeConvergence, useParfor, maxIterations, ...
+                    'eccentricityLookUpTableSamplesNum', 256);
+        end
 
-        whichEye = 'left eye';
-        retinalattice.generatePatch(fovDegs, neuronType, whichEye, ...
-            exportHistoryToFile, visualizeConvergence, useParfor, maxIterations, ...
-             'eccentricityLookUpTableSamplesNum', 256);
+        if (generateLeftEyeMosaic)
+            whichEye = 'left eye';
+            retinalattice.generatePatch(fovDegs, neuronType, whichEye, ...
+                exportHistoryToFile, visualizeConvergence, useParfor, maxIterations, ...
+                 'eccentricityLookUpTableSamplesNum', 256);
+        end
 
         %neuronType = 'midget ganglion cells';
         %retinalattice.generatePatch(fovDegs, neuronType, whichEye);
     else
-        whichEye = 'left eye';
-        retinalattice.savePositionsAtIteration(fovDegs, neuronType, whichEye);
-        %retinalattice.inspectPatch(fovDegs, neuronType, whichEye);
+        if (generateLeftEyeMosaic)
+            whichEye = 'left eye';
+            retinalattice.savePositionsAtIteration(fovDegs, neuronType, whichEye);
+            %retinalattice.inspectPatch(fovDegs, neuronType, whichEye);
+        end
+
+        if (generateRightEyeMosaic)
+            whichEye = 'right eye';
+            retinalattice.savePositionsAtIteration(fovDegs, neuronType, whichEye);
+            %retinalattice.inspectPatch(fovDegs, neuronType, whichEye);
+        end
+
     end
     
 end
