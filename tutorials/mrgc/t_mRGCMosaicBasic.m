7ef6d8bc55faa97a60b95569575ad05aad7f24c9
Nicolas Cottaris <cottaris@upenn.edu>
2025-08-26 12:46:24 +0300

    Comments on basic mRGC tutorial
    Signed-off-by: Nicolas Cottaris <cottaris@upenn.edu>

--- tutorials/mrgc/t_mRGCMosaicBasic.m
+++ tutorials/mrgc/t_mRGCMosaicBasic.m
@@ -2,9 +2,11 @@
 %% Introduction to the 2025 midget RGC mosaic (mRGCMosaic) object.
 %
 % Description:
-%    Demonstrates: how to load of of pre-baked midget RGC mosaics, compute
-%    its response to a checkberboard stimulus, and visualize the response
-%    of the input cone mosaic along with the response of the mRGC mosaic
+%    Demonstrates the following:
+%    (i)   how to load apre-baked midget RGC mosaic, 
+%    (ii)  generate custom optics for the computation,
+%    (iii) compute the response of the mRGC mosaic to a checkberboard stimulus, and
+%    (iv)  visualize the responses of the mRGC mosaic and of its input cone mosaic
 %
 
 
@@ -20,35 +22,54 @@ function t_mRGCMosaicBasic
     % List the locally-available prebaded mosaics
     % themRGCmosaicFileNames = mRGCMosaic.listPrebakedMosaics();
    
-    % Mosaic params
+    % Load one of the prebaked mRGC mosaics. 
+    % We need to specify 3 arguments:
+    % (i) the eccentricity and size of the mosaic
+    % (ii) the surround optimization method
+    % (iii) the optics under which the mosaic was optimized
+    % (iv) the optics to employ for the computation at hand
+
+    % (i) Eccentricity (-4.0) and size (3x3deg) 
     mosaicParams.eccDegs  = [-4 0];
     mosaicParams.sizeDegs = [3 3];
+
+    % (ii) Surround optimization method
     mosaicParams.spatialCompactnessSpectralPurityTradeoff = 1;
     mosaicParams.surroundOptimizationSubString = 'PackerDacey2002H1freeLowH1paramsNarrowVisualSTFparamTolerance_vSTF_1.0_1.0';
+
+    % We will employ a smaller (2x1) patch centered at (-5,0), cropped from
+    % the full mosaic. If cropParams is not specified, we employ the full
+    % mosaic
     mosaicParams.cropParams = struct(...
         'sizeDegs', [2 1], ...
         'eccentricityDegs', [-5 0] ...
         );
 
-    % Optics params
+
+    % (iii) Optics under which the mosaic was optimized
     opticsParams.ZernikeDataBase = 'Polans2015';
     opticsParams.subjectRankOrder = 2; 
-
-    % Adaptive optics
-    opticsParams.type = 'adaptiveOptics6MM';
-    opticsParams. refractiveErrorDiopters = [];
-
-    % native optics + StrehlRatio optimization (what was used to optimize the mosaic
-    opticsParams.type = 'nativeOptics';
-    opticsParams.refractiveErrorDiopters = [];
-
-    % native optics without StrehlRatio optimization
-    %opticsParams.type = 'customRefraction';
-    %opticsParams. refractiveErrorDiopters = 0.0;
     opticsParams.visualizePSFonTopOfConeMosaic = true;
 
-    % Load the desired mRGCmosaic and the associated optics
-    [theMRGCmosaic, theOI] = mRGCMosaic.loadPrebakedMosaic(mosaicParams, opticsParams);
+
+    % (iv) Optics to employ for the computation at hand
+    
+    % EITHER adaptive optics (diffraction limited with 6 mm pupil)
+    %opticsParams.type = 'adaptiveOptics6MM';
+    %opticsParams. refractiveErrorDiopters = [];
+    
+    % OR the native optics + StrehlRatio optimization (what was used to optimize the mosaic
+    %opticsParams.type = 'nativeOptics';
+    %opticsParams.refractiveErrorDiopters = [];
+
+    % OR the native optics without a custom refraction, here -3.5D
+    opticsParams.type = 'customRefraction';
+    opticsParams.refractiveErrorDiopters = -3.5;
+    
+    
+    % Load the desired mRGCmosaic and generated the optics for the
+    % computation
+    [theMRGCmosaic, theOpticsToEmploy] = mRGCMosaic.loadPrebakedMosaic(mosaicParams, opticsParams);
     theMRGCmosaic.visualize();
 
     % Input stimulus
@@ -61,7 +82,7 @@ function t_mRGCMosaicBasic
     theStimulusScene = sceneSet(theStimulusScene, 'fov', imageFOVdegs);
 
     % Compute the retinal image
-    theStimulusRetinalImage = oiCompute(theOI, theStimulusScene);
+    theStimulusRetinalImage = oiCompute(theOpticsToEmploy, theStimulusScene);
 
     % Compute the input cone mosaic response to the retinal image of the stimulus
     [theNoiseFreeConeMosaicExcitationsResponse, ...
@@ -94,12 +115,4 @@ function t_mRGCMosaicBasic
         'axesHandle', ax, ...
         'activation', theNoiseFreeSpatioTemporalMRCMosaicResponse, ...
         'plotTitle', 'mRGC mosaic response');
-
-
 end
-
-
-
-% Supporting functions
-
-
