d9d44f975ec8e25529676bf49b4ed715d7141c69
Nicolas Cottaris <cottaris@upenn.edu>
2025-10-04 20:17:17 -0400

    Updated validation tutorial
    Signed-off-by: Nicolas Cottaris <cottaris@upenn.edu>

--- tutorials/mrgc/t_mRGCMosaicBasicValidationAgainstCronerAndKaplan.m
+++ tutorials/mrgc/t_mRGCMosaicBasicValidationAgainstCronerAndKaplan.m
@@ -6,12 +6,19 @@ function t_mRGCMosaicBasicValidationAgainstCronerAndKaplan
     clearvars;
     close all;
 
-    % Use 6 CPU cores
-    AppleSiliconParPoolManager(4);
+    
 
     % Load an 11x11 deg mosaic that was synthesized at 7 degrees along the nasal meridian
     mosaicParams.eccDegs  = [7 0];
     mosaicParams.sizeDegs = [11 11];
+
+    positionDegs = [5 0];
+    sizeDegs = [1 1];
+    % Crop a small patch (2x2 degs) of the mosaic, centered at 7 degrees
+    mosaicParams.cropParams = struct(...
+            'sizeDegs', sizeDegs, ...
+            'eccentricityDegs', positionDegs ...
+            );
 
 
     % (B) Surround optimization method
@@ -30,65 +37,83 @@ function t_mRGCMosaicBasicValidationAgainstCronerAndKaplan
     % Subdirectory for exporting the generated PDFs
     exportVisualizationPDFdirectory = 'mosaicValidationPDFs';
 
-    
-    positionDegs = [5 0];
-    sizeDegs = [1 1];
-
     % Params for the spatial transfer function
     minSF = 0.01; maxSF = 30; sfSamplesNum = 8;
     stfParams.sfSupport = logspace(log10(minSF), log10(maxSF), sfSamplesNum);
     stfParams.orientationDeltaDegs = 45;
     stfParams.spatialPhaseIncrementDegs = 30;
 
-    theMRGCmosaic = computeSTF(mosaicParams, opticsParams, stfParams, positionDegs, sizeDegs, ...
+    % Compute the input cone mosaic STF responses and the mRGC mosaic STF
+    % responses
+    computeInputConeMosaicResponses = ~true;
+    computeMRGCMosaicResponses = ~true;
+    reAnalyzeSTFData = ~true;
+
+    if (computeInputConeMosaicResponses || computeMRGCMosaicResponses)
+        % Use 4 CPU cores
+        AppleSiliconParPoolManager(4);
+    else
+        AppleSiliconParPoolManager('extreme');
+    end
+
+    computeSTF(mosaicParams, opticsParams, ...
+        stfParams, ...
+        computeInputConeMosaicResponses, ...
+        computeMRGCMosaicResponses, ...
+        reAnalyzeSTFData, ...
         exportVisualizationPDFdirectory);
 
 end
 
-function theMRGCmosaic = computeSTF(mosaicParams, opticsParams, stfParams, positionDegs, sizeDegs, exportVisualizationPDFdirectory)
+function computeSTF(mosaicParams, opticsParams, ...
+    stfParams, ...
+    computeInputConeMosaicResponses, ...
+    computeMRGCMosaicResponses, ...
+    reAnalyzeSTFData, ...
+    exportVisualizationPDFdirectory)
 
-    % Crop a small patch (2x2 degs) of the mosaic, centered at 7 degrees
-    mosaicParams.cropParams = struct(...
-        'sizeDegs', sizeDegs, ...
-        'eccentricityDegs', positionDegs ...
-        );
 
-    % Load the desired mRGCmosaic and generated the optics for the computation
-    [theMRGCmosaic, opticsForSTFresponses, thePSF] = mRGCMosaic.loadPrebakedMosaic(mosaicParams, opticsParams);
+    if (computeInputConeMosaicResponses || computeMRGCMosaicResponses)
+
+        % Load the desired mRGCmosaic and generated the optics for the computation
+        [theMRGCmosaic, opticsForSTFresponses, thePSF] = mRGCMosaic.loadPrebakedMosaic(mosaicParams, opticsParams);
     
-    % Visualize the mosaic
-    visualizeTheMosaic(theMRGCmosaic, exportVisualizationPDFdirectory);
+        % Visualize the mosaic
+        visualizeTheMosaic(theMRGCmosaic, exportVisualizationPDFdirectory);
+
+        if (computeInputConeMosaicResponses)
+            % Determine the stimulus pixel resolution to be a fraction of the minimum cone aperture or cone spacing in the mosaic
+            % here, half of the cone spacing
+            theMetric = 'cone aperture';  % choose from {'cone aperture' or cone spacing'}
+            if ((strcmp(opticsForSTFresponses, 'adaptiveOptics6MM')) || (strcmp(opticsForSTFresponses, 'adaptiveOptics6MMwithLCA')))
+                theFraction = 0.1;
+            else
+                theFraction = 0.25;
+            end
+            targetRGCindices =  1:theMRGCmosaic.rgcsNum;
+            stimulusResolutionDegs = RGCMosaicConstructor.helper.simulateExperiment.stimulusResolutionFromConeApertureOrConeSpacing(...
+                        theMRGCmosaic, targetRGCindices, theFraction, theMetric);
 
 
-    % Determine the stimulus pixel resolution to be a fraction of the minimum cone aperture or cone spacing in the mosaic
-    % here, half of the cone spacing
-    theMetric = 'cone aperture';  % choose from {'cone aperture' or cone spacing'}
-    if ((strcmp(opticsForSTFresponses, 'adaptiveOptics6MM')) || (strcmp(opticsForSTFresponses, 'adaptiveOptics6MMwithLCA')))
-        theFraction = 0.1;
-    else
-        theFraction = 0.25;
-    end
-    targetRGCindices =  1:theMRGCmosaic.rgcsNum;
-    stimulusResolutionDegs = RGCMosaicConstructor.helper.simulateExperiment.stimulusResolutionFromConeApertureOrConeSpacing(...
-                theMRGCmosaic, targetRGCindices, theFraction, theMetric);
+            % Chromaticity for validation STF responses
+            chromaticityForSTFresponses = 'Achromatic';   % Choose between 'LconeIsolating', 'MconeIsolating', 'Achromatic'
+            coneFundamentalsOptimizedForStimPosition = true;
 
+            % Params struct for the STF
+            STFparamsStruct = struct(...
+                'backgroundChromaticity', [0.301 0.301], ...
+                'backgroundLuminanceCdM2', 40.0, ...
+                'chromaticity', 'Achromatic', ...
+                'coneFundamentalsOptimizedForStimPosition', true, ...
+                'resolutionDegs', stimulusResolutionDegs, ...                       % to be determined separately for each optimization position
+                'sfSupport', stfParams.sfSupport, ...
+                'orientationDeltaDegs', stfParams.orientationDeltaDegs, ...
+                'spatialPhaseIncrementDegs', stfParams.spatialPhaseIncrementDegs, ...
+                'positionDegs', theMRGCmosaic.eccentricityDegs, ...  
+                'sizeDegs', 1.1*max(theMRGCmosaic.inputConeMosaic.sizeDegs));
+        end % if (computeInputConeMosaicResponses)
+    end % if (computeInputConeMosaicResponses || computeMRGCMosaicResponses)
 
-    % Chromaticity for validation STF responses
-    chromaticityForSTFresponses = 'Achromatic';   % Choose between 'LconeIsolating', 'MconeIsolating', 'Achromatic'
-    coneFundamentalsOptimizedForStimPosition = true;
-
-    % Params struct for the STF
-    STFparamsStruct = struct(...
-        'backgroundChromaticity', [0.301 0.301], ...
-        'backgroundLuminanceCdM2', 40.0, ...
-        'chromaticity', 'Achromatic', ...
-        'coneFundamentalsOptimizedForStimPosition', true, ...
-        'resolutionDegs', stimulusResolutionDegs, ...                       % to be determined separately for each optimization position
-        'sfSupport', stfParams.sfSupport, ...
-        'orientationDeltaDegs', stfParams.orientationDeltaDegs, ...
-        'spatialPhaseIncrementDegs', stfParams.spatialPhaseIncrementDegs, ...
-        'positionDegs', theMRGCmosaic.eccentricityDegs, ...  
-        'sizeDegs', 1.1*max(theMRGCmosaic.inputConeMosaic.sizeDegs));
 
     % Filenames for intermediate responses
     p = getpref('isetbio');
@@ -109,25 +134,25 @@ function theMRGCmosaic = computeSTF(mosaicParams, opticsParams, stfParams, posit
         intermediateDataDir, theCronerKaplanAnalysisFileName, ...
         'generateMissingSubDirs', true);
 
-    % Whether to visualize the computed input cone mosaic STF responses
-    visualizeInputConeMosaicResponses = ~true;
+    if (computeInputConeMosaicResponses || computeMRGCMosaicResponses)
+        % Whether to visualize the computed input cone mosaic STF responses
+        visualizeInputConeMosaicResponses = ~true;
 
-    % Compute the input cone mosaic STF responses and the mRGC mosaic STF
-    % responses
-    computeInputConeMosaicResponses = ~true;
-    computeMRGCMosaicResponses = ~true;
     
-    RGCMosaicConstructor.helper.simulateExperiment.spatialTransferFunction(...
-        theMRGCmosaic, opticsForSTFresponses, ...
-        STFparamsStruct, ...
-        theInputConeMosaicSTFResponsesFullFileName, ...
-        theMRGCMosaicSTFResponsesFullFileName, ...
-        'computeInputConeMosaicResponses', computeInputConeMosaicResponses, ...
-        'computeMRGCMosaicResponses', computeMRGCMosaicResponses , ...
-        'visualizeResponse', visualizeInputConeMosaicResponses);
+        RGCMosaicConstructor.helper.simulateExperiment.spatialTransferFunction(...
+            theMRGCmosaic, opticsForSTFresponses, ...
+            STFparamsStruct, ...
+            theInputConeMosaicSTFResponsesFullFileName, ...
+            theMRGCMosaicSTFResponsesFullFileName, ...
+            'computeInputConeMosaicResponses', computeInputConeMosaicResponses, ...
+            'computeMRGCMosaicResponses', computeMRGCMosaicResponses , ...
+            'visualizeResponse', visualizeInputConeMosaicResponses);
+
+        return;
+    end
 
 
-    reAnalyzeSTFData = true;
+    
     aggregatePreviouslyAnalyzedRunsFromMultipleTargetVisualSTFs = false;
     aggregatePreviouslyAnalyzedRunsFromMultipleEccentricities = false;
     onlyDisplayCronerKaplanData = false;
@@ -150,6 +175,7 @@ function theMRGCmosaic = computeSTF(mosaicParams, opticsParams, stfParams, posit
     targetedCenterConeNumerosityRange = [];     % [1 1], etc, empty means all numerosities
     targetedCenterPurityRange = [];
 
+
     [RsToRcVarianceCK, intStoCsensVarianceCK, RsToRcVariance, intStoCsensVariance] = ...
         RGCMosaicAnalyzer.compute.CronerAndKaplanSTFanalysis(...
             theMRGCMosaicSTFResponsesFullFileName, ...
@@ -161,6 +187,7 @@ function theMRGCmosaic = computeSTF(mosaicParams, opticsParams, stfParams, posit
             targetedCenterConeNumerosityRange, ...
             targetedCenterPurityRange, ...
             reAnalyzeSTFData, ...
+            exportVisualizationPDFdirectory, ...
             'onlyDisplayCronerKaplanData', onlyDisplayCronerKaplanData, ...
             'fixedOptimalOrientation', fixedOptimalOrientation, ...
             'deltaThresholdForLimitingFittedSTFtoPrimaryPeak', 0.01, ...
@@ -170,10 +197,7 @@ end
 
 
 function visualizeTheMosaic(theMRGCmosaic, exportVisualizationPDFdirectory)
-    % The full path where the generated PDFs will be stored can be found like so:
-    % p = getpref('isetbio');
-    % fullPath = fullfile(p.rgcResources.figurePDFsDir,exportVisualizationPDFdirectory);
-
+    
     % Visualize the mosaic of mRGC RF centers
     % identifying cones that are pooled by the RF center mechanism with
     % a weight >= mRGCMosaic.sensitivityAtPointOfOverlap;
@@ -215,5 +239,18 @@ function visualizeTheMosaic(theMRGCmosaic, exportVisualizationPDFdirectory)
 
     % Finalize figure using the Publication-Ready format
     PublicationReadyPlotLib.applyFormat(ax,ff);
+
+
+    % The full path where the generated PDFs will be stored can be found like so:
+    p = getpref('isetbio');
+    pdfExportRootDir = p.rgcResources.figurePDFsDir;
+
+    theVisualizationPDFfilename = fullfile(pdfExportSubDir, 'mRGCMosaic.pdf');
+    % Generate the path if we need to
+    thePDFFullFileName  = RGCMosaicConstructor.filepathFor.augmentedPathWithSubdirs(...
+                pdfExportRootDir, theVisualizationPDFfilename, ...
+                'generateMissingSubDirs', true);
+
+    NicePlot.exportFigToPDF(thePDFFullFileName, hFig, 300);
 
 end
