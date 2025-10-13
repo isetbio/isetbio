556a0b58926390ee33ca9bdac8bf791bb85a966a
Nicolas Cottaris <cottaris@upenn.edu>
2025-10-13 13:55:52 -0400

    Added case for leviathan

--- isettools/ganglioncells/+RGCMosaicConstructor/+helper/+utils/generateLocalPrefs.m
+++ isettools/ganglioncells/+RGCMosaicConstructor/+helper/+utils/generateLocalPrefs.m
@@ -8,9 +8,17 @@ function generateLocalPrefs(varargin)
 
     p = GetComputerInfo;
     
+    switch (p.MatlabPlatform)
+        case 'GLNXA64'
+            % Linux
+            localHostName = p.networkName;
+        otherwise
+            localHostName = p.localHostName;
+    end % switch
+
     % Retrieve the location of the rgcResourcesRootDir. 
     % This is computer/user specific
-    switch (p.localHostName)
+    switch (localHostName)
         case 'Lefkada'
             % User on Lefkada (Nicolas' laptop)
             rgcResourcesRootDir = '/Volumes/SSDdisk/Aguirre-Brainard Lab Dropbox/Nicolas Cottaris';
@@ -27,6 +35,19 @@ function generateLocalPrefs(varargin)
         case 'Crete'
             % User on Crete (Nicolas' M2 Mac Studio) 
             rgcResourcesRootDir  = '/Volumes/M1ProBackUp/Aguirre-Brainard Lab Dropbox/Nicolas Cottaris';
+            
+            % Directory where all the mosaic generation intermediate data will be stored
+            intermediateDataDir = fullfile(rgcResourcesRootDir,'IBIO_rgcMosaicResources/denovo/intermediateFiles/ONcenterMidgetRGCmosaics');
+            
+            % Directory where all generated figures will be saved
+            figurePDFsDir = fullfile(rgcResourcesRootDir,'ManuscriptSupportMaterials/denovo/PLOS2024/figures');
+
+            % I want the code to stop and ask me if it is OK to generate a missing directory
+            queryUserBeforeGeneratingMissingDir = false;
+
+        case 'Leviathan'
+            % User on Crete (Nicolas' Linux box) 
+            rgcResourcesRootDir  = localDropboxDir;
             
             % Directory where all the mosaic generation intermediate data will be stored
             intermediateDataDir = fullfile(rgcResourcesRootDir,'IBIO_rgcMosaicResources/denovo/intermediateFiles/ONcenterMidgetRGCmosaics');
