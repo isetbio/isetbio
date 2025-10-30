b592ac778047f6857eded8cb2a11b06eb99d2f9b
Brian Wandell <1837145+wandell@users.noreply.github.com>
2025-10-30 10:30:07 -0700

    oiPosition returns. Improve input handling and documentation for optics functions
    Enhanced input parsing and validation in ArtalOptics and oiPosition functions, added detailed documentation and usage examples, and improved support for eye side string inputs. These changes clarify function usage, improve robustness, and ensure correct subject/eye selection for optical image and PSF computations.

--- isettools/opticalimage/oiPosition.m
+++ isettools/opticalimage/oiPosition.m
@@ -1,125 +1,149 @@
 function [oi, psf, support, zCoeffs, subjID]  = oiPosition(zCoeffDatabase,varargin)
-% Return oi and many related parameters 
-% 
+% OIPOSITION Return oi, psf, and related parameters for a subject at a spatial position
+%
+% Synopsis
+%   [oi, psf, support, zCoeffs, subjID]  = oiPosition(zCoeffDatabase,varargin)
+%
+% Brief description
+%   Wrapper function to compute the Optical Image (OI) and Point
+%   Spread Function (PSF) for a selected subject from a specified
+%   database (Artal2012 or Polans2015), at a given retinal
+%   eccentricity and pupil size.
+%
+% Inputs
+%   zCoeffDatabase  - String, must be 'Polans2015' or 'Artal2012'.
+%
+% Optional Key/Value Pairs
+%   'position'        - [H V] vector of eccentricity in degrees. Default is [3 1].
+%   'pupildiameter'   - Scalar, pupil diameter in millimeters. Default is 3 mm.
+%   'subjectrank'     - Scalar, rank order of the subject based on foveal PSF quality (Strehl).
+%                       1 is the best subject available in the database. Default is 1.
+%   'wave'            - Scalar, wavelength in nanometers for which to compute the optics.
+%                       The underlying function uses this as the single wavelength to compute. Default is 550 nm.
+%   'eyeside'         - String, 'left' or 'right' eye. Default is 'right'.
+%   'centerpsf'       - Logical, if true, the PSF is centered around its peak. Default is true.
+%
+% Returns
+%   oi              - The ISETBio Optical Image structure.
+%   psf             - The computed point-spread function (PSF).
+%   support         - Struct containing spatial and wavelength support information:
+%                     .psfX, .psfY: PSF support in microns.
+%                     .wave: The wavelength used in the computation (nm).
+%                     .position: The [H V] eccentricity position used (degrees).
+%   zCoeffs         - The interpolated Zernike coefficients (microns) used for the optics.
+%   subjID          - The selected subject ID from the database.
+%
+% See also
+%   ArtalOptics.oiForSubjectAtEccentricity, PolansOptics.oiForSubjectAtEccentricity,
+%   PolansOptics.constants, ArtalOptics.constants
+%
+% Example:
+% % Get Polans subject with best foveal quality at 5 deg horizontal, 2 mm pupil
+% [oi, psf, support] = oiPosition('Polans2015', 'position', [5 0], 'pupildiameter', 2);
+% figure; mesh(support.psfX,support.psfY,psf);
+%
 
-%Example:
+% Example:
 %{
-
+[oi,psf] = oiPosition('Artal2012');
 %}
 %{
-    [oi, psf, support, zCoeffs, subjID]  = ...
-        oiPosition(zCoeffDatabase, 'position',positionDegs, ...
-        'pupil diameter', pupilDiamMM, 'subject rank', thisSubject, ...
-        'wave',wave, ...
-        'eye side', eyeside,'center psf',centerpsf);
+[oi,psf,support,zcoeffs,subject] = oiPosition('Polans2015');
+ieFigure;
+mesh(support.psfX,support.psfY,psf);
+xlabel('Position (um)');
+ylabel('Position (um)');
+title(sprintf('Subject %d - Wave %d - Position [%d,%d]',subject,support.wave,support.position));
 %}
 
 %% Parse parameters
 
 varargin = ieParamFormat(varargin);
+
 p = inputParser;
-p.addParameter('zCoeffDatabase', @(x)(ismember(x, {'Polans2015', 'Artal2012'})));
-p.addParameter('position',[0 3],@isvector);
-p.addParameter('pupildiameter',3,@isnumeric)
-p.addParameter('subjectrank',1,@isnumeric);
-p.addParameter('wave',550,@isscalar);
+p.addRequired('zCoeffDatabase', @(x)(ismember(x, {'Polans2015', 'Artal2012'})));
+p.addParameter('position',[3 0],@isvector);
+p.addParameter('pupildiameter',3,@isnumeric)   % millimeters
+p.addParameter('subjectrank',1,@isnumeric);    % PSF quality (Strehl)
+p.addParameter('wave',550,@isscalar);          % nanometers
 p.addParameter('eyeside','right',@(x)(ismember(x,{'left','right'})));
 p.addParameter('centerpsf',true,@islogical);
-    
+
 p.parse(zCoeffDatabase,varargin{:});
 
-%%
+wave     = p.Results.wave;
+position = p.Results.position;
+whichEye = p.Results.eyeside;
+pupilDiamMM  = p.Results.pupildiameter;
+centerPSF    = p.Results.centerpsf;
+subjectrank  = p.Results.subjectrank;
+
+micronsPerDegree = 290;
+wavefrontSpatialSamples = 201;
+
+
+%% Get the data, make the oi
 switch (zCoeffDatabase)
     case 'Polans2015'
+        switch whichEye
+            case 'left'
+                whichEye = PolansOptics.constants.leftEye;
+            case 'right'
+                whichEye = PolansOptics.constants.rightEye;
+        end
+
         % Obtain subject IDs ranking in decreasing foveal resolution
-        rankedSubjectIDs = PolansOptics.constants.subjectRanking;
-        testSubjectID = rankedSubjectIDs(subjectRankOrder);
-        
-        % Determine if we need to subtract the subject's central refraction to
-        subtractCentralRefraction = PolansOptics.constants.subjectRequiresCentralRefractionCorrection(testSubjectID);
-        
+        rankedSubjectsID = PolansOptics.constants.subjectRanking;
+        subjID = rankedSubjectsID(min([numel(rankedSubjectsID) subjectrank]));
+
+        % Determine if we need to subtract the subject's central refraction
+        subtractCentralRefraction = PolansOptics.constants.subjectRequiresCentralRefractionCorrection(subjID);
+
+        [oi, psf, psfSupportMinutesX, psfSupportMinutesY, psfSupportWavelength, zCoeffs] = ...
+            PolansOptics.oiForSubjectAtEccentricity(subjID, ...
+                whichEye, position, pupilDiamMM, wave, micronsPerDegree, ...
+               'wavefrontSpatialSamples', wavefrontSpatialSamples, ...
+               'subtractCentralRefraction', subtractCentralRefraction, ...
+               'zeroCenterPSF', centerPSF);       
+
     case 'Artal2012'
+        switch whichEye
+            case 'left'
+                whichEye = ArtalOptics.constants.leftEye;
+            case 'right'
+                whichEye = ArtalOptics.constants.rightEye;
+        end
+
         % Obtain subject IDs ranking in decreasing foveal resolution
-        rankedSubjectIDs = ArtalOptics.constants.subjectRanking(whichEye);
-        testSubjectID = rankedSubjectIDs(subjectRankOrder);
+        rankedSubjectsID = ArtalOptics.constants.subjectRanking(whichEye);
+        subjID = rankedSubjectsID(min([numel(rankedSubjectsID) subjectrank]));
+
+        % Determine if we need to subtract the subject's central refraction
+        subtractCentralRefraction = ArtalOptics.constants.subjectRequiresCentralRefractionCorrection(whichEye, subjID);
+        if (position(2) ~= 0)
+            fprintf(2,'Artal optics not available off the horizontal meridian. Computing for vEcc = 0\n');
+            position(2) = 0;
+        end
         
-        % Determine if we need to subtract the subject's central refraction to
-        subtractCentralRefraction = ArtalOptics.constants.subjectRequiresCentralRefractionCorrection(whichEye, testSubjectID);
+        [oi, psf, psfSupportMinutesX, psfSupportMinutesY, psfSupportWavelength, zCoeffs] = ...
+            ArtalOptics.oiForSubjectAtEccentricity(subjID, ...
+            whichEye, position, pupilDiamMM, wave, micronsPerDegree, ...
+            'wavefrontSpatialSamples', wavefrontSpatialSamples, ...
+            'subtractCentralRefraction', subtractCentralRefraction, ...
+            'zeroCenterPSF', centerPSF);
+
+        if (isempty(oi))
+            error('Bad Zernike coefficents for the %s of Artal subject %d. Choose another subject/eye', obj.whichEye, subjectID);
+        end
 end
 
+% Convert spatial units to microns
+support.psfX = psfSupportMinutesX * (micronsPerDegree/60);
+support.psfY = psfSupportMinutesY * (micronsPerDegree/60);
+support.wave = psfSupportWavelength; 
 
+% Returns the central position in case it was the default.  Always
+% good to know.
+support.position = position;
 
 end
-
-%{
-function eccDependentOptics(opticsZernikeCoefficientsDataBase, subjectRankOrder, whichEye, targetEcc, pupilDiamMM )
-% 
-%{
-  USAGE:
-       subjectRankOrder = 3;
-       whichEye ='right eye';
-       targetEcc = [-10 0];
-       pupilDiamMM = 3.0;
-       opticsZernikeCoefficientsDataBase = 'Polans2015';
-       eccDependentOpticsDemo('Polans2015', subjectRankOrder, whichEye, xyEccDegs , pupilDiamMM);
-       eccDependentOpticsDemo('Artal2012',  subjectRankOrder, whichEye, xyEccDegs , pupilDiamMM);
-%}
-
-   
-    wave = 400:10:750;
-    micronsPerDegree = 290;
-    zeroCenterPSF = true;
-    wavefrontSpatialSamples = 201;
-
-    switch (opticsZernikeCoefficientsDataBase)
-        case 'Polans2015'
-            % Obtain subject IDs ranking in decreasing foveal resolution
-            rankedSujectIDs = PolansOptics.constants.subjectRanking;
-            testSubjectID = rankedSujectIDs(min([numel(rankedSujectIDs) subjectRankOrder]));
-            % Determine if we need to subtract the subject's central refraction
-            subtractCentralRefraction = PolansOptics.constants.subjectRequiresCentralRefractionCorrection(testSubjectID);
-            [theOI, thePSF, psfSupportMinutesX, psfSupportMinutesY, psfSupportWavelength, zCoeffs] = ...
-                PolansOptics.oiForSubjectAtEccentricity(testSubjectID, ...
-                    whichEye, xyEccDegs, pupilDiamMM, wave, micronsPerDegree, ...
-                    'wavefrontSpatialSamples', wavefrontSpatialSamples, ...
-                    'subtractCentralRefraction', subtractCentralRefraction, ...
-                    'zeroCenterPSF', zeroCenterPSF);
-               
-        case 'Artal2012'
-            % Obtain subject IDs ranking in decreasing foveal resolution
-            rankedSujectIDs = ArtalOptics.constants.subjectRanking(whichEye);
-            testSubjectID = rankedSujectIDs(min([numel(rankedSujectIDs) subjectRankOrder]));
-            % Determine if we need to subtract the subject's central refraction
-            subtractCentralRefraction = ArtalOptics.constants.subjectRequiresCentralRefractionCorrection(whichEye, testSubjectID);
-            if (targetEcc(2) ~= 0)
-                fprintf(2,'Artal optics not available off the horizontal meridian. Computing for vEcc = 0\n');
-                targetEcc(2) = 0;
-            end
-            [theOI, thePSF, psfSupportMinutesX, psfSupportMinutesY, psfSupportWavelength, zCoeffs] = ...
-                ArtalOptics.oiForSubjectAtEccentricity(testSubjectID, ...
-                    whichEye, targetEcc, pupilDiamMM, wave, micronsPerDegree, ...
-                    'wavefrontSpatialSamples', wavefrontSpatialSamples, ...
-                    'subtractCentralRefraction', subtractCentralRefraction, ...
-                    'zeroCenterPSF', zeroCenterPSF);
-               
-             if (isempty(theOI))
-                    error('Bad Zernike coefficents for the %s of Artal subject %d. Choose another subject/eye', obj.whichEye, subjectID);
-             end
-        otherwise
-            error('Unknown optics database: ''%s''.', opticsZernikeCoefficientsDataBase);
-    end
-
-    % Plot the PSF
-    hFig = figure();
-    set(hFig, 'Color', [1 1 1]);
-    idx = find(psfSupportWavelength == 550);
-    imagesc(psfSupportMinutesX, psfSupportMinutesY, squeeze(thePSF(:,:,idx)));
-    axis 'square'
-    colormap(gray);
-    xlabel('arc min');
-    ylabel('arc min');
-    set(gca, 'FontSize', 14);
-    title(sprintf('%s, subject #%d', opticsZernikeCoefficientsDataBase, testSubjectID));
-   
-end
-%}
