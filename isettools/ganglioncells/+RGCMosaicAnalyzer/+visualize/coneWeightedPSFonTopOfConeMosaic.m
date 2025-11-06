ac072bc02a67c08173cf2f02c715f39fbfafc878
Brian Wandell <1837145+wandell@users.noreply.github.com>
2025-11-06 11:13:13 -0800

    Add documentation headers to visualization functions
    Added detailed function headers and usage documentation to coneWeightedPSFonTopOfConeMosaic, visualizeOpticsAtTargetWavelength, visualizeOptics, and visualizePSF. These updates improve code readability and clarify input arguments, outputs, and example usage for each visualization function.

--- isettools/ganglioncells/+RGCMosaicAnalyzer/+visualize/coneWeightedPSFonTopOfConeMosaic.m
+++ isettools/ganglioncells/+RGCMosaicAnalyzer/+visualize/coneWeightedPSFonTopOfConeMosaic.m
@@ -1,56 +1,81 @@
 function coneWeightedPSFonTopOfConeMosaic(figNo, theInputConeMosaic, theConeWeightedPSF, thePDFfileName, theTitle)
+% CONEWEIGHTEDPSFONTOPOFCONEMOSAIC Visualizes the cone-weighted Point Spread Function (PSF)
+% on top of a cone mosaic.
+%
+% (Written by CoPilot)
+%
+% Syntax:
+%   coneWeightedPSFonTopOfConeMosaic(figNo, theInputConeMosaic, theConeWeightedPSF,
+%   thePDFfileName, theTitle)
+%
+% Description:
+%   This function creates a visualization of the cone-weighted Point Spread Function (PSF)
+%   overlaid on a specified cone mosaic. It generates a figure with axes for displaying the
+%   PSF and exports the visualization to a PDF file.
+%
+% Inputs:
+%   figNo                    - Figure number for the visualization.
+%   theInputConeMosaic       - The cone mosaic object to visualize the PSF on.
+%   theConeWeightedPSF       - The cone-weighted PSF data to visualize.
+%   thePDFfileName           - Name of the PDF file to export the visualization.
+%   theTitle                 - Title for the visualization plot.
+%
+% Example:
+%   coneWeightedPSFonTopOfConeMosaic(1, coneMosaic, coneWeightedPSF, 'output.pdf', 'Cone PSF Visualization');
+%
+% See also: VISUALIZEPSFATWAVELENGTHONTOPOFCONEMOSAIC, NICEPLOT
 
-    ff = PublicationReadyPlotLib.figureComponents('1x1 standard figure');
-    hFig = figure(figNo); clf;
-    theAxes = PublicationReadyPlotLib.generatePanelAxes(hFig,ff);
+ff = PublicationReadyPlotLib.figureComponents('1x1 standard figure');
+hFig = figure(figNo); clf;
+theAxes = PublicationReadyPlotLib.generatePanelAxes(hFig,ff);
 
-    ax = theAxes{1,1};
-    visualizedWavelengthIndex = 1;
-    maxPSF = 1;
-    infoString = '';
-    xo = theInputConeMosaic.eccentricityDegs(1);
-    yo = theInputConeMosaic.eccentricityDegs(2);
-    RGCMosaicConstructor.helper.optics.visualizePSFAtWavelengthOnTopOfConeMosaic(...
-        hFig, ax, theInputConeMosaic, theConeWeightedPSF, ...
-        visualizedWavelengthIndex, theConeWeightedPSF, ...
-        theTitle, ...
-        'XLimsArcMin', [-12.5 12.5], ...
-        'YLimsArcMin', [-12.5 12.5], ...
-        'XTicksArcMin', xo*60 + (-12:6:12), ...
-        'YTicksArcMin', yo*60 + (-12:6:12), ...
-        'withInfoString', infoString);
-    
-    ff.box = 'on';
-    ff.tickDir = 'in';
-    xlabel(ax, 'eccentricity, x (degs)');
-    ylabel(ax, 'eccentricity, y (degs)');
-    % Finalize figure using the Publication-Ready format
-    PublicationReadyPlotLib.applyFormat(ax,ff);
-    %PublicationReadyPlotLib.offsetAxes(ax, ff, XLims, YLims);
+ax = theAxes{1,1};
+visualizedWavelengthIndex = 1;
+maxPSF = 1;
+infoString = '';
+xo = theInputConeMosaic.eccentricityDegs(1);
+yo = theInputConeMosaic.eccentricityDegs(2);
+RGCMosaicConstructor.helper.optics.visualizePSFAtWavelengthOnTopOfConeMosaic(...
+    hFig, ax, theInputConeMosaic, theConeWeightedPSF, ...
+    visualizedWavelengthIndex, theConeWeightedPSF, ...
+    theTitle, ...
+    'XLimsArcMin', [-12.5 12.5], ...
+    'YLimsArcMin', [-12.5 12.5], ...
+    'XTicksArcMin', xo*60 + (-12:6:12), ...
+    'YTicksArcMin', yo*60 + (-12:6:12), ...
+    'withInfoString', infoString);
 
-    % OLD WAY
-    %theRawFiguresDir = RGCMosaicConstructor.filepathFor.rawFigurePDFsDir();
+ff.box = 'on';
+ff.tickDir = 'in';
+xlabel(ax, 'eccentricity, x (degs)');
+ylabel(ax, 'eccentricity, y (degs)');
+% Finalize figure using the Publication-Ready format
+PublicationReadyPlotLib.applyFormat(ax,ff);
+%PublicationReadyPlotLib.offsetAxes(ax, ff, XLims, YLims);
 
-    %if (isdir(theRawFiguresDir))
-    %    pdfExportSubDir = 'validation';
-    
-    %    thePDFfileName = fullfile(theRawFiguresDir, pdfExportSubDir, thePDFfileName);
-    %    NicePlot.exportFigToPDF(thePDFfileName,hFig,  300);
-    %else
-    %    fprintf('Raw figures directory not found: %s.\nWill not export PDF\n', theRawFiguresDir);
-    %end
+% OLD WAY
+%theRawFiguresDir = RGCMosaicConstructor.filepathFor.rawFigurePDFsDir();
+
+%if (isdir(theRawFiguresDir))
+%    pdfExportSubDir = 'validation';
+
+%    thePDFfileName = fullfile(theRawFiguresDir, pdfExportSubDir, thePDFfileName);
+%    NicePlot.exportFigToPDF(thePDFfileName,hFig,  300);
+%else
+%    fprintf('Raw figures directory not found: %s.\nWill not export PDF\n', theRawFiguresDir);
+%end
 
 
-    p = getpref('isetbio');
-    pdfExportSubDir = fullfile(p.rgcResources.figurePDFsDir);
-    theVisualizationPDFfilename = fullfile('modelComponentVisualizations', thePDFfileName);
-    
-    % Generate the path if we need to
-    RGCMosaicConstructor.filepathFor.augmentedPathWithSubdirs(...
-        pdfExportSubDir, theVisualizationPDFfilename, ...
-        'generateMissingSubDirs', true);
+p = getpref('isetbio');
+pdfExportSubDir = fullfile(p.rgcResources.figurePDFsDir);
+theVisualizationPDFfilename = fullfile('modelComponentVisualizations', thePDFfileName);
 
-    thePDFfileName = fullfile(pdfExportSubDir, theVisualizationPDFfilename);
-    NicePlot.exportFigToPDF(thePDFfileName, hFig, 300);
+% Generate the path if we need to
+RGCMosaicConstructor.filepathFor.augmentedPathWithSubdirs(...
+    pdfExportSubDir, theVisualizationPDFfilename, ...
+    'generateMissingSubDirs', true);
+
+thePDFfileName = fullfile(pdfExportSubDir, theVisualizationPDFfilename);
+NicePlot.exportFigToPDF(thePDFfileName, hFig, 300);
 
 end
--- isettools/wrappers/optics/visualizeOptics.m
+++ isettools/wrappers/optics/visualizeOptics.m
@@ -1,20 +1,45 @@
 function visualizeOptics(theOI, targetWavelength, ...
     spatialSupportArcMin, spatialFrequencySupportCyclePerDeg, varargin)
+% VISUALIZEOPTICS Visualizes the optical properties at a specified target wavelength.
+%
+% Syntax:
+%   visualizeOptics(theOI, targetWavelength, ...
+%   spatialSupportArcMin, spatialFrequencySupportCyclePerDeg, varargin)
+%
+% Description:
+%   This function visualizes the Point Spread Function (PSF) and Optical Transfer Function (OTF)
+%   at a specified target wavelength. It creates a figure with two subplots: one for the PSF
+%   and another for the OTF, allowing for analysis of optical performance at the given wavelength.
+%
+% Inputs:
+%   theOI                     - The optical image object containing the optical properties.
+%   targetWavelength          - The wavelength (in nanometers) at which to visualize the optics.
+%   spatialSupportArcMin      - The spatial support in arc minutes for the PSF visualization.
+%   spatialFrequencySupportCyclePerDeg - The spatial frequency support in cycles per degree for the OTF visualization.
+%
+% Optional Name-Value Pair Arguments:
+%   'extraOTFData'           - Additional data for the OTF visualization (default: []), if needed.
+%
+% Example:
+%   visualizeOptics(opticalImage, 550, 30, 10, 'extraOTFData', someData);
+%
+% See also: VISUALIZEPSF, VISUALIZEOTF
 
-    p = inputParser;
-    p.addParameter('extraOTFData', []);
 
-    % Parse input
-    p.parse(varargin{:});
-    extraOTFData = p.Results.extraOTFData;
-    
-    hFig = figure(); clf;
-    set(hFig, 'Color', [1 1 1]);
-    ax1 = subplot(1,2,1);
-    visualizePSF(theOI, targetWavelength, spatialSupportArcMin, ...
-        'axesHandle', ax1);
+p = inputParser;
+p.addParameter('extraOTFData', []);
 
-    ax2 = subplot(1,2,2);
-    visualizeOTF(theOI, targetWavelength, spatialFrequencySupportCyclePerDeg, ...
-        'axesHandle', ax2, 'extraData', extraOTFData);
+% Parse input
+p.parse(varargin{:});
+extraOTFData = p.Results.extraOTFData;
+
+hFig = figure(); clf;
+set(hFig, 'Color', [1 1 1]);
+ax1 = subplot(1,2,1);
+visualizePSF(theOI, targetWavelength, spatialSupportArcMin, ...
+    'axesHandle', ax1);
+
+ax2 = subplot(1,2,2);
+visualizeOTF(theOI, targetWavelength, spatialFrequencySupportCyclePerDeg, ...
+    'axesHandle', ax2, 'extraData', extraOTFData);
 end
