68db3c974cfe42830e69424295ef07204717d874
Nicolas Cottaris <cottaris@upenn.edu>
2026-04-15 23:05:07 -0400

    Work on post-receptor temporal filter computation code

--- isettools/ganglioncells/+RGCMosaicAnalyzer/+compute/MRGCtemporalFiltersFromPhotocurrentsBasedTTF.m
+++ isettools/ganglioncells/+RGCMosaicAnalyzer/+compute/MRGCtemporalFiltersFromPhotocurrentsBasedTTF.m
@@ -1,59 +1,12 @@
+
+
+
+
 %
 %RGCMosaicAnalyzer.compute.MRGCtemporalFiltersFromPhotocurrentsBasedTTF
 %
 %
 function MRGCtemporalFiltersFromPhotocurrentsBasedTTF(...
-    stimulusShape, ...
-    theTargetRGCwithIndex, ...
-    theMRGCMosaicTTFResponsesFullFileName, ...
-    theAnalyzedTTFsFullFileName)
-
-
-
-    load(theMRGCMosaicTTFResponsesFullFileName, ...
-        'theMRGCMosaic', 'stimParams', 'TTFparamsStruct', ...
-        'computePhotocurrentResponsesOnlyForInputsToSingleRGCwithIndex', ...
-        'theMRGCMosaicTTFresponsesAllConditions', ...
-        'theMRGCMosaicTemporalSupportSecondsAllConditions');
-
-    temporalFrequenciesExamined = TTFparamsStruct.tfSupport;
-
-    responseRange = max(max(abs(squeeze(theMRGCMosaicTTFresponsesAllConditions(:,:,computePhotocurrentResponsesOnlyForInputsToSingleRGCwithIndex)))))*[-1 1];
-    for iTF = 1:numel(temporalFrequenciesExamined)
-
-        % Retrieve the photocurrent response data for this TF
-        theTemporalSupportSecondsForThisTF = squeeze(theMRGCMosaicTemporalSupportSecondsAllConditions(iTF,:));
-        theMRGCMosaicPhotocurrentResponsesForThisTF = squeeze(theMRGCMosaicTTFresponsesAllConditions(iTF,:,:));
-        nanIndices = find(isnan(theTemporalSupportSecondsForThisTF));
-        if (isempty(nanIndices))
-            theTimeBins = 1:numel(theTemporalSupportSecondsForThisTF);
-        else
-            theTimeBins = 1:(nanIndices-1);
-        end
-        theTemporalSupportSecondsForThisTF = theTemporalSupportSecondsForThisTF(theTimeBins);
-        theMRGCMosaicPhotocurrentResponsesForThisTF = theMRGCMosaicPhotocurrentResponsesForThisTF(theTimeBins,computePhotocurrentResponsesOnlyForInputsToSingleRGCwithIndex);
-
-        hFig = figure(1);
-        set(hFig, 'Position', [10 10 500 300]);
-        plot(theTemporalSupportSecondsForThisTF*1e3, theMRGCMosaicPhotocurrentResponsesForThisTF, 'ko-');
-        set(gca, 'XLim', [0 2000], 'YLim', responseRange);
-        xlabel('time (msec)');
-        ylabel('pAmps');
-        title(gca, sprintf('%s, TF = %2.2fHz', stimulusShape, temporalFrequenciesExamined(iTF)));
-        drawnow;
-    end % iTF
-end
-
-
-
-
-
-
-%
-%RGCMosaicAnalyzer.compute.MRGCtemporalFiltersFromPhotocurrentsBasedTTF
-%
-%
-function MRGCtemporalFiltersFromPhotocurrentsBasedTTFnew(...
     stimulusShape, ...
     allowNonZeroBaselineInSineWaveFitsToResponseTimeSeries, ...
     theTargetRGCwithIndex, ...
@@ -942,3 +895,47 @@ end
 
 
 
+
+
+function MRGCtemporalFiltersFromPhotocurrentsBasedTTFOLD(...
+    stimulusShape, ...
+    allowNonZeroBaselineInSineWaveFitsToResponseTimeSeries, ...
+    theTargetRGCwithIndex, ...
+    theMRGCMosaicTTFResponsesFullFileName, ...
+    theAnalyzedTTFsFullFileName)
+
+
+
+    load(theMRGCMosaicTTFResponsesFullFileName, ...
+        'theMRGCMosaic', 'stimParams', 'TTFparamsStruct', ...
+        'computePhotocurrentResponsesOnlyForInputsToSingleRGCwithIndex', ...
+        'theMRGCMosaicTTFresponsesAllConditions', ...
+        'theMRGCMosaicTemporalSupportSecondsAllConditions');
+
+    temporalFrequenciesExamined = TTFparamsStruct.tfSupport;
+
+    responseRange = max(max(abs(squeeze(theMRGCMosaicTTFresponsesAllConditions(:,:,computePhotocurrentResponsesOnlyForInputsToSingleRGCwithIndex)))))*[-1 1];
+    for iTF = 1:numel(temporalFrequenciesExamined)
+
+        % Retrieve the photocurrent response data for this TF
+        theTemporalSupportSecondsForThisTF = squeeze(theMRGCMosaicTemporalSupportSecondsAllConditions(iTF,:));
+        theMRGCMosaicPhotocurrentResponsesForThisTF = squeeze(theMRGCMosaicTTFresponsesAllConditions(iTF,:,:));
+        nanIndices = find(isnan(theTemporalSupportSecondsForThisTF));
+        if (isempty(nanIndices))
+            theTimeBins = 1:numel(theTemporalSupportSecondsForThisTF);
+        else
+            theTimeBins = 1:(nanIndices-1);
+        end
+        theTemporalSupportSecondsForThisTF = theTemporalSupportSecondsForThisTF(theTimeBins);
+        theMRGCMosaicPhotocurrentResponsesForThisTF = theMRGCMosaicPhotocurrentResponsesForThisTF(theTimeBins,computePhotocurrentResponsesOnlyForInputsToSingleRGCwithIndex);
+
+        hFig = figure(1);
+        set(hFig, 'Position', [10 10 2500 300]);
+        plot(theTemporalSupportSecondsForThisTF*1e3, theMRGCMosaicPhotocurrentResponsesForThisTF, 'ko-');
+        set(gca, 'XLim', [0 2000], 'YLim', responseRange);
+        xlabel('time (msec)');
+        ylabel('pAmps');
+        title(gca, sprintf('%s, TF = %2.2fHz', stimulusShape, temporalFrequenciesExamined(iTF)));
+        drawnow;
+    end % iTF
+end
