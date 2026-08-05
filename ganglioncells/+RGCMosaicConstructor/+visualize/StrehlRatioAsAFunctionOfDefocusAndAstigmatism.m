393ab5ddcec44124b76d9b1cde376af6086824db
Nicolas Cottaris <cottaris@upenn.edu>
2026-08-05 12:38:02 +0300

    Added function to convert astigmatism in wavefront microns to diopters and its inverse
    Signed-off-by: Nicolas Cottaris <cottaris@upenn.edu>

--- ganglioncells/+RGCMosaicConstructor/+visualize/StrehlRatioAsAFunctionOfDefocusAndAstigmatism.m
+++ ganglioncells/+RGCMosaicConstructor/+visualize/StrehlRatioAsAFunctionOfDefocusAndAstigmatism.m
@@ -19,20 +19,6 @@ function StrehlRatioAsAFunctionOfDefocusAndAstigmatism(...
     backgroundIsTransparent = p.Results.backgroundIsTransparent;
 
     
-    StrehlRatio3Dmap = reshape(StrehlRatioAsAFunctionOfDefocusAndAstigmatism, [...
-            numel(examinedRefractionErrorDiopters) ...
-            numel(examinedObliqueAstigmatismErrorsMicrons) ...
-            numel(examinedVerticalAstigmatismErrorsMicrons)]);
-
-    idx = find(...
-        (examinedRefractionErrorDioptersObliqueAndVerticalAstigmatismErrorsMicronsGrid(:,1) == theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(1)) & ...
-        (examinedRefractionErrorDioptersObliqueAndVerticalAstigmatismErrorsMicronsGrid(:,2) == theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(2)) & ...
-        (examinedRefractionErrorDioptersObliqueAndVerticalAstigmatismErrorsMicronsGrid(:,3) == theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(3)) ...
-    );
-
-    [i1, i2, i3] = ind2sub(size(StrehlRatio3Dmap), idx);
-
-
     ff = PublicationReadyPlotLib.figureComponents('2x2 standard figure', ...
         'darkScheme', p.Results.darkScheme);
     
@@ -42,12 +28,33 @@ function StrehlRatioAsAFunctionOfDefocusAndAstigmatism(...
 
     theLUT = brewermap(1024, '*greys');
 
+
+
+    the3DSize(1) = numel(examinedRefractionErrorDiopters);
+    the3DSize(2) = numel(examinedObliqueAstigmatismErrorsMicrons);
+    the3DSize(3) = numel(examinedVerticalAstigmatismErrorsMicrons);
+    StrehlRatio3Dmap = reshape(StrehlRatioAsAFunctionOfDefocusAndAstigmatism,the3DSize);
+    [theMaxStrehlRatio, idx] = max(StrehlRatioAsAFunctionOfDefocusAndAstigmatism);
+    [i1, i2, i3] = ind2sub(size(StrehlRatio3Dmap), idx);
+
+    optimalDefocusDiopters = examinedRefractionErrorDiopters(i1);
+    optimalObliqueMicrons = examinedObliqueAstigmatismErrorsMicrons(i2);
+    optimalVerticalMicrons = examinedVerticalAstigmatismErrorsMicrons(i3);
+
+
+    [theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(1) optimalDefocusDiopters]
+    [theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(2) optimalObliqueMicrons]
+    [theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(3) optimalVerticalMicrons]
+    
     % Plot the slice over the defocus x oblique astigmatism plane at the
     % optimal vertical astigmatism
     StrehlRatio2Dslice = squeeze(StrehlRatio3Dmap(:, :, i3));
     imagesc(theAxes{1,1}, examinedRefractionErrorDiopters, examinedObliqueAstigmatismErrorsMicrons, StrehlRatio2Dslice');
     hold(theAxes{1,1}, 'on');
-    plot(theAxes{1,1}, theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(1), theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(2), 'bs', 'MarkerSize', 20);
+    plot(theAxes{1,1}, ...
+        theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(1), ...
+        theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(2), ...
+        'rx', 'MarkerSize', 20);
     xlabel(theAxes{1,1}, 'defocus (D)');
     ylabel(theAxes{1,1}, 'oblique astigmatism (microns)');
     axis(theAxes{1,1}, 'square'); axis(theAxes{1,1}, 'xy'); 
@@ -59,7 +66,10 @@ function StrehlRatioAsAFunctionOfDefocusAndAstigmatism(...
     StrehlRatio2Dslice = squeeze(StrehlRatio3Dmap(:, i2, :));
     imagesc(theAxes{1,2},examinedRefractionErrorDiopters, examinedVerticalAstigmatismErrorsMicrons, StrehlRatio2Dslice');
     hold(theAxes{1,2}, 'on');
-    plot(theAxes{1,2}, theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(1), theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(3), 'bs', 'MarkerSize', 20);
+    plot(theAxes{1,2}, ...
+        theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(1), ...
+        theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(3), ...
+        'rx', 'MarkerSize', 20);
     xlabel(theAxes{1,2}, 'defocus (D)');
     ylabel(theAxes{1,2}, 'vertical astigmatism (microns)');
     axis(theAxes{1,2}, 'square'); axis(theAxes{1,2}, 'xy'); 
@@ -72,7 +82,10 @@ function StrehlRatioAsAFunctionOfDefocusAndAstigmatism(...
     StrehlRatio2Dslice = squeeze(StrehlRatio3Dmap(i1, :, :));
     imagesc(theAxes{2,1},examinedObliqueAstigmatismErrorsMicrons, examinedVerticalAstigmatismErrorsMicrons, StrehlRatio2Dslice');
     hold(theAxes{2,1}, 'on');
-    plot(theAxes{2,1}, theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(1), theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(3), 'bs', 'MarkerSize', 20);
+    plot(theAxes{2,1}, ...
+        theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(1), ...
+        theOptimalStrehlRatioDefocusDioptersObliqueAndVerticalAstigmatismErrorsMicrons(3), ...
+        'rx', 'MarkerSize', 20);
     xlabel(theAxes{2,1}, 'oblique astigmatism (microns)');
     ylabel(theAxes{2,1}, 'vertical astigmatism (microns)');
     axis(theAxes{2,1}, 'square'); axis(theAxes{2,1}, 'xy'); 
@@ -81,12 +94,12 @@ function StrehlRatioAsAFunctionOfDefocusAndAstigmatism(...
     colorbar(theAxes{2,1})
     
     
-    title(ax, theTitle, 'Interpreter', 'none');
 
     % Finalize figure using the Publication-Ready format
     PublicationReadyPlotLib.applyFormat(theAxes{1,1},ff);
     PublicationReadyPlotLib.applyFormat(theAxes{1,2},ff);
     PublicationReadyPlotLib.applyFormat(theAxes{2,1},ff);
+    PublicationReadyPlotLib.applyFormat(theAxes{2,2},ff);
 
     if (backgroundIsTransparent)
         set(hFig, 'Color', 'none');
